a634ef7a363c149e5e10c0dcd04610904717df61
tim.quinn@oracle.com <tim.quinn@oracle.com>
2018-10-10 17:48:23 -0500

    initial start to restful web service guide and code

---
+++ examples/guides/src/main/java/io/helidon/guides/App.java
@@ -0,0 +1,13 @@
+package io.helidon.guides;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
