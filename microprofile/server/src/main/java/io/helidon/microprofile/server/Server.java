238f88dacfeee0d1ab827292811b4aa1a6616a0d
Tomas Langer <tomas.langer@oracle.com>
2018-11-01 15:59:46 +0100

    Support for base path - a path the server redirects to from root path.

--- microprofile/server/src/main/java/io/helidon/microprofile/server/Server.java
+++ microprofile/server/src/main/java/io/helidon/microprofile/server/Server.java
@@ -149,9 +149,11 @@ public interface Server {
         private SeContainer cdiContainer;
         private MpConfig config;
         private String host;
+        private String basePath;
         private int port = -1;
         private boolean containerCreated;
         private Supplier<? extends ExecutorService> defaultExecutorService;
+
 
         private Builder() {
         }
@@ -289,6 +291,21 @@ public interface Server {
          */
         public Builder host(String host) {
             this.host = host;
+            return this;
+        }
+
+        /**
+         * Configure a path to which the server would redirect when a root path is requested.
+         * E.g. when static content is available at "/static" and you want to start there on index.html,
+         * you may want to configure this to "/static/index.html".
+         * When user requests "http://host:port" or "http://host:port/", the user would be redirected to
+         * "http://host:port/static/index.html"
+         *
+         * @param basePath path to redirect user from root path
+         * @return updated builder instance
+         */
+        public Builder basePath(String basePath) {
+            this.basePath = basePath;
             return this;
         }
 
@@ -525,6 +542,10 @@ public interface Server {
 
         ExecutorService getDefaultExecutorService() {
             return defaultExecutorService.get();
+        }
+
+        String basePath() {
+            return basePath;
         }
     }
 }
