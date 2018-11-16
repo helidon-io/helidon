fcc48adfbd37b6041dc02af0b390444eaa08d00b
Romain Grecourt <romain.grecourt@oracle.com>
2018-11-16 15:11:01 -0800

    Expose configuration for java.nio.file.WatchEvent.Modifier in PollingStrategies.FilesystemWatchBuilder
    Fixes #202

--- config/config/src/main/java/io/helidon/config/PollingStrategies.java
+++ config/config/src/main/java/io/helidon/config/PollingStrategies.java
@@ -26,6 +26,7 @@ import io.helidon.config.internal.FilesystemWatchPollingStrategy;
 import io.helidon.config.internal.ScheduledPollingStrategy;
 import io.helidon.config.internal.ScheduledPollingStrategy.RegularRecurringPolicy;
 import io.helidon.config.spi.PollingStrategy;
+import java.nio.file.WatchEvent.Modifier;
 
 /**
  * Built-in {@link io.helidon.config.spi.PollingStrategy} implementations.
@@ -141,8 +142,9 @@ public final class PollingStrategies {
      */
     public static final class FilesystemWatchBuilder implements Supplier<PollingStrategy> {
 
-        private Path path;
-        private ScheduledExecutorService executor;
+        private final Path path;
+        private ScheduledExecutorService executor = null;
+        private Modifier[] modifiers = null;
 
         /*private*/ FilesystemWatchBuilder(Path path) {
             this.path = path;
@@ -162,12 +164,30 @@ public final class PollingStrategies {
         }
 
         /**
+         * Add modifiers to be used when registering the {@link WatchService}.
+         * See {@link Path#register(WatchService, WatchEvent.Kind[], WatchEvent.Modifier[])
+         * Path.register}.
+         *
+         * @param modifiers the modifiers to add
+         * @return a modified builder instance
+         */
+        public FilesystemWatchBuilder modifiers(Modifier ... modifiers){
+            this.modifiers = modifiers;
+            return this;
+        }
+
+        /**
          * Builds a new polling strategy.
          *
          * @return the new instance
          */
         public PollingStrategy build() {
-            return new FilesystemWatchPollingStrategy(path, executor);
+            FilesystemWatchPollingStrategy strategy =
+                    new FilesystemWatchPollingStrategy(path, executor);
+            if(modifiers != null && modifiers.length > 0){
+                strategy.initWatchServiceModifiers(modifiers);
+            }
+            return strategy;
         }
 
         @Override
