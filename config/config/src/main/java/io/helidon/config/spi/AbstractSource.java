22ac2787786a39027a34dd0eedecdc22620bdb19
wojtask9 <wojtask9.kde@gmail.com>
2019-01-25 12:18:35 +0100

    fix checkstyle and copyright year

--- config/config/src/main/java/io/helidon/config/spi/AbstractSource.java
+++ config/config/src/main/java/io/helidon/config/spi/AbstractSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -349,7 +349,7 @@ public abstract class AbstractSource<T, S> implements Source<T> {
         }
 
         /**
-         * Returns current builder instance
+         * Returns current builder instance.
          *
          * @return builder instance
          */
