a616e1499083178a48f665a1a9e111472ee72ddb
Tomas Langer <tomas.langer@gmail.com>
2019-03-22 20:37:44 +0100

    Jaeger tracing integration. (#498)
    * Jaeger tracing integration.
    
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>
    
    * Checkstyle and copyright fixes.
    
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>
    
    * Required module added.
    
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>
    
    * Documentation fixes, unit test and a small fix in TracerBuilder to
    return correct type.
    
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>
    
    * Added to bom pom.
    
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>
    
    * Copyright fix
    
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>
    
    * Fix Jaeger name in comments.
    
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

--- tracing/tracing/src/test/java/io/helidon/tracing/TracerBuilderTest.java
+++ tracing/tracing/src/test/java/io/helidon/tracing/TracerBuilderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -63,6 +63,14 @@ class TracerBuilderTest {
                 .buildAndRegister();
 
         assertThat(tracer, notNullValue());
+        assertThat(tracer, instanceOf(NoopTracer.class));
+    }
+
+    @Test
+    void testCorrectReturnType() {
+        Tracer tracer = TracerBuilder.create(Config.empty())
+                .build();
+
         assertThat(tracer, instanceOf(NoopTracer.class));
     }
 
