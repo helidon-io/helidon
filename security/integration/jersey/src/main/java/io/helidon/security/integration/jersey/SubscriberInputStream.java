b5033eba4dc5ac124d2aaa5bad64c68e3072f649
Bryan Atsatt <bryan.atsatt@oracle.com>
2019-04-01 14:59:38 -0700

    Fix race condition in SubscriberInputStream.

--- security/integration/jersey/src/main/java/io/helidon/security/integration/jersey/SubscriberInputStream.java
+++ security/integration/jersey/src/main/java/io/helidon/security/integration/jersey/SubscriberInputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,6 +89,7 @@ class SubscriberInputStream extends InputStream implements Flow.Subscriber<ByteB
     }
 
     private boolean isClosed() {
+        // avoid race between read() and onComplete()
         synchronized (closed) {
             return closed.get();
         }
