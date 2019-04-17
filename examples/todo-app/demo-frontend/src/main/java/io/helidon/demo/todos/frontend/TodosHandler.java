f4111c48c7ee0e6964409a1ff1bec695aa99bc8c
Tomas Langer <tomas.langer@oracle.com>
2019-04-17 18:07:00 +0200

    Added MP configuration.
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

--- examples/todo-app/demo-frontend/src/main/java/io/helidon/demo/todos/frontend/TodosHandler.java
+++ examples/todo-app/demo-frontend/src/main/java/io/helidon/demo/todos/frontend/TodosHandler.java
@@ -31,6 +31,7 @@ import io.helidon.webserver.ServerRequest;
 import io.helidon.webserver.ServerResponse;
 import io.helidon.webserver.Service;
 
+import io.opentracing.util.GlobalTracer;
 import org.eclipse.microprofile.metrics.Counter;
 import org.eclipse.microprofile.metrics.Metadata;
 import org.eclipse.microprofile.metrics.MetricRegistry;
