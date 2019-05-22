8ca792bdf78a15dcd4b51ac71e4b0e20f71835a8
Laird Nelson <laird.nelson@oracle.com>
2019-05-22 13:17:12 -0700

    Fixes #729
    Signed-off-by: Laird Nelson <laird.nelson@oracle.com>

--- microprofile/tests/tck/tck-jwt-auth/src/test/java/io/helidon/microprofile/jwtauth/tck/UrlResourceProvider.java
+++ microprofile/tests/tck/tck-jwt-auth/src/test/java/io/helidon/microprofile/jwtauth/tck/UrlResourceProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,6 @@ public class UrlResourceProvider implements ResourceProvider {
 
     @Override
     public boolean canProvide(Class<?> type) {
-        return type.isAssignableFrom(URL.class);
+        return URL.class.isAssignableFrom(type);
     }
 }
