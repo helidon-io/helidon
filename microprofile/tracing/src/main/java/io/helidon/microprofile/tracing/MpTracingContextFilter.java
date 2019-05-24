3138f77af1a7950c9a908679fb47492a739cd0e1
Tomas Langer <tomas.langer@oracle.com>
2019-05-24 11:10:48 +0200

    Copyright and checstyle fixes.
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

--- microprofile/tracing/src/main/java/io/helidon/microprofile/tracing/MpTracingContextFilter.java
+++ microprofile/tracing/src/main/java/io/helidon/microprofile/tracing/MpTracingContextFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,8 +23,6 @@ import javax.ws.rs.ConstrainedTo;
 import javax.ws.rs.RuntimeType;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.Context;
 
