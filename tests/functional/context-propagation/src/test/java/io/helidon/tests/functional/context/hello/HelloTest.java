8b110bbf519f0c25407bff38a0d9f16a19492106
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2019-05-28 10:30:17 -0400

    New functional test that verifies context propagation in FT (#717)
    * New functional test that verifies context propagation in FT.
    
    Signed-off-by: Santiago Pericas-Geertsen <Santiago.PericasGeertsen@oracle.com>
    
    * Check that the span context is available from the Helidon context.
    
    Signed-off-by: Santiago Pericas-Geertsen <Santiago.PericasGeertsen@oracle.com>

---
+++ tests/functional/context-propagation/src/test/java/io/helidon/tests/functional/context/hello/HelloTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.tests.functional.context.hello;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.logging.LogManager;
+
+import io.helidon.microprofile.server.Server;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Unit test for {@link HelloResource}.
+ */
+class HelloTest {
+    private static Server server;
+    private static WebTarget baseTarget;
+
+    @BeforeAll
+    static void initClass() throws IOException {
+        LogManager.getLogManager().readConfiguration(HelloTest.class.getResourceAsStream("/logging.properties"));
+        Main.main(new String[0]);
+        server = Main.server();
+        Client client = ClientBuilder.newClient();
+        baseTarget = client.target("http://localhost:" + server.port());
+    }
+
+    @AfterAll
+    static void destroyClass() {
+        server.stop();
+    }
+
+    @Test
+    void TestHello() {
+        WebTarget target = baseTarget.path("/hello");
+        assertOk(target.request().get(), "Hello World");
+    }
+
+    @Test
+    void TestHelloTimeout() {
+        WebTarget target = baseTarget.path("/helloTimeout");
+        assertOk(target.request().get(), "Hello World");
+    }
+
+    @Test
+    void TestHelloAsync() {
+        WebTarget target = baseTarget.path("/helloAsync");
+        assertOk(target.request().get(), "Hello World");
+    }
+
+    private void assertOk(Response response, String expectedMessage) {
+        assertThat(response.getStatus(), is(200));
+        assertThat(response.readEntity(String.class), is(expectedMessage));
+    }
+}
