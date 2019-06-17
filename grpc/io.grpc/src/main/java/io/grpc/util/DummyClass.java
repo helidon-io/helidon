9d7ab3bbade1069af49823e0410a1799e99099de
Jonathan Knight <jk@thegridman.com>
2019-06-17 14:51:50 +0300

    Add module-info.java files to gRPC modules (issue #759)

---
+++ grpc/io.grpc/src/main/java/io/grpc/util/DummyClass.java
@@ -0,0 +1,11 @@
+package io.grpc.util;
+
+/**
+ * A dummy class to allow the re-packaging of grpc-java
+ * with a module-info.java file to work.
+ * <p>
+ * This file will be replaced by the real implementation from grpc-java
+ * as part of the re-packaging process.
+ */
+public class DummyClass {
+}
---
+++ grpc/io.grpc/src/main/java9/module-info.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Module info for re-packaged gRPC Java module.
+ */
+module io.grpc {
+    exports io.grpc;
+    exports io.grpc.inprocess;
+    exports io.grpc.internal;
+    exports io.grpc.stub;
+    exports io.grpc.stub.annotations;
+    exports io.grpc.util;
+
+    requires java.logging;
+    requires java.naming;
+
+    requires com.google.common;
+
+    requires error.prone.annotations;
+
+    uses io.grpc.ManagedChannelProvider;
+    uses io.grpc.NameResolverProvider;
+    uses io.grpc.ServerProvider;
+    uses io.grpc.internal.BinaryLogProvider;
+    uses io.grpc.LoadBalancerProvider;
+
+    provides io.grpc.NameResolverProvider
+            with io.grpc.internal.DnsNameResolverProvider;
+    provides io.grpc.LoadBalancerProvider
+            with io.grpc.internal.PickFirstLoadBalancerProvider;
+}
