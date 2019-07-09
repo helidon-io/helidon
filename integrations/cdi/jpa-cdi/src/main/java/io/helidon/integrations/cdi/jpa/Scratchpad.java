d59cffbc9adefe02c782eabb3af9428f13d6fd4d
Laird Nelson <laird.nelson@oracle.com>
2019-07-09 11:46:15 -0700

    Interim checkin
    Signed-off-by: Laird Nelson <laird.nelson@oracle.com>

--- integrations/cdi/jpa-cdi/src/main/java/io/helidon/integrations/cdi/jpa/Scratchpad.java
+++ integrations/cdi/jpa-cdi/src/main/java/io/helidon/integrations/cdi/jpa/Scratchpad.java
@@ -17,48 +17,138 @@ package io.helidon.integrations.cdi.jpa;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Objects;
 import java.util.Set;
 
 import javax.enterprise.context.Dependent;
+import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.CreationException;
 import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.literal.InjectLiteral;
+import javax.enterprise.inject.literal.NamedLiteral;
+import javax.enterprise.inject.spi.Annotated;
+import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.InjectionTarget;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+import javax.enterprise.inject.spi.ProcessInjectionTarget;
 import javax.enterprise.inject.spi.ProcessProducer;
 import javax.enterprise.inject.spi.Producer;
+import javax.enterprise.inject.spi.WithAnnotations;
 import javax.enterprise.inject.spi.configurator.BeanConfigurator;
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 import javax.persistence.SynchronizationType;
 import javax.transaction.Status;
 import javax.transaction.SystemException;
 import javax.transaction.Transaction;
 
-@Deprecated
-final class Scratchpad {
+import io.helidon.integrations.cdi.delegates.DelegatingInjectionTarget;
 
-    private Scratchpad() {
+/**
+ * An area for rough draft sketching around a new approach to JPA
+ * integration.
+ *
+ * @deprecated This class is slated for removal or renaming.  Do not
+ * use it.
+ */
+@Deprecated
+public final class Scratchpad implements Extension {
+
+    /**
+     * Creates a new {@link Scratchpad}.
+     *
+     * @deprecated This is not a real class yet.
+     */
+    @Deprecated
+    public Scratchpad() {
         super();
     }
 
-    private static <T, U> void installSpecialProducer(final ProcessProducer<T, U> event) {
-        final Producer<U> producer = event.getProducer();
+    /**
+     * Converts something like {@code @PersistenceContext(unitName =
+     * "fred") private EntityManager em;} to
+     * {@code @Inject @Named("fred") @Synchronized @JPATransactionScoped
+     * private EntityManager em;}.
+     */
+    private static <T> void processTypesWithPersistenceContextAnnotations(@Observes @WithAnnotations(PersistenceContext.class)
+                                                                          final ProcessAnnotatedType<T> event) {
+
+        event.configureAnnotatedType()
+            .filterFields(f -> f.isAnnotationPresent(PersistenceContext.class)
+                          && !f.isAnnotationPresent(Inject.class))
+            .forEach(fc -> {
+                    final PersistenceContext pc = fc.getAnnotated().getAnnotation(PersistenceContext.class);
+                    assert pc != null;
+                    fc.remove(a -> a == pc);
+                    fc.add(InjectLiteral.INSTANCE);
+                    fc.add(ContainerManaged.Literal.INSTANCE);
+                    if (PersistenceContextType.EXTENDED.equals(pc.type())) {
+                        fc.add(Extended.Literal.INSTANCE);
+                    } else {
+                        fc.add(JPATransactionScoped.Literal.INSTANCE);
+                    }
+                    if (SynchronizationType.UNSYNCHRONIZED.equals(pc.synchronization())) {
+                        fc.add(Unsynchronized.Literal.INSTANCE);
+                    } else {
+                        fc.add(Synchronized.Literal.INSTANCE);
+                    }
+                    fc.add(NamedLiteral.of(pc.unitName().trim()));
+                });
+    }
+
+    private static <R> void installSpecialProducer(@Observes final ProcessProducer<?, R> event) {
+        final Producer<R> producer = event.getProducer();
+        final Collection<?> keys = getKeys(producer);
+        if (keys != null && !keys.isEmpty()) {
+            event.setProducer(new EntityManagerReferencingProducer<>(producer, keys));
+        }
+    }
+
+    private static <T> void installSpecialInjectionTarget(@Observes final ProcessInjectionTarget<T> event) {
+        final InjectionTarget<T> injectionTarget = event.getInjectionTarget();
+        final Collection<?> keys = getKeys(injectionTarget);
+        if (keys != null && !keys.isEmpty()) {
+            event.setInjectionTarget(new DelegatingInjectionTarget<>(injectionTarget,
+                                                                     new EntityManagerReferencingProducer<>(injectionTarget,
+                                                                                                            keys)));
+        }
+    }
+
+    private static Collection<?> getKeys(final Producer<?> producer) {
         final Set<InjectionPoint> injectionPoints = producer.getInjectionPoints();
         final Set<Object> keys = new HashSet<>();
         for (final InjectionPoint injectionPoint : injectionPoints) {
             final Type type = injectionPoint.getType();
             if (type instanceof Class && EntityManager.class.isAssignableFrom((Class<?>) type)) {
-                // Revisit: get persistence unit name for real
-                final String persistenceUnitName = "";
-                keys.add(persistenceUnitName);
+                final Annotated annotated = injectionPoint.getAnnotated();
+                assert annotated != null;
+                // Because of processing the annotated type stuff
+                // above, we have converted all PersistenceContext
+                // annotations into sequences like:
+                //   @Inject
+                //   @ContainerManaged
+                //   @Named("fred")
+                //   @JPATransactionScoped
+                //   @Synchronized
+                // ...so we look for @Named here.
+                final Named named = annotated.getAnnotation(Named.class);
+                if (named != null) {
+                    keys.add(named.value().trim());
+                } else {
+                    keys.add("");
+                }
             }
         }
-        if (!keys.isEmpty()) {
-            event.setProducer(new EntityManagerReferencingProducer(producer, keys));
-        }
+        return keys;
     }
 
     private static void addJPATransactionScopedEntityManager(final BeanConfigurator<EntityManager> beanConfigurator,
@@ -70,11 +160,10 @@ final class Scratchpad {
             .scope(Dependent.class)
             .addQualifiers(qualifiers)
             .produceWith(instance -> {
-                    // Let's pretend qualifiers has @Default and @Any.
-
-                    // Build up an array of qualifiers that we'll use in
-                    // Instance#select(Class, Annotation...) calls.  Remove @Any
-                    // from the set if it's present.
+                    // Build up an array of qualifiers that we'll use
+                    // in Instance#select(Class, Annotation...) calls.
+                    // Remove @Any from the set if it's present.  We
+                    // don't want to select() @Any anything.
                     final int qualifiersSize;
                     final int qualifiersArraySize;
                     final Annotation[] qualifiersArray;
@@ -108,15 +197,16 @@ final class Scratchpad {
                         }
                     }
 
-                    // Get an Instance that can give us a @Default Transaction.
-                    // We deliberately do not use qualifiers.  It will be in
-                    // TransactionScoped scope.
+                    // Get an Instance that can give us a @Default
+                    // Transaction.  We deliberately do not use
+                    // qualifiers.  It will be in TransactionScoped
+                    // scope.
                     final Instance<Transaction> instanceTransaction = instance.select(Transaction.class);
 
                     // Get an Instance that can give us
                     // a @Default @CDITransactionScoped @Synchronized
-                    // (or @Unsynchronized) EntityManager.  It will be in
-                    // TransactionScoped scope.
+                    // (or @Unsynchronized) EntityManager.  It will be
+                    // in TransactionScoped scope.
                     final Annotation[] qualifiersWithCdiTransactionScopedArray = new Annotation[qualifiersSize + 2];
                     System.arraycopy(qualifiersArray, 0, qualifiersWithCdiTransactionScopedArray, 0, qualifiersArray.length);
                     qualifiersWithCdiTransactionScopedArray[qualifiersWithCdiTransactionScopedArray.length - 2] =
@@ -132,8 +222,9 @@ final class Scratchpad {
                         instance.select(EntityManager.class, qualifiersWithCdiTransactionScopedArray);
 
                     // Get an Instance that can give us
-                    // a @Default @ContainerManaged EntityManagerFactory.  It
-                    // will be in ApplicationScoped scope.
+                    // a @Default @ContainerManaged
+                    // EntityManagerFactory.  It will be in
+                    // ApplicationScoped scope.
                     final Annotation[] qualifiersWithContainerManagedArray = new Annotation[qualifiersSize + 1];
                     System.arraycopy(qualifiersArray, 0, qualifiersWithContainerManagedArray, 0, qualifiersArray.length);
                     qualifiersWithContainerManagedArray[qualifiersWithContainerManagedArray.length - 1] =
@@ -141,9 +232,9 @@ final class Scratchpad {
                     final Instance<EntityManagerFactory> instanceEmf =
                         instance.select(EntityManagerFactory.class, qualifiersWithContainerManagedArray);
 
-                    // Create the actual EntityManager that will be produced.
-                    // It itself will be in Dependent scope but its delegate may
-                    // be something else.
+                    // Create the actual EntityManager that will be
+                    // produced.  It itself will be in Dependent scope
+                    // but its delegate may be something else.
                     final EntityManager returnValue = new DelegatingEntityManager() {
                         @Override
                         protected EntityManager acquireDelegate() {
@@ -175,27 +266,7 @@ final class Scratchpad {
                         }
                     };
                     return returnValue;
-                })
-            .disposeWith((em, instance) -> {
-                    boolean inTransaction = false;
-                    try {
-                        inTransaction = inTransaction(instance.select(Transaction.class));
-                    } catch (final SystemException systemException) {
-                        // Revisit: log
-                        inTransaction = false;
-                    }
-                    if (!inTransaction) {
-                        // Revisit: no, this will just create a new entitymanager,
-                        // then close it.  Closing the non-transaction ems is a
-                        // pain in the neck.  We have to track them somewhere and
-                        // close them here.
-                        //
-                        // Maybe we could close them in DelegatingEntityManager?
-                        // On every operation?  Would that impact the persistence
-                        // context?
-                        ((DelegatingEntityManager) em).delegate().close();
-                    }
-                });
+                }); // deliberately no disposeWith()
 
     }
 
@@ -235,9 +306,6 @@ final class Scratchpad {
                     qualifiersArray = qualifiers.toArray(new Annotation[qualifiers.size() + 1]);
                     qualifiersArray[qualifiers.size() - 1] = ContainerManaged.Literal.INSTANCE;
                 }
-                // Revisit: this should be yet ANOTHER
-                // DelegatingEntityManager, because we have to ensure that its
-                // close() method cannot be invoked.
                 final EntityManagerFactory emf = instance.select(EntityManagerFactory.class, qualifiersArray).get();
                 // Revisit: get properties and pass them too
                 return emf.createEntityManager(synchronizationType);
