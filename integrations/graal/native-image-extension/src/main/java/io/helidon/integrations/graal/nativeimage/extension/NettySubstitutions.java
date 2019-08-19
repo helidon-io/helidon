8cb570f5d4ae8925060c036f2a55f46fa51dc566
Tomas Langer <tomas.langer@oracle.com>
2019-08-19 12:26:03 +0200

    Changes for Zipkin and Netty to run with latest Graal VM native-image
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

--- integrations/graal/native-image-extension/src/main/java/io/helidon/integrations/graal/nativeimage/extension/NettySubstitutions.java
+++ integrations/graal/native-image-extension/src/main/java/io/helidon/integrations/graal/nativeimage/extension/NettySubstitutions.java
@@ -15,8 +15,6 @@
  */
 package io.helidon.integrations.graal.nativeimage.extension;
 
-import com.oracle.svm.core.annotate.Alias;
-import com.oracle.svm.core.annotate.RecomputeFieldValue;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 import io.netty.util.internal.logging.InternalLoggerFactory;
@@ -25,42 +23,10 @@ import io.netty.util.internal.logging.JdkLoggerFactory;
 /**
  * Substitutions for GraalVM native image generation.
  */
-// supressing checkstyle issues, as this class cannot follow usual naming rules
+// suppressing checkstyle issues, as this class cannot follow usual naming rules
 @SuppressWarnings({"StaticVariableName", "VisibilityModifier"})
 public final class NettySubstitutions {
-    @TargetClass(className = "io.netty.util.internal.PlatformDependent")
-    static final class PlatformDependentSvmExtension {
-        /**
-         * The class PlatformDependent caches the byte array base offset by reading the
-         * field from PlatformDependent0. The automatic recomputation of Substrate VM
-         * correctly recomputes the field in PlatformDependent0, but since the caching
-         * in PlatformDependent happens during image building, the non-recomputed value
-         * is cached.
-         */
-        @Alias
-        @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.ArrayBaseOffset, declClass = byte[].class)
-        private static long BYTE_ARRAY_BASE_OFFSET;
-    }
-
-    @TargetClass(className = "io.netty.util.internal.PlatformDependent0")
-    static final class PlatformDependent0SvmExtension {
-        @Alias @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.FieldOffset, declClassName = "java.nio.Buffer", name =
-                "address")
-        private static long ADDRESS_FIELD_OFFSET;
-    }
-
-    @TargetClass(className = "io.netty.util.internal.CleanerJava6")
-    static final class CleanerJava6SvmExtension {
-        @Alias @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.FieldOffset, declClassName = "java.nio.DirectByteBuffer",
-                                    name = "cleaner")
-        private static long CLEANER_FIELD_OFFSET;
-    }
-
-    @TargetClass(className = "io.netty.util.internal.shaded.org.jctools.util.UnsafeRefArrayAccess")
-    static final class UnsafeRefArrayAccessSvmExtension {
-        @Alias @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.ArrayIndexShift, declClass = Object[].class)
-        public static int REF_ELEMENT_SHIFT;
-    }
+    // latest Netty sources now contain Graal VM native-image substitutions, so we could remove them
 
     @TargetClass(className = "io.netty.util.internal.logging.InternalLoggerFactory")
     static final class InternalLoggerFactorySvmExtension {
