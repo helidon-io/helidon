7b3f0dffd551e06ef0244119f0f588cb7fdf077b
Tomas Langer <tomas.langer@oracle.com>
2019-08-30 12:21:39 +0200

    TracerBuilder now uses Helidon service loader. (#967)
    Zipkin and Jaeger providers use default priority.
    Zipkin and Jaeger providers are extensible.
    
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

--- tracing/zipkin/src/main/java/io/helidon/tracing/zipkin/ZipkinTracerBuilder.java
+++ tracing/zipkin/src/main/java/io/helidon/tracing/zipkin/ZipkinTracerBuilder.java
@@ -106,7 +106,7 @@ import zipkin2.reporter.urlconnection.URLConnectionSender;
  * @see <a href="http://zipkin.io/pages/instrumenting.html#core-data-structures">Zipkin Attributes</a>
  * @see <a href="https://github.com/openzipkin/zipkin/issues/962">Zipkin Missing Service Name</a>
  */
-public final class ZipkinTracerBuilder implements TracerBuilder<ZipkinTracerBuilder> {
+public class ZipkinTracerBuilder implements TracerBuilder<ZipkinTracerBuilder> {
     static final Logger LOGGER = Logger.getLogger(ZipkinTracerBuilder.class.getName());
     static final String DEFAULT_PROTOCOL = "http";
     static final int DEFAULT_ZIPKIN_PORT = 9411;
@@ -126,7 +126,10 @@ public final class ZipkinTracerBuilder implements TracerBuilder<ZipkinTracerBuil
     private boolean enabled = DEFAULT_ENABLED;
     private boolean global = true;
 
-    private ZipkinTracerBuilder() {
+    /**
+     * Default constructor, does not modify state.
+     */
+    protected ZipkinTracerBuilder() {
     }
 
     /**
