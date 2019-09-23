fbeb1d3f695cd01ed44d7991ecb33c857e0df440
tim.quinn@oracle.com <tim.quinn@oracle.com>
2019-09-23 09:01:03 -0500

    Restore retrieval of metric to original code, now that registry.counter(name) works correctly

--- microprofile/metrics/src/test/java/io/helidon/microprofile/metrics/MetricsMpServiceTest.java
+++ microprofile/metrics/src/test/java/io/helidon/microprofile/metrics/MetricsMpServiceTest.java
@@ -79,6 +79,6 @@ public class MetricsMpServiceTest {
     }
 
     protected static Counter getCounter(String name) {
-        return registry.getCounters().get(name);
+        return registry.counter(name);
     }
 }
