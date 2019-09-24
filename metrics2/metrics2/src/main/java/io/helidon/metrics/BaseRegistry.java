d84186a9ed6e814a5286354c87d855c1ebe053cb
tim.quinn@oracle.com <tim.quinn@oracle.com>
2019-09-24 15:20:17 -0500

    Add to class JavaDoc explaining how to disable all base metrics

--- metrics2/metrics2/src/main/java/io/helidon/metrics/BaseRegistry.java
+++ metrics2/metrics2/src/main/java/io/helidon/metrics/BaseRegistry.java
@@ -48,13 +48,13 @@ import org.eclipse.microprofile.metrics.Tag;
  * </ul>
  *
  * Each metric can be disabled by using the following configuration property:
- * {@code helidon.metrics.base.${metric_name}.enabled=false}
+ * {@code helidon.metrics.base.${metric_name}.enabled=false}. Further, to suppress
+ * all base metrics set {@code helidon.metrics.base.enabled=false}.
  */
 final class BaseRegistry extends Registry {
 
-    static final String BASE_ENABLED_KEY = "base.enabled";
-
     private static final String CONFIG_METRIC_ENABLED_BASE = "base.";
+    static final String BASE_ENABLED_KEY = CONFIG_METRIC_ENABLED_BASE + "enabled";
 
     private static final Metadata MEMORY_USED_HEAP =
             new HelidonMetadata("memory.usedHeap",
