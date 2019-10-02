fef197b77c1827e54d70498c814d43d8d8867811
Tim Quinn <tim.quinn@oracle.com>
2019-10-02 13:37:31 -0500

    Change visibility and/or suppress JavaDoc for items that users do not/should not use themselves (#1075)

--- metrics/metrics/src/main/java/io/helidon/metrics/Clock.java
+++ metrics/metrics/src/main/java/io/helidon/metrics/Clock.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ package io.helidon.metrics;
  * Clock interface to allow replacing system clock with
  * a custom one (e.g. for unit testing).
  */
-public interface Clock {
+interface Clock {
     /**
      * System clock. Please do not use directly, use {@link #system()}.
      * This is only visible as we cannot do private modifier in interfaces yet.
