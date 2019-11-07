ab271ee1d07de35b2c1bdcec0c4edf2629181eb3
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2019-11-07 16:26:20 -0500

    Fixed copyrights and indentation. (#1139)
    Signed-off-by: Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>

--- microprofile/fault-tolerance/src/main/java/io/helidon/microprofile/faulttolerance/FaultToleranceExtension.java
+++ microprofile/fault-tolerance/src/main/java/io/helidon/microprofile/faulttolerance/FaultToleranceExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -191,13 +191,18 @@ public class FaultToleranceExtension implements Extension {
         registerFaultToleranceMethods(event.getAnnotatedBeanClass());
     }
 
+    /**
+     * Register FT methods for later processing.
+     *
+     * @param type Bean type.
+     */
     private void registerFaultToleranceMethods(AnnotatedType<?> type) {
         for (AnnotatedMethod<?> method : type.getMethods()) {
             if (isFaultToleranceMethod(type.getJavaClass(), method.getJavaMember())) {
                 getRegisteredMethods().add(new BeanMethod(type.getJavaClass(), method.getJavaMember()));
             }
         }
-        }
+    }
 
     /**
      * Registers metrics for all FT methods.
