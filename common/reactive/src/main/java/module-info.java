7c19438a7eabb15541c290c1758c83cebffe6489
Daniel Kec <daniel.kec@oracle.com>
2020-01-08 11:17:48 +0100

    Remove Valve oracle/helidon/issues/514
    Signed-off-by: Daniel Kec <daniel.kec@oracle.com>

--- common/reactive/src/main/java/module-info.java
+++ common/reactive/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,5 +25,4 @@ module io.helidon.common.reactive {
     requires io.helidon.common.mapper;
 
     exports io.helidon.common.reactive;
-    exports io.helidon.common.reactive.valve;
 }
