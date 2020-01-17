36348c7da059bbda8be996602acc306423a7694d
Laird Nelson <laird.nelson@oracle.com>
2020-01-16 16:58:59 -0800

    Interim squashable commit
    Signed-off-by: Laird Nelson <laird.nelson@oracle.com>

--- integrations/cdi/jpa-cdi/src/main/java/io/helidon/integrations/cdi/jpa/BeanManagerBackedDataSourceProvider.java
+++ integrations/cdi/jpa-cdi/src/main/java/io/helidon/integrations/cdi/jpa/BeanManagerBackedDataSourceProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,8 +29,11 @@ import javax.sql.DataSource;
  * DataSource}s.
  *
  * @see PersistenceUnitInfoBean.DataSourceProvider
+ *
+ * @deprecated Please use {@link JtaDataSourceProvider} instead.
  */
-@Vetoed // TODO for now
+@Deprecated
+@Vetoed
 @ApplicationScoped
 class BeanManagerBackedDataSourceProvider implements PersistenceUnitInfoBean.DataSourceProvider {
 
