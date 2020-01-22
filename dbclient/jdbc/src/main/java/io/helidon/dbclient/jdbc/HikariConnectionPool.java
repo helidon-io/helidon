274d87dc51d7ae0dbac8219cb9bcf9a545c0faa3
Tomas Langer <tomas.langer@oracle.com>
2020-01-22 12:59:41 +0100

    Helidon DB API and implementation for JDBC and MongoDB.
    Signed-off-by: Tomas Kraus <Tomas.Kraus@oracle.com>
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

---
+++ dbclient/jdbc/src/main/java/io/helidon/dbclient/jdbc/HikariConnectionPool.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.dbclient.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import io.helidon.dbclient.DbClientException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+/**
+ * Hikari Connection Pool integration.
+ */
+public class HikariConnectionPool implements ConnectionPool {
+    /** Hikari Connection Pool instance. */
+    private final HikariDataSource dataSource;
+
+    /** The type of this database. */
+    private final String dbType;
+
+    /**
+     * Creates an instance of Hikari Connection Pool from common connection pool builder.
+     *
+     * @param url database connection URL
+     * @param username database connection user name
+     * @param password database connection password
+     * @param properties additional connection pool properties (names without {@code dataSource.} prefix)
+     * @param dbType database type
+     */
+    HikariConnectionPool(
+            final String url,
+            final String username,
+            final String password,
+            final Properties properties,
+            final String dbType
+    ) {
+        this.dbType = dbType;
+        final HikariConfig config = new HikariConfig(properties);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        this.dataSource = new HikariDataSource(config);
+    }
+
+    @Override
+    public Connection connection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException ex) {
+            throw new DbClientException(
+                    String.format("Failed to create a connection to %s", dataSource.getJdbcUrl()), ex);
+        }
+    }
+
+    @Override
+    public String dbType() {
+        return dbType;
+    }
+
+}
---
+++ tests/integration/dbclient/common/src/main/java/io/helidon/tests/integration/dbclient/common/tests/transaction/TransactionExceptionalStmtIT.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.tests.integration.dbclient.common.tests.transaction;
+
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
+
+import io.helidon.dbclient.DbClientException;
+import io.helidon.dbclient.DbRow;
+import io.helidon.dbclient.DbRows;
+
+import org.junit.jupiter.api.Test;
+
+import static io.helidon.tests.integration.dbclient.common.AbstractIT.DB_CLIENT;
+import static io.helidon.tests.integration.dbclient.common.AbstractIT.LAST_POKEMON_ID;
+import static io.helidon.tests.integration.dbclient.common.AbstractIT.POKEMONS;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * Test exceptional states.
+ */
+public class TransactionExceptionalStmtIT {
+
+    /** Local logger instance. */
+    private static final Logger LOGGER = Logger.getLogger(TransactionExceptionalStmtIT.class.getName());
+
+    /** Maximum Pokemon ID. */
+    private static final int BASE_ID = LAST_POKEMON_ID + 40;
+
+    /**
+     * Verify that execution of query with non existing named statement throws an exception.
+     *
+     * @throws ExecutionException when database query failed
+     * @throws InterruptedException if the current thread was interrupted
+     */
+    @Test
+    public void testCreateNamedQueryNonExistentStmt() throws ExecutionException, InterruptedException {
+        LOGGER.info(() -> "Starting test");
+        try {
+            DbRows<DbRow> rows = DB_CLIENT.inTransaction(tx -> tx
+                    .createNamedQuery("select-pokemons-not-exists")
+                    .execute()
+            ).toCompletableFuture().get();
+            LOGGER.warning(() -> "Test failed");
+            fail("Execution of non existing statement shall cause an exception to be thrown.");
+        } catch (DbClientException ex) {
+            LOGGER.info(() -> String.format("Expected exception: %s", ex.getMessage()));
+        }
+    }
+
+    /**
+     * Verify that execution of query with both named and ordered arguments throws an exception.
+     *
+     * @throws ExecutionException when database query failed
+     * @throws InterruptedException if the current thread was interrupted
+     */
+    @Test
+    public void testCreateNamedQueryNamedAndOrderArgsWithoutArgs() throws ExecutionException, InterruptedException {
+        LOGGER.info(() -> "Starting test");
+        try {
+            DbRows<DbRow> rows = DB_CLIENT.inTransaction(tx -> tx
+                    .createNamedQuery("select-pokemons-error-arg")
+                    .execute()
+            ).toCompletableFuture().get();
+            LOGGER.warning(() -> "Test failed");
+            fail("Execution of query with both named and ordered parameters without passing any shall fail.");
+        } catch (DbClientException | ExecutionException ex) {
+            LOGGER.info(() -> String.format("Expected exception: %s", ex.getMessage()));
+        }
+    }
+
+    /**
+     * Verify that execution of query with both named and ordered arguments throws an exception.
+     *
+     * @throws ExecutionException when database query failed
+     * @throws InterruptedException if the current thread was interrupted
+     */
+    @Test
+    public void testCreateNamedQueryNamedAndOrderArgsWithArgs() throws ExecutionException, InterruptedException {
+        LOGGER.info(() -> "Starting test");
+        try {
+            DbRows<DbRow> rows = DB_CLIENT.inTransaction(tx -> tx
+                    .createNamedQuery("select-pokemons-error-arg")
+                    .addParam("id", POKEMONS.get(5).getId())
+                    .addParam(POKEMONS.get(5).getName())
+                    .execute()
+            ).toCompletableFuture().get();
+            LOGGER.warning(() -> "Test failed");
+            fail("Execution of query with both named and ordered parameters without passing them shall fail.");
+        } catch (DbClientException | ExecutionException ex) {
+            LOGGER.info(() -> String.format("Expected exception: %s", ex.getMessage()));
+        }
+    }
+
+    /**
+     * Verify that execution of query with named arguments throws an exception while trying to set ordered argument.
+     *
+     * @throws ExecutionException when database query failed
+     * @throws InterruptedException if the current thread was interrupted
+     */
+    @Test
+    public void testCreateNamedQueryNamedArgsSetOrderArg() throws ExecutionException, InterruptedException {
+        LOGGER.info(() -> "Starting test");
+        try {
+            DbRows<DbRow> rows = DB_CLIENT.inTransaction(tx -> tx
+                    .createNamedQuery("select-pokemon-named-arg")
+                    .addParam(POKEMONS.get(5).getName())
+                    .execute()
+            ).toCompletableFuture().get();
+            LOGGER.warning(() -> "Test failed");
+            fail("Execution of query with named parameter with passing ordered parameter value shall fail.");
+        } catch (DbClientException | ExecutionException ex) {
+            LOGGER.info(() -> String.format("Expected exception: %s", ex.getMessage()));
+        }
+    }
+
+    /**
+     * Verify that execution of query with ordered arguments throws an exception while trying to set named argument.
+     *
+     * @throws ExecutionException when database query failed
+     * @throws InterruptedException if the current thread was interrupted
+     */
+    @Test
+    public void testCreateNamedQueryOrderArgsSetNamedArg() throws ExecutionException, InterruptedException {
+        LOGGER.info(() -> "Starting test");
+        try {
+            DbRows<DbRow> rows = DB_CLIENT.inTransaction(tx -> tx
+                .createNamedQuery("select-pokemon-order-arg")
+                .addParam("name", POKEMONS.get(6).getName())
+                .execute()
+            ).toCompletableFuture().get();
+            LOGGER.warning(() -> "Test failed");
+            fail("Execution of query with ordered parameter with passing named parameter value shall fail.");
+        } catch (DbClientException | ExecutionException ex) {
+            LOGGER.info(() -> String.format("Expected exception: %s", ex.getMessage()));
+        }
+    }
+
+}
