6abaeeb304742e29d03da1e25cb8b93c1ee27b37
Tomas Langer <tomas.langer@oracle.com>
2020-02-01 21:14:38 +0100

    Upgrade checkstyle to 8.29 and update plugin + fix issues.

--- common/metrics/src/main/java/io/helidon/common/metrics/Loader.java
+++ common/metrics/src/main/java/io/helidon/common/metrics/Loader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2020 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,6 +53,7 @@ class Loader {
                 + io.helidon.common.metrics.InternalBridge.class.getName() + " to load");
     }
 
-    private Loader() {}
+    private Loader() {
+    }
 
 }
