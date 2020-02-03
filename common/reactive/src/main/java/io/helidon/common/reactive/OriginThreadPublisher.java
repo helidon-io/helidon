eba7eafc04b0d50f70451d61a12bf004f8fd5a79
Tomas Langer <tomas.langer@oracle.com>
2020-02-03 18:20:15 +0100

    Removal of project Reactor.

--- common/reactive/src/main/java/io/helidon/common/reactive/OriginThreadPublisher.java
+++ common/reactive/src/main/java/io/helidon/common/reactive/OriginThreadPublisher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2020 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,9 +33,9 @@ import java.util.logging.Logger;
  * {@link #submit(Object)}. In other words, whenever the source of chunks sends data, the same thread is used to deliver the data
  * to the subscriber.
  * <p>
- * Standard publisher implementations (such as {@link SubmissionPublisher} or Reactor Flux would use the same thread as
- * {@link Subscription#request(long)} was called on to deliver the chunk when the data are already available; this implementation
- * however strictly uses the originating thread.<p>
+ * Standard publisher implementations (such as {@link java.util.concurrent.SubmissionPublisher} or Reactor Flux would use
+ * the same thread as {@link Subscription#request(long)} was called on to deliver the chunk when the data are already available;
+ * this implementation however strictly uses the originating thread.<p>
  * In order to be able to achieve such behavior, this publisher provides hooks on subscription methods: {@link #hookOnCancel()}
  * and {@link #hookOnRequested(long, long)}.
  * </p>
--- config/config/src/main/java/io/helidon/config/ProviderImpl.java
+++ config/config/src/main/java/io/helidon/config/ProviderImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2020 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,13 +27,13 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Flow;
+import java.util.concurrent.SubmissionPublisher;
 import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import io.helidon.common.reactive.SubmissionPublisher;
 import io.helidon.config.internal.ConfigKeyImpl;
 import io.helidon.config.internal.ConfigUtils;
 import io.helidon.config.internal.ObjectNodeBuilderImpl;
