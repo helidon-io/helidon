f8818a3096f7e87486877324d3182ef1b2a82f03
Joe Di Pol <joe.dipol@oracle.com>
2020-02-03 20:23:39 -0800

    Update helidon jdk graalvm maven image to jdk11 and graalvm 19.3.1 Update standalone quickstarts to Java 11

--- etc/dockerfiles/build.sh
+++ etc/dockerfiles/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -58,12 +58,12 @@ else
 fi
 readonly MY_DIR=$(cd $(dirname -- "${SCRIPT_PATH}") ; pwd -P)
 
-readonly GRAALVM_VERSION=19.2.0
+readonly GRAALVM_VERSION=19.3.1
 
-docker build -f ${MY_DIR}/Dockerfile.jdk8-graalvm -t helidon/jdk8-graalvm:${GRAALVM_VERSION} ${MY_DIR}
-docker build -f ${MY_DIR}/Dockerfile.jdk8-graalvm-maven -t helidon/jdk8-graalvm-maven:${GRAALVM_VERSION} ${MY_DIR}
+docker build -f ${MY_DIR}/Dockerfile.jdk11-graalvm -t helidon/jdk11-graalvm:${GRAALVM_VERSION} ${MY_DIR}
+docker build -f ${MY_DIR}/Dockerfile.jdk11-graalvm-maven -t helidon/jdk11-graalvm-maven:${GRAALVM_VERSION} ${MY_DIR}
 
 if [ "${PUSH}" = "true" ] ; then
-    docker push helidon/jdk8-graalvm:${GRAALVM_VERSION}
-    docker push helidon/jdk8-graalvm-maven:${GRAALVM_VERSION}
+    docker push helidon/jdk11-graalvm:${GRAALVM_VERSION}
+    docker push helidon/jdk11-graalvm-maven:${GRAALVM_VERSION}
 fi
