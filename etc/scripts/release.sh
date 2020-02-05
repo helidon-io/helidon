3ecbdc030dea75e35bfb67d78726e35b3a29a074
Joe Di Pol <joe.dipol@oracle.com>
2020-02-05 10:34:53 -0800

    Update gradle files to work with Gradle 5, 6. Fix releases script to update build.gradle

--- etc/scripts/release.sh
+++ etc/scripts/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2018, 2020 Oracle and/or its affiliates. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -180,6 +180,15 @@ update_version(){
           sed -e s@'<helidon.version>.*</helidon.version>'@"<helidon.version>${FULL_VERSION}</helidon.version>"@g \
           > ${pom}.tmp
       mv ${pom}.tmp ${pom}
+  done
+
+  # Hack to update helidon.version in build.gradle files
+  for bfile in `egrep "helidonversion = .*" -r . --include build.gradle | cut -d ':' -f 1 | sort | uniq `
+  do
+      cat ${bfile} | \
+          sed -e s@'helidonversion = .*'@"helidonversion = \'${FULL_VERSION}\'"@g \
+          > ${bfile}.tmp
+      mv ${bfile}.tmp ${bfile}
   done
 
   # Invoke prepare hook
