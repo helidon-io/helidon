ded23ea1096a29ad54bd2ccf203ac529ea2ece25
Tomas Langer <tomas.langer@gmail.com>
2020-02-06 13:00:25 +0100

    Checkstyle upgrade (#1341)
    * Upgrade checkstyle to 8.29 and update plugin + fix issues.
    
    * Fixed copyright text and updated wrong copyrights.

--- common/reactive/src/main/java/io/helidon/common/reactive/Flow.java
+++ common/reactive/src/main/java/io/helidon/common/reactive/Flow.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2020 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -144,7 +144,9 @@ package io.helidon.common.reactive;
  */
 public final class Flow {
 
-    private Flow() {} // uninstantiable
+    // uninstantiable
+    private Flow() {
+    }
 
     /**
      * A producer of items (and related control messages) received by
--- examples/quickstarts/helidon-standalone-quickstart-mp/src/main/java/io/helidon/examples/quickstart/mp/Main.java
+++ examples/quickstarts/helidon-standalone-quickstart-mp/src/main/java/io/helidon/examples/quickstart/mp/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2020 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,8 @@ public final class Main {
     /**
      * Cannot be instantiated.
      */
-    private Main() { }
+    private Main() {
+    }
 
     /**
      * Application main entry point.
