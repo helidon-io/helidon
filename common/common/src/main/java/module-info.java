c9957e384e31a64e59fadfc4ead86389032a20db
Tomas Langer <tomas.langer@oracle.com>
2020-03-09 13:30:04 +0100

    Support for features that cannot use static initialization.

--- common/common/src/main/java/module-info.java
+++ common/common/src/main/java/module-info.java
@@ -21,4 +21,7 @@ module io.helidon.common {
     requires java.logging;
 
     exports io.helidon.common;
+    exports io.helidon.common.spi;
+
+    uses io.helidon.common.spi.HelidonFeatureProvider;
 }
