54ce6eb5d30a33509d76820a7fef3c51a5146562
Tomas Kraus <Tomas.Kraus@oracle.com>
2020-03-10 14:43:38 +0100

    Mappers for database date/time/timestamps.
    Signed-off-by: Tomas Kraus <Tomas.Kraus@oracle.com>

---
+++ dbclient/common/src/main/java/io/helidon/dbclient/common/mapper/SqlTimestampToLocalDateTime.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.dbclient.common.mapper;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import io.helidon.common.mapper.Mapper;
+
+/**
+ * Maps {@link java.sql.Timestamp} to {@link java.time.LocalDateTime}.
+ */
+public class SqlTimestampToLocalDateTime implements Mapper<Timestamp, LocalDateTime> {
+
+    @Override
+    public LocalDateTime map(Timestamp source) {
+        return source != null ? source.toLocalDateTime() : null;
+    }
+
+}
