57e5771088d637415ca52fe75881ec98e24a531b
Daniel Kec <daniel.kec@oracle.com>
2020-03-10 17:32:01 +0100

    MP Reactive Messaging impl (#1287)
    * MP Reactive Messaging impl
    * Arquillian manually managed deploy fix after native compatibility cdi changes
    
    Signed-off-by: Daniel Kec <daniel.kec@oracle.com>

---
+++ microprofile/messaging/src/test/java/io/helidon/microprofile/messaging/inner/ack/incoming/IncomingSubscriberBuilderMsgManualAckBean.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c)  2020 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.helidon.microprofile.messaging.inner.ack.incoming;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import io.helidon.microprofile.messaging.AssertableTestBean;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.eclipse.microprofile.reactive.messaging.Acknowledgment;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
+import org.eclipse.microprofile.reactive.streams.operators.SubscriberBuilder;
+import org.reactivestreams.Publisher;
+
+@ApplicationScoped
+public class IncomingSubscriberBuilderMsgManualAckBean implements AssertableTestBean {
+
+    private static final String TEST_MSG = "test-data";
+    private CompletableFuture<Void> ackFuture = new CompletableFuture<>();
+    private AtomicBoolean completedBeforeProcessor = new AtomicBoolean(false);
+    private AtomicBoolean interceptedMessage = new AtomicBoolean(false);
+
+    @Outgoing("test-channel")
+    public Publisher<Message<String>> produceMessage() {
+        return ReactiveStreams.of(Message.of(TEST_MSG, () -> {
+            ackFuture.complete(null);
+            return CompletableFuture.completedFuture(null);
+        })).buildRs();
+    }
+
+    @Incoming("test-channel")
+    @Acknowledgment(Acknowledgment.Strategy.MANUAL)
+    public SubscriberBuilder<Message<String>, Void> receiveMessage() {
+        return ReactiveStreams.<Message<String>>builder()
+                .forEach(m -> {
+                    completedBeforeProcessor.set(ackFuture.isDone());
+                    interceptedMessage.set(TEST_MSG.equals(m.getPayload()));
+                    m.ack();
+                });
+    }
+
+    @Override
+    public void assertValid() {
+        try {
+            ackFuture.toCompletableFuture().get(1, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            fail(e);
+        }
+        assertFalse(completedBeforeProcessor.get());
+        assertTrue(interceptedMessage.get());
+    }
+}
---
+++ microprofile/messaging/src/test/java/io/helidon/microprofile/messaging/inner/ack/processor/ProcessorProcessorBuilderMsg2MsgManualAckBean.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c)  2020 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.helidon.microprofile.messaging.inner.ack.processor;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import io.helidon.microprofile.messaging.AssertableTestBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.eclipse.microprofile.reactive.messaging.Acknowledgment;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.eclipse.microprofile.reactive.streams.operators.ProcessorBuilder;
+import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
+import org.reactivestreams.Publisher;
+
+@ApplicationScoped
+public class ProcessorProcessorBuilderMsg2MsgManualAckBean implements AssertableTestBean {
+
+    public static final String TEST_DATA = "test-data";
+    private CompletableFuture<Void> ackFuture = new CompletableFuture<>();
+    private AtomicBoolean completedBeforeProcessor = new AtomicBoolean(false);
+    private AtomicReference<String> RESULT_DATA = new AtomicReference<>();
+
+    @Outgoing("inner-processor")
+    public Publisher<Message<String>> produceMessage() {
+        return ReactiveStreams.of(Message.of(TEST_DATA, () -> {
+            ackFuture.complete(null);
+            return ackFuture;
+        })).buildRs();
+    }
+
+    @Incoming("inner-processor")
+    @Outgoing("inner-consumer")
+    @Acknowledgment(Acknowledgment.Strategy.MANUAL)
+    public ProcessorBuilder<Message<String>, Message<String>> process() {
+        return ReactiveStreams.<Message<String>>builder()
+                .map(m -> {
+                    completedBeforeProcessor.set(ackFuture.isDone());
+                    return Message.of(m.getPayload(), m::ack);
+                });
+    }
+
+    @Incoming("inner-consumer")
+    @Acknowledgment(Acknowledgment.Strategy.PRE_PROCESSING)
+    public void receiveMessage(String msg) {
+        RESULT_DATA.set(msg);
+    }
+
+    @Override
+    public void assertValid() {
+        try {
+            ackFuture.toCompletableFuture().get(1, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            fail(e);
+        }
+        assertFalse(completedBeforeProcessor.get());
+        assertEquals(TEST_DATA, RESULT_DATA.get());
+    }
+}
