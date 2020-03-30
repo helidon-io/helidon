356b0e69ac5417476749e4011dbb30d8607bb609
Tomas Langer <tomas.langer@gmail.com>
2020-03-31 00:56:03 +0200

    Support for OpenTracing contrib's TracerResolver.
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

--- microprofile/tracing/src/main/java9/module-info.java
+++ microprofile/tracing/src/main/java9/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2020 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +35,7 @@ module io.helidon.microprofile.tracing {
     requires io.helidon.jersey.common;
     requires transitive io.helidon.tracing;
     requires transitive io.helidon.tracing.jersey;
+    requires io.helidon.tracing.tracerresolver;
 
     requires transitive microprofile.opentracing.api;
     requires microprofile.rest.client.api;
