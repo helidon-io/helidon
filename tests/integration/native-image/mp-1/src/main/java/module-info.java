a0bc50b5596be8b5d285073f82adc4e828cace1a
Tomas Langer <tomas.langer@oracle.com>
2020-03-31 18:07:17 +0200

    Copyright fixes, tests for jar and module path fro MP.
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

--- tests/integration/native-image/mp-1/src/main/java/module-info.java
+++ tests/integration/native-image/mp-1/src/main/java/module-info.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2020 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 module helidon.tests.nimage.mp {
     requires jakarta.enterprise.cdi.api;
     requires java.ws.rs;
@@ -19,5 +34,10 @@ module helidon.tests.nimage.mp {
 
     exports io.helidon.tests.integration.nativeimage.mp1;
 
-    opens io.helidon.tests.integration.nativeimage.mp1 to weld.core.impl,hk2.utils;
+    opens io.helidon.tests.integration.nativeimage.mp1 to weld.core.impl,hk2.utils,io.helidon.webserver;
+
+    // we need to open the static resource on classpath directory to everybody, as otherwise
+    // static content will not see it
+    // if you want to add subdirectories, you will need to open them as well
+    opens web;
 }
