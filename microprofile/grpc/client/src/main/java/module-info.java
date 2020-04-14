329962f5ccf757343d54aaf48829ae8154b53690
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2020-04-14 11:18:48 -0400

    Updated copyright line.
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

--- microprofile/grpc/client/src/main/java/module-info.java
+++ microprofile/grpc/client/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
