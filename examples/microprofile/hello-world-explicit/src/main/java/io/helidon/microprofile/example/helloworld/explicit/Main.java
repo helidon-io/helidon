3de919442311720ea14e3e3f486a8c1323136b60
Joe Di Pol <joe.dipol@oracle.com>
2020-05-04 10:51:21 -0700

    Fix comment

--- examples/microprofile/hello-world-explicit/src/main/java/io/helidon/microprofile/example/helloworld/explicit/Main.java
+++ examples/microprofile/hello-world-explicit/src/main/java/io/helidon/microprofile/example/helloworld/explicit/Main.java
@@ -34,7 +34,7 @@ public class Main {
      */
     public static void main(String[] args) {
         Server server = Server.builder()
-                // using a customized helidon config instance (in this case the default...)
+                // Provide a MicroProfile config instance (in this case the default...)
                 .config(ConfigProviderResolver.instance()
                                 .getBuilder()
                                 .build())
