0b2c7cd72ff38f80fab14359108e4bdfd981c3f9
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2020-05-20 15:27:11 -0400

    Wait for a thread to complete only if its interrupted flag has been set and it is still running. Some new tests.
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

---
+++ microprofile/fault-tolerance/src/test/java/io/helidon/microprofile/faulttolerance/TimeoutNoRetryBean.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.microprofile.faulttolerance;
+
+import java.time.temporal.ChronoUnit;
+import javax.enterprise.context.Dependent;
+
+import org.eclipse.microprofile.faulttolerance.Timeout;
+import org.eclipse.microprofile.faulttolerance.exceptions.TimeoutException;
+
+/**
+ * Class TimeoutNoRetryBean.
+ */
+@Dependent
+public class TimeoutNoRetryBean {
+
+    @Timeout(value=1000, unit=ChronoUnit.MILLIS)
+    public String forceTimeoutSleep() throws InterruptedException, TimeoutException {
+        FaultToleranceTest.printStatus("TimeoutNoRetryBean::forceTimeout()", "failure");
+        Thread.sleep(2000);
+        return "failure";
+    }
+
+    @Timeout(value=1000, unit=ChronoUnit.MILLIS)
+    public String forceTimeoutLoop() throws TimeoutException {
+        FaultToleranceTest.printStatus("TimeoutNoRetryBean::forceTimeoutLoop()", "failure");
+        long start = System.currentTimeMillis();
+        while (System.currentTimeMillis() - start < 2000) {
+            // busy loop
+        }
+        return "success";
+    }
+}
