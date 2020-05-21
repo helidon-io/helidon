1da6b23f1d4bcac100e8de3793e6c882ab69756a
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2020-05-21 10:00:58 -0400

    Wait for thread completion only if interrupted flag set (#1843)
    * Wait for a thread to complete only if its interrupted flag has been set and it is still running. Some new tests.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    * Wait for a thread to complete only if its interrupted flag has been set and it is still running. Some new tests.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    * Updated copyright header.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

--- microprofile/fault-tolerance/src/main/java/io/helidon/microprofile/faulttolerance/FaultToleranceCommand.java
+++ microprofile/fault-tolerance/src/main/java/io/helidon/microprofile/faulttolerance/FaultToleranceCommand.java
@@ -450,17 +450,17 @@ public class FaultToleranceCommand extends HystrixCommand<Object> {
 
     /**
      * <p>After a timeout expires, Hystrix can report an {@link ExecutionException}
-     * while a thread is still running and cannot be interrupted (e.g. busy
-     * loop). Hystrix makes this possible by using another thread to monitor
+     * when a thread has been interrupted but it is still running (e.g. while in a
+     * busy loop). Hystrix makes this possible by using another thread to monitor
      * the command's thread.</p>
      *
      * <p>According to the FT spec, the thread may continue to run, so here
      * we give it a chance to do that before completing the execution of the
      * command. For more information see TCK test {@code
-     * TimeoutUninterruptableTest::timeoutTest}.</p>
+     * TimeoutUninterruptableTest::testTimeout}.</p>
      */
     private void waitForThreadToComplete() {
-        if (!introspector.isAsynchronous() && runThread != null) {
+        if (!introspector.isAsynchronous() && runThread != null && runThread.isInterrupted()) {
             try {
                 int waitTime = 250;
                 while (runThread.getState() == Thread.State.RUNNABLE && waitTime <= threadWaitingPeriod) {
