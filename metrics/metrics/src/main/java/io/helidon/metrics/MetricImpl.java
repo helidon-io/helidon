ae9927744c8de63238e0f55bd00d1b05751f7f1e
Aleksandar Seovic <aleks@seovic.com>
2020-06-09 22:58:48 -0400

    Copyright and checkstyle fix

--- metrics/metrics/src/main/java/io/helidon/metrics/MetricImpl.java
+++ metrics/metrics/src/main/java/io/helidon/metrics/MetricImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2020 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -148,11 +148,11 @@ abstract class MetricImpl implements HelidonMetric {
 
     @Override
     public String toString() {
-        return getClass().getSimpleName() + "{" +
-                "registryType='" + registryType + '\'' +
-                ", metadata=" + metadata +
-                ", name='" + getName() + '\'' +
-                '}';
+        return getClass().getSimpleName() + "{"
+                + "registryType='" + registryType + '\''
+                + ", metadata=" + metadata
+                + ", name='" + getName() + '\''
+                + '}';
     }
 
     @Override
