073d6b5edb957b58854da6b0a9adedf2350224aa
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2020-06-16 09:47:37 -0400

    Updated copyrights.
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

--- jersey/client/src/test/java/io/helidon/jersey/client/ConnectorProviderTest.java
+++ jersey/client/src/test/java/io/helidon/jersey/client/ConnectorProviderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ public class ConnectorProviderTest {
     /**
      * There should be no {@link org.glassfish.jersey.client.spi.ConnectorProvider}'s in
      * path when running this test. This test will fail if the {@code io.helidon.jersey.connector}
-     * modules is available.
+     * module is available.
      *
      * @throws NoSuchElementException If not found.
      */
