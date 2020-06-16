36b7c6c121a017d152a30923bdbe6d5f64d7ad3f
Daniel Kec <daniel.kec@oracle.com>
2020-06-16 19:05:35 +0200

    Copyright fix
    Signed-off-by: Daniel Kec <daniel.kec@oracle.com>

--- examples/messaging/kafkaRun.sh
+++ examples/messaging/kafkaRun.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 #
-# Copyright (c)  2020 Oracle and/or its affiliates.
+# Copyright (c) 2020 Oracle and/or its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 #
 
 if [[ "$(docker images -q helidon-test-kafka 2>/dev/null)" == "" ]]; then
