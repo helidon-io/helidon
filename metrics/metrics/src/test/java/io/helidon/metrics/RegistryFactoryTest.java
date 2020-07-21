4108ba263dc31510ac9397aab5436bd7f288ef74
Tim Quinn <tim.quinn@oracle.com>
2020-07-21 12:51:02 -0500

    Remove FinalRegistry and its use for the base registry; part of moving to MP metrics 2.3 (#2188)
    Signed-off-by: tim.quinn@oracle.com <tim.quinn@oracle.com>

--- metrics/metrics/src/test/java/io/helidon/metrics/RegistryFactoryTest.java
+++ metrics/metrics/src/test/java/io/helidon/metrics/RegistryFactoryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2020 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -80,12 +80,6 @@ public class RegistryFactoryTest {
 
         gauge = baseUn.getGauges().get(METRIC_USED_HEAP);
         assertThat(METRIC_USED_HEAP + " should be available by default", gauge, notNullValue());
-    }
-
-    @Test
-    void testBaseFinal() {
-        assertThrows(UnsupportedOperationException.class, () -> base.counter("new.counter"));
-        assertThrows(UnsupportedOperationException.class, () -> baseUn.counter("new.counter"));
     }
 
     @Test
