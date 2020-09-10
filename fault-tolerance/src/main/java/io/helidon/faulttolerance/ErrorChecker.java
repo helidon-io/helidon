b6d2e917df5ab314a37d264d588e342730d50475
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2020-09-10 11:03:28 -0400

    Make sure that tests are copied due to mutability.
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

--- fault-tolerance/src/main/java/io/helidon/faulttolerance/ErrorChecker.java
+++ fault-tolerance/src/main/java/io/helidon/faulttolerance/ErrorChecker.java
@@ -25,15 +25,18 @@ interface ErrorChecker {
     /**
      * Returns ErrorChecker that skips if throwable is in skipOnSet or if applyOnSet
      * is not empty and throwable is not in it. Note that if applyOnSet is empty, then
-     * it is equivalent to it containing {@code Throwable.class}.
+     * it is equivalent to it containing {@code Throwable.class}. Sets are copied
+     * because they are mutable.
      *
      * @param skipOnSet set of throwables to skip logic on.
      * @param applyOnSet set of throwables to apply logic on.
      * @return An error checker.
      */
     static ErrorChecker create(Set<Class<? extends Throwable>> skipOnSet, Set<Class<? extends Throwable>> applyOnSet) {
-        return throwable -> containsThrowable(skipOnSet, throwable)
-                || !applyOnSet.isEmpty() && !containsThrowable(applyOnSet, throwable);
+        Set<Class<? extends Throwable>> skipOn = Set.copyOf(skipOnSet);
+        Set<Class<? extends Throwable>> applyOn = Set.copyOf(applyOnSet);
+        return throwable -> containsThrowable(skipOn, throwable)
+                || !applyOn.isEmpty() && !containsThrowable(applyOn, throwable);
     }
 
     private static boolean containsThrowable(Set<Class<? extends Throwable>> set, Throwable throwable) {
