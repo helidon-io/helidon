c51ff7f0efbd6306293c96463a9134718cca5f93
tim.quinn@oracle.com <tim.quinn@oracle.com>
2020-11-03 17:05:29 -0600

    Convert tests to use the new infrastructure
    Signed-off-by: tim.quinn@oracle.com <tim.quinn@oracle.com>

--- microprofile/metrics/src/test/java/io/helidon/microprofile/metrics/ReusabilityInterceptorTest.java
+++ microprofile/metrics/src/test/java/io/helidon/microprofile/metrics/ReusabilityInterceptorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2020 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,14 +16,30 @@
  */
 package io.helidon.microprofile.metrics;
 
+import io.helidon.microprofile.tests.junit5.HelidonTest;
+import org.eclipse.microprofile.metrics.Counter;
+import org.eclipse.microprofile.metrics.MetricRegistry;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import javax.inject.Inject;
+
+@HelidonTest
 public class ReusabilityInterceptorTest extends MetricsBaseTest {
+
+    @Inject
+    private MetricRegistry metricRegistry;
 
     @Test
     public void testReusedMetricWithInterceptor() {
         ResourceWithReusedMetricForInvocation resource = newBean(ResourceWithReusedMetricForInvocation.class);
         resource.method1();
 
+        Counter counter = metricRegistry.counter(ResourceWithReusedMetricForInvocation.OTHER_REUSED_NAME,
+                MetricUtil.tags(new String[] {ResourceWithReusedMetricForInvocation.TAG_1,
+                        ResourceWithReusedMetricForInvocation.TAG_2}));
+        assertThat(counter.getCount(), is(1L));
     }
 }
