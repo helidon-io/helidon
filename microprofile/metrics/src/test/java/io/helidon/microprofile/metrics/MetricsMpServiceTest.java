cf06c41d31c4fc9c207e6fd4a29f7096ce246ced
tim.quinn@oracle.com <tim.quinn@oracle.com>
2020-11-10 17:32:06 -0600

    Clear out the base registry of all REST.resource entries BeforeAll
    Signed-off-by: tim.quinn@oracle.com <tim.quinn@oracle.com>

--- microprofile/metrics/src/test/java/io/helidon/microprofile/metrics/MetricsMpServiceTest.java
+++ microprofile/metrics/src/test/java/io/helidon/microprofile/metrics/MetricsMpServiceTest.java
@@ -16,6 +16,7 @@
 
 package io.helidon.microprofile.metrics;
 
+import io.helidon.metrics.RegistryFactory;
 import io.helidon.microprofile.tests.junit5.AddBean;
 import io.helidon.microprofile.tests.junit5.HelidonTest;
 import org.eclipse.microprofile.metrics.Counter;
@@ -25,6 +26,8 @@ import org.eclipse.microprofile.metrics.MetricType;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.Tag;
 import org.eclipse.microprofile.metrics.annotation.RegistryType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeAll;
 
 import javax.inject.Inject;
 
@@ -34,6 +37,18 @@ import javax.inject.Inject;
 @HelidonTest
 @AddBean(HelloWorldResource.class)
 public class MetricsMpServiceTest {
+
+    @BeforeAll
+    public static void initTest() {
+        initSyntheticSimpleTimerRegistry();
+    }
+
+    static MetricRegistry initSyntheticSimpleTimerRegistry() {
+        MetricRegistry result = RegistryFactory.getInstance().getRegistry(MetricRegistry.Type.BASE);
+        result.remove(MetricsCdiExtension.SYNTHETIC_SIMPLE_TIMER_METRIC_NAME);
+        return result;
+    }
+
 
     @Inject
     private MetricRegistry registry;
