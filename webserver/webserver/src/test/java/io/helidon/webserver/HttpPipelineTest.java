223e7b3a3d9e2a7f4d768c28acf7758caca9b42a
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2020-12-08 16:01:31 -0500

    New pipelining test that uses HTTP/2 and OkHttp client.
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

--- webserver/webserver/src/test/java/io/helidon/webserver/HttpPipelineTest.java
+++ webserver/webserver/src/test/java/io/helidon/webserver/HttpPipelineTest.java
@@ -16,14 +16,32 @@
 
 package io.helidon.webserver;
 
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.net.URL;
+import java.security.cert.X509Certificate;
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
 import io.helidon.common.http.Http;
 import io.helidon.webserver.utils.SocketHttpClient;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Interceptor;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -39,9 +57,41 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 public class HttpPipelineTest {
     private static final Logger LOGGER = Logger.getLogger(HttpPipelineTest.class.getName());
 
+    private static X509TrustManager TRUST_MANAGER = new X509TrustManager() {
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+
+        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+        }
+
+        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+        }
+    };
+
     private static WebServer webServer;
     private static AtomicInteger counter = new AtomicInteger(0);
-    private static ScheduledExecutorService executor = new ScheduledThreadPoolExecutor(1);;
+    private static ScheduledExecutorService executor = new ScheduledThreadPoolExecutor(1);
+    private static OkHttpClient client;
+
+    private static class LoggingInterceptor implements Interceptor {
+        @Override
+        public okhttp3.Response intercept(Interceptor.Chain chain) throws IOException {
+            Request request = chain.request();
+
+            long t1 = System.nanoTime();
+            System.out.println(String.format("Sending request %s on %s%n%s",
+                    request.url(), chain.connection(), request.headers()));
+
+            Response response = chain.proceed(request);
+
+            long t2 = System.nanoTime();
+            System.out.println(String.format("Received response for %s in %.1fms%nProtocol is %s%n%s",
+                    response.request().url(), (t2 - t1) / 1e6d, response.protocol(), response.headers()));
+
+            return response;
+        }
+    }
 
     @BeforeAll
     public static void startServer() throws Exception {
@@ -59,6 +109,8 @@ public class HttpPipelineTest {
 
     private static void startServer(int port) throws Exception {
         webServer = WebServer.builder()
+                .experimental(ExperimentalConfiguration.builder().http2(
+                        Http2Configuration.builder().enable(true).build()).build())
                 .port(port)
                 .routing(Routing.builder()
                         .put("/", (req, res) -> {
@@ -67,7 +119,7 @@ public class HttpPipelineTest {
                         })
                         .get("/", (req, res) -> {
                             int n = counter.getAndIncrement();
-                            int delay = (n % 2 == 0) ? 1000 : 0;    // alternate delay and no delay
+                            int delay = (n % 2 == 0) ? 1000 : 0;    // alternate delay 1 second and no delay
                             executor.schedule(() -> res.status(Http.Status.OK_200).send("Response " + n + "\n"),
                                     delay, TimeUnit.MILLISECONDS);
                         })
@@ -76,6 +128,11 @@ public class HttpPipelineTest {
                 .start()
                 .toCompletableFuture()
                 .get(10, TimeUnit.SECONDS);
+
+        OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder()
+                .addNetworkInterceptor(new LoggingInterceptor())
+                .protocols(Collections.singletonList(Protocol.H2_PRIOR_KNOWLEDGE));
+        client = clientBuilder.build();
 
         LOGGER.info("Started server at: https://localhost:" + webServer.port());
     }
@@ -100,5 +157,60 @@ public class HttpPipelineTest {
             String get1 = s.receive();
             assertThat(get1, containsString("Response 1"));
         }
+    }
+
+    /**
+     * Same as previous test but using HTTP/2 and OkHttp as async client.
+     */
+    @Test
+    public void testPipeliningHttp2() throws Exception {
+        MediaType mt = MediaType.get("text/plain");
+        URL url = new URL("http://localhost:" + webServer.port() + "/");
+        Request put = new Request.Builder().url(url).put(RequestBody.create(mt, "")).build();
+        client.newCall(put).execute();
+
+        Request get = new Request.Builder().url(url).build();
+        CompletableFuture<?> cf0 = new CompletableFuture<>();
+        CompletableFuture<?> cf1 = new CompletableFuture<>();
+
+        client.newCall(get).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                cf0.completeExceptionally(e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response r0) throws IOException {
+                LOGGER.info("Received r0");
+                if (cf1.isDone()) {
+                    LOGGER.info("Expected r0 before r1");
+                    cf0.completeExceptionally(new RuntimeException("Expected r0 before r1"));
+                } else {
+                    assertThat(r0.body().string(), containsString("Response 0"));
+                    cf0.complete(null);
+                }
+            }
+        });
+
+        client.newCall(get).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                cf1.completeExceptionally(e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response r1) throws IOException {
+                LOGGER.info("Received r1");
+                try {
+                    cf0.get(100L, TimeUnit.MILLISECONDS);   // give r0 callback a chance
+                    assertThat(r1.body().string(), containsString("Response 1"));
+                    cf1.complete(null);
+                } catch (InterruptedException | ExecutionException | TimeoutException  e) {
+                    LOGGER.info("Expected r0 before r1");
+                    cf1.completeExceptionally(e);
+                }
+            }
+        });
+        CompletableFuture.allOf(cf0, cf1).get(2000L, TimeUnit.MILLISECONDS);
     }
 }
