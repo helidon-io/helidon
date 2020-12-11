bbfdba492fdf9e53a158a4711fd8a48a397b7447
dalexandrov <mitiaalexandrov@gmail.com>
2020-12-11 10:47:12 +0200

    Switch from Testcontainers to Neo4j test harness

--- examples/integrations/neo4j/neo4j-mp/src/test/java/io/helidon/examples/integrations/neo4j/mp/MainTest.java
+++ examples/integrations/neo4j/neo4j-mp/src/test/java/io/helidon/examples/integrations/neo4j/mp/MainTest.java
@@ -25,70 +25,43 @@ import javax.ws.rs.client.ClientBuilder;
 
 import io.helidon.microprofile.server.Server;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.neo4j.driver.AuthTokens;
-import org.neo4j.driver.GraphDatabase;
-import org.testcontainers.containers.Neo4jContainer;
+import org.neo4j.harness.Neo4j;
+import org.neo4j.harness.Neo4jBuilders;
 
 /**
  * Main tests of the application done here.
  */
 class MainTest {
     private static Server server;
-    private static Neo4jContainer neo4jContainer;
+    private static Neo4j embeddedDatabaseServer;
 
-    //@BeforeAll Decide if we need testcontainers.
+    @BeforeAll
     public static void startTheServer() throws Exception {
-        neo4jContainer = new Neo4jContainer<>("neo4j:4.0")
-                .withAdminPassword("secret");
-        neo4jContainer.start();
+
+        embeddedDatabaseServer = Neo4jBuilders.newInProcessBuilder()
+                .withDisabledServer()
+                .withFixture(FIXTURE)
+                .build();
+
+        System.setProperty("neo4j.uri", embeddedDatabaseServer.boltURI().toString());
 
         server = Server.create().start();
 
-        try (var driver = GraphDatabase.driver(neo4jContainer.getBoltUrl(), AuthTokens.basic("neo4j", "secret"));
-                var session = driver.session()
-        ) {
-            session.writeTransaction(tx -> tx.run(""
-                                                          + "CREATE (TheMatrix:Movie {title:'The Matrix', released:1999, tagline:'Welcome to the Real World'})\n"
-                                                          + "CREATE (Keanu:Person {name:'Keanu Reeves', born:1964})\n"
-                                                          + "CREATE (Carrie:Person {name:'Carrie-Anne Moss', born:1967})\n"
-                                                          + "CREATE (Laurence:Person {name:'Laurence Fishburne', born:1961})\n"
-                                                          + "CREATE (Hugo:Person {name:'Hugo Weaving', born:1960})\n"
-                                                          + "CREATE (LillyW:Person {name:'Lilly Wachowski', born:1967})\n"
-                                                          + "CREATE (LanaW:Person {name:'Lana Wachowski', born:1965})\n"
-                                                          + "CREATE (JoelS:Person {name:'Joel Silver', born:1952})\n"
-                                                          + "CREATE\n"
-                                                          + "  (Keanu)-[:ACTED_IN {roles:['Neo']}]->(TheMatrix),\n"
-                                                          + "  (Carrie)-[:ACTED_IN {roles:['Trinity']}]->(TheMatrix),\n"
-                                                          + "  (Laurence)-[:ACTED_IN {roles:['Morpheus']}]->(TheMatrix),\n"
-                                                          + "  (Hugo)-[:ACTED_IN {roles:['Agent Smith']}]->(TheMatrix),\n"
-                                                          + "  (LillyW)-[:DIRECTED]->(TheMatrix),\n"
-                                                          + "  (LanaW)-[:DIRECTED]->(TheMatrix),\n"
-                                                          + "  (JoelS)-[:PRODUCED]->(TheMatrix)").consume()
-            );
-        }
-
-        // Don't know how to set this dynamically otherwise in Helidon
-        System.setProperty("neo4j.uri", neo4jContainer.getBoltUrl());
     }
 
-    //@AfterAll
+    @AfterAll
     static void destroyClass() {
         CDI<Object> current = CDI.current();
         ((SeContainer) current).close();
+        embeddedDatabaseServer.close();
     }
 
-    //@AfterAll
-    public static void stopNeo4j() {
-        neo4jContainer.stop();
-    }
 
-    //@Test
-    //@Ignore// Currently ignore. Decide if we need testcontainers.
+    @Test
     void testMovies() {
 
         Client client = ClientBuilder.newClient();
@@ -105,4 +78,47 @@ class MainTest {
     private String getConnectionString(String path) {
         return "http://localhost:" + server.port() + path;
     }
+
+    static final String FIXTURE = ""
+            + "CREATE (TheMatrix:Movie {title:'The Matrix', released:1999, tagline:'Welcome to the Real World'})\n"
+            + "CREATE (Keanu:Person {name:'Keanu Reeves', born:1964})\n"
+            + "CREATE (Carrie:Person {name:'Carrie-Anne Moss', born:1967})\n"
+            + "CREATE (Laurence:Person {name:'Laurence Fishburne', born:1961})\n"
+            + "CREATE (Hugo:Person {name:'Hugo Weaving', born:1960})\n"
+            + "CREATE (LillyW:Person {name:'Lilly Wachowski', born:1967})\n"
+            + "CREATE (LanaW:Person {name:'Lana Wachowski', born:1965})\n"
+            + "CREATE (JoelS:Person {name:'Joel Silver', born:1952})\n"
+            + "CREATE (KevinB:Person {name:'Kevin Bacon', born:1958})\n"
+            + "CREATE\n"
+            + "(Keanu)-[:ACTED_IN {roles:['Neo']}]->(TheMatrix),\n"
+            + "(Carrie)-[:ACTED_IN {roles:['Trinity']}]->(TheMatrix),\n"
+            + "(Laurence)-[:ACTED_IN {roles:['Morpheus']}]->(TheMatrix),\n"
+            + "(Hugo)-[:ACTED_IN {roles:['Agent Smith']}]->(TheMatrix),\n"
+            + "(LillyW)-[:DIRECTED]->(TheMatrix),\n"
+            + "(LanaW)-[:DIRECTED]->(TheMatrix),\n"
+            + "(JoelS)-[:PRODUCED]->(TheMatrix)\n"
+            + "\n"
+            + "CREATE (Emil:Person {name:\"Emil Eifrem\", born:1978})\n"
+            + "CREATE (Emil)-[:ACTED_IN {roles:[\"Emil\"]}]->(TheMatrix)\n"
+            + "\n"
+            + "CREATE (TheMatrixReloaded:Movie {title:'The Matrix Reloaded', released:2003, tagline:'Free your mind'})\n"
+            + "CREATE\n"
+            + "(Keanu)-[:ACTED_IN {roles:['Neo']}]->(TheMatrixReloaded),\n"
+            + "(Carrie)-[:ACTED_IN {roles:['Trinity']}]->(TheMatrixReloaded),\n"
+            + "(Laurence)-[:ACTED_IN {roles:['Morpheus']}]->(TheMatrixReloaded),\n"
+            + "(Hugo)-[:ACTED_IN {roles:['Agent Smith']}]->(TheMatrixReloaded),\n"
+            + "(LillyW)-[:DIRECTED]->(TheMatrixReloaded),\n"
+            + "(LanaW)-[:DIRECTED]->(TheMatrixReloaded),\n"
+            + "(JoelS)-[:PRODUCED]->(TheMatrixReloaded)\n"
+            + "\n"
+            + "CREATE (TheMatrixRevolutions:Movie {title:'The Matrix Revolutions', released:2003, tagline:'Everything that has a beginning has an end'})\n"
+            + "CREATE\n"
+            + "(Keanu)-[:ACTED_IN {roles:['Neo']}]->(TheMatrixRevolutions),\n"
+            + "(Carrie)-[:ACTED_IN {roles:['Trinity']}]->(TheMatrixRevolutions),\n"
+            + "(Laurence)-[:ACTED_IN {roles:['Morpheus']}]->(TheMatrixRevolutions),\n"
+            + "(KevinB)-[:ACTED_IN {roles:['Unknown']}]->(TheMatrixRevolutions),\n"
+            + "(Hugo)-[:ACTED_IN {roles:['Agent Smith']}]->(TheMatrixRevolutions),\n"
+            + "(LillyW)-[:DIRECTED]->(TheMatrixRevolutions),\n"
+            + "(LanaW)-[:DIRECTED]->(TheMatrixRevolutions),\n"
+            + "(JoelS)-[:PRODUCED]->(TheMatrixRevolutions)\n";
 }
--- examples/integrations/neo4j/neo4j-se/src/test/java/io/helidon/examples/quickstart/se/MainTest.java
+++ examples/integrations/neo4j/neo4j-se/src/test/java/io/helidon/examples/quickstart/se/MainTest.java
@@ -25,59 +25,34 @@ import io.helidon.media.jsonp.JsonpSupport;
 import io.helidon.webclient.WebClient;
 import io.helidon.webserver.WebServer;
 
-import org.junit.Ignore;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.neo4j.driver.AuthTokens;
-import org.neo4j.driver.GraphDatabase;
-import org.testcontainers.containers.Neo4jContainer;
+import org.neo4j.harness.Neo4j;
+import org.neo4j.harness.Neo4jBuilders;
 
 /**
  * Main test class for Neo4j Helidon SE quickstarter.
  */
 public class MainTest {
 
-    private static Neo4jContainer neo4jContainer;
-
     private static WebServer webServer;
     private static WebClient webClient;
-    //private static final JsonBuilderFactory JSON_BUILDER = Json.createBuilderFactory(Collections.emptyMap());
 
+    private static Neo4j embeddedDatabaseServer;
 
-    //@BeforeAll Decide if we need testcontainers.
+    @BeforeAll
     private static void startTheServer() throws Exception {
 
-        neo4jContainer = new Neo4jContainer<>("neo4j:4.0")
-                .withAdminPassword("secret");
-        neo4jContainer.start();
 
-        try (var driver = GraphDatabase.driver(neo4jContainer.getBoltUrl(), AuthTokens.basic("neo4j", "secret"));
-                var session = driver.session()
-        ) {
-            session.writeTransaction(tx -> tx.run(""
-                                                          + "CREATE (TheMatrix:Movie {title:'The Matrix', released:1999, tagline:'Welcome to the Real World'})\n"
-                                                          + "CREATE (Keanu:Person {name:'Keanu Reeves', born:1964})\n"
-                                                          + "CREATE (Carrie:Person {name:'Carrie-Anne Moss', born:1967})\n"
-                                                          + "CREATE (Laurence:Person {name:'Laurence Fishburne', born:1961})\n"
-                                                          + "CREATE (Hugo:Person {name:'Hugo Weaving', born:1960})\n"
-                                                          + "CREATE (LillyW:Person {name:'Lilly Wachowski', born:1967})\n"
-                                                          + "CREATE (LanaW:Person {name:'Lana Wachowski', born:1965})\n"
-                                                          + "CREATE (JoelS:Person {name:'Joel Silver', born:1952})\n"
-                                                          + "CREATE\n"
-                                                          + "  (Keanu)-[:ACTED_IN {roles:['Neo']}]->(TheMatrix),\n"
-                                                          + "  (Carrie)-[:ACTED_IN {roles:['Trinity']}]->(TheMatrix),\n"
-                                                          + "  (Laurence)-[:ACTED_IN {roles:['Morpheus']}]->(TheMatrix),\n"
-                                                          + "  (Hugo)-[:ACTED_IN {roles:['Agent Smith']}]->(TheMatrix),\n"
-                                                          + "  (LillyW)-[:DIRECTED]->(TheMatrix),\n"
-                                                          + "  (LanaW)-[:DIRECTED]->(TheMatrix),\n"
-                                                          + "  (JoelS)-[:PRODUCED]->(TheMatrix)").consume()
-            );
-        }
+        embeddedDatabaseServer = Neo4jBuilders.newInProcessBuilder()
+                .withDisabledServer()
+                .withFixture(FIXTURE)
+                .build();
 
-        // Don't know how to set this dynamically otherwise in Helidon
-        System.setProperty("neo4j.uri", neo4jContainer.getBoltUrl());
+        System.setProperty("neo4j.uri", embeddedDatabaseServer.boltURI().toString());
 
         webServer = Main.startServer();
 
@@ -97,22 +72,17 @@ public class MainTest {
                 .build();
     }
 
-    //@AfterAll
+    @AfterAll
     private static void stopServer() throws Exception {
         if (webServer != null) {
             webServer.shutdown()
                     .toCompletableFuture()
                     .get(10, TimeUnit.SECONDS);
         }
+        embeddedDatabaseServer.close();
     }
 
-    //@AfterAll
-    public static void stopNeo4j() {
-        neo4jContainer.stop();
-    }
-
-    //@Test
-    //@Ignore// Currently ignore. Decide if we need testcontainers.
+    @Test
     void testMovies() throws Exception {
 
         webClient.get()
@@ -124,8 +94,7 @@ public class MainTest {
 
     }
 
-    //@Test
-    //@Ignore// Currently ignore. Decide if we need testcontainers.
+    @Test
     public void testHealth() throws Exception {
 
         webClient.get()
@@ -136,8 +105,7 @@ public class MainTest {
                 .get();
     }
 
-    //@Test
-    //@Ignore// Currently ignore. Decide if we need testcontainers.
+    @Test
     public void testMetrics() throws Exception {
         webClient.get()
                 .path("/metrics")
@@ -147,4 +115,46 @@ public class MainTest {
                 .get();
     }
 
+    static final String FIXTURE = ""
+            + "CREATE (TheMatrix:Movie {title:'The Matrix', released:1999, tagline:'Welcome to the Real World'})\n"
+            + "CREATE (Keanu:Person {name:'Keanu Reeves', born:1964})\n"
+            + "CREATE (Carrie:Person {name:'Carrie-Anne Moss', born:1967})\n"
+            + "CREATE (Laurence:Person {name:'Laurence Fishburne', born:1961})\n"
+            + "CREATE (Hugo:Person {name:'Hugo Weaving', born:1960})\n"
+            + "CREATE (LillyW:Person {name:'Lilly Wachowski', born:1967})\n"
+            + "CREATE (LanaW:Person {name:'Lana Wachowski', born:1965})\n"
+            + "CREATE (JoelS:Person {name:'Joel Silver', born:1952})\n"
+            + "CREATE (KevinB:Person {name:'Kevin Bacon', born:1958})\n"
+            + "CREATE\n"
+            + "(Keanu)-[:ACTED_IN {roles:['Neo']}]->(TheMatrix),\n"
+            + "(Carrie)-[:ACTED_IN {roles:['Trinity']}]->(TheMatrix),\n"
+            + "(Laurence)-[:ACTED_IN {roles:['Morpheus']}]->(TheMatrix),\n"
+            + "(Hugo)-[:ACTED_IN {roles:['Agent Smith']}]->(TheMatrix),\n"
+            + "(LillyW)-[:DIRECTED]->(TheMatrix),\n"
+            + "(LanaW)-[:DIRECTED]->(TheMatrix),\n"
+            + "(JoelS)-[:PRODUCED]->(TheMatrix)\n"
+            + "\n"
+            + "CREATE (Emil:Person {name:\"Emil Eifrem\", born:1978})\n"
+            + "CREATE (Emil)-[:ACTED_IN {roles:[\"Emil\"]}]->(TheMatrix)\n"
+            + "\n"
+            + "CREATE (TheMatrixReloaded:Movie {title:'The Matrix Reloaded', released:2003, tagline:'Free your mind'})\n"
+            + "CREATE\n"
+            + "(Keanu)-[:ACTED_IN {roles:['Neo']}]->(TheMatrixReloaded),\n"
+            + "(Carrie)-[:ACTED_IN {roles:['Trinity']}]->(TheMatrixReloaded),\n"
+            + "(Laurence)-[:ACTED_IN {roles:['Morpheus']}]->(TheMatrixReloaded),\n"
+            + "(Hugo)-[:ACTED_IN {roles:['Agent Smith']}]->(TheMatrixReloaded),\n"
+            + "(LillyW)-[:DIRECTED]->(TheMatrixReloaded),\n"
+            + "(LanaW)-[:DIRECTED]->(TheMatrixReloaded),\n"
+            + "(JoelS)-[:PRODUCED]->(TheMatrixReloaded)\n"
+            + "\n"
+            + "CREATE (TheMatrixRevolutions:Movie {title:'The Matrix Revolutions', released:2003, tagline:'Everything that has a beginning has an end'})\n"
+            + "CREATE\n"
+            + "(Keanu)-[:ACTED_IN {roles:['Neo']}]->(TheMatrixRevolutions),\n"
+            + "(Carrie)-[:ACTED_IN {roles:['Trinity']}]->(TheMatrixRevolutions),\n"
+            + "(Laurence)-[:ACTED_IN {roles:['Morpheus']}]->(TheMatrixRevolutions),\n"
+            + "(KevinB)-[:ACTED_IN {roles:['Unknown']}]->(TheMatrixRevolutions),\n"
+            + "(Hugo)-[:ACTED_IN {roles:['Agent Smith']}]->(TheMatrixRevolutions),\n"
+            + "(LillyW)-[:DIRECTED]->(TheMatrixRevolutions),\n"
+            + "(LanaW)-[:DIRECTED]->(TheMatrixRevolutions),\n"
+            + "(JoelS)-[:PRODUCED]->(TheMatrixRevolutions)\n";
 }
