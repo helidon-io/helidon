401e1853fc041c128479df4d0d2b9786ab71793f
David Kral <david.k.kral@oracle.com>
2021-02-17 16:09:43 +0100

    unused import removed
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- security/providers/jwt/src/main/java/io/helidon/security/providers/jwt/JwtProvider.java
+++ security/providers/jwt/src/main/java/io/helidon/security/providers/jwt/JwtProvider.java
@@ -23,7 +23,6 @@ import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.logging.Logger;
 
 import io.helidon.common.Errors;
