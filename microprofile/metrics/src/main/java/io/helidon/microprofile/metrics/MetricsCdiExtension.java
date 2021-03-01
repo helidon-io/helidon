eac3b9f0b5437d93baff29c2a3eca871252e6a19
tim.quinn@oracle.com <tim.quinn@oracle.com>
2021-03-01 08:12:55 -0600

    Revert changes to microprofile/metrics CDI extension; not needed for this PR

--- microprofile/metrics/src/main/java/io/helidon/microprofile/metrics/MetricsCdiExtension.java
+++ microprofile/metrics/src/main/java/io/helidon/microprofile/metrics/MetricsCdiExtension.java
@@ -113,11 +113,8 @@ import static javax.interceptor.Interceptor.Priority.LIBRARY_BEFORE;
 public class MetricsCdiExtension implements Extension {
     private static final Logger LOGGER = Logger.getLogger(MetricsCdiExtension.class.getName());
 
-    /**
-     * All metrics annotations supported.
-     */
-    public static final List<Class<? extends Annotation>> METRIC_ANNOTATIONS
-            = List.of(Counted.class, Metered.class, Timed.class, ConcurrentGauge.class, SimplyTimed.class);
+    private static final List<Class<? extends Annotation>> METRIC_ANNOTATIONS
+            = Arrays.asList(Counted.class, Metered.class, Timed.class, ConcurrentGauge.class, SimplyTimed.class);
 
     private static final List<Class<? extends Annotation>> JAX_RS_ANNOTATIONS
             = Arrays.asList(GET.class, PUT.class, POST.class, HEAD.class, OPTIONS.class, DELETE.class, PATCH.class);
