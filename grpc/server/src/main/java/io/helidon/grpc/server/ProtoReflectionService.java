9fef66c8574576405fd9206e9d6f844c824543cb
Aleks Seovic <aleks@seovic.com>
2021-03-04 21:47:22 -0500

    Copyright fixes

--- grpc/server/src/main/java/io/helidon/grpc/server/ProtoReflectionService.java
+++ grpc/server/src/main/java/io/helidon/grpc/server/ProtoReflectionService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021, Oracle and/or its affiliates.
+ * Copyright 2021 Oracle and/or its affiliates.
  * Copyright 2016 The gRPC Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
