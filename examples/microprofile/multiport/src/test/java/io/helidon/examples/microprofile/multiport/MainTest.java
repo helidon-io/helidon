4a5fedc88d26f71930eb9d9ac02cc057c78762ea
Joe Di Pol <joe.dipol@oracle.com>
2021-03-05 11:24:28 -0800

    Remove explicit Main. Use HelidonTest

--- examples/microprofile/multiport/src/test/java/io/helidon/examples/microprofile/multiport/MainTest.java
+++ examples/microprofile/multiport/src/test/java/io/helidon/examples/microprofile/multiport/MainTest.java
@@ -16,12 +16,13 @@
 
 package io.helidon.examples.microprofile.multiport;
 
+import javax.inject.Inject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
-import org.junit.jupiter.api.AfterAll;
+import io.helidon.microprofile.tests.junit5.HelidonTest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -31,56 +32,54 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /**
  * Unit test for {@link Main}.
  */
+@HelidonTest
 class MainTest {
-    private static final int PUBLIC_PORT = 8080;
     private static final int PRIVATE_PORT = 8081;
     private static final int ADMIN_PORT = 8082;
 
+    // Used for the default (public) port which HelidonTest will configure for us
+    @Inject
+    private WebTarget publicWebTarget;
+
+    // Used for other (private, admin) ports
     private static Client client;
 
     @BeforeAll
     static void initClass() {
-        Main.main(new String[0]);
         client = ClientBuilder.newClient();
     }
 
-    @AfterAll
-    static void destroyClass() {
-        Main.server().stop();
-    }
-
     @Test
-    void testEndpoint() {
+    void testEndpoints() {
         String base = "http://localhost:";
         Response response;
         WebTarget baseTarget;
 
         // Probe PUBLIC port
-        baseTarget = client.target(base + PUBLIC_PORT);
-        response = baseTarget.path("/public/hello")
+        response = publicWebTarget.path("/public/hello")
                 .request()
                 .get();
-        assertThat("port " + PUBLIC_PORT  + " should be serving public resource",
+        assertThat("default port should be serving public resource",
                 response.getStatusInfo().toEnum(), is(Response.Status.OK));
-        assertThat("port " + PUBLIC_PORT  + " should return public data",
+        assertThat("default port should return public data",
                 response.readEntity(String.class), is("Public Hello World!!"));
 
-        response = baseTarget.path("/private/hello")
+        response = publicWebTarget.path("/private/hello")
                 .request()
                 .get();
-        assertThat("port " + PUBLIC_PORT  + " should NOT be serving private resource",
+        assertThat("default port should NOT be serving private resource",
                 response.getStatusInfo().toEnum(), is(Response.Status.NOT_FOUND));
 
-        response = baseTarget.path("/health")
+        response = publicWebTarget.path("/health")
                 .request()
                 .get();
-        assertThat("port " + PUBLIC_PORT  + " should NOT be serving health",
+        assertThat("default port should NOT be serving health",
                 response.getStatusInfo().toEnum(), is(Response.Status.NOT_FOUND));
 
-        response = baseTarget.path("/metrics")
+        response = publicWebTarget.path("/metrics")
                 .request()
                 .get();
-        assertThat("port " + PUBLIC_PORT  + " should NOT be serving metrics",
+        assertThat("default port should NOT be serving metrics",
                 response.getStatusInfo().toEnum(), is(Response.Status.NOT_FOUND));
 
         // Probe PRIVATE port
