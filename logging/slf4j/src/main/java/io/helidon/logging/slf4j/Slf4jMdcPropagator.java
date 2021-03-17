ca83aa61531b2cd9dc58784485bb0677c9677628
David Kral <david.k.kral@oracle.com>
2021-03-17 15:38:22 +0100

    checkstyle and copyright fixed
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- logging/slf4j/src/main/java/io/helidon/logging/slf4j/Slf4jMdcPropagator.java
+++ logging/slf4j/src/main/java/io/helidon/logging/slf4j/Slf4jMdcPropagator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2021 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
  */
 package io.helidon.logging.slf4j;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
