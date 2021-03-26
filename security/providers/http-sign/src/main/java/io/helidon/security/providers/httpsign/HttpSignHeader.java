a415867adb593bb42cb716f06a3595947357d384
Tomas Langer <tomas.langer@gmail.com>
2021-03-26 11:18:27 +0100

    HTTP Signature fix to work according to specification. (#2884)

--- security/providers/http-sign/src/main/java/io/helidon/security/providers/httpsign/HttpSignHeader.java
+++ security/providers/http-sign/src/main/java/io/helidon/security/providers/httpsign/HttpSignHeader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2021 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,5 +28,9 @@ public enum HttpSignHeader {
      * Creates (or validates) an "Authorization" header, that contains "Signature" as the
      * beginning of its content (the rest of the header is the same as for {@link #SIGNATURE}.
      */
-    AUTHORIZATION
+    AUTHORIZATION,
+    /**
+     * Custom provided using a {@link io.helidon.security.util.TokenHandler}.
+     */
+    CUSTOM
 }
