e48042eeac0530cbb5e5115888453efc5e58cc9f
Tomas Langer <tomas.langer@oracle.com>
2021-03-29 14:28:44 +0200

    AppRole auth

---
+++ integrations/vault/auths/approle/src/main/java/io/helidon/integrations/vault/auths/approle/AppRoleRestApi.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.integrations.vault.auths.approle;
+
+import java.time.Instant;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import io.helidon.common.http.Http;
+import io.helidon.common.reactive.Single;
+import io.helidon.integrations.common.rest.ApiRequest;
+import io.helidon.integrations.vault.VaultTokenBase;
+import io.helidon.integrations.vault.auths.common.VaultRestApi;
+import io.helidon.webclient.WebClientRequestBuilder;
+
+class AppRoleRestApi extends VaultRestApi {
+    private static final Logger LOGGER = Logger.getLogger(AppRoleRestApi.class.getName());
+
+    private final AtomicReference<VaultTokenBase> currentToken = new AtomicReference<>();
+
+    private final AppRoleAuth auth;
+    private final String appRoleId;
+    private final String secretId;
+
+    AppRoleRestApi(Builder builder) {
+        super(builder);
+        this.auth = builder.auth;
+        this.appRoleId = builder.appRoleId;
+        this.secretId = builder.secretId;
+        this.currentToken.set(builder.token);
+    }
+
+    static Builder appRoleBuilder() {
+        return new Builder();
+    }
+    @Override
+    protected Single<WebClientRequestBuilder> updateRequestBuilderCommon(WebClientRequestBuilder requestBuilder,
+                                                                         String path,
+                                                                         ApiRequest<?> request,
+                                                                         Http.RequestMethod method,
+                                                                         String requestId) {
+        VaultTokenBase currentToken = this.currentToken.get();
+
+        if (currentToken != null) {
+            if (!currentToken.renewable() || currentToken.created().plus(currentToken.leaseDuration()).isAfter(Instant.now())) {
+                requestBuilder.headers().add("X-Vault-Token", currentToken.token());
+                return Single.just(requestBuilder);
+            }
+        }
+
+        // we need to renew the token - this may be a concurrent operation as we do not care who wins
+        return auth.login(Login.Request.create(appRoleId, secretId))
+                .map(it -> {
+                    VaultTokenBase token = it.token();
+                    this.currentToken.set(token);
+                    requestBuilder.headers().add("X-Vault-Token", token.token());
+                    return requestBuilder;
+                })
+                .onError(throwable -> LOGGER.log(Level.WARNING, "Failed to renew AppRole token", throwable));
+    }
+
+    static class Builder extends VaultRestApi.BuilderBase<Builder> {
+        private AppRoleAuth auth;
+        private VaultTokenBase token;
+        private String secretId;
+        private String appRoleId;
+
+        private Builder() {
+        }
+
+        public Builder auth(AppRoleAuth auth) {
+            this.auth = auth;
+            return this;
+        }
+
+        public Builder token(VaultTokenBase token) {
+            this.token = token;
+            return this;
+        }
+
+        public Builder secretId(String secretId) {
+            this.secretId = secretId;
+            return this;
+        }
+
+        public Builder appRoleId(String appRoleId) {
+            this.appRoleId = appRoleId;
+            return this;
+        }
+
+        @Override
+        protected VaultRestApi doBuild() {
+            return new AppRoleRestApi(this);
+        }
+
+
+    }
+}
---
+++ integrations/vault/auths/approle/src/main/java/io/helidon/integrations/vault/auths/approle/AppRoleVaultAuth.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.integrations.vault.auths.approle;
+
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import javax.annotation.Priority;
+
+import io.helidon.config.Config;
+import io.helidon.integrations.common.rest.RestApi;
+import io.helidon.integrations.vault.Vault;
+import io.helidon.integrations.vault.VaultApiException;
+import io.helidon.integrations.vault.auths.common.NoVaultAuth;
+import io.helidon.integrations.vault.spi.VaultAuth;
+
+/**
+ * Vault authentication for AppRole.
+ */
+@Priority(1000)
+public class AppRoleVaultAuth implements VaultAuth {
+    private static final Logger LOGGER = Logger.getLogger(AppRoleVaultAuth.class.getName());
+
+    private final String appRoleId;
+    private final String secretId;
+
+    /**
+     * Constructor required for Java Service Loader.
+     *
+     * @deprecated please use {@link #builder()}
+     */
+    @Deprecated
+    public AppRoleVaultAuth() {
+        this.appRoleId = null;
+        this.secretId = null;
+    }
+
+    private AppRoleVaultAuth(Builder builder) {
+        this.appRoleId = builder.appRoleId;
+        this.secretId = builder.secretId;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @Override
+    public Optional<RestApi> authenticate(Config config, Vault.Builder vaultBuilder) {
+        boolean enabled = config.get("auth.app-role.enabled").asBoolean().orElse(true);
+
+        if (!enabled) {
+            return Optional.empty();
+        }
+        Optional<String> maybeAppRoleId = Optional.ofNullable(this.appRoleId)
+                .or(() -> config.get("app-role.role-id")
+                        .asString()
+                        .asOptional());
+
+        if (maybeAppRoleId.isEmpty()) {
+            LOGGER.fine("AppRole vault authentication not used, as app-role.role-id is not defined");
+            return Optional.empty();
+        }
+
+        String appRoleId = maybeAppRoleId.get();
+        String secretId = Optional.ofNullable(this.secretId)
+                .or(() -> config.get("app-role.secret-id")
+                        .asString()
+                        .asOptional())
+                .orElseThrow(() -> new VaultApiException("AppRole ID is defined (" + appRoleId + "), but secret id is not. "
+                                                                 + "Cannot "
+                                                                 + "authenticate."));
+
+        LOGGER.finest("Will try to login to Vault using app role id: " + appRoleId + " and a secret id.");
+
+        // this may be changed in the future, when running with a sidecar (there should be a way to get the address from evn)
+        String address = vaultBuilder.address()
+                .orElseThrow(() -> new VaultApiException("Address is required when using k8s authentication"));
+
+        Vault.Builder loginVaultBuilder = Vault.builder()
+                // explicitly use default
+                .address(address)
+                .disableVaultAuthDiscovery()
+                .updateWebClient(vaultBuilder.webClientUpdater())
+                .faultTolerance(vaultBuilder.ftHandler())
+                .addVaultAuth(NoVaultAuth.create());
+
+        vaultBuilder.baseNamespace().ifPresent(loginVaultBuilder::baseNamespace);
+
+        Vault loginVault = loginVaultBuilder.build();
+
+        AppRoleAuth auth = loginVault.auth(AppRoleAuth.AUTH_METHOD);
+
+        LOGGER.info("Authenticated Vault " + address + " using AppRole, roleId \"" + appRoleId + "\"");
+        return Optional.of(AppRoleRestApi.appRoleBuilder()
+                                   .webClientBuilder(webclient -> {
+                                       webclient.baseUri(address + "/v1");
+                                       vaultBuilder.baseNamespace()
+                                               .ifPresent(ns -> webclient.addHeader("X-Vault-Namespace", ns));
+                                       vaultBuilder.webClientUpdater().accept(webclient);
+                                   })
+                                   .faultTolerance(vaultBuilder.ftHandler())
+                                   .auth(auth)
+                                   .appRoleId(appRoleId)
+                                   .secretId(secretId)
+                                   .build());
+
+    }
+
+    public static class Builder implements io.helidon.common.Builder<AppRoleVaultAuth> {
+        private String appRoleId;
+        private String secretId;
+
+        private Builder() {
+        }
+
+        @Override
+        public AppRoleVaultAuth build() {
+            return new AppRoleVaultAuth(this);
+        }
+
+        public Builder appRoleId(String appRoleId) {
+            this.appRoleId = appRoleId;
+            return this;
+        }
+
+        public Builder secretId(String secretId) {
+            this.secretId = secretId;
+            return this;
+        }
+    }
+}
