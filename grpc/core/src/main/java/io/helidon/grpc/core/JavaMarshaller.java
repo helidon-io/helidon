d9a32c440cade4987486475ad0b20b25a4d1cf18
Aleks Seovic <aleks.seovic@gmail.com>
2021-04-28 18:24:32 -0400

    JavaMarshaller deprecation (#2975)
    * JavaMarshaller deprecation

--- grpc/core/src/main/java/io/helidon/grpc/core/JavaMarshaller.java
+++ grpc/core/src/main/java/io/helidon/grpc/core/JavaMarshaller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2021 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,22 +21,37 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.logging.Logger;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
+
+import io.helidon.config.Config;
 
 import io.grpc.MethodDescriptor;
 
 /**
  * An implementation of a gRPC {@link MethodDescriptor.Marshaller} that
  * uses Java serialization.
+ * <p>
+ * This marshaller is disabled by default starting with Helidon 2.3.0, and must be
+ * enabled explicitly by setting {@code grpc.marshaller.java.enabled} configuration
+ * property to {@code true} before it can be used.
  *
  * @param <T>  the type of value to to be marshalled
+ * @deprecated Not suitable for production use, and will be removed in 3.0.
+ *             Please use one of the other supported marshallers instead.
+ * @see io.helidon.grpc.core.JsonbMarshaller
+ * @see io.helidon.grpc.core.MarshallerSupplier.ProtoMarshallerSupplier
  */
 @Singleton
 @Named(JavaMarshaller.NAME)
+@Deprecated(since = "2.3.0", forRemoval = true)
 public class JavaMarshaller<T>
         implements MethodDescriptor.Marshaller<T> {
+
+    private static final Logger LOG = Logger.getLogger(JavaMarshaller.class.getName());
+    private static final boolean ENABLED = Config.create().get("grpc.marshaller.java.enabled").asBoolean().orElse(false);
 
     /**
      * The name of this marshaller.
@@ -58,6 +73,18 @@ public class JavaMarshaller<T>
         return INSTANCE;
     }
 
+    /**
+     * Construct JavaMarshaller instance.
+     */
+    JavaMarshaller() {
+        if (ENABLED) {
+            LOG.warning("JavaMarshaller is not recommended for production use. "
+                                + "It has been deprecated since Helidon 2.3.0 and it will be removed in 3.0");
+        } else {
+            throw new UnsupportedOperationException(
+                    "JavaMarshaller must be explicitly enabled via grpc.marshaller.java.enabled configuration property");
+        }
+    }
 
     @Override
     public InputStream stream(T obj) {
