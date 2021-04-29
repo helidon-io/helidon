71c83675a4437af7b0c73f0e42328d82a6314e1f
Tomas Kraus <Tomas.Kraus@oracle.com>
2021-04-29 15:33:03 +0200

    DB Client application tests and tools example refactoring. Applied rewiew comments from Romain.
    Signed-off-by: Tomas Kraus <Tomas.Kraus@oracle.com>

--- etc/scripts/test-integ-pgsql.sh
+++ etc/scripts/test-integ-pgsql.sh
@@ -15,32 +15,17 @@
 # limitations under the License.
 #
 
-set -o pipefail || true  # trace ERR through pipes
-set -o errtrace || true # trace ERR through commands and functions
-set -o errexit || true  # exit the script if any statement returns a non-true return value
-
-on_error(){
-    CODE="${?}" && \
-    set +x && \
-    printf "[ERROR] Error(code=%s) occurred at %s:%s command: %s\n" \
-        "${CODE}" "${BASH_SOURCE}" "${LINENO}" "${BASH_COMMAND}"
-}
-trap on_error ERR
-
 # Path to this script
-if [ -h "${0}" ] ; then
-    readonly SCRIPT_PATH="$(readlink "${0}")"
-else
-    readonly SCRIPT_PATH="${0}"
-fi
+[ -h "${0}" ] && readonly SCRIPT_PATH="$(readlink "${0}")" || readonly SCRIPT_PATH="${0}"
 
-# Path to the root of the workspace
-readonly WS_DIR=$(cd $(dirname -- "${SCRIPT_PATH}") ; cd ../.. ; pwd -P)
+# Load pipeline environment setup and define WS_DIR
+. $(dirname -- "${SCRIPT_PATH}")/includes/pipeline-env.sh "${SCRIPT_PATH}" '../..'
 
-source ${WS_DIR}/etc/scripts/pipeline-env.sh
+# Setup error handling using default settings (defined in includes/error_handlers.sh)
+error_trap_setup
 
-JAVA_HOME='/tools/graalvm-ce-java11-21.0.0'
-PATH="${PATH}:${JAVA_HOME}/bin"
+# Set Graal VM into JAVA_HOME and PATH (defined in includes/pipeline-env.sh)
+graalvm
 
 mvn ${MAVEN_ARGS} --version
 
--- tests/integration/tools/example/test.sh
+++ tests/integration/tools/example/test.sh
@@ -16,28 +16,13 @@
 #
 
 # Path to this script
-if [ -h "${0}" ] ; then
-    readonly SCRIPT_PATH="$(readlink "${0}")"
-else
-    readonly SCRIPT_PATH="${0}"
-fi
+[ -h "${0}" ] && readonly SCRIPT_PATH="$(readlink "${0}")" || readonly SCRIPT_PATH="${0}"
 
-# Path to the root of the workspace
-readonly WS_DIR=$(cd $(dirname -- "${SCRIPT_PATH}") ; cd ../../../.. ; pwd -P)
+# Load pipeline environment setup and define WS_DIR
+. $(dirname -- "${SCRIPT_PATH}")/../../../../etc/scripts/includes/local-env.sh "${SCRIPT_PATH}" '../../../..'
 
-print_help() {
-    echo 'Usage: dbclient.sh [-h] [-c] -d <database> [-j] [-n] [-p]'
-    echo ''
-    echo '  -h print this help and exit'
-    echo '  -d <database> select database'
-    echo '  -c start and stop Docker containers'
-    echo '  -s execute simple tests (default when no -s, -j or -n is passed)'
-    echo '  -j execute remote application tests in Java VM mode'
-    echo '  -n execute remote application tests in native image mode'
-    echo '  -p toggle pipeline environment mode'
-}
-
-on_error() {
+# Local error handler
+test_on_error() {
     CODE="${?}" && \
     set +x && \
     printf "[ERROR] Error(code=%s) occurred at %s:%s command: %s\n" \
@@ -45,33 +30,38 @@ on_error() {
     docker_stop "${DOCKER_CONT_NAME}" 'FLAG_C_RUN'
 }
 
+# Setup error handling using default settings (defined in includes/error_handlers.sh)
+error_trap_setup 'test_on_error'
+
+print_help() {
+    echo 'Usage: test.sh [-hcjn] -d <database>'
+    echo ''
+    echo '  -h print this help and exit'
+    echo '  -c start and stop Docker containers'
+    echo '  -j execute remote application tests in Java VM mode (default)'
+    echo '  -n execute remote application tests in native image mode'
+    echo '  -d <database> select database'
+    echo '     <database> :: mysql | pgsql'
+}
+
 # Evaluate command line arguments
 if [ "$#" -gt '0' ]; then
-    while getopts 'hcsjnpd:' flag 2> /dev/null; do
+    while getopts 'hcjnd:' flag 2> /dev/null; do
         case "${flag}" in
             h) print_help && exit;;
             c) readonly FLAG_C='1';;
             d) readonly FLAG_D=${OPTARG};;
-            s) readonly FLAG_S='1';;
             j) readonly FLAG_J='1';;
             n) readonly FLAG_N='1';;
-            p) readonly FLAG_P='1';;
         esac
     done
-fi
-
-# Load includes
-. ${WS_DIR}/etc/scripts/tests/integration/include/env.sh
-. ${WS_DIR}/etc/scripts/tests/integration/include/docker.sh
-if [ -n "${FLAG_P}" ]; then
-    . ${WS_DIR}/etc/scripts/tests/integration/include/pipeline.sh
 fi
 
 # Load database setup
 if [ -n "${FLAG_D}" ]; then
     case "${FLAG_D}" in
-        mysql) . ${WS_DIR}/etc/scripts/tests/integration/mysql.conf;;
-        pgsql) . ${WS_DIR}/etc/scripts/tests/integration/pgsql.conf;;
+        mysql) . ${WS_DIR}/etc/scripts/includes/mysql.sh;;
+        pgsql) . ${WS_DIR}/etc/scripts/includes/pgsql.sh;;
         *)     echo 'ERROR: Unknown database name, exitting.' && exit 1;;
     esac
 else
@@ -82,65 +72,49 @@ fi
 # Start docker Container
 if [ -n "${FLAG_C}" ]; then
     readonly DOCKER_CONT_NAME="helidon-tests-dbclient-${FLAG_D}"
-    docker_start "${DOCKER_IMG}" \
-                 "${DOCKER_CONT_NAME}" \
-                 "${DB_HOST}:${DB_PORT}:${DB_PORT}" \
-                 "${DOCKER_ENV}" \
-                 'FLAG_C_RUN' \
-                 'FLAG_C'
+    docker_db_start "${DOCKER_IMG}" \
+                    "${DOCKER_CONT_NAME}" \
+                    "${DB_HOST}:${DB_PORT}:${DB_PORT}" \
+                    "${DOCKER_ENV}" \
+                    'FLAG_C_RUN' \
+                    'FLAG_C'
 fi
 
-# Turn simple tests on when no test was selected
-[ -z "${FLAG_J}" -a -z "${FLAG_N}" -a -z "${FLAG_S}" ] && \
-    readonly FLAG_S='1'
+# Turn Java VM mode on when no mode was selected
+[ -z "${FLAG_J}" -a -z "${FLAG_N}" ] && \
+    readonly FLAG_J='1'
 
-# Run simple JDBC tests
-[ -n "${FLAG_S}" ] && \
-    (cd ${WS_DIR}/tests/integration/dbclient && \
-        echo mvn -P${DB_PROFILE} -pl common,jdbc \
+# Run remote application tests in Java VM mode
+[ -n "${FLAG_J}" ] && \
+    (cd ${WS_DIR}/tests/integration/tools/example && \
+        echo mvn -P${DB_PROFILE} \
             -Dapp.config=${TEST_CONFIG} \
             -Ddb.user=${DB_USER} \
             -Ddb.password=${DB_PASSWORD} \
             -Ddb.url="${DB_URL}" \
-             verify && \
-        mvn -P${DB_PROFILE} -pl common,jdbc \
+            verify && \
+        mvn -P${DB_PROFILE} \
             -Dapp.config=${TEST_CONFIG} \
             -Ddb.user=${DB_USER} \
             -Ddb.password=${DB_PASSWORD} \
             -Ddb.url="${DB_URL}" \
             verify)
 
-# Run remote application tests in Java VM mode
-[ -n "${FLAG_J}" ] && \
-    (cd ${WS_DIR}/tests/integration/dbclient && \
-        echo mvn -P${DB_PROFILE} \
-            -Dapp.config=${TEST_CONFIG} \
-            -Ddb.user=${DB_USER} \
-            -Ddb.password=${DB_PASSWORD} \
-            -Ddb.url="${DB_URL}" \
-            -pl appl verify && \
-        mvn -P${DB_PROFILE} \
-            -Dapp.config=${TEST_CONFIG} \
-            -Ddb.user=${DB_USER} \
-            -Ddb.password=${DB_PASSWORD} \
-            -Ddb.url="${DB_URL}" \
-            -pl appl verify)
-
 # Run remote application tests in native image mode
 [ -n "${FLAG_N}" ] && \
-    (cd ${WS_DIR}/tests/integration/dbclient && \
+    (cd ${WS_DIR}/tests/integration/tools/example && \
         echo mvn -P${DB_PROFILE} -Pnative-image \
             -Dapp.config=${TEST_CONFIG} \
             -Ddb.user=${DB_USER} \
             -Ddb.password=${DB_PASSWORD} \
             -Ddb.url="${DB_URL}" \
-            -pl appl verify && \
+            verify && \
         mvn -P${DB_PROFILE} -Pnative-image \
             -Dapp.config=${TEST_CONFIG} \
             -Ddb.user=${DB_USER} \
             -Ddb.password=${DB_PASSWORD} \
             -Ddb.url="${DB_URL}" \
-            -pl appl verify)
+            verify)
 
 # Stop docker Container
 docker_stop "${DOCKER_CONT_NAME}" 'FLAG_C_RUN'
