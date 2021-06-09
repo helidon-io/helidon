0b6e579bef908cf764e253e67355bfcd354e46c2
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2021-06-09 15:40:26 -0400

    Ignore path parameters while matching routes. Some new tests.
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

---
+++ webserver/webserver/src/test/java/io/helidon/webserver/UriParamsServerTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.webserver;
+
+import java.util.concurrent.ExecutionException;
+
+import io.helidon.webclient.WebClientException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+class UriParamsServerTest extends BaseServerTest {
+
+    @BeforeAll
+    static void startServer() throws Exception {
+        Routing routing = Routing.builder()
+                .get("/shop",
+                        (req, res) -> res.send("shop"))
+                .get("/admin",
+                        (req, res) -> res.send("admin"))
+                .build();
+        startServer(0, routing);
+    }
+
+    @Test
+    void testEndpoints() throws Exception {
+        String s = webClient().get()
+                .path("shop")
+                .request(String.class)
+                .get();
+        assertThat(s, is("shop"));
+        s = webClient().get()
+                .path("admin")
+                .request(String.class)
+                .get();
+        assertThat(s, is("admin"));
+    }
+
+    @Test
+    void testEndpointsWithParams() {
+        try {
+            String s = webClient().get()
+                    .path("shop;a=b")
+                    .request(String.class)
+                    .get();
+        } catch (ExecutionException | InterruptedException e) {
+            Throwable t = e.getCause();
+            assertThat(t, is(instanceOf(WebClientException.class)));
+        }
+    }
+}
