893aa6dbb705046a3f2235338187d179853fc688
Daniel Kec <daniel.kec@oracle.com>
2021-06-25 14:11:45 +0200

    Checkstyle
    Signed-off-by: Daniel Kec <daniel.kec@oracle.com>

--- webserver/webserver/src/main/java/io/helidon/webserver/BareResponseImpl.java
+++ webserver/webserver/src/main/java/io/helidon/webserver/BareResponseImpl.java
@@ -21,7 +21,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Flow;
 import java.util.concurrent.atomic.AtomicBoolean;
