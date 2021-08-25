e87a5ff2b8458b53f1a0ac325034febfd6e0b6be
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2021-08-25 09:30:00 -0400

    Make sure our built-in MP health checks are marked as built in. (#3315)

--- microprofile/messaging/health/src/main/java/module-info.java
+++ microprofile/messaging/health/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2021 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ module io.helidon.microprofile.messaging.health {
     requires io.helidon.microprofile.messaging;
     requires io.helidon.microprofile.health;
     requires microprofile.health.api;
+    requires io.helidon.health.common;
 
     exports io.helidon.microprofile.messaging.health;
 }
