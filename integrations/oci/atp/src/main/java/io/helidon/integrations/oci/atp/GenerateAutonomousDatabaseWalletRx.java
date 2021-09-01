1a9dc19b6a7eafc932adf28a3970880557afd818
Arjav Desai <arjav.desai@oracle.com>
2021-09-01 15:16:46 -0500

    OCI ATP Integration Part 1 (#3236)
    Initial work where calling api will generate the wallet and download it locally on end user's machine.

---
+++ integrations/oci/atp/src/main/java/io/helidon/integrations/oci/atp/GenerateAutonomousDatabaseWalletRx.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.integrations.oci.atp;
+
+import java.nio.ByteBuffer;
+
+import io.helidon.common.http.DataChunk;
+import io.helidon.common.reactive.Multi;
+import io.helidon.integrations.oci.connect.OciRequestBase;
+import io.helidon.integrations.oci.connect.OciResponseParser;
+
+/**
+ * GenerateAutonomousDatabaseWalletRx request and response.
+ */
+public final class GenerateAutonomousDatabaseWalletRx {
+    private GenerateAutonomousDatabaseWalletRx() {
+    }
+
+    /**
+     * Request object. Can be configured with additional headers, query parameters etc.
+     */
+    public static class Request extends OciRequestBase<Request> {
+        private Request() {
+        }
+
+        /**
+         * Fluent API builder for configuring a request.
+         * The request builder is passed as is, without a build method.
+         * The equivalent of a build method is {@link #toJson(javax.json.JsonBuilderFactory)}
+         * used by the {@link io.helidon.integrations.common.rest.RestApi}.
+         *
+         * @return new request builder
+         */
+        public static Request builder() {
+            return new Request();
+        }
+
+        /**
+         * Set explicit password to encrypt the keys inside the wallet.
+         *
+         * @param walletPassword walletPassword
+         * @return updated request
+         */
+        public Request password(String walletPassword) {
+            return add("password", walletPassword);
+        }
+    }
+
+    /**
+     * Response object parsed from JSON returned by the {@link io.helidon.integrations.common.rest.RestApi}.
+     */
+    public static class Response extends OciResponseParser {
+        private final Multi<DataChunk> publisher;
+
+        private Response(Multi<DataChunk> publisher) {
+            this.publisher = publisher;
+        }
+
+        static Response create(Multi<DataChunk> publisher) {
+            return new Response(publisher);
+        }
+
+        /**
+         * Get a publisher of {@link io.helidon.common.http.DataChunk}, this is useful
+         * when the result is sent via WebServer or WebClient that also use it.
+         *
+         * @return publisher of data chunks
+         */
+        public Multi<DataChunk> publisher() {
+            return publisher;
+        }
+
+        /**
+         * Get a publisher of byte buffers.
+         *
+         * @return publisher of byte buffers
+         */
+        public Multi<ByteBuffer> bytePublisher() {
+            return publisher.flatMap(it -> Multi.just(it.data()));
+        }
+    }
+}
