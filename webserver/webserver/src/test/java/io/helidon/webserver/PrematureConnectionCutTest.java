0f1eb6455671ffd8465fd20b31bee5719cbd0521
Daniel Kec <daniel.kec@oracle.com>
2021-10-04 11:50:58 +0200

    Intermittent test investigation
    Signed-off-by: Daniel Kec <daniel.kec@oracle.com>

--- webserver/webserver/src/test/java/io/helidon/webserver/PrematureConnectionCutTest.java
+++ webserver/webserver/src/test/java/io/helidon/webserver/PrematureConnectionCutTest.java
@@ -32,20 +32,25 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.logging.Logger;
 
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.anyOf;
-import org.junit.jupiter.api.Test;
-import org.mockito.AdditionalMatchers;
+import org.junit.jupiter.api.RepeatedTest;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class PrematureConnectionCutTest {
 
+    Logger LOGGER = Logger.getLogger(PrematureConnectionCutTest.class.getName());
     private static final Duration TIMEOUT = Duration.of(3, ChronoUnit.SECONDS);
 
-    @Test
+    @RepeatedTest(3000)
     void cutConnectionBefore100Continue() throws Exception {
         CountDownLatch latch = new CountDownLatch(5);
 
@@ -57,19 +62,30 @@ public class PrematureConnectionCutTest {
             webServer = WebServer.builder()
                     .port(0)
                     .routing(Routing.builder()
-                            .post((req, res) -> req.content()
-                                    .as(InputStream.class)
-                                    .thenAccept(is -> {
-                                        threadFactory.newThread(() -> {
-                                            try {
-                                                is.readAllBytes(); // this is where thread could get blocked indefinitely
-                                                res.send();
-                                            } catch (IOException e) {
-                                                exceptions.add(e);
-                                                latch.countDown();
-                                            }
-                                        }).start();
-                                    })
+                            .post((req, res) -> {
+                                        Integer testCnt = req.headers().first("test-cnt").map(Integer::parseInt).get();
+                                        LOGGER.info("Test request number: " + testCnt + " received.");
+                                        req.content()
+                                                .as(InputStream.class)
+                                                .thenAccept(is -> {
+                                                    threadFactory.newThread(() -> {
+                                                        String threadName = Thread.currentThread().getName();
+                                                        LOGGER.info("Test req " + testCnt
+                                                                + " on " + threadName + " started.");
+                                                        try {
+                                                            is.readAllBytes(); // this is where thread could get blocked indefinitely
+                                                            res.send();
+                                                            fail("Test req " + testCnt + " thread " + threadName
+                                                                    + " readAllBytes should have throw an exception");
+                                                        } catch (IOException e) {
+                                                            exceptions.add(e);
+                                                            LOGGER.info("Test req " + testCnt + " on " + threadName
+                                                                    + " expected exception intercepted. " + e.getMessage());
+                                                            latch.countDown();
+                                                        }
+                                                    }).start();
+                                                });
+                                    }
                             )
                     )
                     .build()
@@ -78,12 +94,17 @@ public class PrematureConnectionCutTest {
 
             for (int i = 0; i < 5; i++) {
                 //Sending request with fake content length, but not continuing after 100 code
-                incomplete100Call(webServer);
+                incomplete100Call(webServer, i);
             }
 
             // Wait for error from each thread
-            // latch result is ignored for better assertion message later
-            latch.await(TIMEOUT.getSeconds(), TimeUnit.SECONDS);
+            if (!latch.await(TIMEOUT.getSeconds(), TimeUnit.SECONDS)) {
+                LOGGER.severe("Timeout when waiting for all the requests");
+            }
+
+            assertThat(threadFactory.uncaughtException.get(), is(nullValue()));
+
+            assertThat(exceptions.size(), is(5));
 
             exceptions.forEach(e -> {
                 assertThat(e.getCause(), anyOf(
@@ -94,10 +115,7 @@ public class PrematureConnectionCutTest {
                         instanceOf(IOException.class))
                 );
             });
-            threadFactory.threads.forEach(t -> {
-                assertThat("Thread " + t.getName() + " is in invalid state.", t.getState(), is(Thread.State.TERMINATED));
-            });
-            assertThat(exceptions.size(), is(5));
+
         } finally {
             if (webServer != null) {
                 webServer.shutdown();
@@ -108,12 +126,13 @@ public class PrematureConnectionCutTest {
     /**
      * Force Netty to avoid auto close and wait for content with fake content-length
      */
-    private void incomplete100Call(WebServer webServer) throws Exception {
+    private void incomplete100Call(WebServer webServer, int cnt) throws Exception {
         try (Socket socket = new Socket(InetAddress.getLocalHost(), webServer.port())) {
             PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
             pw.println("POST / HTTP/1.1");
             pw.println("Host: 127.0.0.1");
             pw.println("Expect: 100-continue");
+            pw.println("test-cnt: " + cnt);
             pw.println("content-length: 100");
             pw.println("");
             pw.flush();
@@ -121,11 +140,17 @@ public class PrematureConnectionCutTest {
     }
 
     private static class TestThreadFactory implements ThreadFactory {
-        List<Thread> threads = new ArrayList<>();
+        List<Thread> threads = Collections.synchronizedList(new ArrayList<>());
+        AtomicInteger threadCnt = new AtomicInteger(0);
+        AtomicReference<Throwable> uncaughtException = new AtomicReference<>();
 
         @Override
         public Thread newThread(Runnable r) {
             Thread t = new Thread(r);
+            t.setUncaughtExceptionHandler((th, e) -> {
+                uncaughtException.set(e);
+            });
+            t.setName("test-thread-" + threadCnt.incrementAndGet());
             threads.add(t);
             return t;
         }
