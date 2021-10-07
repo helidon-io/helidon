74730847d5c428f4aba27839c61c0865e2cae806
Tomas Langer <tomas.langer@oracle.com>
2021-10-07 12:38:49 +0200

    Changed to @HelidonTest
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

--- examples/todo-app/backend/src/test/java/io/helidon/demo/todos/backend/BackendTests.java
+++ examples/todo-app/backend/src/test/java/io/helidon/demo/todos/backend/BackendTests.java
@@ -16,72 +16,70 @@
 
 package io.helidon.demo.todos.backend;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
-import io.helidon.common.http.Http;
-import io.helidon.config.Config;
-import io.helidon.config.ConfigSources;
-import io.helidon.media.jsonp.JsonpSupport;
-import io.helidon.microprofile.server.Server;
-import io.helidon.webclient.WebClient;
-import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Properties;
 
+import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
-import java.io.IOException;
-import java.util.Base64;
-import java.util.List;
-import java.util.Properties;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 
-import static io.helidon.config.ConfigSources.classpath;
+import io.helidon.common.http.Http;
+import io.helidon.config.mp.MpConfigSources;
+import io.helidon.config.yaml.YamlMpConfigSource;
+import io.helidon.microprofile.tests.junit5.Configuration;
+import io.helidon.microprofile.tests.junit5.HelidonTest;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@HelidonTest
+@Configuration(useExisting = true)
 class BackendTests {
 
-    private static WebClient client;
-    private static Server server;
     private final static String CASSANDRA_HOST = "127.0.0.1";
+
+    @Inject
+    private WebTarget webTarget;
 
     @BeforeAll
     static void init() throws IOException {
         Properties cassandraProperties = initCassandra();
 
-        Config config = Config.builder()
-                .sources(List.of(
-                        classpath("test-application.yaml"),
-                        ConfigSources.create(cassandraProperties)
-                ))
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        ConfigProviderResolver configResolver = ConfigProviderResolver.instance();
+
+        org.eclipse.microprofile.config.Config mpConfig = configResolver.getBuilder()
+                .withSources(YamlMpConfigSource.create(cl.getResource("test-application.yaml")),
+                             MpConfigSources.create(cassandraProperties))
                 .build();
 
-        server = Server.builder()
-                .config(config)
-                .build();
-
-        server.start();
-
-        client = WebClient.builder()
-                .baseUri("http://0.0.0.0:" + server.port())
-                .addMediaSupport(JsonpSupport.create())
-                .build();
+        configResolver.registerConfig(mpConfig, null);
     }
 
     @AfterAll
     static void stopServer() {
         EmbeddedCassandraServerHelper.cleanEmbeddedCassandra();
-        server.stop();
     }
 
     private static Properties initCassandra() throws IOException {
         EmbeddedCassandraServerHelper.startEmbeddedCassandra(EmbeddedCassandraServerHelper.CASSANDRA_RNDPORT_YML_FILE, 20000L);
         Properties prop = new Properties();
-        prop.put("cassandra.port", EmbeddedCassandraServerHelper.getNativeTransportPort());
+        prop.put("cassandra.port", String.valueOf(EmbeddedCassandraServerHelper.getNativeTransportPort()));
         prop.put("cassandra.servers.host.host", CASSANDRA_HOST);
 
-        Cluster cluster  = Cluster.builder()
+        Cluster cluster = Cluster.builder()
                 .withoutMetrics()
                 .addContactPoint(CASSANDRA_HOST)
                 .withPort(EmbeddedCassandraServerHelper.getNativeTransportPort())
@@ -89,7 +87,9 @@ class BackendTests {
 
         Session session = cluster.connect();
         session.execute("CREATE KEYSPACE backend WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1};");
-        session.execute("CREATE TABLE backend.backend (id ascii, user ascii, message ascii, completed Boolean, created timestamp, PRIMARY KEY (id));");
+        session.execute(
+                "CREATE TABLE backend.backend (id ascii, user ascii, message ascii, completed Boolean, created timestamp, "
+                        + "PRIMARY KEY (id));");
         session.execute("select * from backend.backend;");
 
         session.close();
@@ -99,81 +99,58 @@ class BackendTests {
     }
 
     @Test
-    void testTODOScenario() {
-        final String encodingID = Base64.getEncoder().encodeToString("john:password".getBytes());
+    void testTodoScenario() {
+        String basicAuth = "Basic " + Base64.getEncoder().encodeToString("john:password".getBytes());
         JsonObject todo = Json.createObjectBuilder()
-                .add("title",  "todo title")
+                .add("title", "todo title")
                 .build();
 
         // Add a new todo
-        final JsonObject returnedTodo = client.post()
+        JsonObject returnedTodo = webTarget
                 .path("/api/backend")
-                .headers(headers ->  {
-                    headers.add(Http.Header.AUTHORIZATION, "Basic " + encodingID);
-                    return headers;
-                })
-                .submit(todo, JsonObject.class)
-                .await();
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .header(Http.Header.AUTHORIZATION, basicAuth)
+                .post(Entity.json(todo), JsonObject.class);
 
-        Assertions.assertEquals("john", returnedTodo.getString("user"));
-        Assertions.assertEquals(todo.getString("title"), returnedTodo.getString("title"));
+        assertEquals("john", returnedTodo.getString("user"));
+        assertEquals(todo.getString("title"), returnedTodo.getString("title"));
 
         // Get the todo created earlier
-        client.get()
-                .path("/api/backend/" + returnedTodo.getString("id"))
-                .headers(headers ->  {
-                    headers.add(Http.Header.AUTHORIZATION, "Basic " + encodingID);
-                    return headers;
-                })
-                .request(JsonObject.class)
-                .thenAccept(jsonObject -> {
-                    Assertions.assertEquals(returnedTodo, jsonObject);
-                })
-                .await();
+        JsonObject fromServer = webTarget.path("/api/backend/" + returnedTodo.getString("id"))
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .header(Http.Header.AUTHORIZATION, basicAuth)
+                .get(JsonObject.class);
+
+        assertEquals(returnedTodo, fromServer);
 
         // Update the todo created earlier
-        JsonObject updatedTODO = Json.createObjectBuilder()
-                .add("title",  "updated title")
+        JsonObject updatedTodo = Json.createObjectBuilder()
+                .add("title", "updated title")
                 .add("completed", false)
                 .build();
 
-        client.put()
-                .path("/api/backend/" + returnedTodo.getString("id"))
-                .headers(headers ->  {
-                    headers.add(Http.Header.AUTHORIZATION, "Basic " + encodingID);
-                    return headers;
-                })
-                .submit(updatedTODO, JsonObject.class)
-                .thenAccept(jsonObject -> {
-                    Assertions.assertEquals(updatedTODO.getString("title"), jsonObject.getString("title"));
-                })
-                .await();
+        fromServer = webTarget.path("/api/backend/" + returnedTodo.getString("id"))
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .header(Http.Header.AUTHORIZATION, basicAuth)
+                .put(Entity.json(updatedTodo), JsonObject.class);
+
+        assertEquals(updatedTodo.getString("title"), fromServer.getString("title"));
 
         // Delete the todo created earlier
-        client.delete()
-                .path("/api/backend/" + returnedTodo.getString("id"))
-                .headers(headers ->  {
-                    headers.add(Http.Header.AUTHORIZATION, "Basic " + encodingID);
-                    return headers;
-                })
-                .request(JsonObject.class)
-                .thenAccept(jsonObject -> {
-                    Assertions.assertEquals(returnedTodo.getString("id"), jsonObject.getString("id"));
-                })
-                .await();
+        fromServer = webTarget.path("/api/backend/" + returnedTodo.getString("id"))
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .header(Http.Header.AUTHORIZATION, basicAuth)
+                .delete(JsonObject.class);
+
+        assertEquals(returnedTodo.getString("id"), fromServer.getString("id"));
 
         // Get list of todos
-        client.get()
-                .path("/api/backend")
-                .headers(headers ->  {
-                    headers.add(Http.Header.AUTHORIZATION, "Basic " + encodingID);
-                    return headers;
-                })
-                .request(JsonArray.class)
-                .thenAccept(jsonValues -> {
-                    Assertions.assertEquals(0, jsonValues.size());
-                })
-                .await();
+        JsonArray jsonValues = webTarget.path("/api/backend")
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .header(Http.Header.AUTHORIZATION, basicAuth)
+                .get(JsonArray.class);
+
+        assertEquals(0, jsonValues.size(), "There should be no todos on server");
     }
 
 }
