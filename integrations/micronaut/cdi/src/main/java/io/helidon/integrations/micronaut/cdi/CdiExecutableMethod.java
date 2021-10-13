fc9715604dd350ff35e0783c0bc7198c7a348b89
Tomas Langer <tomas.langer@oracle.com>
2021-10-13 13:24:49 +0200

    Spotbugs fix.
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

--- integrations/micronaut/cdi/src/main/java/io/helidon/integrations/micronaut/cdi/CdiExecutableMethod.java
+++ integrations/micronaut/cdi/src/main/java/io/helidon/integrations/micronaut/cdi/CdiExecutableMethod.java
@@ -154,7 +154,10 @@ final class CdiExecutableMethod extends AbstractExecutableMethod {
         for (String miAnnotationName : miAnnotationNames) {
             try {
                 Annotation annotation = miAnnotated.synthesize((Class<? extends Annotation>) Class.forName(miAnnotationName));
-                annotations.put(annotation.annotationType(), annotation);
+                if (annotation != null) {
+                    // annotation is present
+                    annotations.put(annotation.annotationType(), annotation);
+                }
             } catch (Throwable ignored) {
                 // this annotation is not on the classpath, we can ignore it
             }
