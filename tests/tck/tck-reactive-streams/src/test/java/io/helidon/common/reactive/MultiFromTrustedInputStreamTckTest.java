83c4b35e7cf3c6ab8ada10e7ae1fb0e662250b92
Daniel Kec <daniel.kec@oracle.com>
2021-10-28 18:31:06 +0200

    MultiFromBlockingInputStream RC fix
    Signed-off-by: Daniel Kec <daniel.kec@oracle.com>

--- tests/tck/tck-reactive-streams/src/test/java/io/helidon/common/reactive/MultiFromTrustedInputStreamTckTest.java
+++ tests/tck/tck-reactive-streams/src/test/java/io/helidon/common/reactive/MultiFromTrustedInputStreamTckTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ public class MultiFromTrustedInputStreamTckTest extends FlowPublisherVerificatio
     static final int BUFFER_SIZE = 4;
 
     public MultiFromTrustedInputStreamTckTest() {
-        super(new TestEnvironment(50));
+        super(new TestEnvironment(500));
     }
 
     @Override
