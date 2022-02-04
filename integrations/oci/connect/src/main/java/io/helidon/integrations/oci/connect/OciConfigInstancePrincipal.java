1fae9e6dc4e0ef13dd6ca6f2a3efe142b8848b19
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2022-02-04 10:41:20 -0500

    Use resource /instance/canonicalRegionName to get region and construct auth URL.
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

--- integrations/oci/connect/src/main/java/io/helidon/integrations/oci/connect/OciConfigInstancePrincipal.java
+++ integrations/oci/connect/src/main/java/io/helidon/integrations/oci/connect/OciConfigInstancePrincipal.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -569,7 +569,7 @@ public class OciConfigInstancePrincipal implements OciConfigProvider {
 
             // returns string, such as eu-frankfurt-1
             return webClient.get()
-                            .path("/instance/region")
+                            .path("/instance/canonicalRegionName")
                             .request(String.class)
                             .await(10, TimeUnit.SECONDS);
         }
