ecc97bb613a26e5c389db945f0c80cd5cf9611fb
Laird Nelson <laird.nelson@oracle.com>
2022-03-07 15:47:51 -0800

    Squashable commit; refining
    Signed-off-by: Laird Nelson <laird.nelson@oracle.com>

--- integrations/cdi/oci/src/test/java/io/helidon/integrations/cdi/oci/TestSpike.java
+++ integrations/cdi/oci/src/test/java/io/helidon/integrations/cdi/oci/TestSpike.java
@@ -203,13 +203,39 @@ final class TestSpike {
 
 
         @Inject
-        private ExampleBean(AIServiceLanguage serviceInterface,
+        private ExampleBean(// The service* parameters below are
+                            // arbitrary; pick another OCI service and
+                            // substitute the classes as appropriate
+                            // and this will still work (assuming of
+                            // course you also add the proper jar file
+                            // to this project's test-scoped
+                            // dependencies).
+                            AIServiceLanguage serviceInterface,
                             AIServiceLanguageAsync serviceAsyncInterface,
                             AIServiceLanguageAsyncClient serviceAsyncClient,
                             AIServiceLanguageAsyncClient.Builder serviceAsyncClientBuilder,
                             AIServiceLanguageClient serviceClient,
                             AIServiceLanguageClient.Builder serviceClientBuilder,
+                            // This one is an example of something
+                            // that looks like a service client but
+                            // isn't; see the comments in the
+                            // constructor body below.  It should be
+                            // unsatisfied.
                             Instance<com.oracle.bmc.circuitbreaker.JaxRsCircuitBreaker> unresolvedJaxRsCircuitBreakerInstance,
+                            // Streaming turns out to be the only
+                            // convention-violating service in the
+                            // entire portfolio.  Specifically, its
+                            // root subpackage features two main
+                            // domain objects (Stream, StreamAdmin)
+                            // and only one of them (StreamAdmin)
+                            // fully follows the service client
+                            // pattern.  The other one (Stream)
+                            // features a builder class that is not a
+                            // nested class (StreamClientBuilder) but
+                            // maybe should be.  We test this
+                            // explicitly here because, again, it is
+                            // the only service in the entire
+                            // portfolio that breaks the pattern.
                             Stream streamingServiceInterface,
                             StreamAdmin streamingAdminServiceInterface,
                             StreamAdminClient streamingAdminServiceClient,
@@ -217,9 +243,28 @@ final class TestSpike {
                             StreamAsync streamingServiceAsyncInterface,
                             StreamAsyncClient streamingServiceAsyncClient,
                             StreamAsyncClientBuilder streamingServiceAsyncClientBuilder,
-                            StreamClient streamingServiceClient,
-                            StreamClientBuilder streamingServiceClientBuilder) {
+                            StreamClient streamingServiceClient, // oddball
+                            StreamClientBuilder streamingServiceClientBuilder) { // oddball
             super();
+            // unresolvedJaxRsCircuitBreakerInstance
+            // (Instance<com.oracle.bmc.circuitbreaker.JaxRsCircuitBreaker>)
+            // is an example of something that seems like it obeys the
+            // OCI client service pattern.  But it deviates in a
+            // couple ways.
+            //
+            // First, JaxRsCircuitBreaker is part of the common
+            // runtime (see
+            // https://docs.oracle.com/en-us/iaas/tools/java/2.18.0/overview-summary.html#:~:text=Oracle%20Cloud%20Infrastructure%20Common%20Runtime),
+            // and hence is not a service itself.
+            //
+            // Second, there are no JaxRsCircuitBreakerAsync,
+            // JaxRsCircuitBreakerAsyncClient,
+            // JaxRsCircuitBreakerAsyncClient$Builder,
+            // JaxRsCircuitBreakerClient or
+            // JaxRsCircuitBreakerClient$Builder classes.
+            //
+            // For all these reasons it should be excluded from
+            // processing (and is, or this test would fail).
             assertTrue(unresolvedJaxRsCircuitBreakerInstance.isUnsatisfied());
         }
 
