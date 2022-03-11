a257befa1257f1ceeba704e917461a6e1cb4fb8f
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2022-03-11 12:28:00 -0500

    Fixed problem supporting config profiles with JSON and HOCON (master) (#3958)
    * Fixed problem supporting config profiles with JSON and HOCON. Created new tests that mix JSON/HOCON and YAML and define two different profiles.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    * Jakarta packages.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

---
+++ tests/functional/config-profiles/src/main/java/io/helidon/tests/configprofile/Main.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2022 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.tests.configprofile;
+
+import io.helidon.common.LogConfig;
+import io.helidon.common.reactive.Single;
+import io.helidon.config.Config;
+import io.helidon.media.jsonp.JsonpSupport;
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.WebServer;
+
+public final class Main {
+
+    private Main() {
+    }
+
+    public static void main(final String[] args) {
+        startServer();
+    }
+
+    static Single<WebServer> startServer() {
+
+        LogConfig.configureRuntime();
+
+        Config config = Config.create();
+
+        WebServer server = WebServer.builder(createRouting(config))
+                .config(config.get("server"))
+                .addMediaSupport(JsonpSupport.create())
+                .build();
+
+        Single<WebServer> webserver = server.start();
+
+        webserver.thenAccept(ws -> {
+                    System.out.println("WEB server is up! http://localhost:" + ws.port() + "/greet");
+                    ws.whenShutdown().thenRun(() -> System.out.println("WEB server is DOWN. Good bye!"));
+                })
+                .exceptionallyAccept(t -> {
+                    System.err.println("Startup failed: " + t.getMessage());
+                    t.printStackTrace(System.err);
+                });
+
+        return webserver;
+    }
+
+    private static Routing createRouting(Config config) {
+        GreetService greetService = new GreetService(config);
+        return Routing.builder()
+                .register("/greet", greetService)
+                .build();
+    }
+}
