8832492f1f2d5d064961a3b98cb4712945499fbd
Tomas Langer <tomas.langer@oracle.com>
2022-05-04 13:59:17 +0200

    GraalVM support refactored to new common.

--- integrations/graal/native-image-extension/src/main/java/io/helidon/integrations/graal/nativeimage/extension/HelidonReflectionFeature.java
+++ integrations/graal/native-image-extension/src/main/java/io/helidon/integrations/graal/nativeimage/extension/HelidonReflectionFeature.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,9 +32,9 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import io.helidon.common.HelidonFeatures;
 import io.helidon.common.LogConfig;
 import io.helidon.common.Reflected;
+import io.helidon.common.features.HelidonFeatures;
 import io.helidon.config.mp.MpConfigProviderResolver;
 
 import com.oracle.svm.core.jdk.Resources;
