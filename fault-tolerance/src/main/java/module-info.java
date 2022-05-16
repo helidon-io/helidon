c7ce55c1b9174aa3701b7c33132122f15f42aa96
Joe Di Pol <joe.dipol@oracle.com>
2022-05-16 14:48:56 -0700

    Remove dependency on helidon-common-reactive from config

--- fault-tolerance/src/main/java/module-info.java
+++ fault-tolerance/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@
 module io.helidon.faulttolerance {
     requires io.helidon.config;
     requires io.helidon.common.configurable;
+    requires io.helidon.common.reactive;
     requires java.logging;
 
     exports io.helidon.faulttolerance;
--- messaging/messaging/src/main/java/module-info.java
+++ messaging/messaging/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ module io.helidon.messaging {
 
     requires io.helidon.common.context;
     requires io.helidon.common.configurable;
+    requires io.helidon.common.reactive;
     requires io.helidon.config.mp;
     requires transitive io.helidon.config;
     requires transitive org.reactivestreams;
