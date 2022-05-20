f06a01e59f8b5bb78eab941c1fb0e1c15a5da644
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2022-05-20 10:28:29 -0400

    Enable thread pool growth _at_ the threshold instead of above it. These semantics prevent unwanted queueing. See issue 4727.
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

--- common/configurable/src/main/java/io/helidon/common/configurable/ThreadPool.java
+++ common/configurable/src/main/java/io/helidon/common/configurable/ThreadPool.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -782,7 +782,7 @@ public class ThreadPool extends ThreadPoolExecutor {
 
             // Is the queue above the threshold?
 
-            if (queueSize > queueThreshold) {
+            if (queueSize >= queueThreshold) {
 
                 // Yes. Should we grow?
                 // Note that this random number generator is quite fast, and on average is faster than or equivalent to
