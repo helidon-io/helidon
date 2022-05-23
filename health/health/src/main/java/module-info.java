c9205807f04df3d12e4a762816853a47c1cc6265
tim.quinn@oracle.com <tim.quinn@oracle.com>
2022-05-23 16:23:51 -0500

    Merge corrections
    Signed-off-by: tim.quinn@oracle.com <tim.quinn@oracle.com>

--- health/health/src/main/java/module-info.java
+++ health/health/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2018, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,6 @@ module io.helidon.health {
     requires io.helidon.media.jsonp;
     requires jakarta.json;
     requires io.helidon.faulttolerance;
-    requires io.helidon.servicecommon.rest;
 
     exports io.helidon.health;
     provides org.eclipse.microprofile.health.spi.HealthCheckResponseProvider with io.helidon.health.HealthCheckResponseProviderImpl;
