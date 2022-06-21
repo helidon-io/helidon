21c4b83782d0996d538e051b08137e648c930ea4
tim.quinn@oracle.com <tim.quinn@oracle.com>
2022-06-21 14:36:36 -0500

    Fix config doc annotations

--- openapi/src/main/java/module-info.java
+++ openapi/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +35,8 @@ module io.helidon.openapi {
     requires org.yaml.snakeyaml;
 
     requires microprofile.openapi.api;
+
+    requires static io.helidon.config.metadata;
 
     exports io.helidon.openapi;
 }
