aaa9b57ad5e38b7e2d530828fd0b2a7f94afec32
tim.quinn@oracle.com <tim.quinn@oracle.com>
2022-06-24 12:41:29 -0500

    Correct return of path from MP CORS request adapter; add test
    Signed-off-by: tim.quinn@oracle.com <tim.quinn@oracle.com>

--- microprofile/cors/src/test/java/io/helidon/microprofile/cors/AdapterTest.java
+++ microprofile/cors/src/test/java/io/helidon/microprofile/cors/AdapterTest.java
@@ -21,17 +21,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.Response;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.Response;
 
 import io.helidon.common.http.Http;
 import io.helidon.microprofile.tests.junit5.AddBean;
