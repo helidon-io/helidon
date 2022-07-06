0bffa56e019ed96997e56cef3311fdebc92c9a21
ShintaroOba <57422625+ShintaroOba@users.noreply.github.com>
2022-07-07 06:59:56 +0900

    Java doc typo fixing (#4473)
    * Update Parameters.java
    
    Co-authored-by: Romain Grecourt <romain.grecourt@oracle.com>

--- common/http/src/main/java/io/helidon/common/http/Parameters.java
+++ common/http/src/main/java/io/helidon/common/http/Parameters.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2018, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -179,7 +179,7 @@ public interface Parameters {
     Parameters putAll(Parameters parameters);
 
     /**
-     * Adds specified values tu association with the specified key (optional operation).
+     * Adds specified values to association with the specified key (optional operation).
      * If parameters doesn't contains mapping, new mapping is created.
      *
      * @param key    key with which the specified value is to be associated
@@ -191,7 +191,7 @@ public interface Parameters {
     Parameters add(String key, String... values);
 
     /**
-     * Adds specified values tu association with the specified key (optional operation).
+     * Adds specified values to association with the specified key (optional operation).
      * If parameters doesn't contains mapping, new mapping is created.
      *
      * @param key    key with which the specified value is to be associated
