f47cdf6647795b020cc27c92219e1ddf6d4cd00f
Tomas Langer <tomas.langer@oracle.com>
2022-07-07 15:14:03 +0200

    Use target package of the bean producer, not the bean.
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

---
+++ tests/integration/native-image/mp-3/src/main/java/io/helidon/tests/integration/nativeimage/mp3/Mp3Main.java
@@ -0,0 +1,9 @@
+package io.helidon.tests.integration.nativeimage.mp3;
+
+import io.helidon.microprofile.cdi.Main;
+
+public class Mp3Main {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
