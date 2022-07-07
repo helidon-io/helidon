128d46859232e7310ced55f6c065021651fa10b7
Tomas Langer <tomas.langer@oracle.com>
2022-07-07 22:35:50 +0200

    Disable MP native image tests.

--- tests/integration/native-image/mp-3/src/main/java/io/helidon/tests/integration/nativeimage/mp3/Mp3Main.java
+++
@@ -1,9 +0,0 @@
-package io.helidon.tests.integration.nativeimage.mp3;
-
-import io.helidon.microprofile.cdi.Main;
-
-public class Mp3Main {
-    public static void main(String[] args) {
-        Main.main(args);
-    }
-}
