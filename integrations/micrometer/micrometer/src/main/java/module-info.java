72a9b3d3af9ce6507d10e3d0acc9a38519767c33
tim.quinn@oracle.com <tim.quinn@oracle.com>
2022-07-13 15:57:52 -0500

    First commit of MP metrics doc reorg

--- integrations/micrometer/micrometer/src/main/java/module-info.java
+++ integrations/micrometer/micrometer/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,8 @@ module io.helidon.integrations.micrometer {
     requires io.helidon.servicecommon.rest;
     requires io.helidon.config;
     requires io.helidon.webserver.cors;
+
+    requires static io.helidon.config.metadata;
 
     requires micrometer.core;
     requires micrometer.registry.prometheus;
