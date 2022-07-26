b5d594cd12540fac686a3b39734ccd9b255e805c
tim.quinn@oracle.com <tim.quinn@oracle.com>
2022-07-26 15:34:21 -0500

    Import ordering

--- examples/microprofile/http-status-count-mp/src/main/java/io/helidon/examples/mp/httpstatuscount/SimpleGreetResource.java
+++ examples/microprofile/http-status-count-mp/src/main/java/io/helidon/examples/mp/httpstatuscount/SimpleGreetResource.java
@@ -17,9 +17,9 @@
 package io.helidon.examples.mp.httpstatuscount;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
