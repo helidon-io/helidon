699010727afbe702609910ef3058d8a8754383c2
Keith Lustria <keith.lustria@oracle.com>
2022-08-02 12:19:16 -0700

    Configuration parameter 'cookie-encryption-password' takes only a single character rather than a string (#4512)

--- security/providers/oidc-common/src/test/java/io/helidon/security/providers/oidc/common/OidcConfigFromBuilderTest.java
+++ security/providers/oidc-common/src/test/java/io/helidon/security/providers/oidc/common/OidcConfigFromBuilderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2018, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,14 @@
 
 package io.helidon.security.providers.oidc.common;
 
+import io.helidon.config.Config;
+import io.helidon.config.ConfigSources;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import java.net.URI;
+import java.util.Arrays;
+import java.util.Map;
 
 /**
  * Unit test for {@link OidcConfig}.
@@ -42,5 +49,28 @@ class OidcConfigFromBuilderTest extends OidcConfigAbstractTest {
     @Override
     OidcConfig getConfig() {
         return oidcConfig;
+    }
+
+    @Test
+    void testCookieEncryptionPasswordFromBuilderConfig() {
+        // Allow a String
+        getBuilder(Map.of("cookie-encryption-password", "PasswordString"));
+        // Don't allow empty password values
+        for (String passwordValue : Arrays.asList("", "    ")) {
+            try {
+                getBuilder(Map.of("cookie-encryption-password", passwordValue));
+                fail("Empty value for cookie-encryption-password is not allowed");
+            } catch (IllegalArgumentException ie) {
+                // this is expected
+            }
+        }
+    }
+
+    private OidcConfig.Builder getBuilder(Map<String, String> map) {
+        return OidcConfig.builder().config(
+                Config.builder()
+                        .sources(ConfigSources.create(map))
+                        .build()
+                );
     }
 }
