7427688a5a72e42abc7ce7f9870c326e9fe053a5
Keith Lustria <34235093+klustria@users.noreply.github.com>
2022-08-04 08:10:11 -0700

    Configuration parameter 'cookie-encryption-password' takes only a single character rather than a string (#4512) (#4657) (#4675)
    * Configuration parameter 'cookie-encryption-password' takes only a single character rather than a string (#4512)
    
    * Simplify cookie-password-encryption conversion and improve the unit test

--- security/providers/oidc-common/src/main/java/io/helidon/security/providers/oidc/common/OidcConfig.java
+++ security/providers/oidc-common/src/main/java/io/helidon/security/providers/oidc/common/OidcConfig.java
@@ -1282,7 +1282,9 @@ public final class OidcConfig {
             config.get("header-token").as(TokenHandler.class).ifPresent(this::headerTokenHandler);
             // encryption of cookies
             config.get("cookie-encryption-enabled").asBoolean().ifPresent(this::cookieEncryptionEnabled);
-            config.get("cookie-encryption-password").as(char[].class).ifPresent(this::cookieEncryptionPassword);
+            config.get("cookie-encryption-password").as(String.class)
+                    .map(String::toCharArray)
+                    .ifPresent(this::cookieEncryptionPassword);
             config.get("cookie-encryption-name").asString().ifPresent(this::cookieEncryptionName);
 
             // OIDC server configuration
