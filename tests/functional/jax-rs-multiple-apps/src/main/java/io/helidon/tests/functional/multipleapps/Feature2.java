93693d6c0b4aa7c549e036ba4b145107098c2606
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2022-08-22 08:59:19 -0400

    Make Application subclasses available via our context during Feature executions (#4745)
    * A global/shared Feature is executed for each Application subclass. This change makes the Application subclass available to the Feature during its execution. Just like for requests, the availability is through the Helidon context. Updated functional tests for multiple apps.
    
    * Small code improvement and fixed typo in copyright.
    
    * Handle case when Application subclass is not available via CDI.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

--- tests/functional/jax-rs-multiple-apps/src/main/java/io/helidon/tests/functional/multipleapps/Feature2.java
+++ tests/functional/jax-rs-multiple-apps/src/main/java/io/helidon/tests/functional/multipleapps/Feature2.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ import jakarta.ws.rs.container.ResourceInfo;
 import jakarta.ws.rs.core.FeatureContext;
 
 @ConstrainedTo(RuntimeType.SERVER)
-public class MyFeature implements DynamicFeature {
+public class Feature2 implements DynamicFeature {
 
     @Override
     public void configure(ResourceInfo resourceInfo, FeatureContext featureContext) {
--- tests/functional/jax-rs-multiple-apps/src/test/java/io/helidon/tests/functional/multipleapps/MainTest.java
+++ tests/functional/jax-rs-multiple-apps/src/test/java/io/helidon/tests/functional/multipleapps/MainTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,6 +26,8 @@ import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -68,5 +70,11 @@ class MainTest {
         JsonObject jsonObject = response.readEntity(JsonObject.class);
         assertEquals("Hello World 2!", jsonObject.getString("message"),
                 "default message");
+    }
+
+    @Test
+    void testContextApps() {
+        assertThat(SharedFeature.applications(), hasItem(GreetApplication1.class));
+        assertThat(SharedFeature.applications(), hasItem(GreetApplication2.class));
     }
 }
