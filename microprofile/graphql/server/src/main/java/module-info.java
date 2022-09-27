08fbcaef4c786029ab83bc6002699e1f2ba05884
Tim Middleton <tim.middleton@oracle.com>
2022-09-27 09:59:08 +0800

    Update graphql-java to 17.4
    Signed-off-by: Tim Middleton <tim.middleton@oracle.com>

--- microprofile/graphql/server/src/main/java/module-info.java
+++ microprofile/graphql/server/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ module io.helidon.microprofile.graphql.server {
     requires io.helidon.microprofile.cdi;
     requires io.helidon.microprofile.server;
 
-    requires graphql.java;
+    requires com.graphqljava;
     requires graphql.java.extended.scalars;
     requires microprofile.graphql.api;
     requires microprofile.config.api;
