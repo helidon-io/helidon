24aee0e2fe79987f0df33b061d795daa0c24dd3e
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2022-10-10 13:26:58 -0400

    Fix merge problem.
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

--- health/health/src/test/java/io/helidon/health/HealthSupportTest.java
+++ health/health/src/test/java/io/helidon/health/HealthSupportTest.java
@@ -1,9 +1,5 @@
 /*
-<<<<<<< HEAD
- * Copyright (c) 2018, 2021 Oracle and/or its affiliates.
-=======
  * Copyright (c) 2018, 2022 Oracle and/or its affiliates.
->>>>>>> 8496afdea... Use lazy values to initialized HealthSupport FT handlers (#5106)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
