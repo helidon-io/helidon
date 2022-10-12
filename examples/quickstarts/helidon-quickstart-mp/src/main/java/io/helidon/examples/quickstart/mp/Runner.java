f713d0ace88f6586b98e519996550ad9559772d5
Dmitry Aleksandrov <dmitry.aleksandrov@oracle.com>
2022-10-12 19:08:56 +0300

    New Policies checkstyle fix

--- examples/quickstarts/helidon-quickstart-mp/src/main/java/io/helidon/examples/quickstart/mp/Runner.java
+++
@@ -1,9 +0,0 @@
-package io.helidon.examples.quickstart.mp;
-
-import io.helidon.microprofile.cdi.Main;
-
-public class Runner {
-    public static void main(String[] args) {
-        Main.main(args);
-    }
-}
