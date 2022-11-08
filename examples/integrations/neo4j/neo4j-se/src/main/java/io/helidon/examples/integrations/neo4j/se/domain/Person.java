4a70ddae018d7c00f49be52ce0565adaf867f13d
Andrei Arlou <andrei.arlou@gmail.com>
2022-11-08 20:43:47 +0200

    Remove trailing empty line in copyright comment (part 5) (#5346)

--- examples/integrations/neo4j/neo4j-se/src/main/java/io/helidon/examples/integrations/neo4j/se/domain/Person.java
+++ examples/integrations/neo4j/neo4j-se/src/main/java/io/helidon/examples/integrations/neo4j/se/domain/Person.java
@@ -32,7 +32,6 @@ package io.helidon.examples.integrations.neo4j.se.domain;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 /**
