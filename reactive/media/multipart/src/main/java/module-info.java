ac8fb076bf7ee8aa493cdd29ccbd6b02be2c2d83
Dmitry Aleksandrov <dmitry.aleksandrov@oracle.com>
2022-11-16 15:53:29 +0200

    [4.x] - [features migration] - Reactive Media (#5399)
    * Features migration media.
    
    * Features migration media. Fix path.
    
    * Features migration media. Fix FC.
    
    * Apply Comments.

--- reactive/media/multipart/src/main/java/module-info.java
+++ reactive/media/multipart/src/main/java/module-info.java
@@ -14,10 +14,19 @@
  * limitations under the License.
  */
 
+import io.helidon.common.features.api.Feature;
+import io.helidon.common.features.api.HelidonFlavor;
+
 /**
  * Media MultiPart support classes.
  */
+@Feature(value = "Multi-part",
+        description = "Media support for Multi-part entities",
+        path = {"Media", "Multipart"},
+        in = HelidonFlavor.SE)
 module io.helidon.reactive.media.multipart {
+    requires static io.helidon.common.features.api;
+
     requires io.helidon.common;
     requires io.helidon.common.http;
     requires io.helidon.common.mapper;
