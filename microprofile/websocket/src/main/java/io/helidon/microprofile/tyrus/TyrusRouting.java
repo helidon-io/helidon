312d6eb4dd1e6b04fb6b4d24d6c2a56275c8a9b7
Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>
2022-11-28 09:33:13 -0500

    Tyrus integration into Nima (#5464)
    * Restored Tyrus module in Nima MP.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    * Updated dependencies and socket routing code.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    * Initial integration of Tyrus with Nima. Simple echo test works using the JDK's WS client.
    
    * Fixed checkstyle issues.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    * Simplified TyrusConnection by not extending WsConnection. Restored some changes in Nima's WS module.
    
    * Some general code cleanup.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    * Renamed some classes for consistency. Refactored tests for declarative and programmatic styles and to use @HelidonTest.
    
    * Support for @RoutingPath annotation.
    
    * Fixed checkstyle errors.
    
    * New test for extensions that uses the Tyrus client after fixes in Nima.
    
    * New test that combines REST and WS endpoint on same port.
    
    * Restored examples that use websockets.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    * Order of protected fields and use putIfAbsent.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

---
+++ microprofile/websocket/src/main/java/io/helidon/microprofile/tyrus/TyrusRouting.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2022 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.microprofile.tyrus;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import io.helidon.common.http.HttpPrologue;
+import io.helidon.common.http.PathMatchers;
+import io.helidon.nima.webserver.Routing;
+
+import jakarta.websocket.Extension;
+import jakarta.websocket.server.ServerEndpoint;
+import jakarta.websocket.server.ServerEndpointConfig;
+
+/**
+ * Routing class for Tyrus.
+ */
+public class TyrusRouting implements Routing {
+    private static final TyrusRouting EMPTY = TyrusRouting.builder().build();
+
+    private final Set<Extension> extensions;
+    private final List<TyrusRoute> routes;
+
+    private TyrusRouting(Builder builder) {
+        this.routes = builder.routes;
+        this.extensions = builder.extensions;
+    }
+
+    /**
+     * Builder for WebSocket routing.
+     *
+     * @return new builder
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Emtpy WebSocket routing.
+     *
+     * @return empty routing
+     */
+    public static TyrusRouting empty() {
+        return EMPTY;
+    }
+
+    Set<Extension> extensions() {
+        return extensions;
+    }
+
+    List<TyrusRoute> routes() {
+        return routes;
+    }
+
+    /**
+     * Returns {@code true} if this route corresponds to one of the registered or
+     * discovered WS endpoint.
+     *
+     * @param prologue HTTP prologue
+     * @return outcome of test
+     */
+    TyrusRoute findRoute(HttpPrologue prologue) {
+        for (TyrusRoute route : routes) {
+            PathMatchers.MatchResult accepts = route.accepts(prologue);
+            if (accepts.accepted()) {
+                return route;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Fluent API builder for {@link TyrusRouting}.
+     */
+    public static class Builder implements io.helidon.common.Builder<Builder, Routing> {
+
+        private final List<TyrusRoute> routes = new ArrayList<>();
+        private final Set<Extension> extensions = new HashSet<>();      // mutable
+
+        private Builder() {
+        }
+
+        /**
+         * Add endpoint.
+         *
+         * @param path path of the endpoint
+         * @param endpointClass annotated endpoint class
+         * @return updated builder
+         */
+        public Builder endpoint(String path, Class<?> endpointClass) {
+            this.routes.add(new TyrusRoute(path, endpointClass, null,
+                    PathMatchers.pattern(pathConcat(path, serverEndpoint(endpointClass)))));
+            return this;
+        }
+
+        /**
+         * Add endpoint.
+         *
+         * @param endpointClass annotated endpoint class
+         * @return updated builder
+         */
+        public Builder endpoint(Class<?> endpointClass) {
+            return endpoint("/", endpointClass);
+        }
+
+        /**
+         * Add endpoint.
+         *
+         * @param path path of the endpoint
+         * @param serverEndpointConfig Jakarta WebSocket endpoint configuration
+         * @return updated builder
+         */
+        public Builder endpoint(String path, ServerEndpointConfig serverEndpointConfig) {
+            this.routes.add(new TyrusRoute(path, null, serverEndpointConfig,
+                    PathMatchers.pattern(pathConcat(path, serverEndpointConfig.getPath()))));
+            return this;
+        }
+
+        /**
+         * Add endpoint.
+         *
+         * @param serverEndpointConfig Jakarta WebSocket endpoint configuration
+         * @return updated builder
+         */
+        public Builder endpoint(ServerEndpointConfig serverEndpointConfig) {
+            return endpoint("/", serverEndpointConfig);
+        }
+
+        /**
+         * Add Jakarta WebSocket extension.
+         *
+         * @param extension Jakarta WebSocket extension
+         * @return updated builder
+         */
+        public Builder extension(Extension extension) {
+            this.extensions.add(extension);
+            return this;
+        }
+
+        @Override
+        public TyrusRouting build() {
+            return new TyrusRouting(this);
+        }
+
+        private static String pathConcat(String rootPath, String path) {
+            StringBuilder result = new StringBuilder();
+            if (rootPath.equals("/")) {
+                rootPath = "";
+            } else if (!rootPath.startsWith("/")) {
+                result.append("/");
+            }
+            result.append(rootPath);
+            if (!rootPath.endsWith("/") && !path.startsWith("/")) {
+                result.append("/");
+            }
+            result.append(path);
+            return result.toString();
+        }
+
+        private static String serverEndpoint(Class<?> endpointClass) {
+            ServerEndpoint annot = endpointClass.getAnnotation(ServerEndpoint.class);
+            if (annot == null) {
+                throw new IllegalArgumentException("Endpoint class " + endpointClass.getName()
+                        + " missing @ServerEndpoint");
+            }
+            return annot.value();
+        }
+    }
+}
--- microprofile/websocket/src/test/java/io/helidon/microprofile/tyrus/ExtensionEndpointTest.java
+++ microprofile/websocket/src/test/java/io/helidon/microprofile/tyrus/ExtensionEndpointTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Oracle and/or its affiliates.
+ * Copyright (c) 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,53 +21,29 @@ import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 
-import io.helidon.microprofile.server.ServerCdiExtension;
-
-import jakarta.enterprise.inject.se.SeContainer;
-import jakarta.enterprise.inject.se.SeContainerInitializer;
-import jakarta.enterprise.inject.spi.CDI;
+import io.helidon.microprofile.tests.junit5.AddBean;
+import io.helidon.microprofile.tests.junit5.HelidonTest;
 import jakarta.websocket.Extension;
 import jakarta.websocket.OnMessage;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-/**
- * A test that mixes Websocket endpoints and extensions in the same application.
- */
-public class WebSocketExtensionEndpointTest {
-
-    static SeContainer container;
-
-    @BeforeAll
-    static void initClass() {
-        container = SeContainerInitializer.newInstance()
-                .addBeanClasses(ExtensionEndpointAnnot.class, TestExtension.class)
-                .initialize();
-    }
-
-    @AfterAll
-    static void destroyClass() {
-        container.close();
-    }
-
-    public int port() {
-        ServerCdiExtension cdiExtension = CDI.current().getBeanManager().getExtension(ServerCdiExtension.class);
-        return cdiExtension.port();
-    }
+@HelidonTest
+@AddBean(ExtensionEndpointTest.TestExtension.class)
+@AddBean(ExtensionEndpointTest.ExtensionEndpoint.class)
+class ExtensionEndpointTest extends EchoEndpointBaseTest {
 
     @Test
     public void test() throws Exception {
-        URI echoUri = URI.create("ws://localhost:" + port() + "/extAnnot");
+        URI echoUri = URI.create("ws://localhost:" + serverPort() + "/extAnnot");
         EchoClient echoClient = new EchoClient(echoUri, new TestExtension());
         echoClient.echo("hi", "how are you?");
     }
 
     @ServerEndpoint("/extAnnot")
-    public static class ExtensionEndpointAnnot {
-        private static final Logger LOGGER = Logger.getLogger(ExtensionEndpointAnnot.class.getName());
+    public static class ExtensionEndpoint {
+        private static final Logger LOGGER = Logger.getLogger(ExtensionEndpoint.class.getName());
 
         @OnMessage
         public void echo(Session session, String message) throws Exception {
