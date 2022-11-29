7cb7c131e75c9ebad3eb91f94abe6a1b54e3871f
Dmitry Aleksandrov <dmitry.aleksandrov@oracle.com>
2022-11-29 16:09:45 +0200

    Fix JBatch intermittent test fail

--- examples/jbatch/src/test/java/io/helidon/examples/jbatch/TestJBatchEndpoint.java
+++ examples/jbatch/src/test/java/io/helidon/examples/jbatch/TestJBatchEndpoint.java
@@ -24,8 +24,8 @@ import javax.json.JsonObject;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @HelidonTest
@@ -46,18 +46,23 @@ public class TestJBatchEndpoint {
         Integer responseJobId = jsonObject.getInt("Started a job with Execution ID: ");
         assertNotNull(responseJobId, "Response Job Id");
 
-        //Wait a bit for it to complete
-        Thread.sleep(3000);
+        boolean result = false;
+        for (int i = 1; i < 10; i++) {
+            //Wait a bit for it to complete
+            Thread.sleep(i*1000);
 
-        //Examine the results
-        jsonObject = webTarget
-                .path("batch/status/" + responseJobId)
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .get(JsonObject.class);
+            //Examine the results
+            jsonObject = webTarget
+                    .path("batch/status/" + responseJobId)
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get(JsonObject.class);
 
-        String responseString = jsonObject.toString();
+            String responseString = jsonObject.toString();
+            result = responseString.equals("{\"Steps executed\":\"[step1, step2]\",\"Status\":\"COMPLETED\"}");
 
-        assertEquals("{\"Steps executed\":\"[step1, step2]\",\"Status\":\"COMPLETED\"}",
-                responseString, "Job Result string");
+            if (result) break;
+        }
+
+        assertTrue(result, "Job Result string");
     }
 }
