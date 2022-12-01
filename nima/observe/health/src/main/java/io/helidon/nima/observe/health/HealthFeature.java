35b2c15b3c6000f8e01fd083851aa30ccbe5bc2a
tim.quinn@oracle.com <tim.quinn@oracle.com>
2022-12-01 09:03:40 -0600

    Remove unused imports

--- nima/observe/health/src/main/java/io/helidon/nima/observe/health/HealthFeature.java
+++ nima/observe/health/src/main/java/io/helidon/nima/observe/health/HealthFeature.java
@@ -19,15 +19,12 @@ package io.helidon.nima.observe.health;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.ServiceLoader;
-import java.util.stream.Collectors;
 
 import io.helidon.common.HelidonServiceLoader;
 import io.helidon.config.Config;
 import io.helidon.config.ConfigSources;
-import io.helidon.config.ConfigValue;
 import io.helidon.config.spi.ConfigSource;
 import io.helidon.health.HealthCheck;
 import io.helidon.health.HealthCheckType;
