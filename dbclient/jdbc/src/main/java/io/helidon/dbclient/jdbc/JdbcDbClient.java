10dbf92cb619498e52dfb713034bb8fa4bf591ab
Arjav Desai <arjav.desai@oracle.com>
2022-12-12 10:54:39 -0600

    handle exception on apply

--- dbclient/jdbc/src/main/java/io/helidon/dbclient/jdbc/JdbcDbClient.java
+++ dbclient/jdbc/src/main/java/io/helidon/dbclient/jdbc/JdbcDbClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -79,7 +79,14 @@ class JdbcDbClient implements DbClient {
                 dbMapperManager,
                 mapperManager);
 
-        T result = executor.apply(execute);
+        T result;
+
+        try {
+            result = executor.apply(execute);
+        } catch (RuntimeException e) {
+            execute.doRollback();
+            throw e;
+        }
 
         if (result instanceof Multi) {
             Multi<U> multi = (Multi<U>) result;
