135bf6bf3f88641a8556e00e24572357445185a2
Andrii Serkes <andrii.serkes@oracle.com>
2022-12-13 14:37:16 +0100

    4269 openapi tools examples (#5250)
    OpenApi Tools examples
    
    Signed-off-by: aserkes <andrii.serkes@oracle.com>

---
+++ examples/openapi-tools/quickstart-mp/mp-server/src/main/java/org/openapitools/server/JavaTimeFormatter.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2022 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openapitools.server;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+/**
+ * Class that add parsing/formatting support for Java 8+ {@code OffsetDateTime} class.
+ * It's generated for java clients when {@code AbstractJavaCodegen#dateLibrary} specified as {@code java8}.
+ */
+@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaHelidonServerCodegen", date = "2022-10-25T09:58"
+        + ":58.439110277+02:00[Europe/Prague]")
+public class JavaTimeFormatter {
+
+    private DateTimeFormatter offsetDateTimeFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
+    /**
+     * Get the date format used to parse/format {@code OffsetDateTime} parameters.
+     *
+     * @return DateTimeFormatter
+     */
+    public DateTimeFormatter getOffsetDateTimeFormatter() {
+        return offsetDateTimeFormatter;
+    }
+
+    /**
+     * Set the date format used to parse/format {@code OffsetDateTime} parameters.
+     *
+     * @param offsetDateTimeFormatter {@code DateTimeFormatter}
+     */
+    public void setOffsetDateTimeFormatter(DateTimeFormatter offsetDateTimeFormatter) {
+        this.offsetDateTimeFormatter = offsetDateTimeFormatter;
+    }
+
+    /**
+     * Parse the given string into {@code OffsetDateTime} object.
+     *
+     * @param str String
+     * @return {@code OffsetDateTime}
+     */
+    public OffsetDateTime parseOffsetDateTime(String str) {
+        try {
+            return OffsetDateTime.parse(str, offsetDateTimeFormatter);
+        } catch (DateTimeParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Format the given {@code OffsetDateTime} object into string.
+     *
+     * @param offsetDateTime {@code OffsetDateTime}
+     * @return {@code OffsetDateTime} in string format
+     */
+    public String formatOffsetDateTime(OffsetDateTime offsetDateTime) {
+        return offsetDateTimeFormatter.format(offsetDateTime);
+    }
+}
---
+++ examples/openapi-tools/quickstart-se/se-client/src/main/java/org/openapitools/client/Main.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2022 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openapitools.client;
+
+import io.helidon.common.LogConfig;
+import io.helidon.common.reactive.Single;
+import io.helidon.config.Config;
+import io.helidon.media.jackson.JacksonSupport;
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.WebServer;
+import org.openapitools.client.api.MessageService;
+
+public final class Main {
+
+    /**
+     * Cannot be instantiated.
+     */
+    private Main() {
+    }
+
+    /**
+     * Application main entry point.
+     *
+     * @param args command line arguments.
+     */
+    public static void main(final String[] args) {
+        startServer();
+    }
+
+    /**
+     * Start the server.
+     *
+     * @return the created {@link WebServer} instance
+     */
+    static Single<WebServer> startServer() {
+
+        // load logging configuration
+        LogConfig.configureRuntime();
+
+        // By default this will pick up application.yaml from the classpath
+        Config config = Config.create();
+
+        WebServer server = WebServer.builder(createRouting(config))
+                                    .config(config.get("server"))
+                                    .addMediaSupport(JacksonSupport.create())
+                                    .build();
+
+        Single<WebServer> webserver = server.start();
+
+        // Try to start the server. If successful, print some info and arrange to
+        // print a message at shutdown. If unsuccessful, print the exception.
+        webserver.thenAccept(ws -> {
+                     System.out.println("WEB server is up! http://localhost:8081");
+                     ws.whenShutdown().thenRun(() -> System.out.println("WEB server is DOWN. Good bye!"));
+                 })
+                 .exceptionallyAccept(t -> {
+                     System.err.println("Startup failed: " + t.getMessage());
+                     t.printStackTrace(System.err);
+                 });
+
+        return webserver;
+    }
+
+    /**
+     * Creates new {@link Routing}.
+     *
+     * @param config configuration of this server
+     * @return routing configured with JSON support, a health check, and a service
+     */
+    private static Routing createRouting(Config config) {
+
+        return Routing.builder()
+                      .register("/", new MessageService())
+                      .build();
+    }
+}
