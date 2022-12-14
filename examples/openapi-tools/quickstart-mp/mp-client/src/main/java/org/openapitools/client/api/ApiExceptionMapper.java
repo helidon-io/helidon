34484df4fea66140513eb81dbab8f7a4056c476a
Andrii Serkes <andrii.serkes@oracle.com>
2022-12-14 14:09:05 +0100

    [3.x] Backport of #4269  OpenAPI generator examples (#5649)
    OpenApi generator examples
    
    Signed-off-by: aserkes <andrii.serkes@oracle.com>

---
+++ examples/openapi-tools/quickstart-mp/mp-client/src/main/java/org/openapitools/client/api/ApiExceptionMapper.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openapitools.client.api;
+
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+import org.eclipse.microprofile.rest.client.ext.ResponseExceptionMapper;
+
+@Provider
+public class ApiExceptionMapper implements ResponseExceptionMapper<ApiException> {
+
+    @Override
+    public boolean handles(int status, MultivaluedMap<String, Object> headers) {
+        return status >= 400;
+    }
+
+    @Override
+    public ApiException toThrowable(Response response) {
+        return new ApiException(response);
+    }
+}
