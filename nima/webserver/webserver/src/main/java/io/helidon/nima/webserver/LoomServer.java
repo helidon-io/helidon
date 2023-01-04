bb46d38cb7deebe8d0d3fbf7afe7c0f934fe9c4e
David <david.k.kral@oracle.com>
2023-01-04 17:44:33 +0100

    JWT Auth TCK tests enabled
    Signed-off-by: David <david.k.kral@oracle.com>

--- nima/webserver/webserver/src/main/java/io/helidon/nima/webserver/LoomServer.java
+++ nima/webserver/webserver/src/main/java/io/helidon/nima/webserver/LoomServer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -96,7 +96,7 @@ class LoomServer implements WebServer {
         this.listeners = Map.copyOf(listeners);
         this.executorService = Executors.newThreadPerTaskExecutor(Thread.ofVirtual()
                                                                           .allowSetThreadLocals(true)
-                                                                          .inheritInheritableThreadLocals(false)
+                                                                          .inheritInheritableThreadLocals(true)
                                                                           .factory());
     }
 
