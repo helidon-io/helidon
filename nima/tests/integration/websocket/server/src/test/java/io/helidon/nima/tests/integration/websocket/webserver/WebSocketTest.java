b70c986ed4e146e3943a02113ae8f2793c3a995d
Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>
2023-01-10 13:49:41 -0500

    HTTP upgrade handler for Websockets (#5569)
    * Prototype for an HTTP upgrade handler that is part of the WsListener interface. Updated sample to show use case thatnegotiates a sub-protocol.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    * Restored protected variable.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    * Added sample code that shows negotation of extensions. JDK client does not support adding the extensions header manually so only server-side code updated.
    
    * Fixed problem with buffer sharing and updated copyrights.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    * Added support for subProtocol in WsSession. Fixed some copyrights.
    
    * Make static field private.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    * Copyright and minor fix in WsConnection.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

--- nima/tests/integration/websocket/server/src/test/java/io/helidon/nima/tests/integration/websocket/webserver/WebSocketTest.java
+++ nima/tests/integration/websocket/server/src/test/java/io/helidon/nima/tests/integration/websocket/webserver/WebSocketTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -81,8 +81,11 @@ class WebSocketTest {
         TestListener listener = new TestListener();
 
         java.net.http.WebSocket ws = client.newWebSocketBuilder()
+                .subprotocols("chat", "mute")
+                // .header(EXTENSIONS.defaultCase(), "nima") rejected by client
                 .buildAsync(URI.create("ws://localhost:" + port + "/echo"), listener)
                 .get(5, TimeUnit.SECONDS);
+        assertThat(ws.getSubprotocol(), is("chat"));    // negotiated
         ws.request(10);
 
         ws.sendText("Hello", true).get(5, TimeUnit.SECONDS);
