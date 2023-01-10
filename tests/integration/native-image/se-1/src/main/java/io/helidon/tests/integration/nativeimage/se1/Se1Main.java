bbe36422eb3eb065181398f082fa0163aa449ead
Tim Quinn <tim.quinn@oracle.com>
2023-01-10 10:09:30 -0600

    Add config enum mapping support (#5788)
    Signed-off-by: tim.quinn@oracle.com <tim.quinn@oracle.com>
    
    Signed-off-by: tim.quinn@oracle.com <tim.quinn@oracle.com>

--- tests/integration/native-image/se-1/src/main/java/io/helidon/tests/integration/nativeimage/se1/Se1Main.java
+++ tests/integration/native-image/se-1/src/main/java/io/helidon/tests/integration/nativeimage/se1/Se1Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -120,6 +120,7 @@ public final class Se1Main {
 
         MetricsSupport metrics = MetricsSupport.create();
         GreetService greetService = new GreetService(config);
+        ColorService colorService = new ColorService(config);
         MockZipkinService zipkinService = new MockZipkinService(Set.of("helidon-webclient"));
         WebClientService webClientService = new WebClientService(config, zipkinService);
         HealthSupport health = HealthSupport.builder()
@@ -138,6 +139,7 @@ public final class Se1Main {
                 .register(health)                   // Health at "/health"
                 .register(metrics)                  // Metrics at "/metrics"
                 .register("/greet", greetService)
+                .register("/color", colorService)
                 .register("/wc", webClientService)
                 .register("/zipkin", zipkinService)
                 .register("/ws",
