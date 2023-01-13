4de161a6d0cf5d76b2d57150403c73e4cf932c4f
tim.quinn@oracle.com <tim.quinn@oracle.com>
2023-01-13 08:21:48 -0600

    Suppress incorrect start-up log message related to requested URI discovery

--- webserver/webserver/src/main/java/io/helidon/webserver/SocketConfiguration.java
+++ webserver/webserver/src/main/java/io/helidon/webserver/SocketConfiguration.java
@@ -1098,7 +1098,7 @@ public interface SocketConfiguration {
                 }
             } else {
                 // Discovery is disabled so ignore any explicit settings of discovery type and use HOST discovery.
-                if (!requestedUriDiscoveryTypes.isEmpty()) {
+                if (!requestedUriDiscoveryTypes.isEmpty() && !isDiscoveryTypesOnlyHost()) {
                     LOGGER.log(Level.INFO, """
                             Ignoring explicit settings of requested-uri-discovery types and trusted-proxies because
                             requested-uri-discovery.enabled {0} to false
