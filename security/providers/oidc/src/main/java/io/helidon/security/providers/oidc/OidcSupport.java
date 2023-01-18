f2a776ef88b27d5c6cd6457e129c4e981121ec15
David Král <david.k.kral@oracle.com>
2023-01-18 15:37:52 +0100

    Default tenant is not included for propagation (#5898)
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- security/providers/oidc/src/main/java/io/helidon/security/providers/oidc/OidcSupport.java
+++ security/providers/oidc/src/main/java/io/helidon/security/providers/oidc/OidcSupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2018, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -413,7 +413,10 @@ public final class OidcSupport implements Service {
         } else {
             uri = oidcConfig.redirectUriWithHost();
         }
-        return uri + (uri.contains("?") ? "&" : "?") + encode(oidcConfig.tenantParamName()) + "=" + encode(tenantName);
+        if (!DEFAULT_TENANT_ID.equals(tenantName)) {
+            return uri + (uri.contains("?") ? "&" : "?") + encode(oidcConfig.tenantParamName()) + "=" + encode(tenantName);
+        }
+        return uri;
     }
 
     private String processJsonResponse(ServerRequest req,
@@ -428,7 +431,9 @@ public final class OidcSupport implements Service {
         res.status(Http.Status.TEMPORARY_REDIRECT_307);
         if (oidcConfig.useParam()) {
             state += (state.contains("?") ? "&" : "?") + encode(oidcConfig.paramName()) + "=" + tokenValue;
-            state += "&" + encode(oidcConfig.tenantParamName()) + "=" + encode(tenantName);
+            if (!DEFAULT_TENANT_ID.equals(tenantName)) {
+                state += "&" + encode(oidcConfig.tenantParamName()) + "=" + encode(tenantName);
+            }
         }
 
         state = increaseRedirectCounter(state);
