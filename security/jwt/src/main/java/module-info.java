2a3b3aee17c65d9997ae51972bc64a91d0660453
aserkes <andrii.serkes@oracle.com>
2023-01-20 16:44:31 +0100

    System.Logger for helidon-security-jwt
    Signed-off-by: aserkes <andrii.serkes@oracle.com>

--- security/jwt/src/main/java/module-info.java
+++ security/jwt/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2018, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,6 @@ module io.helidon.security.jwt {
     requires transitive io.helidon.common.configurable;
     requires transitive jakarta.json;
     requires io.helidon.security.util;
-    requires java.logging;
 
     exports io.helidon.security.jwt;
     exports io.helidon.security.jwt.jwk;
