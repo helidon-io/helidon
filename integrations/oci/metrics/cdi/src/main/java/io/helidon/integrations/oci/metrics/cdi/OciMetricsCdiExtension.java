a6a6ebfa9ed4a5db3c4279d55d26eb19e3a3d82b
Keith Lustria <keith.lustria@oracle.com>
2023-01-23 12:38:18 -0800

    Add Helidon Metrics integration with OCI
    The change includes the following:
    1. Port of v3 PR https://github.com/helidon-io/helidon/pull/5829 that includes additional changes and fixes.
    2. Changes related to OciMetricsSupport use of Nima instead of SE Webserver dependency.

---
+++ integrations/oci/metrics/cdi/src/main/java/io/helidon/integrations/oci/metrics/cdi/OciMetricsCdiExtension.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022, 2023 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.integrations.oci.metrics.cdi;
+
+import jakarta.enterprise.event.Observes;
+import jakarta.enterprise.inject.spi.BeforeBeanDiscovery;
+import jakarta.enterprise.inject.spi.Extension;
+
+/**
+ * OCI metrics integration CDI extension.
+ */
+public class OciMetricsCdiExtension implements Extension {
+
+    // A new bean is added to handle the Observer Method as injection does not work here
+    void addOciMetricsBean(@Observes BeforeBeanDiscovery event) {
+        event.addAnnotatedType(OciMetricsBean.class, OciMetricsBean.class.getName());
+    }
+}
