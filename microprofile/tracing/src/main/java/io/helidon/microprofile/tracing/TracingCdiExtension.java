8d27ba31c8ee99423af80f3988f3b19612e9754c
Tomas Langer <tomas.langer@oracle.com>
2023-01-27 19:33:17 +0100

    Fix order of initialization of tracing and security.
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

--- microprofile/tracing/src/main/java/io/helidon/microprofile/tracing/TracingCdiExtension.java
+++ microprofile/tracing/src/main/java/io/helidon/microprofile/tracing/TracingCdiExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2020 Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.helidon.microprofile.tracing;
 
 import java.util.List;
@@ -20,6 +21,7 @@ import java.util.logging.Logger;
 
 import io.helidon.common.context.Contexts;
 import io.helidon.config.Config;
+import io.helidon.microprofile.cdi.RuntimeStart;
 import io.helidon.microprofile.server.JaxRsApplication;
 import io.helidon.microprofile.server.JaxRsCdiExtension;
 import io.helidon.microprofile.server.ServerCdiExtension;
@@ -37,7 +39,6 @@ import jakarta.enterprise.inject.spi.BeanManager;
 import jakarta.enterprise.inject.spi.BeforeBeanDiscovery;
 import jakarta.enterprise.inject.spi.DeploymentException;
 import jakarta.enterprise.inject.spi.Extension;
-import org.eclipse.microprofile.config.ConfigProvider;
 
 import static jakarta.interceptor.Interceptor.Priority.PLATFORM_BEFORE;
 
@@ -45,6 +46,11 @@ import static jakarta.interceptor.Interceptor.Priority.PLATFORM_BEFORE;
  * CDI extension for Microprofile Tracing implementation.
  */
 public class TracingCdiExtension implements Extension {
+    private static final Logger LOGGER = Logger.getLogger(TracingCdiExtension.class.getName());
+
+    private io.helidon.tracing.Tracer tracer;
+    private Config config;
+
     /**
      * Add our beans to CDI, so we do not need to use {@code beans.xml}.
      *
@@ -55,29 +61,21 @@ public class TracingCdiExtension implements Extension {
         bbd.addAnnotatedType(TracerProducer.class, "TracingTracerProducer");
     }
 
-    // must be configured before security
-    private void prepareTracer(@Observes @Priority(PLATFORM_BEFORE + 1) @Initialized(ApplicationScoped.class) Object event,
-                               BeanManager bm) {
-        JaxRsCdiExtension jaxrs = bm.getExtension(JaxRsCdiExtension.class);
-        ServerCdiExtension server = bm.getExtension(ServerCdiExtension.class);
+    // must be higher priority than security, so tracer is ready when IDCS and OIDC uses webclient
+    // the client is used in security configuration
+    private void setupTracer(@Observes @Priority(PLATFORM_BEFORE) @RuntimeStart Config rootConfig,
+                             BeanManager bm) {
 
-        Config config = ((Config) ConfigProvider.getConfig()).get("tracing");
-
-        // we need that `tracing.service` is a required configuration, yet we do not want to just fail
-        // if not present. Let's make a "guess" about the service name
-        List<JaxRsApplication> jaxRsApps = jaxrs.applicationsToRun();
-
-        String serviceName = jaxrs.serviceName();
-
-        io.helidon.tracing.Tracer helidonTracer = TracerBuilder.create(serviceName)
+        String serviceName = bm.getExtension(JaxRsCdiExtension.class).serviceName();
+        config = rootConfig.get("tracing");
+        tracer = TracerBuilder.create(serviceName)
                 .config(config)
                 .build();
 
-        if (!helidonTracer.enabled()) {
-            Logger.getLogger(TracingCdiExtension.class.getName())
-                    .warning("helidon-microprofile-tracing is on the classpath, yet there is no tracer implementation "
-                                     + "library. Tracing uses a no-op tracer. As a result, no tracing will be configured"
-                                     + " for WebServer and JAX-RS");
+        if (!tracer.enabled()) {
+            LOGGER.warning("helidon-microprofile-tracing is on the classpath, yet there is no tracer "
+                                   + "implementation library or tracing is explicitly disabled. Tracing uses a no-op tracer. "
+                                   + "As a result, no tracing will be configured for WebServer and JAX-RS");
 
             Contexts.globalContext().register(io.helidon.tracing.Tracer.noOp());
             Contexts.globalContext().register(GlobalTracer.get());
@@ -87,32 +85,45 @@ public class TracingCdiExtension implements Extension {
 
         Tracer registeredTracer;
         try {
-            registeredTracer = helidonTracer.unwrap(Tracer.class);
+            registeredTracer = tracer.unwrap(Tracer.class);
         } catch (Exception e) {
             try {
-                io.opentelemetry.api.trace.Tracer otelTracer = helidonTracer.unwrap(io.opentelemetry.api.trace.Tracer.class);
+                io.opentelemetry.api.trace.Tracer otelTracer = tracer.unwrap(io.opentelemetry.api.trace.Tracer.class);
                 registeredTracer = OpenTracingShim.createTracerShim(otelTracer);
             } catch (Exception ex) {
                 throw new DeploymentException("MicroProfile tracing requires an OpenTracing or OpenTelemetry based tracer", ex);
             }
         }
 
-        Tracer tracer = registeredTracer;
+        Tracer openTracingTracer = registeredTracer;
 
         // tracer is available in global
-        Contexts.globalContext().register(tracer);
-        Contexts.globalContext().register(helidonTracer);
-
-        server.serverBuilder()
-                .tracer(helidonTracer);
+        Contexts.globalContext().register(openTracingTracer);
+        Contexts.globalContext().register(this.tracer);
 
         Contexts.context()
-                .ifPresent(ctx -> ctx.register(tracer));
+                .ifPresent(ctx -> ctx.register(openTracingTracer));
+    }
 
+    private void serverTracer(@Observes @Priority(PLATFORM_BEFORE + 1) @Initialized(ApplicationScoped.class) Object event,
+                              BeanManager bm) {
+        ServerCdiExtension server = bm.getExtension(ServerCdiExtension.class);
 
+        server.serverBuilder()
+                .tracer(this.tracer);
+
+        if (!tracer.enabled()) {
+            return;
+        }
+
+        JaxRsCdiExtension jaxrs = bm.getExtension(JaxRsCdiExtension.class);
 
         server.serverRoutingBuilder()
                 .register(WebTracingConfig.create(config));
+
+        // we need that `tracing.service` is a required configuration, yet we do not want to just fail
+        // if not present. Let's make a "guess" about the service name
+        List<JaxRsApplication> jaxRsApps = jaxrs.applicationsToRun();
 
         jaxRsApps
                 .forEach(app -> app.resourceConfig().register(MpTracingFilter.class));
---
+++ tests/integration/mp-gh-5328/src/main/java/io/helidon/tests/integration/gh5328/OffendingExtension.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2023 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.tests.integration.gh5328;
+
+import java.time.Duration;
+
+import io.helidon.config.Config;
+import io.helidon.microprofile.cdi.RuntimeStart;
+import io.helidon.webclient.WebClient;
+import io.helidon.webclient.tracing.WebClientTracing;
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.WebServer;
+
+import jakarta.annotation.Priority;
+import jakarta.enterprise.event.Observes;
+import jakarta.enterprise.inject.spi.Extension;
+
+import static jakarta.interceptor.Interceptor.Priority.PLATFORM_BEFORE;
+
+public class OffendingExtension implements Extension {
+    private static final Duration TIMEOUT = Duration.ofSeconds(15);
+
+    private String response;
+
+    private void configure(@Observes @RuntimeStart @Priority(PLATFORM_BEFORE + 2) Config config) {
+        // start a local server and connect to it (random port)
+        WebServer ws = WebServer.builder()
+                .routing(Routing.builder()
+                        .get("/", (req, res) -> res.send("Hello")))
+                .build()
+                .start()
+                .await(TIMEOUT);
+
+        WebClient wc = WebClient.builder()
+                // explicitly add tracing, so we need tracer at this moment
+                .addService(WebClientTracing.create())
+                .baseUri("http://localhost:" + ws.port())
+                .build();
+
+        response = wc.get()
+                .request(String.class)
+                .await(TIMEOUT);
+    }
+
+    String response() {
+        return response;
+    }
+}
