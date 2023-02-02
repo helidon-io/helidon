d657c875750f32ce3ed5411edee4f3dd64cfdcb1
Keith Lustria <keith.lustria@oracle.com>
2023-02-02 01:44:55 -0800

    Register OciMetricsSupport service only when enable flag is set to true
    Changes include:
    1. Add a getter enabled method in OciMetricsSupport.Builder to provide boolean value indicating whether OciMetricsSupport service will be activated or not.
    2. In OciMetricsBean, use the new enabled method to determine whether OciMetricsSupport service will be registered in the default routing or not.
    3. Refactor routing registration of OciMetricsSupport, so it can be properly unit tested to verify if it is skipped when disabled or invoked when enabled.
    4. Add unit test validation when enable flag is set to true, false, or not set (which defaults to true).
    
    Additional relevant changes backported from 3.x/4.x
    1. Change MonitoringClient.class to Monitoring.class for mocking using Mockito in the unit test as the OCI Java SDK v3 converted some of the methods in MonitoringClient as Final making them difficult to mock.
    2. Trim the OCI Metadata value which contains the metric description if the value exceeds 256 characters, or otherwise it will fail.
    3. OCI Monitoring service only allows a maximum of 50 metrics per posting, hence additional configuration parameters were added to control sending metrics in batches. The configuration parameters are: (a.) batchSize - Maximum no. of metrics to send in a batch. Defaults to 50 which is what OCI allows. (b.) batchDelay - Interval between batch posting For example if there are 51 metrics and batchSize is set to 25 and batchDelay to 5 seconds, the Helidon metric integration module will divide the posting to 3 batches sending 25 metrics on the 1st and 2nd batches and 1 metric on the 3rd batch with 5 seconds interval between batch posting.
    4. Refactor OciMetricsCdiExtension to add a new bean (OciMetricsBean) to handle the Observer method which will inject Monitoring. Previous code of OciMetricsCdiExtension cannot independently handle instantiation of Monitoring client via CDI.
    5. Add unit tests to verify batch posting feature.
    6. Move shutdown hook init rules.onNewWebServer(this::prepareShutdown) only after enabled config is verified to be true.
    7. Add non null check on builder methods where appropriate.
    8. Update builder method ConfiguredOption annotation to reflect default values.

--- integrations/oci/metrics/metrics/src/test/java/io/helidon/integrations/oci/metrics/OciMetricsSupportTest.java
+++ integrations/oci/metrics/metrics/src/test/java/io/helidon/integrations/oci/metrics/OciMetricsSupportTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,8 @@
  */
 package io.helidon.integrations.oci.metrics;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
@@ -26,7 +28,7 @@ import io.helidon.metrics.api.RegistryFactory;
 import io.helidon.webserver.Routing;
 import io.helidon.webserver.WebServer;
 
-import com.oracle.bmc.monitoring.MonitoringClient;
+import com.oracle.bmc.monitoring.Monitoring;
 import com.oracle.bmc.monitoring.model.MetricDataDetails;
 import com.oracle.bmc.monitoring.model.PostMetricDataDetails;
 import com.oracle.bmc.monitoring.requests.PostMetricDataRequest;
@@ -44,13 +46,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
 public class OciMetricsSupportTest {
     private final OciMetricsSupport.NameFormatter nameFormatter = new OciMetricsSupport.NameFormatter() { };
-    private static final MonitoringClient monitoringClient = mock(MonitoringClient.class);
+    private static final Monitoring monitoringClient = mock(Monitoring.class);
     private final Type[] types = {Type.BASE, Type.VENDOR, Type.APPLICATION};
 
     private static volatile Double[] testMetricUpdateCounterValue = new Double[2];
@@ -58,6 +61,7 @@ public class OciMetricsSupportTest {
     // Use CountDownLatch to signal when to start testing, for example, test only after results has been retrieved.
     private static CountDownLatch countDownLatch1;
     private static int noOfExecutions;
+    private static int noOfMetrics;
 
     private final RegistryFactory rf = RegistryFactory.getInstance();
     private final MetricRegistry baseMetricRegistry = rf.getRegistry(Type.BASE);
@@ -113,7 +117,8 @@ public class OciMetricsSupportTest {
                 .initialDelay(1L)
                 .delay(2L)
                 .descriptionEnabled(false)
-                .monitoringClient(monitoringClient);
+                .monitoringClient(monitoringClient)
+                .enabled(true);
 
         Routing routing = createRouting(ociMetricsSupportBuilder);
 
@@ -123,9 +128,94 @@ public class OciMetricsSupportTest {
         // Wait for metric updates to complete
         countDownLatch1.await(10, java.util.concurrent.TimeUnit.SECONDS);
 
+        assertThat(ociMetricsSupportBuilder.enabled(), is(true));
         // Test the 1st and 2nd metric counter updates
         assertThat(testMetricUpdateCounterValue[0].intValue(), is(equalTo(1)));
         assertThat(testMetricUpdateCounterValue[1].intValue(), is(equalTo(2)));
+
+        webServer.shutdown().await(10, java.util.concurrent.TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testBatchSize() throws InterruptedException {
+        baseMetricRegistry.counter("baseDummyCounter1").inc();
+        baseMetricRegistry.counter("baseDummyCounter2").inc();
+        baseMetricRegistry.counter("baseDummyCounter3").inc();
+
+        vendorMetricRegistry.counter("vendorDummyCounter1").inc();
+        vendorMetricRegistry.counter("vendorDummyCounter2").inc();
+        vendorMetricRegistry.counter("vendorDummyCounter3").inc();
+
+        appMetricRegistry.counter("appDummyCounter1").inc();
+        appMetricRegistry.counter("appDummyCounter2").inc();
+        appMetricRegistry.counter("appDummyCounter3").inc();
+        appMetricRegistry.counter("appDummyCounter4").inc();
+
+        // Should be 10 metrics
+        int totalMetrics =
+                baseMetricRegistry.getMetrics().size() +
+                        vendorMetricRegistry.getMetrics().size() +
+                        appMetricRegistry.getMetrics().size();
+
+        int batchSize = 3;
+        long batchDelay = 100L;
+        // Should be 1
+        int remainder = totalMetrics % batchSize;
+        // Should be 4
+        int noOfBatches = Math.round(totalMetrics / batchSize) + (remainder > 0 ? 1 : 0);
+
+        countDownLatch1 = new CountDownLatch(1);
+        noOfExecutions = 0;
+        noOfMetrics = 0;
+
+        doAnswer(invocationOnMock -> {
+            noOfExecutions++;
+            PostMetricDataRequest postMetricDataRequest = invocationOnMock.getArgument(0);
+            PostMetricDataDetails postMetricDataDetails = postMetricDataRequest.getPostMetricDataDetails();
+            int metricSizeToPost = postMetricDataDetails.getMetricData().size();
+            noOfMetrics += metricSizeToPost;
+            // Give signal that the last remaining metric in the last batch has been posted
+            if (metricSizeToPost == remainder) {
+                countDownLatch1.countDown();
+            }
+            return PostMetricDataResponse.builder()
+                    .__httpStatusCode__(200)
+                    .build();
+        }).when(monitoringClient).postMetricData(any());
+
+        OciMetricsSupport.Builder ociMetricsSupportBuilder = OciMetricsSupport.builder()
+                .compartmentId("compartmentId")
+                .namespace("namespace")
+                .resourceGroup("resourceGroup")
+                .initialDelay(0L)
+                .delay(20000L)
+                .batchDelay(batchDelay)
+                .schedulingTimeUnit(TimeUnit.MILLISECONDS)
+                .descriptionEnabled(false)
+                .batchSize(batchSize)
+                .monitoringClient(monitoringClient);
+
+
+        Instant start = Instant.now();
+
+        Routing routing = createRouting(ociMetricsSupportBuilder);
+
+        WebServer webServer = createWebServer(routing);
+
+        // Wait for last batch to be completed
+        countDownLatch1.await(10, java.util.concurrent.TimeUnit.SECONDS);
+        Instant finish = Instant.now();
+
+        // Batch size of 3 for 10 metrics should yield 4 batches to post
+        assertThat(totalMetrics, is(10));
+        assertThat(noOfBatches, is(4));
+        assertThat(noOfExecutions, is(noOfBatches));
+        assertThat(noOfMetrics, is(totalMetrics));
+
+        // Last batch is not delayed so compute only the delays for prior batches
+        long estimatedTotalBatchDelay = batchDelay * (noOfBatches - 1);
+        // Test total batch delay
+        assertThat(Duration.between(start, finish).toMillis(), is(greaterThanOrEqualTo(estimatedTotalBatchDelay)));
 
         webServer.shutdown().await(10, java.util.concurrent.TimeUnit.SECONDS);
     }
@@ -200,6 +290,7 @@ public class OciMetricsSupportTest {
         delay(1000L);
 
         webServer.shutdown().await(10, java.util.concurrent.TimeUnit.SECONDS);
+        assertThat(ociMetricsSupportBuilder.enabled(), is(false));
         // metric count should remain 0 as metrics is disabled
         assertThat(testMetricCount, is(equalTo(0)));
 
