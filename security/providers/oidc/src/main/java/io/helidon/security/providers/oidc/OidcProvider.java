3c7c6ad48a912c34129f5776ce8aa159765de6b7
David Kral <david.k.kral@oracle.com>
2023-02-09 11:16:00 +0100

    OIDC tenant SPI default priority changed
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- security/providers/oidc/src/main/java/io/helidon/security/providers/oidc/OidcProvider.java
+++ security/providers/oidc/src/main/java/io/helidon/security/providers/oidc/OidcProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2018, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -253,10 +253,10 @@ public final class OidcProvider implements AuthenticationProvider, OutboundSecur
 
         private final HelidonServiceLoader.Builder<TenantConfigProvider> tenantConfigProviders = HelidonServiceLoader
                 .builder(ServiceLoader.load(TenantConfigProvider.class))
-                .defaultPriority(BUILDER_PRIORITY);
+                .defaultPriority(DEFAULT_PRIORITY);
         private final HelidonServiceLoader.Builder<TenantIdProvider> tenantIdProviders = HelidonServiceLoader
                 .builder(ServiceLoader.load(TenantIdProvider.class))
-                .defaultPriority(BUILDER_PRIORITY);
+                .defaultPriority(DEFAULT_PRIORITY);
         private boolean optional = false;
         private OidcConfig oidcConfig;
         private List<TenantIdFinder> tenantIdFinders;
