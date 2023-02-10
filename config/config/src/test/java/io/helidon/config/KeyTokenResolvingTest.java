4ecb3ab1c394aa18713bb4762e1bd27bd88f374a
Tomas Langer <tomas.langer@oracle.com>
2023-02-10 15:13:12 +0100

    Configuration fixes: - allow unresolved key references (configurable) - config created from another config honors node types

--- config/config/src/test/java/io/helidon/config/KeyTokenResolvingTest.java
+++ config/config/src/test/java/io/helidon/config/KeyTokenResolvingTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2017, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -186,7 +186,7 @@ public class KeyTokenResolvingTest {
 
             config.traverse().forEach(System.out::println);
         });
-        assertThat(ex.getMessage(), startsWith("Missing value in token 'region' definition"));
+        assertThat(ex.getMessage(), startsWith("Missing token 'region' to resolve a key reference"));
     }
 
     @Test
