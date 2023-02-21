104c449e464cc369ce9aa6a4b7d7d698ed5aa0c8
Andrei Arlou <andrei.arlou@gmail.com>
2023-02-21 05:55:33 +0200

    Use Hamcrest assertions instead of JUnit in tests/functional/config-profiles - backport main (#1749) (#6254)

--- tests/functional/config-profiles/src/test/java/io/helidon/tests/configprofile/DevTest.java
+++ tests/functional/config-profiles/src/test/java/io/helidon/tests/configprofile/DevTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,8 @@ import jakarta.json.JsonObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class DevTest extends BaseTest {
 
@@ -36,6 +37,6 @@ public class DevTest extends BaseTest {
                 .path("/greet")
                 .request(JsonObject.class)
                 .await();
-        assertEquals("Hello Dev World!", jsonObject.getString("message"));
+        assertThat(jsonObject.getString("message"), is("Hello Dev World!"));
     }
 }
