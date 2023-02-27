bcb68ed7c7b527266958db77fe3b471e982c4ee8
Keith Lustria <keith.lustria@oracle.com>
2023-02-27 10:57:41 -0800

    Minor update to remove unnecessary use of this when calling close method.

--- nima/webclient/webclient/src/main/java/io/helidon/nima/webclient/http1/Http1ClientConnection.java
+++ nima/webclient/webclient/src/main/java/io/helidon/nima/webclient/http1/Http1ClientConnection.java
@@ -199,7 +199,7 @@ class Http1ClientConnection implements ClientConnection {
             }
         }
         // Close if unable to add to queue
-        this.close();
+        close();
     }
 
     private void debugTls(SSLSocket sslSocket) {
