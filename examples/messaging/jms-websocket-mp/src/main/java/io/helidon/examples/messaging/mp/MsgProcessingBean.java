2f5736b74a38b43c055fee36ff90bb68ea777a42
Daniel Kec <daniel.kec@oracle.com>
2023-03-02 18:24:56 +0100

    [4.x] 6303 JMS JNDI destination support (#6305)
    * 6303 JMS JNDI destination support

--- examples/messaging/jms-websocket-mp/src/main/java/io/helidon/examples/messaging/mp/MsgProcessingBean.java
+++ examples/messaging/jms-websocket-mp/src/main/java/io/helidon/examples/messaging/mp/MsgProcessingBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,8 @@
 
 package io.helidon.examples.messaging.mp;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.SubmissionPublisher;
 
 import io.helidon.common.reactive.Multi;
@@ -77,22 +79,26 @@ public class MsgProcessingBean {
      * Broadcasts an event.
      *
      * @param msg Message to broadcast
+     * @return completed stage
      */
     @Incoming("fromJms")
-    public void broadcast(JmsMessage<String> msg) {
+    public CompletionStage<Void> broadcast(JmsMessage<String> msg) {
         // Broadcast to all subscribers
         broadCaster.submit(msg.getPayload());
+        return CompletableFuture.completedFuture(null);
     }
 
     /**
      * Same JMS session, different connector.
      *
      * @param msg Message to broadcast
+     * @return completed stage
      */
     @Incoming("fromJmsSameSession")
-    public void sameSession(JmsMessage<String> msg) {
+    public CompletionStage<Void> sameSession(JmsMessage<String> msg) {
         // Broadcast to all subscribers
         broadCaster.submit(msg.getPayload());
+        return CompletableFuture.completedFuture(null);
     }
 
     /**
