0e282b52501a41732339d43e081e91b15b7ac509
Romain Grecourt <romain.grecourt@oracle.com>
2023-04-27 09:25:02 -0700

    Use checkNested(Throwable) for req.next(Throwable) to always propagate the exception. (#6699)
    Fixes #6550

--- webserver/webserver/src/main/java/io/helidon/webserver/RequestRouting.java
+++ webserver/webserver/src/main/java/io/helidon/webserver/RequestRouting.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2017, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -416,7 +416,7 @@ class RequestRouting implements Routing {
 
         @Override
         public void next(Throwable t) {
-            checkNexted();
+            checkNexted(t);
 
             nextNoCheck(t);
         }
