46e4501bee83261a0370863450b2ac16d6cbfa6a
David Kral <david.k.kral@oracle.com>
2023-05-03 00:48:18 +0200

    Vault example fixes

--- examples/security/vaults/src/main/java/io/helidon/examples/security/vaults/EncryptionService.java
+++ examples/security/vaults/src/main/java/io/helidon/examples/security/vaults/EncryptionService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,17 +41,23 @@ class EncryptionService implements Service {
         String configName = req.path().param("config");
         String text = req.path().param("text");
 
-        security.encrypt(configName, text.getBytes(StandardCharsets.UTF_8))
-                .forSingle(res::send)
-                .exceptionally(res::send);
+        try {
+            String encrypted = security.encrypt(configName, text.getBytes(StandardCharsets.UTF_8));
+            res.send(encrypted);
+        } catch (Exception e) {
+            res.send(e);
+        }
     }
 
     private void decrypt(ServerRequest req, ServerResponse res) {
         String configName = req.path().param("config");
         String cipherText = req.path().param("cipherText");
 
-        security.decrypt(configName, cipherText)
-                .forSingle(res::send)
-                .exceptionally(res::send);
+        try {
+            byte[] decrypted = security.decrypt(configName, cipherText);
+            res.send(decrypted);
+        } catch (Exception e) {
+            res.send(e);
+        }
     }
 }
--- examples/security/vaults/src/main/java/io/helidon/examples/security/vaults/SecretsService.java
+++ examples/security/vaults/src/main/java/io/helidon/examples/security/vaults/SecretsService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,8 +36,11 @@ class SecretsService implements Service {
 
     private void secret(ServerRequest req, ServerResponse res) {
         String secretName = req.path().param("name");
-        security.secret(secretName, "default-" + secretName)
-                .forSingle(res::send)
-                .exceptionally(res::send);
+        try {
+            String secret = security.secret(secretName, "default-" + secretName);
+            res.send(secret);
+        } catch (Exception e) {
+            res.send(e);
+        }
     }
 }
