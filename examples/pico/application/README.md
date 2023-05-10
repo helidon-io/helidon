2ce858908e5a9cc9843ac32041c14af94f5492f1
Jeff Trent <jeff.trent@oracle.com>
2023-05-10 08:21:24 -0400

    checkpoint

--- examples/pico/application/README.md
+++ examples/pico/application/README.md
@@ -1,17 +1,17 @@
-# Helidon Pico Providers Example
+# Helidon Pico Application Example
 
-This example shows how providers can be leveraged to develop using Helidon Pico. The
-[Main.java](./src/main/java/io/helidon/examples/pico/providers/Main.java) class shows:
+This example shows how a multi-module application can be created using Helidon Pico. The
+[Main.java](./src/main/java/io/helidon/examples/pico/application/Main.java) class shows:
 
 * multi-module usage (i.e., this example extends [basics](../basics), [providers](../providers), and [configdriven](../configdriven) ).
-* compile-time generation for the entire multi-module project using the pico-maven-plugin (see [pom.xml](./pom.xml)).
+* compile-time generation for the entire multi-module project using the _pico-maven-plugin_ (see [pom.xml](./pom.xml)).
 * TestingSupport
 
 ## Build and run
 
 ```bash
 mvn package
-java -jar target/helidon-examples-pico-providers.jar
+java -jar target/helidon-examples-pico-application.jar
 ```
 
 Expected Output:
---
+++ examples/pico/application/src/test/java/io/helidon/examples/pico/application/PicoApplicationTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2023 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.examples.pico.application;
+
+import io.helidon.config.Config;
+import io.helidon.config.ConfigSources;
+import io.helidon.examples.pico.basics.Main;
+import io.helidon.pico.api.PicoServices;
+import io.helidon.pico.api.Services;
+import io.helidon.pico.testing.PicoTestingSupport;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import static io.helidon.pico.testing.PicoTestingSupport.testableServices;
+
+class PicoApplicationTest {
+
+    protected PicoServices picoServices;
+    protected Services services;
+
+    @AfterAll
+    static void tearDown() {
+        PicoTestingSupport.resetAll();
+    }
+
+    protected void resetWith(Config config) {
+        PicoTestingSupport.resetAll();
+        this.picoServices = testableServices(config);
+        this.services = picoServices.services();
+    }
+
+    @Test
+    void main() {
+        Config config = Config.builder()
+                .addSource(ConfigSources.classpath("application.yaml"))
+                .disableSystemPropertiesSource()
+                .disableEnvironmentVariablesSource()
+                .build();
+        resetWith(config);
+        Main.main();
+    }
+
+}
