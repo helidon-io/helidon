0ef4b2bd17520dc3b55c279c8e8106311b0d0b99
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2023-05-11 14:54:27 -0400

    Initial work to switch to WebClient in Nima.
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

--- jersey/connector/src/main/java/module-info.java
+++ jersey/connector/src/main/java/module-info.java
@@ -28,7 +28,9 @@ module io.helidon.jersey.connector {
     requires jersey.common;
     requires io.helidon.common.reactive;
     requires io.helidon.reactive.webclient;
+    requires io.helidon.nima.webclient;
     requires io.netty.codec.http;
+    requires io.helidon.nima.faulttolerance;
 
     exports io.helidon.jersey.connector;
     provides org.glassfish.jersey.client.spi.ConnectorProvider with HelidonConnectorProvider;
