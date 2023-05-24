f7182702eea0111faddfd24163e4575cad98696e
Romain Grecourt <romain.grecourt@oracle.com>
2023-05-24 15:49:58 -0700

    Integrate build tools 3.0.5 (#6903)
    * Integrate build-tools 3.0.5 for everything except cli and archetype.
    
    * Fix copyright errors identified by new enforcer

--- microprofile/lra/jax-rs/src/test/java/io/helidon/microprofile/lra/resources/Work.java
+++ microprofile/lra/jax-rs/src/test/java/io/helidon/microprofile/lra/resources/Work.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -58,7 +58,7 @@ public enum Work {
     public Response doWork(URI lraId) {
         return worker.apply(lraId);
     }
-    
+
     public Integer[] expectedResponseStatuses(){
         return expectedStatuses;
     }
