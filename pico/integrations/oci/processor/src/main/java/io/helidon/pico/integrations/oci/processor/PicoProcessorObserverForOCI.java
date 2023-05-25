28c10228e39cf90f33d46073316732bb7f91e95b
Jeff Trent <jeff.trent@oracle.com>
2023-05-25 16:03:01 -0400

    oci activator code generation completed

--- pico/integrations/oci/processor/src/main/java/io/helidon/pico/integrations/oci/processor/PicoProcessorObserverForOCI.java
+++ pico/integrations/oci/processor/src/main/java/io/helidon/pico/integrations/oci/processor/PicoProcessorObserverForOCI.java
@@ -33,7 +33,6 @@ import javax.tools.JavaFileObject;
 import io.helidon.common.types.TypeInfo;
 import io.helidon.common.types.TypeName;
 import io.helidon.common.types.TypedElementInfo;
-import io.helidon.pico.processor.GeneralProcessorUtils;
 import io.helidon.pico.processor.spi.PicoAnnotationProcessorObserver;
 import io.helidon.pico.processor.spi.ProcessingEvent;
 import io.helidon.pico.tools.TemplateHelper;
@@ -42,6 +41,8 @@ import io.helidon.pico.tools.ToolsException;
 import jakarta.inject.Inject;
 
 import static io.helidon.common.types.TypeNameDefault.create;
+import static io.helidon.pico.processor.GeneralProcessorUtils.findFirst;
+import static io.helidon.pico.processor.GeneralProcessorUtils.isProviderType;
 
 /**
  * Implementation that will monitor {@link io.helidon.pico.processor.PicoAnnotationProcessor} for all injection points
@@ -49,14 +50,22 @@ import static io.helidon.common.types.TypeNameDefault.create;
  * {@link io.helidon.pico.api.ModuleComponent}, etc.
  */
 public class PicoProcessorObserverForOCI implements PicoAnnotationProcessorObserver {
-
     static final String OCI_ROOT_PACKAGE_NAME_PREFIX = "com.oracle.bmc.";
     static final String GENERATED_PREFIX = "generated.";
+    static final String GENERATED_CLIENT_SUFFIX = "$$Oci$$Client";
+    static final String GENERATED_CLIENT_BUILDER_SUFFIX = GENERATED_CLIENT_SUFFIX + "Builder";
     static final String GENERATED_OCI_ROOT_PACKAGE_NAME_PREFIX = GENERATED_PREFIX + OCI_ROOT_PACKAGE_NAME_PREFIX;
-    static final String TAG_TEMPLATE_SERVICE_PROVIDER_NAME = "service-provider.hbs";
+    static final String TAG_TEMPLATE_SERVICE_CLIENT_PROVIDER_NAME = "service-client-provider.hbs";
+    static final String TAG_TEMPLATE_SERVICE_CLIENT_BUILDER_PROVIDER_NAME = "service-client-builder-provider.hbs";
     static final Set<String> TYPENAME_EXCEPTIONS =
             Set.of(OCI_ROOT_PACKAGE_NAME_PREFIX + "Region",
-                   OCI_ROOT_PACKAGE_NAME_PREFIX + "auth.AbstractAuthenticationDetailsProvider");
+                   OCI_ROOT_PACKAGE_NAME_PREFIX + "auth.AbstractAuthenticationDetailsProvider",
+                   OCI_ROOT_PACKAGE_NAME_PREFIX + "circuitbreaker.OciCircuitBreaker"
+            );
+    static final Set<String> NO_DOT_EXCEPTIONS =
+            Set.of(OCI_ROOT_PACKAGE_NAME_PREFIX + "streaming.Stream",
+                   OCI_ROOT_PACKAGE_NAME_PREFIX + "streaming.StreamAsync"
+            );
 
     /**
      * Service loader based constructor.
@@ -76,52 +85,81 @@ public class PicoProcessorObserverForOCI implements PicoAnnotationProcessorObser
     }
 
     private void process(TypedElementInfo element,
-                         ProcessingEnvironment proccessingEnv) {
+                         ProcessingEnvironment processingEnv) {
         if (TypeInfo.KIND_FIELD.equalsIgnoreCase(element.elementTypeKind())) {
-            process(element.typeName(), proccessingEnv);
+            process(element.typeName(), processingEnv);
         } else if (TypeInfo.KIND_METHOD.equalsIgnoreCase(element.elementTypeKind())
                 || TypeInfo.KIND_CONSTRUCTOR.equalsIgnoreCase(element.elementTypeKind())) {
             element.parameterArguments().stream()
-                    .filter(it -> shouldProcess(it.typeName(), proccessingEnv))
-                    .forEach(it -> process(it.typeName(), proccessingEnv));
+                    .filter(it -> shouldProcess(it.typeName(), processingEnv))
+                    .forEach(it -> process(it.typeName(), processingEnv));
         }
     }
 
     private void process(TypeName ociServiceTypeName,
-                         ProcessingEnvironment proccessingEnv) {
-        if (GeneralProcessorUtils.isProviderType(ociServiceTypeName)) {
+                         ProcessingEnvironment processingEnv) {
+        if (isProviderType(ociServiceTypeName)
+                || ociServiceTypeName.isOptional()) {
             ociServiceTypeName = ociServiceTypeName.typeArguments().get(0);
         }
+        assert (!ociServiceTypeName.generic()) : ociServiceTypeName.name();
         assert (ociServiceTypeName.name().startsWith(OCI_ROOT_PACKAGE_NAME_PREFIX)) : ociServiceTypeName.name();
-        TypeName generatedOciActivatorTypeName = toGeneratedTypeName(ociServiceTypeName);
-        String body = toBody(ociServiceTypeName, generatedOciActivatorTypeName);
-        Filer filer = proccessingEnv.getFiler();
+
+        TypeName generatedOciServiceClientTypeName = toGeneratedServiceClientTypeName(ociServiceTypeName);
+        String serviceClientBody = toBody(TAG_TEMPLATE_SERVICE_CLIENT_PROVIDER_NAME,
+                                          ociServiceTypeName,
+                                          generatedOciServiceClientTypeName);
+        codegen(generatedOciServiceClientTypeName, serviceClientBody, processingEnv);
+
+        TypeName generatedOciServiceClientBuilderTypeName = toGeneratedServiceClientBuilderTypeName(ociServiceTypeName);
+        String serviceClientBuilderBody = toBody(TAG_TEMPLATE_SERVICE_CLIENT_BUILDER_PROVIDER_NAME,
+                                          ociServiceTypeName,
+                                          generatedOciServiceClientTypeName);
+        codegen(generatedOciServiceClientBuilderTypeName, serviceClientBuilderBody, processingEnv);
+    }
+
+    private void codegen(TypeName typeName,
+                         String body,
+                         ProcessingEnvironment processingEnv) {
+        Filer filer = processingEnv.getFiler();
         try {
-            JavaFileObject javaSrc = filer.createSourceFile(generatedOciActivatorTypeName.name());
+            JavaFileObject javaSrc = filer.createSourceFile(typeName.name());
             try (Writer os = javaSrc.openWriter()) {
                 os.write(body);
             }
         } catch (FilerException x) {
-            proccessingEnv.getMessager().printWarning("Failed to write java file: " + x);
+            processingEnv.getMessager().printWarning("Failed to write java file: " + x);
         } catch (Exception x) {
             System.getLogger(getClass().getName()).log(System.Logger.Level.ERROR, "Failed to write java file: " + x, x);
-            proccessingEnv.getMessager().printError("Failed to write java file: " + x);
+            processingEnv.getMessager().printError("Failed to write java file: " + x);
         }
     }
 
-    private String toBody(TypeName ociServiceTypeName,
+    static String toBody(String templateName,
+                          TypeName ociServiceTypeName,
                           TypeName generatedOciActivatorTypeName) {
         TemplateHelper templateHelper = TemplateHelper.create();
-        String template = loadTemplate(TAG_TEMPLATE_SERVICE_PROVIDER_NAME);
+        String template = loadTemplate(templateName);
         Map<String, Object> subst = new HashMap<>();
         subst.put("classname", ociServiceTypeName.name());
         subst.put("simpleclassname", ociServiceTypeName.className());
         subst.put("packagename", generatedOciActivatorTypeName.packageName());
-        subst.put("generatedanno", templateHelper.generatedStickerFor(getClass().getName()));
+        subst.put("generatedanno", templateHelper.generatedStickerFor(PicoAnnotationProcessorObserver.class.getName()));
+        subst.put("dot", maybeDot(ociServiceTypeName));
+        subst.put("usesRegion", usesRegion(ociServiceTypeName));
         return templateHelper.applySubstitutions(template, subst, true).trim();
     }
 
-    private static String loadTemplate(String name) {
+    static String maybeDot(TypeName ociServiceTypeName) {
+        return NO_DOT_EXCEPTIONS.contains(ociServiceTypeName.name()) ? "" : ".";
+    }
+
+    static boolean usesRegion(TypeName ociServiceTypeName) {
+        // it turns out that the same exceptions used for dotting the builder also applies to whether it uses region
+        return !NO_DOT_EXCEPTIONS.contains(ociServiceTypeName.name());
+    }
+
+    static String loadTemplate(String name) {
         String path = "templates/pico/oci/default/" + name;
         try {
             InputStream in = PicoProcessorObserverForOCI.class.getClassLoader().getResourceAsStream(path);
@@ -138,20 +176,16 @@ public class PicoProcessorObserverForOCI implements PicoAnnotationProcessorObser
 
     static boolean shouldProcess(TypedElementInfo element,
                                  ProcessingEnvironment processingEnv) {
-        if (GeneralProcessorUtils.findFirst(Inject.class, element.annotations()).isEmpty()) {
+        if (findFirst(Inject.class, element.annotations()).isEmpty()) {
             return false;
         }
 
         if (TypeInfo.KIND_FIELD.equalsIgnoreCase(element.elementTypeKind())) {
-            if (!shouldProcess(element.typeName(), processingEnv)) {
-                return false;
-            }
+            return shouldProcess(element.typeName(), processingEnv);
         } else if (TypeInfo.KIND_METHOD.equalsIgnoreCase(element.elementTypeKind())
                 || TypeInfo.KIND_CONSTRUCTOR.equalsIgnoreCase(element.elementTypeKind())) {
-            if (element.parameterArguments().stream()
-                    .anyMatch(it -> shouldProcess(it.typeName(), processingEnv))) {
-                return true;
-            }
+            return element.parameterArguments().stream()
+                    .anyMatch(it -> shouldProcess(it.typeName(), processingEnv));
         }
 
         return false;
@@ -159,24 +193,35 @@ public class PicoProcessorObserverForOCI implements PicoAnnotationProcessorObser
 
     static boolean shouldProcess(TypeName typeName,
                                  ProcessingEnvironment processingEnv) {
-        if (GeneralProcessorUtils.isProviderType(typeName) && typeName.typeArguments().size() > 0) {
+        if ((typeName.typeArguments().size() > 0)
+                && (isProviderType(typeName) || typeName.isOptional())) {
             typeName = typeName.typeArguments().get(0);
         }
 
-        if (!typeName.name().startsWith(OCI_ROOT_PACKAGE_NAME_PREFIX)
-                || TYPENAME_EXCEPTIONS.contains(typeName.name())
-                || typeName.name().endsWith("$$Pico$$Provider")) {
+        String name = typeName.name();;
+        if (!name.startsWith(OCI_ROOT_PACKAGE_NAME_PREFIX)
+                || TYPENAME_EXCEPTIONS.contains(name)
+                || name.endsWith(".Builder")
+                || name.endsWith("Client")
+                || name.endsWith("ClientBuilder")) {
             return false;
         }
 
-        // check to see if we already generated it before
-        TypeElement typeElement = processingEnv.getElementUtils().getTypeElement(toGeneratedTypeName(typeName).name());
+        // check to see if we already generated it before, and if so we can skip creating it again
+        String generatedTypeName = toGeneratedServiceClientTypeName(typeName).name();
+        TypeElement typeElement = processingEnv.getElementUtils()
+                .getTypeElement(generatedTypeName);
         return (typeElement == null);
     }
 
-    private static TypeName toGeneratedTypeName(TypeName typeName) {
+    private static TypeName toGeneratedServiceClientTypeName(TypeName typeName) {
         return create(GENERATED_PREFIX + typeName.packageName(),
-                      typeName.className() + "$$Pico$$Provider");
+                      typeName.className() + GENERATED_CLIENT_SUFFIX);
+    }
+
+    private static TypeName toGeneratedServiceClientBuilderTypeName(TypeName typeName) {
+        return create(GENERATED_PREFIX + typeName.packageName(),
+                      typeName.className() + GENERATED_CLIENT_BUILDER_SUFFIX);
     }
 
 }
