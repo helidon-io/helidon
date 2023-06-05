1ccdee20f952c15dee494ed3bfadb60416ef7207
tim.quinn@oracle.com <tim.quinn@oracle.com>
2023-06-05 08:32:17 -0500

    Fix LRA TCK test failures due to Jandex API change

--- microprofile/lra/jax-rs/src/main/java/io/helidon/microprofile/lra/ParticipantValidationModel.java
+++ microprofile/lra/jax-rs/src/main/java/io/helidon/microprofile/lra/ParticipantValidationModel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -154,8 +154,8 @@ class ParticipantValidationModel {
         }
 
         String nameWithParams() {
-            return methodInfo.name() + methodInfo.parameters().stream()
-                    .map(MethodParameterInfo::name)
+            return methodInfo.name() + methodInfo.parameterTypes().stream()
+                    .map(pt -> pt.name().toString())
                     .collect(Collectors.joining());
         }
 
