4e75db0a0640f395732dd66686c1267ee8ee5f0c
Joe DiPol <joe.dipol@oracle.com>
2023-06-06 08:10:55 -0700

    3.x: Upgrade graphql to 18.5 and scalars to 18.3 (#6940)
    * Upgrade graphql-java to 18.5 and scalars to 18.3

--- microprofile/graphql/server/src/main/java/module-info.java
+++ microprofile/graphql/server/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ module io.helidon.microprofile.graphql.server {
     requires io.helidon.microprofile.server;
 
     requires com.graphqljava;
-    requires graphql.java.extended.scalars;
+    requires com.graphqljava.extendedscalars;
     requires microprofile.graphql.api;
     requires microprofile.config.api;
 
