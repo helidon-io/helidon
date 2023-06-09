6e62a0358ce2bbab458029e1da988cebdd41340c
Tim Middleton <tim.middleton@oracle.com>
2023-06-10 04:11:40 +0800

    Upgrade graphql-java to 18.x (#6965)

--- microprofile/graphql/server/src/main/java/module-info.java
+++ microprofile/graphql/server/src/main/java/module-info.java
@@ -47,7 +47,7 @@ module io.helidon.microprofile.graphql.server {
     requires io.helidon.microprofile.server;
 
     requires com.graphqljava;
-    requires graphql.java.extended.scalars;
+    requires com.graphqljava.extendedscalars;
     requires microprofile.graphql.api;
     requires microprofile.config.api;
 
