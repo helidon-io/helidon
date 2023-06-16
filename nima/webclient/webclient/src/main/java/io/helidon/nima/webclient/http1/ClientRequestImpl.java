a3d6fa18afb54f4cd98b6c2f597f56423e99d754
tvallin <thibault.vallin@oracle.com>
2023-06-16 10:51:06 +0200

    Use current context
    Signed-off-by: tvallin <thibault.vallin@oracle.com>

--- nima/webclient/webclient/src/main/java/io/helidon/nima/webclient/http1/ClientRequestImpl.java
+++ nima/webclient/webclient/src/main/java/io/helidon/nima/webclient/http1/ClientRequestImpl.java
@@ -27,6 +27,7 @@ import java.util.function.Function;
 
 import io.helidon.common.buffers.BufferData;
 import io.helidon.common.context.Context;
+import io.helidon.common.context.Contexts;
 import io.helidon.common.http.ClientRequestHeaders;
 import io.helidon.common.http.Http;
 import io.helidon.common.http.Http.HeaderValue;
@@ -332,7 +333,7 @@ class ClientRequestImpl implements Http1ClientRequest {
                                                                         query,
                                                                         UriFragment.empty(),
                                                                         headers,
-                                                                        Context.create(),
+                                                                        Contexts.context().orElse(Context.create()),
                                                                         requestId,
                                                                         whenComplete,
                                                                         whenSent,
