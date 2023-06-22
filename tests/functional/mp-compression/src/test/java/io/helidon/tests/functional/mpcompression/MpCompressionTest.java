8aa79da6f0d79c40606cc4af9723e4383a9a3b6e
Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
2023-06-22 14:07:49 -0400

    Removed default encoding test.
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

--- tests/functional/mp-compression/src/test/java/io/helidon/tests/functional/mpcompression/MpCompressionTest.java
+++ tests/functional/mp-compression/src/test/java/io/helidon/tests/functional/mpcompression/MpCompressionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,16 +42,9 @@ class MpCompressionTest {
     }
 
     @Test
-    void testDeflate() throws Exception {
+    void testDeflate() {
         Response response = target.request().header("accept-encoding", "deflate").get();
         assertOk(response, "Hello World: deflate");
-    }
-
-    @Test
-    void testDefault() {
-        Response response = target.request().get();
-        // client default is gzip
-        assertOk(response, "Hello World: gzip");
     }
 
     private void assertOk(Response response, String expectedMessage) {
