e8b12987afaeae5c8c44b3760903f25241ef386a
Romain Grecourt <romain.grecourt@oracle.com>
2023-06-27 12:57:08 -0700

    Fix post merge compile error in examples.micrometer.se.MainTest (#7115)

--- examples/integrations/micrometer/se/src/test/java/io/helidon/examples/micrometer/se/MainTest.java
+++ examples/integrations/micrometer/se/src/test/java/io/helidon/examples/micrometer/se/MainTest.java
@@ -23,8 +23,7 @@ import io.helidon.nima.testing.junit5.webserver.ServerTest;
 import io.helidon.nima.testing.junit5.webserver.SetUpServer;
 import io.helidon.nima.webclient.http1.Http1Client;
 import io.helidon.nima.webclient.http1.Http1ClientResponse;
-import io.helidon.nima.webserver.ServerConfig;
-import io.helidon.nima.webserver.ServerConfig.Builder;
+import io.helidon.nima.webserver.WebServerConfig.Builder;
 
 import jakarta.json.Json;
 import jakarta.json.JsonBuilderFactory;
