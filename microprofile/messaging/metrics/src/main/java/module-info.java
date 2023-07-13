746535c711117288282f70a19e61bfcf9ca2e56a
Dmitry Aleksandrov <dmitry.aleksandrov@oracle.com>
2023-07-13 13:43:41 +0300

    MP Messaging Metrics modules fix.
    Signed-off-by: Dmitry Aleksandrov <dmitry.aleksandrov@oracle.com>

--- microprofile/messaging/metrics/src/main/java/module-info.java
+++ microprofile/messaging/metrics/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,8 +20,8 @@
 module io.helidon.microprofile.messaging.metrics {
     requires java.logging;
 
-    requires static jakarta.cdi;
-    requires static jakarta.inject;
-    requires io.helidon.microprofile.messaging;
-    requires io.helidon.microprofile.metrics;
+    requires jakarta.cdi;
+    requires jakarta.inject;
+    requires transitive io.helidon.microprofile.messaging;
+    requires transitive io.helidon.microprofile.metrics;
 }
