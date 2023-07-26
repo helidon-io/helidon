605488c99fb08f44337a46db2c80f118675747ae
Joe Di Pol <joe.dipol@oracle.com>
2023-07-26 09:51:21 -0700

    Work-around modularity issues with okio (https://github.com/square/okio/issues/1306)

--- tracing/jaeger/src/main/java/module-info.java
+++ tracing/jaeger/src/main/java/module-info.java
@@ -25,6 +25,12 @@ module io.helidon.tracing.jaeger {
     requires io.helidon.common.context;
     requires io.helidon.common.configurable;
 
+    // Hack because okhttp and okio are not modularized
+    // but kotlin.stdlib is, and therefore kotlin.stdlib
+    // will be missing from module graph unless some module
+    // requires it.
+    requires kotlin.stdlib;
+
     requires static io.helidon.config.metadata;
 
     requires java.logging;
