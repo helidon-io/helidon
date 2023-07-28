70707889f95687d8dd6fd153930be814dcf61f73
Joe DiPol <joe.dipol@oracle.com>
2023-07-28 08:33:18 -0700

    2.x: upgrade okio to 3.4.0 (#7259)
    * Upgrade okio to 3.4.0 and work-around modularity issues with okio

--- tracing/jaeger/src/main/java/module-info.java
+++ tracing/jaeger/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,12 @@ module io.helidon.tracing.jaeger {
     requires io.helidon.tracing;
     requires io.helidon.common.context;
     requires static io.helidon.config.metadata;
+
+    // Hack because okhttp and okio are not modularized
+    // but kotlin.stdlib is, and therefore kotlin.stdlib
+    // will be missing from module graph unless some module
+    // requires it.
+    requires kotlin.stdlib;
 
     requires java.logging;
     requires io.opentracing.util;
