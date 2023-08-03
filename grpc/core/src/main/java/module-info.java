8463a20edad0b30699f1b71a552cc828a307cf17
Joe DiPol <joe.dipol@oracle.com>
2023-08-03 07:56:44 -0700

    2.x: Upgrade grpc-java to 1.57.1 and remove repackaging of io.grpc (#7300)
    * Upgrade grpc-java to 1.57.1 and no longer repackage
    * Use grpc-bom to manage version

--- grpc/core/src/main/java/module-info.java
+++ grpc/core/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2020 Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,9 +33,9 @@ module io.helidon.grpc.core {
 
     requires grpc.netty;
     requires transitive grpc.protobuf;
+    requires transitive grpc.api;
     requires grpc.protobuf.lite;
     requires transitive grpc.stub;
-    requires transitive io.grpc;
     requires io.netty.handler;
     requires io.netty.transport;
     requires transitive com.google.protobuf;
