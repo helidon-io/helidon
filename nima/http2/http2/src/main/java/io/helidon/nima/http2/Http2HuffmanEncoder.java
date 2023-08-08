8b2cc2485d31522873bc708f80f15dab424b7398
Tomas Langer <tomas.langer@oracle.com>
2023-08-08 13:50:45 +0200

    Javadoc fix.

--- nima/http2/http2/src/main/java/io/helidon/nima/http2/Http2HuffmanEncoder.java
+++ nima/http2/http2/src/main/java/io/helidon/nima/http2/Http2HuffmanEncoder.java
@@ -37,7 +37,7 @@ package io.helidon.nima.http2;
 import io.helidon.common.buffers.BufferData;
 
 /**
- * Implementation of HPack Huffman decoding and encoding.
+ * Implementation of HPack Huffman decoding.
  */
 public class Http2HuffmanEncoder {
     private static final int HUFFMAN_ENCODED = 1 << 7;
