2b7467d4ac2bb15a1a680ee30a42f793725ea914
tim.quinn@oracle.com <tim.quinn@oracle.com>
2023-08-14 14:47:43 -0500

    Further gauge tests

---
+++ metrics/providers/micrometer/src/test/java/io/helidon/metrics/micrometer/TestGauge.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2023 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.metrics.micrometer;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import io.helidon.metrics.api.Counter;
+import io.helidon.metrics.api.Gauge;
+import io.helidon.metrics.api.MeterRegistry;
+import io.helidon.metrics.api.Metrics;
+import io.helidon.metrics.api.MetricsConfig;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class TestGauge {
+
+    private static MeterRegistry meterRegistry;
+
+    @BeforeAll
+    static void prep() {
+        meterRegistry = Metrics.createMeterRegistry(MetricsConfig.create());
+    }
+
+    @Test
+    void testUnwrap() {
+        long initialValue = 4L;
+        long incr = 2L;
+        AtomicLong value = new AtomicLong(initialValue);
+        Gauge g = meterRegistry.getOrCreate(Gauge.builder("a",
+                                                          value,
+                                                          v -> (double)v.get()));
+
+        io.micrometer.core.instrument.Gauge mGauge = g.unwrap(io.micrometer.core.instrument.Gauge.class);
+        assertThat("Initial value", mGauge.value(), is((double) initialValue));
+        value.addAndGet(incr);
+        assertThat("Updated value", mGauge.value(), is((double) initialValue + incr));
+    }
+}
