d251a3cfdbb98deb940335294c758d354b6d323c
Dmitry Aleksandrov <dmitry.aleksandrov@oracle.com>
2023-08-24 09:34:12 +0300

    Set Enum in Method
    Signed-off-by: Dmitry Aleksandrov <dmitry.aleksandrov@oracle.com>

--- tracing/providers/jaeger/src/main/java/io/helidon/tracing/providers/jaeger/JaegerTracerBuilder.java
+++ tracing/providers/jaeger/src/main/java/io/helidon/tracing/providers/jaeger/JaegerTracerBuilder.java
@@ -305,8 +305,8 @@ public class JaegerTracerBuilder implements TracerBuilder<JaegerTracerBuilder> {
 
         config.get("global").asBoolean().ifPresent(this::registerGlobal);
 
-        config.get("span-processor-type").asString().ifPresent(this::spanProcessorType);
-        config.get("exporter-timeout").asLong().ifPresent(it -> exporterTimeout(Duration.ofMillis(it)));
+        config.get("span-processor-type").asString()
+                .ifPresent(it -> spanProcessorType(SpanProcessorType.valueOf(it.toUpperCase())));        config.get("exporter-timeout").asLong().ifPresent(it -> exporterTimeout(Duration.ofMillis(it)));
         config.get("schedule-delay").asInt().ifPresent(it -> scheduleDelay(Duration.ofMillis(it)));
         config.get("max-queue-size").asInt().ifPresent(this::maxQueueSize);
         config.get("max-export-batch-size").asInt().ifPresent(this::maxExportBatchSize);
@@ -357,8 +357,8 @@ public class JaegerTracerBuilder implements TracerBuilder<JaegerTracerBuilder> {
      * @return updated builder
      */
     @ConfiguredOption(key = "span-processor-type", value = "batch")
-    public JaegerTracerBuilder spanProcessorType(String spanProcessorType) {
-        this.spanProcessorType = SpanProcessorType.valueOf(spanProcessorType.toUpperCase());
+    public JaegerTracerBuilder spanProcessorType(SpanProcessorType spanProcessorType) {
+        this.spanProcessorType = spanProcessorType;
         return this;
     }
 
