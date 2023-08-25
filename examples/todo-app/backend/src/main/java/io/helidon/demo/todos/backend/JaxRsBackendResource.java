944aa606efc8dad6b06e0e20b1924578460fdc1c
Romain Grecourt <romain.grecourt@oracle.com>
2023-08-25 10:10:58 -0700

    [2.x] Refactor TODO app examples to not use NodeJS/NPM
    - Refactor UI to use JQuery + MustacheJS + Material icons (single .css file & single .js file)
    - Refactor frontend service to use WebClient instead of JAXRS Client
    - Simplify BackendServiceClient
    - Update TodoServiceTest to write the backend mockup using WebServer instead of JAXRS
    - Removed Docker and k8s to simplify the running steps
    - Updated README.md
    
    Fixes #7462
    
    * Fix checkstyle error
    
    * Update app.js

--- examples/todo-app/backend/src/main/java/io/helidon/demo/todos/backend/JaxRsBackendResource.java
+++ examples/todo-app/backend/src/main/java/io/helidon/demo/todos/backend/JaxRsBackendResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,7 +34,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -44,7 +43,8 @@ import io.helidon.security.Subject;
 import io.helidon.security.annotations.Authenticated;
 import io.helidon.security.annotations.Authorized;
 
-import io.opentracing.Span;
+import io.opentracing.Tracer;
+import org.eclipse.microprofile.opentracing.Traced;
 
 /**
  * The TODO backend REST service.
@@ -57,44 +57,34 @@ public class JaxRsBackendResource {
 
     private static final JsonBuilderFactory JSON = Json.createBuilderFactory(Collections.emptyMap());
 
-    /**
-     * The database service facade.
-     */
     private final DbService backendService;
+    private final Tracer tracer;
 
     /**
      * Create new {@code JaxRsBackendResource} instance.
      * @param dbs the database service facade to use
+     * @param tracer tracer to use
      */
     @Inject
-    public JaxRsBackendResource(final DbService dbs) {
+    public JaxRsBackendResource(DbService dbs, Tracer tracer) {
         this.backendService = dbs;
+        this.tracer = tracer;
     }
 
     /**
      * Retrieve all TODO entries.
      *
      * @param context security context to map the user
-     * @param headers HTTP headers
      * @return the response with the retrieved entries as entity
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response list(@Context final SecurityContext context,
-                         @Context final HttpHeaders headers) {
-
-        Span span = context.tracer().buildSpan("jaxrs:list")
-                .asChildOf(context.tracingSpan())
-                .start();
-
+    @Traced(operationName = "jaxrs:list")
+    public Response list(@Context SecurityContext context) {
         JsonArrayBuilder builder = JSON.createArrayBuilder();
-        backendService.list(context.tracingSpan(), getUserId(context))
+        backendService.list(tracer.activeSpan().context(), getUserId(context))
                       .forEach(data -> builder.add(data.forRest()));
-
-        Response response = Response.ok(builder.build()).build();
-
-        span.finish();
-        return response;
+        return Response.ok(builder.build()).build();
     }
 
     /**
@@ -106,11 +96,10 @@ public class JaxRsBackendResource {
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response get(@PathParam("id") final String id,
-                        @Context final SecurityContext context) {
+    public Response get(@PathParam("id") String id, @Context SecurityContext context) {
 
         return backendService
-                .get(context.tracingSpan(), id, getUserId(context))
+                .get(tracer.activeSpan().context(), id, getUserId(context))
                 .map(Todo::forRest)
                 .map(Response::ok)
                 .orElse(Response.status(Response.Status.NOT_FOUND))
@@ -126,11 +115,10 @@ public class JaxRsBackendResource {
     @DELETE
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response delete(@PathParam("id") final String id,
-                           @Context final SecurityContext context) {
+    public Response delete(@PathParam("id") String id, @Context SecurityContext context) {
 
         return backendService
-                .delete(context.tracingSpan(), id, getUserId(context))
+                .delete(tracer.activeSpan().context(), id, getUserId(context))
                 .map(Todo::forRest)
                 .map(Response::ok)
                 .orElse(Response.status(Response.Status.NOT_FOUND))
@@ -146,14 +134,13 @@ public class JaxRsBackendResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createIt(final JsonObject jsonObject,
-                             @Context final SecurityContext context) {
+    public Response createIt(JsonObject jsonObject, @Context SecurityContext context) {
 
         String newId = UUID.randomUUID().toString();
         String userId = getUserId(context);
         Todo newBackend = Todo.newTodoFromRest(jsonObject, userId, newId);
 
-        backendService.insert(context.tracingSpan(), newBackend);
+        backendService.insert(tracer.activeSpan().context(), newBackend);
 
         return Response.ok(newBackend.forRest()).build();
     }
@@ -169,13 +156,9 @@ public class JaxRsBackendResource {
     @Path("/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response update(@PathParam("id") final String id,
-                           final JsonObject jsonObject,
-                           final @Context SecurityContext context) {
-
+    public Response update(@PathParam("id") String id, JsonObject jsonObject, @Context SecurityContext context) {
         return backendService
-                .update(context.tracingSpan(),
-                        Todo.fromRest(jsonObject, getUserId(context), id))
+                .update(tracer.activeSpan().context(), Todo.fromRest(jsonObject, getUserId(context), id))
                 .map(Todo::forRest)
                 .map(Response::ok)
                 .orElse(Response.status(Response.Status.NOT_FOUND))
@@ -187,7 +170,7 @@ public class JaxRsBackendResource {
      * @param context the security context
      * @return user id found in the context or {@code <ANONYMOUS>} otherwise
      */
-    private String getUserId(final SecurityContext context) {
+    private String getUserId(SecurityContext context) {
         return context.user()
                 .map(Subject::principal)
                 .map(Principal::id)
