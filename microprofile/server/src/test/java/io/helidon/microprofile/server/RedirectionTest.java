00564f325360fbf2bcc69afb758c2f1098a835cf
Tomas Langer <tomas.langer@oracle.com>
2023-08-28 01:43:50 +0200

    Use javax instead of jakarta (backported...)
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

--- microprofile/server/src/test/java/io/helidon/microprofile/server/RedirectionTest.java
+++ microprofile/server/src/test/java/io/helidon/microprofile/server/RedirectionTest.java
@@ -23,15 +23,15 @@ import io.helidon.microprofile.tests.junit5.AddExtension;
 import io.helidon.microprofile.tests.junit5.DisableDiscovery;
 import io.helidon.microprofile.tests.junit5.HelidonTest;
 
-import jakarta.enterprise.inject.spi.CDI;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.Context;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.UriInfo;
+import javax.enterprise.inject.spi.CDI;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import org.glassfish.jersey.ext.cdi1x.internal.CdiComponentProvider;
 import org.junit.jupiter.api.Test;
 
