a33a1791e12ed9b11509aeeee85a2a58f771ae66
tvallin <thibault.vallin@oracle.com>
2023-08-29 11:18:34 +0200

    Update metrics filtering
    Signed-off-by: tvallin <thibault.vallin@oracle.com>

--- examples/metrics/filtering/mp/src/main/java/io/helidon/examples/metrics/filtering/mp/GreetResource.java
+++ examples/metrics/filtering/mp/src/main/java/io/helidon/examples/metrics/filtering/mp/GreetResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,13 +16,8 @@
 
 package io.helidon.examples.metrics.filtering.mp;
 
-import java.util.Collections;
-
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
-import jakarta.json.Json;
-import jakarta.json.JsonBuilderFactory;
-import jakarta.json.JsonObject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PUT;
@@ -43,8 +38,6 @@ public class GreetResource {
 
     static final String TIMER_FOR_GETS = "timerForGets";
     static final String COUNTER_FOR_PERSONALIZED_GREETINGS = "counterForPersonalizedGreetings";
-
-    private static final JsonBuilderFactory JSON = Json.createBuilderFactory(Collections.emptyMap());
 
     /**
      * The greeting message provider.
@@ -70,7 +63,7 @@ public class GreetResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Timed(name = TIMER_FOR_GETS, absolute = true)
-    public JsonObject getDefaultMessage() {
+    public GreetingMessage getDefaultMessage() {
         return createResponse("World");
     }
 
@@ -84,14 +77,14 @@ public class GreetResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Timed(name = TIMER_FOR_GETS, absolute = true)
-    public JsonObject getMessage(@PathParam("name") String name) {
+    public GreetingMessage getMessage(@PathParam("name") String name) {
         return createResponse(name);
     }
 
     /**
      * Set the greeting to use in future messages.
      *
-     * @param jsonObject JSON containing the new greeting
+     * @param message JSON containing the new greeting
      * @return {@link Response}
      */
     @Path("/greeting")
@@ -99,26 +92,19 @@ public class GreetResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Counted(name = COUNTER_FOR_PERSONALIZED_GREETINGS, absolute = true)
-    public Response updateGreeting(JsonObject jsonObject) {
-
-        if (!jsonObject.containsKey("greeting")) {
-            JsonObject entity = JSON.createObjectBuilder()
-                    .add("error", "No greeting provided")
-                    .build();
+    public Response updateGreeting(GreetingMessage message) {
+        if (message.getMessage() == null) {
+            GreetingMessage entity = new GreetingMessage("No greeting provided");
             return Response.status(Response.Status.BAD_REQUEST).entity(entity).build();
         }
 
-        String newGreeting = jsonObject.getString("greeting");
-
-        greetingProvider.setMessage(newGreeting);
+        greetingProvider.setMessage(message.getMessage());
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 
-    private JsonObject createResponse(String who) {
+    private GreetingMessage createResponse(String who) {
         String msg = String.format("%s %s!", greetingProvider.getMessage(), who);
 
-        return JSON.createObjectBuilder()
-                .add("message", msg)
-                .build();
+        return new GreetingMessage(msg);
     }
 }
