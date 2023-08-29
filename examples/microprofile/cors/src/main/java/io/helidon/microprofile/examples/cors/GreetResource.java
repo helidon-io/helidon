f996959c4b30fe5dbad5afb081db9df8a1cde198
tvallin <thibault.vallin@oracle.com>
2023-08-29 10:39:34 +0200

    Update microprofile-cors
    Signed-off-by: tvallin <thibault.vallin@oracle.com>

--- examples/microprofile/cors/src/main/java/io/helidon/microprofile/examples/cors/GreetResource.java
+++ examples/microprofile/cors/src/main/java/io/helidon/microprofile/examples/cors/GreetResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,15 +16,10 @@
 
 package io.helidon.microprofile.examples.cors;
 
-import java.util.Collections;
-
 import io.helidon.microprofile.cors.CrossOrigin;
 
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
-import jakarta.json.Json;
-import jakarta.json.JsonBuilderFactory;
-import jakarta.json.JsonObject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.HttpMethod;
@@ -49,8 +44,6 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 @RequestScoped
 public class GreetResource {
 
-    private static final JsonBuilderFactory JSON = Json.createBuilderFactory(Collections.emptyMap());
-
     /**
      * The greeting message provider.
      */
@@ -70,11 +63,11 @@ public class GreetResource {
     /**
      * Return a worldly greeting message.
      *
-     * @return {@link JsonObject}
+     * @return {@link GreetingMessage}
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public JsonObject getDefaultMessage() {
+    public GreetingMessage getDefaultMessage() {
         return createResponse("World");
     }
 
@@ -82,19 +75,19 @@ public class GreetResource {
      * Return a greeting message using the name that was provided.
      *
      * @param name the name to greet
-     * @return {@link JsonObject}
+     * @return {@link GreetingMessage}
      */
     @Path("/{name}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public JsonObject getMessage(@PathParam("name") String name) {
+    public GreetingMessage getMessage(@PathParam("name") String name) {
         return createResponse(name);
     }
 
     /**
      * Set the greeting to use in future messages.
      *
-     * @param jsonObject JSON containing the new greeting
+     * @param message {@link GreetingMessage} containing the new greeting
      * @return {@link Response}
      */
     @Path("/greeting")
@@ -109,18 +102,14 @@ public class GreetResource {
             @APIResponse(name = "normal", responseCode = "204", description = "Greeting updated"),
             @APIResponse(name = "missing 'greeting'", responseCode = "400",
                     description = "JSON did not contain setting for 'greeting'")})
-    public Response updateGreeting(JsonObject jsonObject) {
+    public Response updateGreeting(GreetingMessage message) {
 
-        if (!jsonObject.containsKey("greeting")) {
-            JsonObject entity = JSON.createObjectBuilder()
-                    .add("error", "No greeting provided")
-                    .build();
+        if (message.getMessage() == null) {
+            GreetingMessage entity = new GreetingMessage("No greeting provided");
             return Response.status(Response.Status.BAD_REQUEST).entity(entity).build();
         }
 
-        String newGreeting = jsonObject.getString("greeting");
-
-        greetingProvider.setMessage(newGreeting);
+        greetingProvider.setMessage(message.getMessage());
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 
@@ -155,11 +144,8 @@ public class GreetResource {
     public void optionsForRetrievingNamedGreeting() {
     }
 
-    private JsonObject createResponse(String who) {
+    private GreetingMessage createResponse(String who) {
         String msg = String.format("%s %s!", greetingProvider.getMessage(), who);
-
-        return JSON.createObjectBuilder()
-                .add("message", msg)
-                .build();
+        return new GreetingMessage(msg);
     }
 }
