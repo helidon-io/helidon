e0300c969cb4ac60f94d4d0f3583139c6e916f0b
Dmitry Aleksandrov <dmitry.aleksandrov@oracle.com>
2023-09-27 11:47:36 +0300

    Move Opentracing to Helidon Tracing API
    Signed-off-by: Dmitry Aleksandrov <dmitry.aleksandrov@oracle.com>

--- integrations/common/rest/src/main/java/module-info.java
+++ integrations/common/rest/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,8 +27,6 @@ module io.helidon.integrations.common.rest {
     requires java.logging;
     requires jakarta.json;
 
-    requires io.opentracing.api;
-
     requires io.helidon.common.http;
     requires io.helidon.common;
     requires io.helidon.common.reactive;
@@ -36,6 +34,7 @@ module io.helidon.integrations.common.rest {
     requires io.helidon.config;
     requires io.helidon.webclient;
     requires io.helidon.media.jsonp;
+    requires io.helidon.tracing;
 
     exports io.helidon.integrations.common.rest;
 }
