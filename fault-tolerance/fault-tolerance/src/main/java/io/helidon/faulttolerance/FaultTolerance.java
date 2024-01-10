c8af7b9b30b5c23ba62c6c04014468e9d377f8e5
Tomas Langer <tomas.langer@oracle.com>
2024-01-10 18:09:08 +0100

    Remove annotations that will be re-introduced as part of Helidon declarative.
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

--- fault-tolerance/fault-tolerance/src/main/java/io/helidon/faulttolerance/FaultTolerance.java
+++ fault-tolerance/fault-tolerance/src/main/java/io/helidon/faulttolerance/FaultTolerance.java
@@ -16,12 +16,6 @@
 
 package io.helidon.faulttolerance;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.time.temporal.ChronoUnit;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -32,7 +26,6 @@ import java.util.function.Supplier;
 
 import io.helidon.common.LazyValue;
 import io.helidon.config.Config;
-import io.helidon.inject.service.Interception;
 
 import static java.lang.System.Logger.Level.ERROR;
 
@@ -62,8 +55,8 @@ public final class FaultTolerance {
 
     static {
         EXECUTOR.set(LazyValue.create(() -> Executors.newThreadPerTaskExecutor(Thread.ofVirtual()
-                                                          .name("helidon-ft-", 0)
-                                                          .factory())));
+                                                                                       .name("helidon-ft-", 0)
+                                                                                       .factory())));
     }
 
     private FaultTolerance() {
@@ -111,7 +104,7 @@ public final class FaultTolerance {
      * executing. Simulates a scheduled executor when using VTs.
      *
      * @param runnable the runnable
-     * @param millis the time to sleep
+     * @param millis   the time to sleep
      * @return the new runnable
      */
     public static Runnable toDelayedRunnable(Runnable runnable, long millis) {
@@ -131,9 +124,9 @@ public final class FaultTolerance {
      * executing. Simulates a scheduled executor when using VTs.
      *
      * @param callable the callable
-     * @param millis the time to sleep
+     * @param millis   the time to sleep
+     * @param <T>      type of value returned
      * @return the new callable
-     * @param <T> type of value returned
      */
     public static <T> Callable<T> toDelayedCallable(Callable<T> callable, long millis) {
         return () -> {
@@ -366,309 +359,5 @@ public final class FaultTolerance {
                 return next.get();
             }
         }
-    }
-
-    /**
-     * The annotated method (or all methods on annotated type) will be retried according to the configuration.
-     */
-    @Retention(RetentionPolicy.CLASS)
-    @Documented
-    @Interception.Trigger
-    public @interface Retry {
-        /**
-         * If named, an attempt will be made to find a named {@link io.helidon.faulttolerance.Retry} instance in service
-         * registry. If not found, a new retry will be created based on values on this annotation.
-         *
-         * @return name of this retry
-         */
-        String name() default "";
-
-        /**
-         * Number of calls (first try + retries).
-         *
-         * @return number of desired calls, must be 1 (means no retries) or higher.
-         */
-        int calls() default 3;
-
-        /**
-         * Amount to delay, unit is specified by {@link #timeUnit()} and defaults to milliseconds.
-         * Defaults to {@code 200}. Make sure you change this when changing {@link #timeUnit()}.
-         *
-         * @return amount of units to delay between retries (combines with retry policy)
-         */
-        long delayTime() default 200;
-
-        /**
-         * Units used for specification of time for this retry.
-         * If changed, make sure you accordingly modify all times on this retry.
-         *
-         * @return chrono unit that defines the amounts of time on this retry
-         * @see #delayTime()
-         * @see #jitterTime()
-         */
-        ChronoUnit timeUnit() default ChronoUnit.MILLIS;
-
-        /**
-         * Delay retry policy factor. If unspecified (value of {@code -1}), Jitter retry policy would be used, unless
-         * jitter time is also unspecified.
-         * <p>
-         * Default when {@link io.helidon.faulttolerance.Retry.DelayingRetryPolicy} is used is {@code 2}.
-         *
-         * @return delay factor for delaying retry policy
-         */
-        double delayFactor() default -1;
-
-        /**
-         * Jitter for {@link io.helidon.faulttolerance.Retry.JitterRetryPolicy}. If unspecified (value of {@code -1}),
-         * delaying retry policy is used. If both this value, and {@link #delayFactor()} are specified, delaying retry policy
-         * would be used.
-         *
-         * @return jitter, using {@link #timeUnit()}
-         */
-        long jitterTime() default -1;
-
-        /**
-         * Amount of overall timeout, unit is specified by {@link #timeUnit()} and defaults to milliseconds.
-         * Defaults to {@code 1000}. Make sure you change this when changing {@link #timeUnit()}.
-         *
-         * @return amount of units to measure overall timeout of all retries
-         */
-        long overallTimeout() default 1000;
-
-        /**
-         * These throwables will be considered retriable.
-         *
-         * @return throwable classes to trigger retries
-         * @see #skipOn()
-         */
-        Class<? extends Throwable>[] applyOn() default {};
-
-        /**
-         * These throwables will not be considered retriable, all other will.
-         *
-         * @return throwable classes to skip retries
-         * @see #applyOn()
-         */
-        Class<? extends Throwable>[] skipOn() default {};
-    }
-
-    /**
-     * The annotated method will fallback to the defined method on failure.
-     * <p>
-     * The fallback method must have the same signature (types and number of parameters), or have one additional parameter of
-     * type {@code Throwable} to receive the last exception thrown.
-     * <p>
-     * Fault tolerance will add all intermediate exceptions as {@link Throwable#addSuppressed(Throwable)}.
-     */
-    @Retention(RetentionPolicy.CLASS)
-    @Documented
-    @Interception.Trigger
-    @Target(ElementType.METHOD)
-    public @interface Fallback {
-        /**
-         * Name of the method to fallback to.
-         * The method must follow the signature rules defined on this class.
-         *
-         * @return method name on the same instance (can also be a static method)
-         */
-        String value();
-
-        /**
-         * List of exception types that this fallback should be executed on.
-         *
-         * @return throwables that trigger fallback
-         */
-        Class<? extends Throwable>[] applyOn() default {Throwable.class};
-
-        /**
-         * List of exceptions that will not execute a fallback. For these exceptions, the throwable will be propagated
-         * to the caller.
-         *
-         * @return throwables that are re-thrown
-         */
-        Class<? extends Throwable>[] skipOn() default {};
-    }
-
-    /**
-     * Runs the annotated method asynchronously.
-     */
-    @Retention(RetentionPolicy.CLASS)
-    @Documented
-    @Interception.Trigger
-    @Target(ElementType.METHOD)
-    public @interface Async {
-        /**
-         * If named, an attempt will be made to find a named {@link io.helidon.faulttolerance.Async} instance in service
-         * registry. If not found, a new async will be created based on values on this annotation.
-         *
-         * @return name of this async
-         */
-        String name() default "";
-
-        /**
-         * Name of an executor service to use. An attempt will be done to discover the executor service in service registry.
-         * If none found, uses the default fault tolerance executor service.
-         *
-         * @return name of the executor service to use
-         */
-        String executorName() default "";
-    }
-
-    /**
-     * The annotated method (or all methods on annotated type) will time out according to the configuration.
-     */
-    @Retention(RetentionPolicy.CLASS)
-    @Documented
-    @Interception.Trigger
-    public @interface Timeout {
-        /**
-         * If named, an attempt will be made to find a named {@link io.helidon.faulttolerance.Timeout} instance in service
-         * registry. If not found, a new timeout will be created based on values on this annotation.
-         *
-         * @return name of this timeout
-         */
-        String name() default "";
-
-        /**
-         * Amount of time for timeout, unit is specified by {@link #timeUnit()} and defaults to seconds.
-         * Defaults to {@code 10}. Make sure you change this when changing {@link #timeUnit()}.
-         *
-         * @return amount of units to timeout
-         */
-        long time() default 10;
-
-        /**
-         * Units used for specification of time for this timeout.
-         * If changed, make sure you accordingly modify all times on this timeout.
-         *
-         * @return chrono unit that defines the amounts of time on this timeout
-         * @see #time()
-         */
-        ChronoUnit timeUnit() default ChronoUnit.SECONDS;
-
-        /**
-         * Flag to indicate that code must be executed in current thread instead
-         * of in an executor's thread. This flag is {@code false} by default.
-         *
-         * @return whether to run on current thread ({@code true}), or in an executor's thread ({@code false}, default)
-         */
-        boolean currentThread() default false;
-    }
-
-    /**
-     * The annotated method will fallback to the defined method on failure.
-     * <p>
-     * The fallback method must have the same signature (types and number of parameters), or have one additional parameter of
-     * type {@code Throwable} to receive the last exception thrown.
-     * <p>
-     * Fault tolerance will add all intermediate exceptions as {@link Throwable#addSuppressed(Throwable)}.
-     */
-    @Retention(RetentionPolicy.CLASS)
-    @Documented
-    @Interception.Trigger
-    @Target(ElementType.METHOD)
-    public @interface Bulkhead {
-        /**
-         * Maximal number of parallel requests going through this bulkhead.
-         * When the limit is reached, additional requests are enqueued.
-         *
-         * @return maximal number of parallel calls, defaults is {@value BulkheadConfigBlueprint#DEFAULT_LIMIT}
-         */
-        int limit() default BulkheadConfigBlueprint.DEFAULT_LIMIT;
-
-        /**
-         * Maximal number of enqueued requests waiting for processing.
-         * When the limit is reached, additional attempts to invoke
-         * a request will receive a {@link BulkheadException}.
-         *
-         * @return length of the queue
-         */
-        int queueLength() default BulkheadConfigBlueprint.DEFAULT_QUEUE_LENGTH;
-
-        /**
-         * Name for debugging, error reporting, monitoring.
-         *
-         * @return name of this bulkhead
-         */
-        String name() default "";
-    }
-
-    /**
-     * The annotated method (or all methods on annotated type) will have circuit breaker according to the configuration.
-     */
-    @Retention(RetentionPolicy.CLASS)
-    @Documented
-    @Interception.Trigger
-    public @interface CircuitBreaker {
-        /**
-         * If named, an attempt will be made to find a named {@link io.helidon.faulttolerance.CircuitBreaker} instance
-         * in service registry. If not found, a new circuit breaker will be created based on values on this annotation.
-         *
-         * @return name of this retry
-         */
-        String name() default "";
-
-        /**
-         * Units used for specification of time for this circuit breaker.
-         * If changed, make sure you accordingly modify all times on this annotation.
-         *
-         * @return chrono unit that defines the amounts of time on this circuit breaker, defaults to seconds
-         * @see #delayTime()
-         */
-        ChronoUnit timeUnit() default ChronoUnit.SECONDS;
-
-        /**
-         * Amount to delay, unit is specified by {@link #timeUnit()} and defaults to milliseconds.
-         * Defaults to {@code 5} seconds. Make sure you change this when changing {@link #timeUnit()}.
-         *
-         * @return amount of units to wait before transitioning from open to half-open state
-         */
-        long delayTime() default 5;
-
-        /**
-         * How many failures out of 100 will trigger the circuit to open.
-         * This is adapted to the {@link #volume()} used to handle the window of requests.
-         * <p>If errorRatio is 40, and volume is 10, 4 failed requests will open the circuit.
-         * Default is {@value CircuitBreakerConfigBlueprint#DEFAULT_ERROR_RATIO}.
-         *
-         * @return percent of failure that trigger the circuit to open
-         * @see #volume()
-         */
-        int errorRatio() default CircuitBreakerConfigBlueprint.DEFAULT_ERROR_RATIO;
-
-        /**
-         * Rolling window size used to calculate ratio of failed requests.
-         * Default is {@value CircuitBreakerConfigBlueprint#DEFAULT_VOLUME}.
-         *
-         * @return how big a window is used to calculate error errorRatio
-         * @see #errorRatio()
-         */
-        int volume() default CircuitBreakerConfigBlueprint.DEFAULT_VOLUME;
-
-        /**
-         * How many successful calls will close a half-open circuit.
-         * Nevertheless, the first failed call will open the circuit again.
-         * Default is {@value CircuitBreakerConfigBlueprint#DEFAULT_SUCCESS_THRESHOLD}.
-         *
-         * @return number of calls
-         */
-        int successThreshold() default CircuitBreakerConfigBlueprint.DEFAULT_SUCCESS_THRESHOLD;
-
-        /**
-         * These throwables will not be considered failures, all other will.
-         *
-         * @return throwable classes to not be considered a failure
-         * @see #applyOn()
-         */
-        Class<? extends Throwable>[] skipOn() default {};
-
-        /**
-         * These throwables will be considered failures.
-         *
-         * @return throwable classes to be considered a failure
-         * @see #skipOn()
-         */
-        Class<? extends Throwable>[] applyOn() default {};
-
     }
 }
--- http/http/src/main/java/io/helidon/http/Http.java
+++ http/http/src/main/java/io/helidon/http/Http.java
@@ -16,17 +16,12 @@
 
 package io.helidon.http;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 /**
- * Container for Helidon Declarative HTTP types.
- * <p>
- * All types that used to be in this class are moved to top-level classes.
+ * All types from this class are moved to top-level classes.
+ * This "container" class will be reused later, kept here for deprecation purposes to point to the right place
+ * for the time being.
  *
+ * @deprecated please use the top level classes in this package
  * @see io.helidon.http.Method
  * @see io.helidon.http.Status
  * @see io.helidon.http.HeaderName
@@ -36,158 +31,8 @@ import java.lang.annotation.Target;
  * @see io.helidon.http.HeaderValues
  * @see io.helidon.http.DateTime
  */
+@Deprecated(since = "4.0.0")
 public final class Http {
     private Http() {
-    }
-
-    /**
-     * Path of an endpoint, or sub-path of a method.
-     * Path can be overridden using configuration for a class annotation (not for method annotations).
-     */
-    @Target({ElementType.TYPE, ElementType.METHOD})
-    @Retention(RetentionPolicy.RUNTIME)
-    @Documented
-    public @interface Path {
-        /**
-         * Configuration key of the routing path, appended after the fully qualified class name (does not contain the leading dot).
-         */
-        String CONFIG_KEY_PATH = "routing-path.path";
-
-        /**
-         * Path to use, defaults to {@code /}.
-         *
-         * @return path to use
-         */
-        String value() default "/";
-    }
-
-    /**
-     * Listener socket assigned to this endpoint.
-     * This only makes sense for server side, as it is binding endpoint to a server socket.
-     */
-    @Target(ElementType.TYPE)
-    @Retention(RetentionPolicy.RUNTIME)
-    @Documented
-    public @interface Listener {
-        /**
-         * Configuration key of the routing name, appended after the fully qualified class name (does not contain the leading dot).
-         */
-        String CONFIG_KEY_NAME = "routing-name.name";
-        /**
-         * Configuration key of the routing name required flag,
-         * appended after the fully qualified class name (does not contain the leading dot).
-         */
-        String CONFIG_KEY_REQUIRED = "routing-name.required";
-
-        /**
-         * Name of a routing to bind this application/service to.
-         * @return name of a routing (or listener host/port) on WebServer
-         */
-        String value();
-
-        /**
-         * Set to true if the {@link #value()} MUST be configured.
-         *
-         * @return {@code true} to enforce existence of the named routing
-         */
-        boolean required() default false;
-    }
-
-    /**
-     * Inject entity into a method parameter.
-     */
-    @Target(ElementType.PARAMETER)
-    @Retention(RetentionPolicy.CLASS)
-    @Documented
-    public @interface Entity {
-    }
-
-    /**
-     * GET method of an HTTP endpoint.
-     */
-    @Retention(RetentionPolicy.RUNTIME)
-    @Documented
-    @HttpMethod("GET")
-    public @interface GET {
-
-    }
-
-    /**
-     * Inject header into a method parameter.
-     */
-    @Target(ElementType.PARAMETER)
-    @Retention(RetentionPolicy.CLASS)
-    @Documented
-    public @interface HeaderParam {
-        /**
-         * Name of the header.
-         * @return name of the header
-         */
-        String value();
-    }
-
-    /**
-     * HTTP Method. Can be used as a meta annotation.
-     */
-    @Retention(RetentionPolicy.RUNTIME)
-    @Documented
-    @Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
-    public @interface HttpMethod {
-        /**
-         * Text of the HTTP method.
-         * @return method
-         */
-        String value();
-    }
-
-    /**
-     * Inject path parameter into a method parameter.
-     * Path parameters are obtained from the path template of the routing method.
-     */
-    @Target(ElementType.PARAMETER)
-    @Retention(RetentionPolicy.CLASS)
-    @Documented
-    public @interface PathParam {
-        /**
-         * Name of the parameter.
-         * @return name of the path parameter
-         */
-        String value();
-    }
-
-    /**
-     * POST method of an HTTP endpoint.
-     */
-    @Retention(RetentionPolicy.RUNTIME)
-    @Documented
-    @HttpMethod("POST")
-    public @interface POST {
-
-    }
-
-    /**
-     * Inject query parameter into a method parameter.
-     */
-    @Target(ElementType.PARAMETER)
-    @Retention(RetentionPolicy.CLASS)
-    @Documented
-    public @interface QueryParam {
-        /**
-         * Configured value meaning the annotation has a default value.
-         */
-        String NO_DEFAULT_VALUE = "io.helidon.http.HttpEndpoint.QueryParam_NO_DEFAULT_VALUE";
-
-        /**
-         * Name of the parameter.
-         * @return name of the query parameter
-         */
-        String value();
-
-        /**
-         * Default value to use if the query parameter is not available.
-         *
-         * @return default value, if none specified, the query parameter is considered required
-         */
-        String defaultValue() default NO_DEFAULT_VALUE;
     }
 }
