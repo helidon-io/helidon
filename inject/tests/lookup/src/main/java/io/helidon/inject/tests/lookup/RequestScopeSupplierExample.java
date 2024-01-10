276078da5502553ff2f77ca816ba54333a4b794e
Tomas Langer <tomas.langer@oracle.com>
2024-01-10 15:36:26 +0100

    Full set of lookup method tests (singleton scope, request scope, no scope) Copyright fixes
    Signed-off-by: Tomas Langer <tomas.langer@oracle.com>

--- inject/tests/lookup/src/main/java/io/helidon/inject/tests/lookup/RequestScopeSupplierExample.java
+++ inject/tests/lookup/src/main/java/io/helidon/inject/tests/lookup/RequestScopeSupplierExample.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2024 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.helidon.inject.tests.lookup;
 
 import java.util.function.Supplier;
@@ -6,16 +22,16 @@ import io.helidon.common.Weight;
 import io.helidon.common.Weighted;
 import io.helidon.inject.service.Injection;
 
-@Injection.Service
+@Injection.RequestScope
 @Weight(Weighted.DEFAULT_WEIGHT + 1) // higher than other no-scope, lower than singleton supplier
-class RequestScopeSupplierExample implements Supplier<ContractRequestScope> {
-    private static final ContractRequestScope FIRST = new First();
+class RequestScopeSupplierExample implements Supplier<ContractRequestScopeNoIpProvider> {
+    private static final ContractRequestScopeNoIpProvider FIRST = new First();
 
     @Override
-    public ContractRequestScope get() {
+    public ContractRequestScopeNoIpProvider get() {
         return FIRST;
     }
 
-    static class First implements ContractRequestScope {
+    static class First implements ContractRequestScopeNoIpProvider {
     }
 }
--- inject/tests/lookup/src/main/java/io/helidon/inject/tests/lookup/SingletonSupplierExample.java
+++ inject/tests/lookup/src/main/java/io/helidon/inject/tests/lookup/SingletonSupplierExample.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2024 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.helidon.inject.tests.lookup;
 
 import java.util.function.Supplier;
@@ -8,14 +24,14 @@ import io.helidon.inject.service.Injection;
 
 @Injection.Singleton
 @Weight(Weighted.DEFAULT_WEIGHT + 2) // the only weighted one, should be first
-class SingletonSupplierExample implements Supplier<ContractSingleton> {
-    private static final ContractSingleton FIRST = new First();
+class SingletonSupplierExample implements Supplier<ContractSingletonNoIpProvider> {
+    private static final ContractSingletonNoIpProvider FIRST = new First();
 
     @Override
-    public ContractSingleton get() {
+    public ContractSingletonNoIpProvider get() {
         return FIRST;
     }
 
-    static class First implements ContractSingleton {
+    static class First implements ContractSingletonNoIpProvider {
     }
 }
