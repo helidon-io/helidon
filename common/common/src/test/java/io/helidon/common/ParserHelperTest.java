13b4ef01b8cd37f1d160a34f172fe852a05baac6
Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>
2024-01-10 14:09:37 -0500

    Check result of Integer.parseUnsignedInt() to be non-negative (#8215)
    * Check result of Integer.parseUnsignedInt() to be non-negative.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    * Parser class renamed.
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>
    
    ---------
    
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

---
+++ common/common/src/test/java/io/helidon/common/ParserHelperTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2024 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ParserHelperTest {
+
+    @Test
+    void testNormalParse() {
+        int v = ParserHelper.parseNonNegative(
+                "7FFFFFFF",
+                16,
+                () -> new RuntimeException("Oops"));
+        assertThat(v, is(Integer.MAX_VALUE));
+    }
+
+    @Test
+    void testNegativeParse() {
+        assertThrows(RuntimeException.class, () ->
+                ParserHelper.parseNonNegative("FFFFFFFF",
+                16,
+                () -> new RuntimeException("Value is invalid")));
+    }
+}
