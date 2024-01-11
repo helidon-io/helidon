e76aaf4f8735b19cffe9f055e23cf9270d59725d
David Král <david.k.kral@oracle.com>
2024-01-11 18:26:54 +0100

    WebClientService duplication fix (#8224)
    WebClientService duplication fixed
    
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- webclient/tracing/src/main/java/io/helidon/webclient/tracing/WebClientTracing.java
+++ webclient/tracing/src/main/java/io/helidon/webclient/tracing/WebClientTracing.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2023, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -73,6 +73,11 @@ public class WebClientTracing implements WebClientService {
      */
     public static WebClientService create(Tracer tracer) {
         return new WebClientTracing(tracer);
+    }
+
+    @Override
+    public String type() {
+        return "tracing";
     }
 
     @Override
