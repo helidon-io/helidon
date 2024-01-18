1eccb1ed315d30686229c17c1db576bb9cd5532d
Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>
2024-01-18 09:40:42 -0500

    Some write optimizations for buffers and lazy initialization of headers. (#8242)
    Signed-off-by: Santiago Pericasgeertsen <santiago.pericasgeertsen@oracle.com>

--- common/buffers/src/main/java/io/helidon/common/buffers/CompositeListBufferData.java
+++ common/buffers/src/main/java/io/helidon/common/buffers/CompositeListBufferData.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,6 +56,13 @@ class CompositeListBufferData extends ReadOnlyBufferData implements CompositeBuf
 
     @Override
     public void writeTo(OutputStream out) {
+        if (data.size() == 1) {
+            BufferData datum = data.get(0);
+            if (!(datum instanceof CompositeBufferData)) {
+                datum.writeTo(out);
+                return;
+            }
+        }
         copy().writeTo(out);
     }
 
