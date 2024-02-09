f771953427bdbc6ef0361b14035c03042cd7cfe0
Tomas Langer <tomas.langer@oracle.com>
2024-02-09 17:56:21 +0100

    Never send entity with status 204

--- examples/metrics/http-status-count-se/src/test/java/io/helidon/examples/se/httpstatuscount/StatusService.java
+++ examples/metrics/http-status-count-se/src/test/java/io/helidon/examples/se/httpstatuscount/StatusService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.helidon.examples.se.httpstatuscount;
 
 import io.helidon.http.Status;
@@ -43,6 +44,12 @@ public class StatusService implements HttpService {
             status = Status.INTERNAL_SERVER_ERROR_500.code();
             msg = "Unsuccessful conversion";
         }
-        response.status(status).send(msg);
+        Status httpStatus = Status.create(status);
+        response.status(status);
+        if (httpStatus != Status.NO_CONTENT_204) {
+            response.send(msg);
+        } else {
+            response.send();
+        }
     }
 }
