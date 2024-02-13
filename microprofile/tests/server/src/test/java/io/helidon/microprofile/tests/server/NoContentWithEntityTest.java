509ec525d8f1179661ea4cd317d4a6e6e77183ff
Tomas Langer <tomas.langer@oracle.com>
2024-02-13 12:07:20 +0100

    4.x: Return 500 when 204 with entity is sent from routing. (#8357)
    * Return 500 when 204 with entity is sent from routing.
    * Never send entity with status 204 (from our own code)
    * Updated to include 205 and 304 statuses

---
+++ microprofile/tests/server/src/test/java/io/helidon/microprofile/tests/server/NoContentWithEntityTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2024 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.microprofile.tests.server;
+
+import io.helidon.http.Status;
+import io.helidon.microprofile.server.JaxRsCdiExtension;
+import io.helidon.microprofile.server.ServerCdiExtension;
+import io.helidon.microprofile.testing.junit5.AddBean;
+import io.helidon.microprofile.testing.junit5.AddExtension;
+import io.helidon.microprofile.testing.junit5.DisableDiscovery;
+import io.helidon.microprofile.testing.junit5.HelidonTest;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Response;
+import org.glassfish.jersey.ext.cdi1x.internal.CdiComponentProvider;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@HelidonTest
+@DisableDiscovery
+@AddBean(NoContentWithEntityTest.TestResource.class)
+@AddExtension(ServerCdiExtension.class)
+@AddExtension(JaxRsCdiExtension.class)
+@AddExtension(CdiComponentProvider.class)
+class NoContentWithEntityTest {
+    @Inject
+    WebTarget target;
+
+    @Test
+    void streamingOutput() {
+        Response response = target.path("/noContent")
+                .request()
+                .get();
+
+        assertThat(response.getStatus(), is(Status.INTERNAL_SERVER_ERROR_500.code()));
+
+        response = target.path("/ok")
+                .request()
+                .get();
+        assertThat(response.getStatus(), is(Status.OK_200.code()));
+        assertThat(response.readEntity(String.class), is("hello"));
+    }
+
+    @Path("/")
+    public static class TestResource {
+
+        @GET
+        @Path("/noContent")
+        public Response noContent() {
+            return Response.noContent()
+                    .entity("hello")
+                    .build();
+        }
+
+        @GET
+        @Path("/ok")
+        public Response ok() {
+            return Response
+                    .ok("hello")
+                    .build();
+        }
+    }
+}
