23c656830b733e592d5566e6d4e1a341cf13f974
Andrei Arlou <andrei.arlou@gmail.com>
2024-02-14 04:33:40 +0200

    Replace deprecated MediaType.fullType() on MediaType.text() (#8340)

--- http/http/src/main/java/io/helidon/http/HttpMediaTypeImpl.java
+++ http/http/src/main/java/io/helidon/http/HttpMediaTypeImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -107,7 +107,7 @@ final class HttpMediaTypeImpl implements HttpMediaType {
 
     @Override
     public String text() {
-        StringBuilder result = new StringBuilder(mediaType.fullType());
+        StringBuilder result = new StringBuilder(mediaType.text());
         for (Map.Entry<String, String> param : parameters.entrySet()) {
             result.append("; ")
                     .append(param.getKey())
