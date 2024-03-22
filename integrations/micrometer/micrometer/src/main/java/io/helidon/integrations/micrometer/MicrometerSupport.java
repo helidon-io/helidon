9e39199d6788dcf572cce94ea1530378bafaecc5
Tim Quinn <tim.quinn@oracle.com>
2024-03-21 23:34:20 -0500

    Respect named port selection in MicrometerSupport for endpoint
    Signed-off-by: Tim Quinn <tim.quinn@oracle.com>

--- integrations/micrometer/micrometer/src/main/java/io/helidon/integrations/micrometer/MicrometerSupport.java
+++ integrations/micrometer/micrometer/src/main/java/io/helidon/integrations/micrometer/MicrometerSupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -100,7 +100,7 @@ public class MicrometerSupport extends HelidonRestServiceSupport {
 
     @Override
     protected void postConfigureEndpoint(Routing.Rules defaultRules, Routing.Rules serviceEndpointRoutingRules) {
-        defaultRules
+        serviceEndpointRoutingRules
                 .any(new MetricsContextHandler(registry()))
                 .get(context(), this::getOrOptions)
                 .options(context(), this::getOrOptions);
