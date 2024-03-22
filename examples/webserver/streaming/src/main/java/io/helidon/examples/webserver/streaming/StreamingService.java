b2b719935d66c9c5d34cbff231cb8b5ed26eb897
Joe Di Pol <joe.dipol@oracle.com>
2024-03-21 17:16:29 -0700

    Fix copyright and cleanup includes

--- examples/webserver/streaming/src/main/java/io/helidon/examples/webserver/streaming/StreamingService.java
+++ examples/webserver/streaming/src/main/java/io/helidon/examples/webserver/streaming/StreamingService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2018, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,12 +19,10 @@ package io.helidon.examples.webserver.streaming;
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.Objects;
 import java.util.logging.Logger;
 
 import io.helidon.http.Status;
