a5c4dd803557d04d767233e0016679bf30343320
Andrei Arlou <andrei.arlou@gmail.com>
2024-05-08 23:43:36 +0300

    4.x: Capitalized received message in io.helidon.examples.webserver.websocket.MessageBoardEndpoint (#8725) (#8731)

--- examples/webserver/websocket/src/test/java/io/helidon/examples/webserver/websocket/MessageBoardTest.java
+++ examples/webserver/websocket/src/test/java/io/helidon/examples/webserver/websocket/MessageBoardTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -83,7 +83,7 @@ public class MessageBoardTest {
 
             @Override
             public void onOpen(WsSession session) {
-                session.send("send", false);
+                session.send("SEND", false);
             }
         });
 
