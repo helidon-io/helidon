71bc198004dace2b0c77444c625a8b00f785689e
Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>
2024-05-21 15:33:50 -0400

    New document that describes the ExecuteOn annotation (#8756)
    * New document that describes the ExecuteOn annotation.
    
    * Fixed import problems in snippets file.
    
    Signed-off-by: Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>
    
    * Updates sitegen.yaml to include threading page.
    
    Signed-off-by: Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>
    
    ---------
    
    Signed-off-by: Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>

---
+++ docs/src/main/java/io/helidon/docs/mp/ExecuteOnSnippets.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2024 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.docs.mp;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import io.helidon.microprofile.cdi.ExecuteOn;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Named;
+
+import static io.helidon.microprofile.cdi.ExecuteOn.ThreadType;
+
+@SuppressWarnings("ALL")
+class ExecuteOnSnippets {
+
+    // stub
+    static class PiDigitCalculator {
+        static int nthDigitOfPi(int n) {
+            return 0;
+        }
+    }
+
+    // tag::snippet_1[]
+    public class MyPlaformBean {
+
+        @ExecuteOn(ThreadType.PLATFORM)
+        int cpuIntensive(int n) {
+            return PiDigitCalculator.nthDigitOfPi(n);
+        }
+    }
+    // end::snippet_1[]
+
+    // tag::snippet_2[]
+    public class MyExecutorBean {
+
+        @ExecuteOn(value = ThreadType.EXECUTOR, executorName = "my-executor")
+        int cpuIntensive(int n) {
+            return PiDigitCalculator.nthDigitOfPi(n);
+        }
+
+        @Produces
+        @Named("my-executor")
+        ExecutorService myExecutor() {
+            return Executors.newFixedThreadPool(2);
+        }
+    }
+    // end::snippet_2[]
+
+    // tag::snippet_3[]
+    public class MyVirtualBean {
+
+        @ExecuteOn(ThreadType.VIRTUAL)
+        void someTask() {
+            // run task on virtual thread
+        }
+    }
+    // end::snippet_3[]
+}
