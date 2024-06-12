aabcb5c4a81274980b33e0920ac4c8d617b16225
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2024-06-12 14:29:16 +0200

    Create PinnedThreadValidation
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- webserver/testing/junit5/junit5/src/main/java/module-info.java
+++ webserver/testing/junit5/junit5/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,6 +24,7 @@
 module io.helidon.webserver.testing.junit5 {
 
     requires io.helidon.logging.common;
+    requires jdk.jfr;
 
     requires transitive hamcrest.all;
     requires transitive io.helidon.common.testing.http.junit5;
