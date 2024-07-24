500780ad162692dffdb0707f42fcb34aa56ce99f
David Král <david.k.kral@oracle.com>
2024-07-24 15:38:15 +0200

    4.x MDC propagation without context (#8957)
    MDC propagation without context fixed
    
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- logging/log4j/src/test/java/io/helidon/logging/log4j/Log4jMdcTest.java
+++ logging/log4j/src/test/java/io/helidon/logging/log4j/Log4jMdcTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,7 +51,7 @@ public class Log4jMdcTest {
     }
 
     @Test
-    public void testThreadPropagation() {
+    public void testThreadPropagationWithContext() {
         HelidonMdc.set(TEST_KEY, TEST_VALUE);
         Context context = Context.create();
         ExecutorService executor = Contexts.wrap(Executors.newFixedThreadPool(1));
@@ -64,6 +64,19 @@ public class Log4jMdcTest {
                 throw new ExecutorException("failed to execute", e);
             }
         });
+    }
+
+    @Test
+    public void testThreadPropagationWithoutContext() {
+        HelidonMdc.set(TEST_KEY, TEST_VALUE);
+        ExecutorService executor = Contexts.wrap(Executors.newFixedThreadPool(1));
+
+        try {
+            String value = executor.submit(new TestCallable()).get();
+            assertThat(value, is(TEST_VALUE));
+        } catch (Exception e) {
+            throw new ExecutorException("failed to execute", e);
+        }
     }
 
     private static final class TestCallable implements Callable<String> {
