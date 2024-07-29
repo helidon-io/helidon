113f79c78c2449aea8201347bdb6e44e0be37b52
Tomas Langer <tomas.langer@oracle.com>
2024-07-29 21:51:44 +0200

    Introduction of HSON library to write and parse Helidon metadata files.

---
+++ metadata/hson/src/main/java/io/helidon/metadata/hson/HsonObject.java
@@ -0,0 +1,361 @@
+/*
+ * Copyright (c) 2024 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.metadata.hson;
+
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+final class HsonObject implements Hson.Object {
+    private final Map<String, Hson.Value<?>> values;
+
+    private HsonObject(Map<String, Hson.Value<?>> values) {
+        this.values = values;
+    }
+
+    @Override
+    public Hson.Object value() {
+        return this;
+    }
+
+    @Override
+    public Optional<Hson.Value<?>> value(String key) {
+        return Optional.ofNullable(values.get(key));
+    }
+
+    @Override
+    public Optional<Boolean> booleanValue(String key) {
+        return getValue(key, Hson.Type.BOOLEAN);
+    }
+
+    @Override
+    public boolean booleanValue(String key, boolean defaultValue) {
+        return booleanValue(key).orElse(defaultValue);
+    }
+
+    @Override
+    public Optional<Hson.Object> objectValue(String key) {
+        return getValue(key, Hson.Type.OBJECT);
+    }
+
+    @Override
+    public Optional<String> stringValue(String key) {
+        return getValue(key, Hson.Type.STRING);
+    }
+
+    @Override
+    public String stringValue(String key, String defaultValue) {
+        return stringValue(key).orElse(defaultValue);
+    }
+
+    @Override
+    public Optional<Integer> intValue(String key) {
+        return this.<BigDecimal>getValue(key, Hson.Type.NUMBER).map(BigDecimal::intValue);
+    }
+
+    @Override
+    public int intValue(String key, int defaultValue) {
+        return intValue(key).orElse(defaultValue);
+    }
+
+    @Override
+    public Optional<Double> doubleValue(String key) {
+        return this.<BigDecimal>getValue(key, Hson.Type.NUMBER).map(BigDecimal::doubleValue);
+    }
+
+    @Override
+    public double doubleValue(String key, double defaultValue) {
+        return doubleValue(key).orElse(defaultValue);
+    }
+
+    @Override
+    public Optional<BigDecimal> numberValue(String key) {
+        return this.getValue(key, Hson.Type.NUMBER);
+    }
+
+    @Override
+    public BigDecimal numberValue(String key, BigDecimal defaultValue) {
+        return numberValue(key).orElse(defaultValue);
+    }
+
+    @Override
+    public Optional<List<String>> stringArray(String key) {
+        return getTypedList(key, Hson.Array::getStrings);
+    }
+
+    @Override
+    public Optional<List<Hson.Object>> objectArray(String key) {
+        return getTypedList(key, Hson.Array::getObjects);
+    }
+
+    @Override
+    public Optional<List<BigDecimal>> numberArray(String key) {
+        return getTypedList(key, Hson.Array::getNumbers);
+    }
+
+    @Override
+    public Optional<List<Boolean>> booleanArray(String key) {
+        return getTypedList(key, Hson.Array::getBooleans);
+    }
+
+    @Override
+    public Optional<Hson.Array> arrayValue(String key) {
+        Hson.Value<?> jValue = values.get(key);
+        if (jValue == null) {
+            return Optional.empty();
+        }
+        if (jValue.type() != Hson.Type.ARRAY) {
+            throw new HsonException(exceptionMessage(key, " array requested, yet this key is not an array, but " + type()));
+        }
+
+        return Optional.of((Hson.Array) jValue);
+    }
+
+    @Override
+    public void write(PrintWriter writer) {
+        Objects.requireNonNull(writer);
+
+        writer.write('{');
+        AtomicBoolean first = new AtomicBoolean(true);
+
+        values.forEach((key, value) -> {
+            writeNext(writer, first);
+            writer.write('\"');
+            writer.write(key);
+            writer.write("\":");
+            value.write(writer);
+        });
+
+        writer.write('}');
+    }
+
+    @Override
+    public Hson.Type type() {
+        return Hson.Type.OBJECT;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof HsonObject object)) {
+            return false;
+        }
+        return Objects.equals(values, object.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(values);
+    }
+
+    @Override
+    public String toString() {
+        return "{"
+                + values
+                + '}';
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> Optional<T> getValue(String key, Hson.Type type) {
+        Hson.Value<?> jValue = values.get(key);
+        if (jValue == null || jValue.type() == Hson.Type.NULL) {
+            return Optional.empty();
+        }
+        if (jValue.type() != type) {
+            throw new HsonException(exceptionMessage(key, "requested as a " + type
+                    + ", but it is of type " + jValue.type()));
+        }
+        return Optional.of((T) jValue.value());
+    }
+
+    private <T> Optional<List<T>> getTypedList(String key, Function<Hson.Array, List<T>> arrayFunction) {
+        try {
+            return arrayValue(key).map(arrayFunction);
+        } catch (HsonException e) {
+            throw new HsonException(exceptionMessage(key, " failed to get typed array"), e);
+        }
+    }
+
+    private String exceptionMessage(String key, String message) {
+        return "Object key \"" + key + "\": " + message;
+    }
+
+    private void writeNext(PrintWriter metaWriter, AtomicBoolean first) {
+        if (first.get()) {
+            first.set(false);
+            return;
+        }
+        metaWriter.write(',');
+    }
+
+    static class Builder implements Hson.Object.Builder {
+        private final Map<String, Hson.Value<?>> values = new LinkedHashMap<>();
+
+        Builder() {
+        }
+
+        @Override
+        public Hson.Object build() {
+            return new HsonObject(new LinkedHashMap<>(values));
+        }
+
+        @Override
+        public Builder unset(String key) {
+            Objects.requireNonNull(key, "key cannot be null");
+
+            values.remove(key);
+            return this;
+        }
+
+        @Override
+        public Hson.Object.Builder setNull(String key) {
+            values.put(key, HsonValues.NullValue.INSTANCE);
+            return this;
+        }
+
+        @Override
+        public Builder set(String key, Hson.Value<?> value) {
+            values.put(key, value);
+            return this;
+        }
+
+        @Override
+        public Builder set(String key, String value) {
+            Objects.requireNonNull(key, "key cannot be null");
+            Objects.requireNonNull(value, "value cannot be null");
+
+            values.put(key, HsonValues.StringValue.create(value));
+            return this;
+        }
+
+        @Override
+        public Builder set(String key, boolean value) {
+            Objects.requireNonNull(key, "key cannot be null");
+
+            values.put(key, HsonValues.BooleanValue.create(value));
+            return this;
+        }
+
+        @Override
+        public Builder set(String key, double value) {
+            Objects.requireNonNull(key, "key cannot be null");
+
+            values.put(key, HsonValues.NumberValue.create(new BigDecimal(value)));
+            return this;
+        }
+
+        @Override
+        public Builder set(String key, int value) {
+            Objects.requireNonNull(key, "key cannot be null");
+
+            values.put(key, HsonValues.NumberValue.create(new BigDecimal(value)));
+            return this;
+        }
+
+        @Override
+        public Builder set(String key, long value) {
+            Objects.requireNonNull(key, "key cannot be null");
+
+            values.put(key, HsonValues.NumberValue.create(new BigDecimal(value)));
+            return this;
+        }
+
+        @Override
+        public Builder set(String key, BigDecimal value) {
+            Objects.requireNonNull(key, "key cannot be null");
+            Objects.requireNonNull(value, "value cannot be null");
+
+            values.put(key, HsonValues.NumberValue.create(value));
+            return this;
+        }
+
+        @Override
+        public Builder set(String key, Hson.Array value) {
+            Objects.requireNonNull(key, "key cannot be null");
+            Objects.requireNonNull(value, "value cannot be null");
+
+            values.put(key, value);
+            return this;
+        }
+
+        @Override
+        public Builder setObjects(String key, List<Hson.Object> value) {
+            Objects.requireNonNull(key, "key cannot be null");
+            Objects.requireNonNull(value, "value cannot be null");
+
+            values.put(key, Hson.Array.create(value));
+            return this;
+        }
+
+        @Override
+        public Builder setStrings(String key, List<String> value) {
+            Objects.requireNonNull(key, "key cannot be null");
+            Objects.requireNonNull(value, "value cannot be null");
+
+            values.put(key, Hson.Array.createStrings(value));
+            return this;
+        }
+
+        @Override
+        public Builder setLongs(String key, List<Long> value) {
+            Objects.requireNonNull(key, "key cannot be null");
+            Objects.requireNonNull(value, "value cannot be null");
+
+            values.put(key, Hson.Array.createNumbers(value.stream()
+                                                             .map(BigDecimal::new)
+                                                             .collect(Collectors.toUnmodifiableList())));
+            return this;
+        }
+
+        @Override
+        public Builder setDoubles(String key, List<Double> value) {
+            Objects.requireNonNull(key, "key cannot be null");
+            Objects.requireNonNull(value, "value cannot be null");
+            values.put(key, Hson.Array.createNumbers(value.stream()
+                                                             .map(BigDecimal::new)
+                                                             .collect(Collectors.toUnmodifiableList())));
+            return this;
+        }
+
+        @Override
+        public Builder setNumbers(String key, List<BigDecimal> value) {
+            Objects.requireNonNull(key, "key cannot be null");
+            Objects.requireNonNull(value, "value cannot be null");
+
+            values.put(key, Hson.Array.createNumbers(value));
+            return this;
+        }
+
+        @Override
+        public Builder setBooleans(String key, List<Boolean> value) {
+            Objects.requireNonNull(key, "key cannot be null");
+            Objects.requireNonNull(value, "value cannot be null");
+
+            values.put(key, Hson.Array.createBooleans(value));
+            return this;
+        }
+    }
+}
---
+++ metadata/hson/src/test/java/io/helidon/metadata/hson/HsonTest.java
@@ -0,0 +1,332 @@
+/*
+ * Copyright (c) 2024 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.metadata.hson;
+
+import java.io.ByteArrayInputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+import static io.helidon.common.testing.junit5.OptionalMatcher.optionalEmpty;
+import static io.helidon.common.testing.junit5.OptionalMatcher.optionalPresent;
+import static io.helidon.common.testing.junit5.OptionalMatcher.optionalValue;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class HsonTest {
+    @Test
+    void testWrongTypes() {
+        Hson.Object object = Hson.objectBuilder()
+                .set("number", 1)
+                .set("string", "hi")
+                .setLongs("numbers", List.of(1L, 2L, 3L))
+                .setStrings("strings", List.of("hi", "there"))
+                .build();
+
+        assertThrows(HsonException.class,
+                     () -> object.stringValue("number"));
+        assertThrows(HsonException.class,
+                     () -> object.booleanValue("number"));
+        assertThrows(HsonException.class,
+                     () -> object.doubleValue("string"));
+        assertThrows(HsonException.class,
+                     () -> object.objectValue("string"));
+        assertThrows(HsonException.class,
+                     () -> object.stringArray("string"));
+        assertThrows(HsonException.class,
+                     () -> object.objectArray("string"));
+        assertThrows(HsonException.class,
+                     () -> object.booleanArray("string"));
+        assertThrows(HsonException.class,
+                     () -> object.numberArray("string"));
+
+        assertThrows(HsonException.class,
+                     () -> object.stringValue("strings"));
+        assertThrows(HsonException.class,
+                     () -> object.booleanValue("strings"));
+        assertThrows(HsonException.class,
+                     () -> object.doubleValue("strings"));
+        assertThrows(HsonException.class,
+                     () -> object.objectValue("strings"));
+        assertThrows(HsonException.class,
+                     () -> object.stringArray("numbers"));
+        assertThrows(HsonException.class,
+                     () -> object.objectArray("strings"));
+        assertThrows(HsonException.class,
+                     () -> object.booleanArray("strings"));
+        assertThrows(HsonException.class,
+                     () -> object.numberArray("strings"));
+    }
+
+    @Test
+    void testReads() {
+        Hson.Object empty = Hson.Object.create();
+
+        assertThat(empty.booleanValue("anyKey"), optionalEmpty());
+        assertThat(empty.booleanValue("anyKey", true), is(true));
+
+        assertThat(empty.intValue("anyKey"), optionalEmpty());
+        assertThat(empty.intValue("anyKey", 876), is(876));
+
+        assertThat(empty.doubleValue("anyKey"), optionalEmpty());
+        assertThat(empty.doubleValue("anyKey", 876), is(876d));
+
+        assertThat(empty.stringValue("anyKey"), optionalEmpty());
+        assertThat(empty.stringValue("anyKey", "default"), is("default"));
+
+        assertThat(empty.numberValue("anyKey"), optionalEmpty());
+        BigDecimal bd = new BigDecimal(14);
+        assertThat(empty.numberValue("anyKey", bd), sameInstance(bd));
+
+        assertThat(empty.stringArray("anyKey"), optionalEmpty());
+        assertThat(empty.objectArray("anyKey"), optionalEmpty());
+        assertThat(empty.numberArray("anyKey"), optionalEmpty());
+    }
+
+    @Test
+    void testSetNumbers() {
+
+        BigDecimal one = new BigDecimal(14);
+        BigDecimal two = new BigDecimal(15);
+
+        Hson.Object jObject = Hson.objectBuilder()
+                .setNumbers("numbers", List.of(one, two))
+                .build();
+
+        List<BigDecimal> objects = jObject.numberArray("numbers")
+                .orElseThrow(() -> new IllegalStateException("numbers key should be filled with array"));
+
+        assertThat(objects, hasItems(one, two));
+    }
+
+    @Test
+    void testSetBooleans() {
+        Hson.Object jObject = Hson.objectBuilder()
+                .setBooleans("booleans", List.of(true, false, true))
+                .build();
+
+        List<Boolean> objects = jObject.booleanArray("booleans")
+                .orElseThrow(() -> new IllegalStateException("booleans key should be filled with array"));
+
+        assertThat(objects, hasItems(true, false, true));
+    }
+
+    @Test
+    void testWriteStringArray() {
+        Hson.Array array = Hson.Array.createStrings(List.of("a", "b", "c"));
+
+        StringWriter sw = new StringWriter();
+        try (PrintWriter pw = new PrintWriter(sw)) {
+            array.write(pw);
+        }
+
+        String string = sw.toString();
+        assertThat(string, is("[\"a\",\"b\",\"c\"]"));
+    }
+
+    @Test
+    void testWriteLongArray() {
+        Hson.Array array = Hson.Array.create(2L, 3L, 4L);
+
+        StringWriter sw = new StringWriter();
+        try (PrintWriter pw = new PrintWriter(sw)) {
+            array.write(pw);
+        }
+
+        String string = sw.toString();
+        assertThat(string, is("[2,3,4]"));
+    }
+
+    @Test
+    void testWriteDoubleArray() {
+        Hson.Array array = Hson.Array.createNumbers(List.of(new BigDecimal(2), new BigDecimal(3), new BigDecimal("4.2")));
+
+        StringWriter sw = new StringWriter();
+        try (PrintWriter pw = new PrintWriter(sw)) {
+            array.write(pw);
+        }
+
+        String string = sw.toString();
+        assertThat(string, is("[2,3,4.2]"));
+    }
+
+    @Test
+    void testWriteBooleanArray() {
+        Hson.Array array = Hson.Array.createBooleans(List.of(true, false, true));
+
+        StringWriter sw = new StringWriter();
+        try (PrintWriter pw = new PrintWriter(sw)) {
+            array.write(pw);
+        }
+
+        String string = sw.toString();
+        assertThat(string, is("[true,false,true]"));
+    }
+
+    @Test
+    void testWriteObjectArray() {
+        Hson.Object first = Hson.objectBuilder()
+                .set("string", "value")
+                .set("long", 4L)
+                .set("double", 4d)
+                .set("boolean", true)
+                .setStrings("strings", List.of("a", "b"))
+                .setLongs("longs", List.of(1L, 2L))
+                .setDoubles("doubles", List.of(1.5d, 2.5d))
+                .setBooleans("booleans", List.of(true, false))
+                .build();
+        Hson.Object second = Hson.objectBuilder()
+                .set("string", "value2")
+                .build();
+
+        Hson.Array array = Hson.Array.create(List.of(first, second));
+
+        StringWriter sw = new StringWriter();
+        try (PrintWriter pw = new PrintWriter(sw)) {
+            array.write(pw);
+        }
+
+        String string = sw.toString();
+        String expected = "[{\"string\":\"value\","
+                + "\"long\":4,"
+                + "\"double\":4,"
+                + "\"boolean\":true,"
+                + "\"strings\":[\"a\",\"b\"],"
+                + "\"longs\":[1,2],"
+                + "\"doubles\":[1.5,2.5],"
+                + "\"booleans\":[true,false]},"
+                + "{\"string\":\"value2\"}]";
+        assertThat(string, is(expected));
+
+        Hson.Value<?> read = Hson.parse(new ByteArrayInputStream(string.getBytes(StandardCharsets.UTF_8)));
+        assertThat(read.type(), is(Hson.Type.ARRAY));
+
+        Hson.Array objects = read.asArray();
+        List<Hson.Object> value = objects.getObjects();
+        assertThat(value, hasSize(2));
+        Hson.Object readFirst = value.get(0);
+        assertThat(readFirst, is(first));
+        Hson.Object readSecond = value.get(1);
+        assertThat(readSecond, is(second));
+    }
+
+    void testSetObjects() {
+        Hson.Object first = Hson.objectBuilder()
+                .set("key", "value1")
+                .build();
+        Hson.Object second = Hson.objectBuilder()
+                .set("key", "value2")
+                .build();
+        Hson.Object withArray = Hson.objectBuilder()
+                .setObjects("objects", List.of(first, second))
+                .build();
+
+        List<Hson.Object> objects = withArray.objectArray("objects").get();
+        assertThat(objects, hasItems(first, second));
+    }
+
+    @Test
+    void testNull() {
+        Hson.Object object = Hson.objectBuilder()
+                .setNull("null-key")
+                .build();
+
+        assertThat(object.value("null-key"), optionalPresent());
+        assertThat(object.value("null-key").get().type(), is(Hson.Type.NULL));
+    }
+
+    @Test
+    void testUnset() {
+        Hson.Object object = Hson.objectBuilder()
+                .setNull("null-key")
+                .unset("null-key")
+                .build();
+
+        assertThat(object.value("null-key"), optionalEmpty());
+    }
+
+    @Test
+    void testFeatures() {
+        Hson.Value<?> parsedValue = Hson.parse(HsonTest.class.getResourceAsStream("/json-features.json"));
+        assertThat(parsedValue.type(), is(Hson.Type.OBJECT));
+        Hson.Object object = parsedValue.asObject();
+
+        testFeaturesNull(object);
+        testFeaturesArray(object);
+        testFeaturesEscapes(object);
+        testFeaturesNumbers(object);
+    }
+
+    private void testFeaturesNumbers(Hson.Object object) {
+        Optional<Hson.Object> maybeObject = object.objectValue("numbers");
+        assertThat(maybeObject, optionalPresent());
+        Hson.Object numbers = maybeObject.get();
+
+        assertThat(numbers.numberValue("number1"), optionalValue(is(new BigDecimal(1))));
+        assertThat(numbers.numberValue("number2"), optionalValue(is(new BigDecimal("1.5"))));
+        assertThat(numbers.numberValue("number3"), optionalValue(is(new BigDecimal("1.5e2"))));
+        assertThat(numbers.numberValue("number4"), optionalValue(is(new BigDecimal("1.5e-2"))));
+        assertThat(numbers.numberValue("number5"), optionalValue(is(new BigDecimal("1.5e2"))));
+    }
+
+    private void testFeaturesEscapes(Hson.Object object) {
+        Optional<Hson.Object> maybeObject = object.objectValue("escapes");
+        assertThat(maybeObject, optionalPresent());
+        Hson.Object escapes = maybeObject.get();
+
+        assertThat(escapes.stringValue("newline"), optionalValue(is("a\nb")));
+        assertThat(escapes.stringValue("quotes"), optionalValue(is("a\"b")));
+        assertThat(escapes.stringValue("backslash"), optionalValue(is("a\\b")));
+        assertThat(escapes.stringValue("slash"), optionalValue(is("a/b")));
+        assertThat(escapes.stringValue("backspace"), optionalValue(is("a\bb")));
+        assertThat(escapes.stringValue("formfeed"), optionalValue(is("a\fb")));
+        assertThat(escapes.stringValue("cr"), optionalValue(is("a\rb")));
+        assertThat(escapes.stringValue("tab"), optionalValue(is("a\tb")));
+        assertThat(escapes.stringValue("unicode"), optionalValue(is("aHb")));
+
+    }
+
+    private void testFeaturesArray(Hson.Object object) {
+        Optional<Hson.Array> maybeArray = object.arrayValue("array");
+        assertThat(maybeArray, optionalPresent());
+        List<Hson.Value<?>> value = maybeArray.get().value();
+        assertThat("There should be 6 elements in array", value, hasSize(6));
+        assertThat(value.get(0).type(), is(Hson.Type.BOOLEAN));
+        assertThat(value.get(1).type(), is(Hson.Type.NULL));
+        assertThat(value.get(2).type(), is(Hson.Type.STRING));
+        assertThat(value.get(3).type(), is(Hson.Type.NUMBER));
+        assertThat(value.get(4).type(), is(Hson.Type.OBJECT));
+        assertThat(value.get(5).type(), is(Hson.Type.ARRAY));
+    }
+
+    private void testFeaturesNull(Hson.Object object) {
+        Optional<Hson.Value<?>> maybeValue = object.objectValue("nulls")
+                .flatMap(it -> it.value("field"));
+        assertThat(maybeValue, optionalPresent());
+        Hson.Value<?> value = maybeValue.get();
+        assertThat(value.type(), is(Hson.Type.NULL));
+    }
+}
