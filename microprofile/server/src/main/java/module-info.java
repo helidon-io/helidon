45e7d4a21bc0b40a5bd9c886277998f04ba1f4aa
Joe Di Pol <joe.dipol@oracle.com>
2024-07-31 10:43:57 -0700

    Upgrade hibernate-validator to 7.0.5 and EE validation-api to 3.0.2

--- microprofile/server/src/main/java/module-info.java
+++ microprofile/server/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2018, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,7 @@ module io.helidon.microprofile.server {
     requires transitive jakarta.cdi;
     requires transitive jakarta.ws.rs;
     requires jakarta.interceptor.api;
+    requires jakarta.validation;
     requires transitive jakarta.json;
     requires io.helidon.jersey.media.jsonp;
 
