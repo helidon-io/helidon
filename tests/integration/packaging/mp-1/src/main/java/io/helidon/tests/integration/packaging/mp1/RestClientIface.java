dc3b9d2ed56bf210ac466da8bf3305853219727d
Romain Grecourt <romain.grecourt@oracle.com>
2024-08-05 19:54:22 -0700

    Packaging Integration Tests
    Refactor tests/integration/native-image into a more general "packaging" test suite.

---
+++ tests/integration/packaging/mp-1/src/main/java/io/helidon/tests/integration/packaging/mp1/RestClientIface.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2019, 2024 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.tests.integration.packaging.mp1;
+
+import jakarta.json.JsonObject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+/**
+ * Rest client example.
+ */
+@RegisterRestClient(baseUri = "http://localhost:8087/cdi")
+@Path("/")
+public interface RestClientIface {
+    @GET
+    @Path("/property")
+    String message();
+
+    @GET
+    @Path("/beantype")
+    String beanType();
+
+    @GET
+    @Path("/jsonp")
+    JsonObject jsonProcessing();
+
+    @GET
+    @Path("/jsonb")
+    @Produces(MediaType.APPLICATION_JSON)
+    TestDto jsonBinding();
+
+    @GET
+    @Path("/queryparam")
+    String queryParam(@QueryParam("long") Long longParam);
+
+    @GET
+    @Path("/queryparam2")
+    String queryParam(@QueryParam("boolean") Boolean booleanParam);
+
+    @GET
+    @Path("/queryparam3")
+    String queryParam(@QueryParam("int") int intParam);
+}
---
+++ tests/integration/packaging/mp-3/src/test/java/io/helidon/tests/integration/packaging/mp3/Mp3NativeTestIT.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.tests.integration.packaging.mp3;
+
+import io.helidon.tests.integration.harness.ProcessRunner.ExecMode;
+
+import org.junit.jupiter.api.Test;
+
+class Mp3NativeTestIT extends Mp3PackagingTestIT {
+
+    @Override
+    ExecMode execMode() {
+        return ExecMode.NATIVE;
+    }
+
+    @Test
+    void testExitOnStarted() {
+        doTestExitOnStarted();
+    }
+
+    @Test
+    void testGreetResource() {
+        doTestGreetResource();
+    }
+}
