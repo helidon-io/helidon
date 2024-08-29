7f7da14f5681d2f2657739beb1868be82955d0cc
Romain Grecourt <romain.grecourt@oracle.com>
2024-08-29 11:36:19 -0700

    Fix nightly script (#9221)
    - Add tests profile to the prime build
    - Remove fault-tolerance tck as it's part of the PR gate

--- etc/scripts/test-nightly.sh
+++ etc/scripts/test-nightly.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 #
 # Copyright (c) 2022, 2024 Oracle and/or its affiliates.
 #
@@ -20,18 +20,18 @@ set -o errtrace || true # trace ERR through commands and functions
 set -o errexit || true  # exit the script if any statement returns a non-true return value
 
 on_error(){
-    CODE="${?}" && \
-    set +x && \
-    printf "[ERROR] Error(code=%s) occurred at %s:%s command: %s\n" \
-        "${CODE}" "${BASH_SOURCE[0]}" "${LINENO}" "${BASH_COMMAND}"
+  CODE="${?}" && \
+  set +x && \
+  printf "[ERROR] Error(code=%s) occurred at %s:%s command: %s\n" \
+      "${CODE}" "${BASH_SOURCE[0]}" "${LINENO}" "${BASH_COMMAND}"
 }
 trap on_error ERR
 
 # Path to this script
 if [ -h "${0}" ] ; then
-    SCRIPT_PATH="$(readlink "${0}")"
+  SCRIPT_PATH="$(readlink "${0}")"
 else
-    SCRIPT_PATH="${0}"
+  SCRIPT_PATH="${0}"
 fi
 readonly SCRIPT_PATH
 
@@ -40,19 +40,23 @@ readonly SCRIPT_PATH
 WS_DIR=$(cd $(dirname -- "${SCRIPT_PATH}") ; cd ../.. ; pwd -P)
 readonly WS_DIR
 
-readonly MODULES_TO_BUILD="\
- microprofile/tests/tck/tck-fault-tolerance\
-"
+readonly MODULES=(
+)
+
+if [ ${#MODULES[*]} -eq 0 ]; then
+  echo "Modules list is empty"
+  exit 0
+fi
 
 cd "${WS_DIR}"
 
 # Do priming build
-mvn clean install -DskipTests
+mvn -T8 clean install -DskipTests -Ptests
 
 # Build/run tests
-for M in ${MODULES_TO_BUILD}; do
-    cd "${WS_DIR}/${M}"
-    mvn clean install
+for M in "${MODULES[@]}"; do
+  cd "${WS_DIR}/${M}"
+  mvn clean install
 done
 
-echo "Completed ${MODULES_TO_BUILD}"
+echo "Completed ${MODULES[*]}"
