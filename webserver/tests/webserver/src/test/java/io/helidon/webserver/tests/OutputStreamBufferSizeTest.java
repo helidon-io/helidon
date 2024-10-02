100fc633924bb3e763d8cbc3cdd43cae98f2b22c
Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>
2024-10-02 12:21:15 -0400

    Allows the webserver's write buffer size to be set to 0 (#9313)
    * Allows the webserver's write buffer size to be set to 0 (or less). The underlying output stream shall not be wrapped into a buffered output stream one unless this value is greater than 0.
    
    * Improves Javadoc and renames same variables for better code readability.

---
+++ webserver/tests/webserver/src/test/java/io/helidon/webserver/tests/OutputStreamBufferSizeTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2024 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.webserver.tests;
+
+import io.helidon.webclient.http1.Http1Client;
+import io.helidon.webserver.WebServerConfig;
+import io.helidon.webserver.testing.junit5.ServerTest;
+import io.helidon.webserver.testing.junit5.SetUpServer;
+
+@ServerTest
+class OutputStreamBufferSizeTest extends OutputStreamTest {
+
+    OutputStreamBufferSizeTest(Http1Client client) {
+        super(client);
+    }
+
+    @SetUpServer
+    static void setup(WebServerConfig.Builder builder) {
+        builder.writeBufferSize(0);         // should accept buffer size of 0
+    }
+}
