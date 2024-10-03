c7c9a6e335e36d79550b9c184fc7bcff752f3954
Tim Quinn <tim.quinn@oracle.com>
2024-10-03 15:26:44 -0500

    Correct config prefix in doc for excluding health checks (#9321)
    Signed-off-by: Tim Quinn <tim.quinn@oracle.com>

--- health/health-checks/src/main/java/io/helidon/health/checks/DiskSpaceHealthCheck.java
+++ health/health-checks/src/main/java/io/helidon/health/checks/DiskSpaceHealthCheck.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2018, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,7 +56,7 @@ import org.eclipse.microprofile.health.Liveness;
  *</p>
  * <p>
  * This health check can be referred to in properties as {@code diskSpace}. So for example, to exclude this
- * health check from being exposed, use {@code helidon.health.exclude: diskSpace}.
+ * health check from being exposed, use {@code health.exclude: diskSpace}.
  * </p>
  */
 @Liveness
