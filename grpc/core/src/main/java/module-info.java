3df5dbc69496875af8756ddaf18c3dea517f1491
Joe Di Pol <joe.dipol@oracle.com>
2024-11-14 10:39:33 -0800

    Upgrade grpc-java to 1.65.1

--- grpc/core/src/main/java/module-info.java
+++ grpc/core/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,6 @@ module io.helidon.grpc.core {
 
     requires io.grpc.netty;
     requires transitive io.grpc.protobuf;
-    requires io.grpc.protobuf.lite;
     requires transitive io.grpc.stub;
     requires transitive io.grpc;
     requires io.netty.handler;
