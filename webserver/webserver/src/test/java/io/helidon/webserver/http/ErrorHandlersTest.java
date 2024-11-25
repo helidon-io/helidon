a5868d613327038745b9ef29f72e39a7a69cbce3
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-11-25 20:10:56 +0100

    Refactor assertions to use `assertThat` with appropriate matchers (#9503)

--- webserver/webserver/src/test/java/io/helidon/webserver/http/ErrorHandlersTest.java
+++ webserver/webserver/src/test/java/io/helidon/webserver/http/ErrorHandlersTest.java
@@ -44,7 +44,6 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -138,7 +137,7 @@ class ErrorHandlersTest {
             });
             fail("It is expected a CloseConnectionException");
         } catch (CloseConnectionException e) {
-            assertEquals(OtherException.class, e.getCause().getClass());
+            assertThat(e.getCause(), instanceOf(OtherException.class));
         }
     }
 
