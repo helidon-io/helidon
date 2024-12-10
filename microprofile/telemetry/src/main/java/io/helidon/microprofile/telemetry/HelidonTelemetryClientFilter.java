647d0c7b5585f8d4463c6ef5daad08d1205025e2
Tim Quinn <tim.quinn@oracle.com>
2024-12-10 11:15:06 -0600

    Add telemetry filter helper feature so developer code can influence automatic span creation (#9552)
    * Add telemetry filter helper feature so developer code can influence whether incoming or outgoing spans are automatically created
    
    * Add doc describing filter helpers; also fix some Javadoc warnings in related files
    
    * Use CDI instead of Java service loading to locate helpers

--- microprofile/telemetry/src/main/java/io/helidon/microprofile/telemetry/HelidonTelemetryClientFilter.java
+++ microprofile/telemetry/src/main/java/io/helidon/microprofile/telemetry/HelidonTelemetryClientFilter.java
@@ -17,8 +17,11 @@ package io.helidon.microprofile.telemetry;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.ServiceLoader;
 import java.util.Set;
 
+import io.helidon.common.HelidonServiceLoader;
+import io.helidon.microprofile.telemetry.spi.HelidonTelemetryClientFilterHelper;
 import io.helidon.tracing.HeaderConsumer;
 import io.helidon.tracing.HeaderProvider;
 import io.helidon.tracing.Scope;
@@ -26,6 +29,7 @@ import io.helidon.tracing.Span;
 
 import io.opentelemetry.api.baggage.Baggage;
 import io.opentelemetry.context.Context;
+import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.client.ClientRequestContext;
 import jakarta.ws.rs.client.ClientRequestFilter;
@@ -55,16 +59,31 @@ class HelidonTelemetryClientFilter implements ClientRequestFilter, ClientRespons
             Response.Status.Family.CLIENT_ERROR,
             Response.Status.Family.SERVER_ERROR);
 
+    private static final String HELPER_START_SPAN_PROPERTY = HelidonTelemetryClientFilterHelper.class.getName() + ".startSpan";
+
     private final io.helidon.tracing.Tracer helidonTracer;
 
-    @Inject
-    HelidonTelemetryClientFilter(io.helidon.tracing.Tracer helidonTracer) {
-        this.helidonTracer = helidonTracer;
-    }
+    private final List<HelidonTelemetryClientFilterHelper> helpers;
 
+    @Inject
+    HelidonTelemetryClientFilter(io.helidon.tracing.Tracer helidonTracer,
+                                 Instance<HelidonTelemetryClientFilterHelper> helpersInstance) {
+        this.helidonTracer = helidonTracer;
+        helpers = helpersInstance.stream().toList();
+    }
 
     @Override
     public void filter(ClientRequestContext clientRequestContext) {
+
+        boolean startSpan = helpers.stream().allMatch(h -> h.shouldStartSpan(clientRequestContext));
+        clientRequestContext.setProperty(HELPER_START_SPAN_PROPERTY, startSpan);
+        if (!startSpan) {
+            if (LOGGER.isLoggable(System.Logger.Level.TRACE)) {
+                LOGGER.log(System.Logger.Level.TRACE,
+                           "Client filter helper(s) voted to not start a span for " + clientRequestContext.getUri());
+            }
+            return;
+        }
 
         if (LOGGER.isLoggable(System.Logger.Level.TRACE)) {
             LOGGER.log(System.Logger.Level.TRACE, "Starting Span in a Client Request");
@@ -99,9 +118,13 @@ class HelidonTelemetryClientFilter implements ClientRequestFilter, ClientRespons
                              new RequestContextHeaderInjector(clientRequestContext.getHeaders()));
     }
 
-
     @Override
     public void filter(ClientRequestContext clientRequestContext, ClientResponseContext clientResponseContext) {
+
+        Boolean startSpanObj = (Boolean) clientRequestContext.getProperty(HELPER_START_SPAN_PROPERTY);
+        if (startSpanObj != null && !startSpanObj) {
+            return;
+        }
 
         if (LOGGER.isLoggable(System.Logger.Level.TRACE)) {
             LOGGER.log(System.Logger.Level.TRACE, "Closing Span in a Client Response");
@@ -126,6 +149,10 @@ class HelidonTelemetryClientFilter implements ClientRequestFilter, ClientRespons
 
         clientRequestContext.removeProperty(SPAN);
         clientRequestContext.removeProperty(SPAN_SCOPE);
+    }
+
+    private static List<HelidonTelemetryClientFilterHelper> helpers() {
+        return HelidonServiceLoader.create(ServiceLoader.load(HelidonTelemetryClientFilterHelper.class)).asList();
     }
 
     private static class RequestContextHeaderInjector implements HeaderConsumer {
