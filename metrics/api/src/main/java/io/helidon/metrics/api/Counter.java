9cebc589b47ad374c3fcc3fc9f13addee74b4db2
Tim Quinn <tim.quinn@oracle.com>
2025-01-07 16:39:43 -0600

    Enhance metrics doc and Javadoc on Helidon metrics API types
    Signed-off-by: Tim Quinn <tim.quinn@oracle.com>

--- metrics/api/src/main/java/io/helidon/metrics/api/Counter.java
+++ metrics/api/src/main/java/io/helidon/metrics/api/Counter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2024 Oracle and/or its affiliates.
+ * Copyright (c) 2023, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 package io.helidon.metrics.api;
 
 /**
- * Records a monotonically increasing value.
+ * Records a monotonically increasing value that is updated by invoking methods on the {@code Counter} instance.
  */
 public interface Counter extends Meter {
 
