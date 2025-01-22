17a1ebc4589111a0636a8eac5898a71ef45b5883
Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>
2025-01-22 13:28:57 -0500

    Switches from Netty to our gRPC client.
    Signed-off-by: Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>

--- config/etcd/src/main/java/module-info.java
+++ config/etcd/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2024 Oracle and/or its affiliates.
+ * Copyright (c) 2017, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,6 +29,7 @@ module io.helidon.config.etcd {
     requires io.grpc.stub;
     requires io.helidon.common.media.type;
     requires io.helidon.common;
+    requires io.helidon.webclient.grpc;
 
     requires static java.annotation;
 
