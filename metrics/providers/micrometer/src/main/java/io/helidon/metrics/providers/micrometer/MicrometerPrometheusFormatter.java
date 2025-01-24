6edf93e49c73aa7c99f57211209f79f20c984e7b
Tim Quinn <tim.quinn@oracle.com>
2025-01-24 13:35:11 -0600

    Allow colon in Prometheus meter name; add test (#9689)

--- metrics/providers/micrometer/src/main/java/io/helidon/metrics/providers/micrometer/MicrometerPrometheusFormatter.java
+++ metrics/providers/micrometer/src/main/java/io/helidon/metrics/providers/micrometer/MicrometerPrometheusFormatter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2024 Oracle and/or its affiliates.
+ * Copyright (c) 2023, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -92,7 +92,7 @@ public class MicrometerPrometheusFormatter implements MeterRegistryFormatter {
         }
 
         // Replace non-identifier characters.
-        result = result.replaceAll("[^A-Za-z0-9_]", "_");
+        result = result.replaceAll("[^A-Za-z0-9_:]", "_");
 
         return result;
     }
