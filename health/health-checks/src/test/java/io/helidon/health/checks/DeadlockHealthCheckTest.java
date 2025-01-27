4dc7600a58b4e9e2b07888e98d680f3383c00957
Tim Quinn <tim.quinn@oracle.com>
2025-01-27 11:19:52 -0600

    4.x fix status from deadlock health check if invoking the MBean fails (#9694)
    * Report ERROR instead of DOWN if MBean invocation fails
    
    * Fix confusing comment

--- health/health-checks/src/test/java/io/helidon/health/checks/DeadlockHealthCheckTest.java
+++ health/health-checks/src/test/java/io/helidon/health/checks/DeadlockHealthCheckTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2018, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,5 +62,14 @@ class DeadlockHealthCheckTest {
         DeadlockHealthCheck check = new DeadlockHealthCheck(threadBean);
         HealthCheckResponse response = check.call();
         assertThat(response.status(), is(HealthCheckResponse.Status.UP));
+    }
+
+    @Test
+
+    void errorInvokingMBean() {
+        Mockito.when(threadBean.findDeadlockedThreads()).thenThrow(new RuntimeException("Simulated error invoking MBean"));
+        DeadlockHealthCheck check = new DeadlockHealthCheck(threadBean);
+        HealthCheckResponse response = check.call();
+        assertThat(response.status(), is(HealthCheckResponse.Status.ERROR));
     }
 }
