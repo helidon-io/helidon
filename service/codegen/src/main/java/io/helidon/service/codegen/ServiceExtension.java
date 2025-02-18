939e1c90cc007ed2c04f68ddb448061391af5a3d
Tomas Langer <tomas.langer@oracle.com>
2025-02-18 10:55:22 +0100

    Checkstyle/copyright fixes.

--- service/codegen/src/main/java/io/helidon/service/codegen/ServiceExtension.java
+++ service/codegen/src/main/java/io/helidon/service/codegen/ServiceExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2024 Oracle and/or its affiliates.
+ * Copyright (c) 2023, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,6 @@ import java.util.stream.Collectors;
 import io.helidon.codegen.CodegenException;
 import io.helidon.codegen.CodegenOptions;
 import io.helidon.common.types.Annotation;
-import io.helidon.common.types.ElementKind;
 import io.helidon.common.types.TypeInfo;
 import io.helidon.common.types.TypeName;
 import io.helidon.common.types.TypedElementInfo;
