23c7fe1354be8bcf881061c92c21b3d58ca5d093
Laird Nelson <laird.nelson@oracle.com>
2025-02-20 19:39:13 -0800

    Adds afterStart(WebServer) implementations to various delegating ServerLifecycle implementations that were missing it
    Signed-off-by: Laird Nelson <laird.nelson@oracle.com>

--- webserver/webserver/src/main/java/io/helidon/webserver/ServerFeatureContextImpl.java
+++ webserver/webserver/src/main/java/io/helidon/webserver/ServerFeatureContextImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2024 Oracle and/or its affiliates.
+ * Copyright (c) 2023, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -322,6 +322,11 @@ class ServerFeatureContextImpl implements ServerFeature.ServerFeatureContext {
         @Override
         public void beforeStart() {
             delegate.get().beforeStart();
+        }
+
+        @Override
+        public void afterStart(WebServer webServer) {
+            delegate.get().afterStart(webServer);
         }
 
         @Override
--- webserver/webserver/src/main/java/io/helidon/webserver/http/HttpRouteImpl.java
+++ webserver/webserver/src/main/java/io/helidon/webserver/http/HttpRouteImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ import io.helidon.http.HttpPrologue;
 import io.helidon.http.Method;
 import io.helidon.http.PathMatcher;
 import io.helidon.http.PathMatchers;
+import io.helidon.webserver.WebServer;
 
 class HttpRouteImpl extends HttpRouteBase implements HttpRoute {
     private final Handler handler;
@@ -51,6 +52,11 @@ class HttpRouteImpl extends HttpRouteBase implements HttpRoute {
     @Override
     public void beforeStart() {
         handler.beforeStart();
+    }
+
+    @Override
+    public void afterStart(WebServer webServer) {
+        handler.afterStart(webServer);
     }
 
     @Override
