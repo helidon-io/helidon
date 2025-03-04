af0d40a37eb7b3cd89cb768550b9aa558144b0c0
Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>
2025-03-04 12:22:55 -0500

    Accepts colon ':' as a valid character in a URI as mandated by RFC 3986. (#9864)

--- common/uri/src/main/java/io/helidon/common/uri/UriValidator.java
+++ common/uri/src/main/java/io/helidon/common/uri/UriValidator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Oracle and/or its affiliates.
+ * Copyright (c) 2024, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -151,7 +151,7 @@ public final class UriValidator {
         }
 
         // query = *( pchar / "/" / "?" )
-        // pchar = unreserved / pct-encoded / sub-delims / "@"
+        // pchar = unreserved / pct-encoded / sub-delims / ":" / "@"
         // unreserved = ALPHA / DIGIT / "-" / "." / "_" / "~"
         // pct-encoded = "%" HEXDIG HEXDIG
         // sub-delims = "!" / "$" / "&" / "'" / "(" / ")" / "*" / "+" / "," / ";" / "="
@@ -167,6 +167,9 @@ public final class UriValidator {
                 continue;
             }
             if (c == '@') {
+                continue;
+            }
+            if (c == ':') {
                 continue;
             }
             if (c == '/') {
