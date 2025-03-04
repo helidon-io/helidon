a955059f82e44d8f2b7d84ed3b7c15695c91373f
Tim Quinn <tim.quinn@oracle.com>
2025-03-04 10:06:55 -0600

    Unused import

--- tracing/providers/opentelemetry/src/main/java/io/helidon/tracing/providers/opentelemetry/OpenTelemetryTracerBuilder.java
+++ tracing/providers/opentelemetry/src/main/java/io/helidon/tracing/providers/opentelemetry/OpenTelemetryTracerBuilder.java
@@ -21,7 +21,6 @@ import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Supplier;
