62738917802f6812702b46b43c775ad7fe0e1ce3
Tomáš Kraus <tomas.kraus@oracle.com>
2025-03-13 16:30:34 +0100

    Fixed tests and renamed test modules and packages.
    Signed-off-by: Tomáš Kraus <tomas.kraus@oracle.com>

---
+++ data/tests/sql/hikari/src/test/java/io/helidon/data/tests/sql/hikari/TestHikari.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2024, 2025 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.data.tests.sql.hikari;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+import javax.sql.DataSource;
+
+import io.helidon.common.types.TypeName;
+import io.helidon.config.Config;
+import io.helidon.data.sql.datasource.DataSourceConfig;
+import io.helidon.data.sql.datasource.hikari.HikariDataSourceConfig;
+import io.helidon.service.registry.Service;
+import io.helidon.service.registry.ServiceRegistry;
+import io.helidon.service.registry.ServiceRegistryManager;
+import io.helidon.testing.junit5.suite.Suite;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@Suite(MySqlSuite.class)
+@Testcontainers(disabledWithoutDocker = true)
+class TestHikari {
+
+    private static ServiceRegistryManager registryManager;
+    private static ServiceRegistry registry;
+
+    @BeforeAll
+    public static void initRegistry() {
+        registryManager = ServiceRegistryManager.create();
+        registry = registryManager.registry();
+    }
+
+    @AfterAll
+    public static void tearDownRegistry() {
+        registryManager.shutdown();
+    }
+
+    @Test
+    void testDataSourceConfig(Config config) {
+        assertThat(config.exists(), is(true));
+        DataSourceConfig dataSourceConfig = DataSourceConfig.create(config.get("data-source.0"));
+        HikariDataSourceConfig hikariDataSourceConfig = (HikariDataSourceConfig) dataSourceConfig.provider();
+        assertThat(hikariDataSourceConfig.username().isPresent(), is(true));
+        assertThat(hikariDataSourceConfig.username().get(),
+                   is(config.get("data-source.0.provider.hikari.username").as(String.class).get()));
+        assertThat(hikariDataSourceConfig.password().isPresent(), is(true));
+        assertThat(new String(hikariDataSourceConfig.password().get()),
+                   is(config.get("data-source.0.provider.hikari.password").as(String.class).get()));
+        assertThat(hikariDataSourceConfig.connectionString().isPresent(), is(true));
+        assertThat(hikariDataSourceConfig.connectionString().get(),
+                   is(config.get("data-source.0.provider.hikari.connection-string").as(String.class).get()));
+    }
+
+    @Test
+    void testDataSourceRegistry() {
+        TypeName hikariName = TypeName.create("io.helidon.data.sql.datasource.hikari.HikariDataSourceProviderService");
+        Supplier<Service.ServicesFactory<DataSource>> provider = registry.supply(hikariName);
+        assertThat(provider, notNullValue());
+        List<Service.QualifiedInstance<DataSource>> services = provider.get().services();
+        assertThat(services.size(), is(1));
+    }
+
+}
