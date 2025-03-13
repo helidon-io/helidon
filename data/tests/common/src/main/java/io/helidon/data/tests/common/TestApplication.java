300ac6c80447b458eed62d4732c05055394d1a3f
Tomáš Kraus <tomas.kraus@oracle.com>
2025-03-13 16:30:34 +0100

    Fixed tests and renamed test modules and packages.
    Signed-off-by: Tomáš Kraus <tomas.kraus@oracle.com>

---
+++ data/tests/common/src/main/java/io/helidon/data/tests/common/TestApplication.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2025 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.data.tests.common;
+
+import java.util.Optional;
+
+import io.helidon.data.DataConfig;
+import io.helidon.data.ProviderConfig;
+import io.helidon.data.jakarta.persistence.DataJpaConfig;
+import io.helidon.data.tests.application.ApplicationData;
+import io.helidon.data.tests.model.Pokemon;
+import io.helidon.data.tests.repository.PokemonRepository;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class TestApplication {
+
+    private static final System.Logger LOGGER = System.getLogger(TestApplication.class.getName());
+
+    // Verify full Data instance cycle from application context
+    @Test
+    void testDataInitialization(DataConfig dataConfig) throws Exception {
+        LOGGER.log(System.Logger.Level.DEBUG, "Running testDataInitialization");
+        // Turn off DDL drop-and-create
+        DataConfig.Builder builder = DataConfig.builder()
+                .name(dataConfig.name());
+
+        ProviderConfig provider = dataConfig.provider();
+        if (provider instanceof DataJpaConfig jakartaConfig) {
+            // this is the expected one
+            builder.provider(DataJpaConfig.builder()
+                                     .from(jakartaConfig)
+                                     .putProperty("jakarta.persistence.schema-generation.database.action", "none")
+                                     .build());
+        }
+        DataConfig newDataConfig = builder.build();
+
+        ApplicationData applicationData = new ApplicationData(newDataConfig);
+        MatcherAssert.assertThat(applicationData.data(), notNullValue());
+        io.helidon.data.DataRegistry data = applicationData.data();
+        PokemonRepository pokemonRepository = data.repository(PokemonRepository.class);
+        Optional<Pokemon> pokemon = pokemonRepository.findById(InitialData.POKEMONS[1].getId());
+        assertThat(pokemon.isPresent(), is(true));
+        assertThat(pokemon.get(), is(InitialData.POKEMONS[1]));
+        data.close();
+    }
+
+}
---
+++ data/tests/model/src/main/java/io/helidon/data/tests/model/League.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2025 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.data.tests.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+/**
+ * Tests Model: Pokemon league.
+ */
+@Entity
+@Table(name = "LEAGUE")
+public class League {
+
+    @Id
+    private int id;
+
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "REGION_ID")
+    private Region region;
+
+    public League() {
+        this(-1, null, null);
+    }
+
+    public League(int id, String name, Region region) {
+        this.id = id;
+        this.name = name;
+        this.region = region;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        return id == ((League) obj).id
+                && Objects.equals(name, ((League) obj).name)
+                && Objects.equals(region, ((League) obj).region);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.getClass().getSimpleName());
+        sb.append(" {id=");
+        sb.append(id);
+        sb.append(", name=");
+        sb.append(name);
+        sb.append(", region=");
+        sb.append(region.toString());
+        sb.append("}");
+        return sb.toString();
+    }
+
+}
