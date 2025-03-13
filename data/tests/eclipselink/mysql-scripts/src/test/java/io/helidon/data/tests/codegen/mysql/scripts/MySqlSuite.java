f3236471247fb30d9f277b8c8959131d9be5a481
Tomáš Kraus <tomas.kraus@oracle.com>
2025-03-13 16:30:34 +0100

    Fixed tests and renamed test modules and packages.
    Signed-off-by: Tomáš Kraus <tomas.kraus@oracle.com>

---
+++ data/tests/eclipselink/mysql-scripts/src/test/java/io/helidon/data/tests/codegen/mysql/scripts/MySqlSuite.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2024, 2025 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.data.tests.codegen.mysql.scripts;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.helidon.config.Config;
+import io.helidon.config.ConfigSources;
+import io.helidon.data.DataConfig;
+import io.helidon.data.sql.testing.SqlTestContainerConfig;
+import io.helidon.testing.junit5.suite.AfterSuite;
+import io.helidon.testing.junit5.suite.BeforeSuite;
+import io.helidon.testing.junit5.suite.SuiteResolver;
+import io.helidon.testing.junit5.suite.spi.SuiteProvider;
+
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+/**
+ * MySQL suite.
+ */
+public class MySqlSuite implements SuiteProvider, SuiteResolver {
+
+    private static final System.Logger LOGGER = System.getLogger(MySqlSuite.class.getName());
+
+    private static final String URL_NODE = "data.provider.jakarta.connection-string";
+    private static final int DB_PORT = 3306;
+    @Container
+    private final MySQLContainer<?> container;
+    private Config config;
+    private DataConfig dataConfig;
+
+    public MySqlSuite() {
+        this.config = Config.just(ConfigSources.classpath("application.yaml"));
+        this.container = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"));
+        this.dataConfig = null;
+        SqlTestContainerConfig.configureContainer(config, container);
+    }
+
+    @BeforeSuite
+    public void beforeSuite() {
+        container.start();
+        // Update URL in config with exposed port
+        String url = SqlTestContainerConfig.replacePortInUrl(config.get(URL_NODE)
+                .as(String.class).get(), container.getMappedPort(DB_PORT));
+        Map<String, String> updatedNodes = new HashMap<>(1);
+        updatedNodes.put(URL_NODE, url);
+        config = Config.create(ConfigSources.create(updatedNodes), ConfigSources.create(config));
+        dataConfig = DataConfig.create(config.get("data"));
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        container.stop();
+    }
+
+    @Override
+    public boolean supportsParameter(Type type) {
+        return (type instanceof Class<?> cls)
+                && (Config.class.isAssignableFrom(cls)
+                        || DataConfig.class.isAssignableFrom(cls));
+    }
+
+    @Override
+    public Object resolveParameter(Type type) {
+        if (type instanceof Class<?> cls) {
+            if (Config.class.isAssignableFrom(cls)) {
+                return config;
+            } else if (DataConfig.class.isAssignableFrom(cls)) {
+                return dataConfig;
+            }
+        }
+        throw new IllegalArgumentException(String.format("Cannot resolve parameter Type %s", type.getTypeName()));
+    }
+
+}
--- transaction/narayana/src/test/java/io/helidon/transaction/narayana/TestLookup.java
+++ transaction/narayana/src/test/java/io/helidon/transaction/narayana/TestLookup.java
@@ -15,6 +15,8 @@
  */
 package io.helidon.transaction.narayana;
 
+import java.util.Optional;
+
 import io.helidon.service.registry.Services;
 import io.helidon.transaction.jta.JtaProvider;
 
@@ -30,23 +32,20 @@ import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.startsWith;
 
 public class TestLookup {
-/*
     // JtaProvider.get() shall return NarayanaProvider instance
     @Test
     void testLookup() {
-        assertThat(Services.first(JtaProvider.class).isPresent(), is(true));
-        JtaProvider provider = JtaProvider.provider().get();
-        assertThat(provider, notNullValue());
-        assertThat(provider, instanceOf(NarayanaProvider.class));
+        Optional<JtaProvider> provider = Services.first(JtaProvider.class);
+        assertThat(provider.isPresent(), is(true));
+        assertThat(provider.get(), instanceOf(NarayanaProvider.class));
     }
 
     // TransactionManager instance shall have "com.arjuna.ats" package prefix
     @Test
     void testTransactionManager() {
-        assertThat(JtaProvider.provider().isPresent(), is(true));
-        JtaProvider provider = JtaProvider.provider().get();
-        assertThat(provider, notNullValue());
-        TransactionManager tm = provider.transactionManager();
+        Optional<JtaProvider> provider = Services.first(JtaProvider.class);
+        assertThat(provider.isPresent(), is(true));
+        TransactionManager tm = provider.get().transactionManager();
         assertThat(tm, notNullValue());
         assertThat(tm.getClass().getName(), startsWith("com.arjuna.ats"));
     }
@@ -54,10 +53,9 @@ public class TestLookup {
     // UserTransaction instance shall have "com.arjuna.ats" package prefix
     @Test
     void testUserTransaction() {
-        assertThat(JtaProvider.provider().isPresent(), is(true));
-        JtaProvider provider = JtaProvider.provider().get();
-        assertThat(provider, notNullValue());
-        UserTransaction ut = provider.userTransaction();
+        Optional<JtaProvider> provider = Services.first(JtaProvider.class);
+        assertThat(provider.isPresent(), is(true));
+        UserTransaction ut = provider.get().userTransaction();
         assertThat(ut, notNullValue());
         assertThat(ut.getClass().getName(), startsWith("com.arjuna.ats"));
     }
@@ -65,12 +63,11 @@ public class TestLookup {
     // TransactionSynchronizationRegistry instance shall have "com.arjuna.ats" package prefix
     @Test
     void testTransactionSynchronizationRegistry() {
-        assertThat(JtaProvider.provider().isPresent(), is(true));
-        JtaProvider provider = JtaProvider.provider().get();
-        assertThat(provider, notNullValue());
-        TransactionSynchronizationRegistry tsr = provider.transactionSynchronizationRegistry();
+        Optional<JtaProvider> provider = Services.first(JtaProvider.class);
+        assertThat(provider.isPresent(), is(true));
+        TransactionSynchronizationRegistry tsr = provider.get().transactionSynchronizationRegistry();
         assertThat(tsr, notNullValue());
         assertThat(tsr.getClass().getName(), startsWith("com.arjuna.ats"));
     }
-*/
+
 }
