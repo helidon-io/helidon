3c4101f6d08e6a0b5fb992406d27b08f225c6f09
Tomas Langer <tomas.langer@oracle.com>
2025-04-01 14:56:22 +0200

    4.x: Fix static content handling, where jar file was closed when it should… (#9943)
    * Fix static content handling, where jar file was closed when it should not have been closed.
    * Closed ZIP file test #9908
    ---------
    
    Signed-off-by: Daniel Kec <daniel.kec@oracle.com>
    Co-authored-by: Daniel Kec <daniel.kec@oracle.com>

--- webserver/static-content/src/main/java/io/helidon/webserver/staticcontent/ClassPathContentHandler.java
+++ webserver/static-content/src/main/java/io/helidon/webserver/staticcontent/ClassPathContentHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2024 Oracle and/or its affiliates.
+ * Copyright (c) 2017, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -224,8 +224,13 @@ class ClassPathContentHandler extends FileBasedContentHandler {
         var contentType = detectType(fileName(url));
         Optional<Instant> lastModified;
 
-        try (JarFile jarFile = jarUrlConnection.getJarFile()) {
+        JarFile jarFile = jarUrlConnection.getJarFile();
+        try {
             lastModified = lastModified(jarFile.getName());
+        } finally {
+            if (!jarUrlConnection.getUseCaches()) {
+                jarFile.close();
+            }
         }
 
         var lastModifiedHandler = lastModifiedHandler(lastModified);
