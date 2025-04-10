85aa8c6c3555047ebf77354c288a7d26bbe1cfd8
Tomáš Kraus <tomas.kraus@oracle.com>
2025-04-10 19:45:02 +0200

    Resource local transactions in JPA finished.
    Signed-off-by: Tomáš Kraus <tomas.kraus@oracle.com>

--- data/jakarta-persistence/jakarta-persistence/src/main/java/io/helidon/data/jakarta/persistence/DataJpaSupport.java
+++ data/jakarta-persistence/jakarta-persistence/src/main/java/io/helidon/data/jakarta/persistence/DataJpaSupport.java
@@ -17,21 +17,14 @@
 package io.helidon.data.jakarta.persistence;
 
 import java.util.Set;
-import java.util.concurrent.Callable;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
-import io.helidon.common.Functions;
 import io.helidon.data.DataConfig;
-import io.helidon.data.DataException;
 import io.helidon.data.jakarta.persistence.spi.JakartaPersistenceExtension;
 import io.helidon.data.spi.DataSupport;
 import io.helidon.data.spi.RepositoryFactory;
-import io.helidon.transaction.Tx;
 
 import jakarta.persistence.EntityManagerFactory;
-
-import static io.helidon.data.jakarta.persistence.JpaExtensionImpl.TRANSACTION_TYPE;
 
 @SuppressWarnings("deprecation")
 class DataJpaSupport implements DataSupport {
@@ -64,124 +57,6 @@ class DataJpaSupport implements DataSupport {
     }
 
     @Override
-    public <T> T transaction(Tx.Type type, Callable<T> task) {
-        // Jakarta Persistence 3.2 compliant code disabled
-        // PersistenceUnitTransactionType transactionType = factory.getTransactionType();
-        // Jakarta Persistence 3.1 workaround
-        PersistenceUnitTransactionType transactionType
-                = (PersistenceUnitTransactionType) factory.getProperties().get(TRANSACTION_TYPE);
-        int counter = initTransaction(factory, transactionType);
-        try {
-            beginTransaction(transactionType, type);
-            T result = task.call();
-            commitTransaction(transactionType, type);
-            return result;
-        } catch (DataException e) {
-            if (LOGGER.isLoggable(System.Logger.Level.DEBUG)) {
-                LOGGER.log(System.Logger.Level.DEBUG, String.format("Data repository transaction failed: %s", e.getMessage()));
-            }
-            rollbackTransaction(transactionType, type);
-            throw e;
-        } catch (Exception e) {
-            if (LOGGER.isLoggable(System.Logger.Level.DEBUG)) {
-                LOGGER.log(System.Logger.Level.DEBUG, String.format("Data repository transaction failed: %s", e.getMessage()));
-            }
-            rollbackTransaction(transactionType, type);
-            throw new DataException("Execution of transaction task failed", e);
-            // Also validates whether transaction life-cycle was handled properly
-        } finally {
-            closeTransaction(counter);
-        }
-    }
-
-    @Override
-    public <E extends Throwable> void transaction(Tx.Type type, Functions.CheckedRunnable<E> task) {
-        // Jakarta Persistence 3.2 compliant code disabled
-        // PersistenceUnitTransactionType transactionType = factory.getTransactionType();
-        // Jakarta Persistence 3.1 workaround
-        PersistenceUnitTransactionType transactionType
-                = (PersistenceUnitTransactionType) factory.getProperties().get(TRANSACTION_TYPE);
-        int counter = initTransaction(factory, transactionType);
-        try {
-            beginTransaction(transactionType, type);
-            task.run();
-            commitTransaction(transactionType, type);
-        } catch (DataException e) {
-            if (LOGGER.isLoggable(System.Logger.Level.DEBUG)) {
-                LOGGER.log(System.Logger.Level.DEBUG, String.format("Data repository transaction failed: %s", e.getMessage()));
-            }
-            rollbackTransaction(transactionType, type);
-            throw e;
-        } catch (Throwable e) {
-            if (LOGGER.isLoggable(System.Logger.Level.DEBUG)) {
-                LOGGER.log(System.Logger.Level.DEBUG, String.format("Data repository transaction failed: %s", e.getMessage()));
-            }
-            rollbackTransaction(transactionType, type);
-            throw new DataException("Execution of transaction task failed", e);
-            // Also validates whether transaction life-cycle was handled properly
-        } finally {
-            closeTransaction(counter);
-        }
-    }
-
-    @Override
-    public <T> T transaction(Tx.Type type, Function<Tx.Transaction, T> task) {
-        // Jakarta Persistence 3.2 compliant code disabled
-        // PersistenceUnitTransactionType transactionType = factory.getTransactionType();
-        // Jakarta Persistence 3.1 workaround
-        PersistenceUnitTransactionType transactionType
-                = (PersistenceUnitTransactionType) factory.getProperties().get(TRANSACTION_TYPE);
-        int counter = initTransaction(factory, transactionType);
-        beginManualTransaction(counter, transactionType, type);
-        try {
-            return task.apply(new ManualTransaction(transactionType, type));
-        } catch (RuntimeException e) {
-            if (LOGGER.isLoggable(System.Logger.Level.DEBUG)) {
-                LOGGER.log(System.Logger.Level.DEBUG, String.format("Data repository transaction failed: %s", e.getMessage()));
-            }
-            throw e;
-            // Also validates whether user managed transaction life-cycle was handled properly
-        } finally {
-            closeTransaction(counter);
-        }
-    }
-
-    @Override
-    public void transaction(Tx.Type type, Consumer<Tx.Transaction> task) {
-        // Jakarta Persistence 3.2 compliant code disabled
-        // PersistenceUnitTransactionType transactionType = factory.getTransactionType();
-        // Jakarta Persistence 3.1 workaround
-        PersistenceUnitTransactionType transactionType
-                = (PersistenceUnitTransactionType) factory.getProperties().get(TRANSACTION_TYPE);
-        int counter = initTransaction(factory, transactionType);
-        beginManualTransaction(counter, transactionType, type);
-        try {
-            task.accept(new ManualTransaction(transactionType, type));
-        } catch (RuntimeException e) {
-            if (LOGGER.isLoggable(System.Logger.Level.DEBUG)) {
-                LOGGER.log(System.Logger.Level.DEBUG, String.format("Data repository transaction failed: %s", e.getMessage()));
-            }
-            throw e;
-            // Also validates whether user managed transaction life-cycle was handled properly
-        } finally {
-            closeTransaction(counter);
-        }
-    }
-
-    @Override
-    public Tx.Transaction transaction(Tx.Type type) {
-        // Jakarta Persistence 3.2 compliant code disabled
-        // PersistenceUnitTransactionType transactionType = factory.getTransactionType();
-        // Jakarta Persistence 3.1 workaround
-        PersistenceUnitTransactionType transactionType
-                = (PersistenceUnitTransactionType) factory.getProperties().get(TRANSACTION_TYPE);
-        int counter = initTransaction(factory, transactionType);
-        beginManualTransaction(counter, transactionType, type);
-        // No validation of transaction life-cycle is possible without UserTransaction being AutoClosable
-        return new UserTransaction(counter, transactionType, type);
-    }
-
-    @Override
     public void close() {
         factory.close();
     }
@@ -194,39 +69,6 @@ class DataJpaSupport implements DataSupport {
     @Override
     public String toString() {
         return type() + " data support, config: " + config;
-    }
-
-    private static void beginManualTransaction(int counter, PersistenceUnitTransactionType transactionType, Tx.Type type) {
-        try {
-            beginTransaction(transactionType, type);
-        } catch (RuntimeException ex) {
-            try {
-                rollbackTransaction(transactionType, type);
-            } finally {
-                closeTransaction(counter);
-            }
-            throw ex;
-        }
-    }
-
-    private static int initTransaction(EntityManagerFactory factory, PersistenceUnitTransactionType transactionType) {
-        return TransactionContext.getInstance().initTransaction(factory, transactionType);
-    }
-
-    private static void closeTransaction(int counter) {
-        TransactionContext.getInstance().closeTransaction(counter);
-    }
-
-    private static void beginTransaction(PersistenceUnitTransactionType transactionType, Tx.Type type) {
-        TransactionContext.getInstance().beginTransaction(transactionType, type);
-    }
-
-    private static void commitTransaction(PersistenceUnitTransactionType transactionType, Tx.Type type) {
-        TransactionContext.getInstance().commitTransaction(transactionType, type);
-    }
-
-    private static void rollbackTransaction(PersistenceUnitTransactionType transactionType, Tx.Type type) {
-        TransactionContext.getInstance().rollbackTransaction(transactionType, type);
     }
 
     private static class RepositoryFactoryImpl implements RepositoryFactory {
@@ -244,44 +86,4 @@ class DataJpaSupport implements DataSupport {
         }
     }
 
-    // Manual transaction handler
-    private record ManualTransaction(PersistenceUnitTransactionType transactionType,
-                                     Tx.Type type) implements Tx.Transaction {
-
-        @Override
-        public void commit() {
-            commitTransaction(transactionType, type);
-        }
-
-        @Override
-        public void rollback() {
-            rollbackTransaction(transactionType, type);
-        }
-    }
-
-    // User transaction handler
-    // Transaction context cleanup is done as part of commit and rollback calls. Cleanup must be done even with RuntimeException
-    // being thrown.
-    private record UserTransaction(int counter,
-                                   PersistenceUnitTransactionType transactionType,
-                                   Tx.Type type) implements Tx.Transaction {
-
-        @Override
-        public void commit() {
-            try {
-                commitTransaction(transactionType, type);
-            } finally {
-                closeTransaction(counter);
-            }
-        }
-
-        @Override
-        public void rollback() {
-            try {
-                rollbackTransaction(transactionType, type);
-            } finally {
-                closeTransaction(counter);
-            }
-        }
-    }
 }
