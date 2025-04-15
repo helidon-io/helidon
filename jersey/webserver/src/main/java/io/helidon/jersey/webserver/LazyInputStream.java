8219743a4171b129f87d5143e6d633c9d71468a5
Tomas Langer <tomas.langer@oracle.com>
2025-04-15 23:29:52 +0200

    4.x: Support Jersey on Helidon SE WebServer (#9985)
    * Support Jersey on Helidon SE WebServer
    * Javadoc fix, as Jakarta inject now has module-info
    * Comment update to align with new module location

--- jersey/webserver/src/main/java/io/helidon/jersey/webserver/LazyInputStream.java
+++ jersey/webserver/src/main/java/io/helidon/jersey/webserver/LazyInputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Oracle and/or its affiliates.
+ * Copyright (c) 2024, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.helidon.microprofile.server;
+package io.helidon.jersey.webserver;
 
 import java.io.IOException;
 import java.io.InputStream;
