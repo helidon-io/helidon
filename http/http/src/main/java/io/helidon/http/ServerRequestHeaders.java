2a66aebd932b964b15af7d76cd7395ab36c2d1e6
Tomas Langer <tomas.langer@oracle.com>
2025-04-29 00:52:56 +0200

    Added support for matching server request headers in `HttpRoute`, to enable routes for specific content types, and/or Accept headers (and any other headers in the request). Added a few convenience methods to headers for easier implementation. Added test

--- http/http/src/main/java/io/helidon/http/ServerRequestHeaders.java
+++ http/http/src/main/java/io/helidon/http/ServerRequestHeaders.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, 2024 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -106,19 +106,23 @@ public interface ServerRequestHeaders extends Headers {
      * A media type is accepted if the {@code Accept} header is not present in the
      * request or if it contains the provided media type.
      *
-     * @param mediaType the media type to test
+     * @param mediaTypes the media type(s) to test
      * @return {@code true} if provided type is acceptable, {@code false} otherwise
      * @throws NullPointerException if the provided type is {@code null}.
      */
-    default boolean isAccepted(MediaType mediaType) {
+    @Override
+    default boolean isAccepted(MediaType... mediaTypes) {
         List<HttpMediaType> accepted = acceptedTypes();
         if (accepted.isEmpty()) {
             return true;
         }
         for (HttpMediaType acceptedType : accepted) {
-            if (acceptedType.test(mediaType)) {
-                return true;
+            for (MediaType type : mediaTypes) {
+                if (acceptedType.test(type)) {
+                    return true;
+                }
             }
+
         }
         return false;
     }
@@ -225,5 +229,33 @@ public interface ServerRequestHeaders extends Headers {
                     .map(URI::create);
         }
         return Optional.empty();
+    }
+
+    /**
+     * Check if the content type provided over the network matches one of the content types.
+     * If any of the media types is {@link io.helidon.common.media.type.MediaTypes#WILDCARD}, this method always returns
+     * {@code true}.
+     * If {@link #contentType()} is not defined, this method returns false.
+     *
+     * @param mediaTypes media types that we check against
+     * @return {@code true} if any of the media types provided matches the {@link #contentType()}
+     */
+    default boolean testContentType(MediaType... mediaTypes) {
+        for (MediaType mediaType : mediaTypes) {
+            if (mediaType.isWildcardType() && mediaType.isWildcardSubtype()) {
+                return true;
+            }
+        }
+        Optional<HttpMediaType> httpMediaType = contentType();
+        if (httpMediaType.isEmpty()) {
+            return false;
+        }
+        HttpMediaType contentType = httpMediaType.get();
+        for (MediaType mediaType : mediaTypes) {
+            if (contentType.test(mediaType)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
