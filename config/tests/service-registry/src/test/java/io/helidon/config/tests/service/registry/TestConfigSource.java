35320f4bb732929860e08636aba5e5cb5478b81c
Tomas Langer <tomas.langer@oracle.com>
2025-04-29 00:24:03 +0200

    4.x: CDI bridge for service registry + config factory improvements (#9845)
    * Add support for injection of io.helidon.config.Config
    * Reduce weight of the config factory, so it can be easily overridden
    * Fix error in javadoc of GlobalConfig
    * Update test to validate the new feature
    * Use service registry to create Config even in MP
    * updated service registry extension to fix a few discovered issues

--- config/tests/service-registry/src/test/java/io/helidon/config/tests/service/registry/TestConfigSource.java
+++ config/tests/service-registry/src/test/java/io/helidon/config/tests/service/registry/TestConfigSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2024 Oracle and/or its affiliates.
+ * Copyright (c) 2023, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import io.helidon.config.spi.ConfigNode;
 import io.helidon.config.spi.NodeConfigSource;
 import io.helidon.service.registry.Service;
 
-@Service.Provider
+@Service.Singleton
 @Weight(200)
 class TestConfigSource implements NodeConfigSource {
 
