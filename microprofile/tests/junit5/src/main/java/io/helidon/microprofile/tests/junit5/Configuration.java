8c19d319157bcde57ca9aa9269954f9e6c911543
Andrei Arlou <andrei.arlou@gmail.com>
2025-05-08 00:36:36 +0300

    3.x: Add docs about configuration ordering in tests (#9994) (#10062)

--- microprofile/tests/junit5/src/main/java/io/helidon/microprofile/tests/junit5/Configuration.java
+++ microprofile/tests/junit5/src/main/java/io/helidon/microprofile/tests/junit5/Configuration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@ public @interface Configuration {
     boolean useExisting() default false;
 
     /**
-     * Class path properties config sources to add to configuration of this test class or method.
+     * Class path properties or yaml config sources to add to configuration of this test class or method.
      *
      * @return config sources to add
      */
