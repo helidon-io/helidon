8861191a159da4fd1e6b6993c57c92c61010a145
Prasanth Omanakuttan <pomanakuttan9642@gmail.com>
2025-05-13 05:43:26 +0530

    Update sampler type options in JaegerTracerBuilder. (#9257)
    * Update sampler type options in JaegerTracerBuilder.
    
    * Update JaegerTracerBuilder.java
    
    Fix copyright year
    
    ---------
    
    Co-authored-by: Romain Grecourt <romain.grecourt@oracle.com>

--- tracing/providers/jaeger/src/main/java/io/helidon/tracing/providers/jaeger/JaegerTracerBuilder.java
+++ tracing/providers/jaeger/src/main/java/io/helidon/tracing/providers/jaeger/JaegerTracerBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2024 Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -646,7 +646,7 @@ public class JaegerTracerBuilder implements TracerBuilder<JaegerTracerBuilder> {
 
     /**
      * Sampler type definition.
-     * Available options are "const", "probabilistic", "ratelimiting" and "remote".
+     * Available options are "const" and "ratio".
      */
     public enum SamplerType {
         /**
