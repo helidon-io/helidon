0c2d748d66ae6494c206d89ef772a435b80a6ae6
Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>
2025-05-13 14:16:28 -0400

    Compares values of the Upgrade header ignoring case. Unfortunately, some servers may return 'WebSocket' and others 'websocket' for example.
    Signed-off-by: Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>

--- webclient/http1/src/main/java/io/helidon/webclient/http1/Http1ClientRequestImpl.java
+++ webclient/http1/src/main/java/io/helidon/webclient/http1/Http1ClientRequestImpl.java
@@ -153,8 +153,8 @@ class Http1ClientRequestImpl extends ClientRequestBase<Http1ClientRequest, Http1
         }
 
         if (response.status() == Status.SWITCHING_PROTOCOLS_101) {
-            // yep, this is the response we want
-            if (response.headers().contains(requestedUpgrade)) {
+            // is the upgrade request successful?
+            if (isUpgradeSuccessful(requestedUpgrade, response.headers().get(HeaderNames.UPGRADE))) {
                 if (LOGGER.isLoggable(System.Logger.Level.TRACE)) {
                     response.connection()
                             .helidonSocket().log(LOGGER,
@@ -201,6 +201,22 @@ class Http1ClientRequestImpl extends ClientRequestBase<Http1ClientRequest, Http1
 
     Http1ClientImpl http1Client() {
         return http1Client;
+    }
+
+    /**
+     * Check upgrade protocols. Protocol names are case insensitive.
+     *
+     * @param requestUpgrade request upgrade header
+     * @param responseUpgrade response upgrade header
+     * @return check if protocol upgrade can proceed
+     */
+    static boolean isUpgradeSuccessful(Header requestUpgrade, Header responseUpgrade) {
+        for (String protocol : requestUpgrade.allValues()) {
+            if (protocol.equalsIgnoreCase(responseUpgrade.get())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     Http1ClientResponseImpl invokeRequestWithEntity(byte[] entity) {
