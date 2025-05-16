373ab235d0dc9915a41999c50d374b688f829324
Arjav Desai <arjav.desai@oracle.com>
2025-05-16 07:34:33 -0500

    add coherence provider - embedding store support

---
+++ integrations/langchain4j/providers/coherence/src/main/java/io/helidon/integrations/langchain4j/providers/coherence/CoherenceEmbeddingStoreFactory.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2025 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.integrations.langchain4j.providers.coherence;
+
+import java.util.List;
+
+import io.helidon.common.Weight;
+import io.helidon.common.Weighted;
+import io.helidon.common.config.Config;
+import io.helidon.service.registry.Lookup;
+import io.helidon.service.registry.Qualifier;
+import io.helidon.service.registry.Service;
+import io.helidon.service.registry.ServiceRegistry;
+import io.helidon.service.registry.ServiceRegistryException;
+
+import dev.langchain4j.store.embedding.coherence.CoherenceEmbeddingStore;
+
+/**
+ * Factory class for creating a configured {@link dev.langchain4j.store.embedding.coherence.CoherenceEmbeddingStore}.
+ *
+ * @see dev.langchain4j.store.embedding.coherence.CoherenceEmbeddingStore
+ * @see CoherenceEmbeddingStoreConfig
+ */
+@Service.Singleton
+@Service.Named(Service.Named.WILDCARD_NAME)
+@Weight(Weighted.DEFAULT_WEIGHT - 10)
+class CoherenceEmbeddingStoreFactory implements Service.ServicesFactory<CoherenceEmbeddingStore> {
+    private static final Qualifier COHERENCE_QUALIFIER = Qualifier.createNamed("coherence");
+
+    private final CoherenceEmbeddingStore store;
+    private final boolean enabled;
+
+    /**
+     * Creates {@link CoherenceEmbeddingStore}.
+     */
+    @Service.Inject
+    CoherenceEmbeddingStoreFactory(ServiceRegistry registry, Config config) {
+        var storeConfig = CoherenceEmbeddingStoreConfig.create(config.get(CoherenceEmbeddingStoreConfig.CONFIG_ROOT));
+
+        this.enabled = storeConfig.enabled();
+
+        if (enabled) {
+            this.store = buildStore(registry, storeConfig);
+        } else {
+            this.store = null;
+        }
+    }
+
+    @Override
+    public List<Service.QualifiedInstance<CoherenceEmbeddingStore>> services() {
+        if (enabled) {
+            return List.of(Service.QualifiedInstance.create(store),
+                           Service.QualifiedInstance.create(store, COHERENCE_QUALIFIER));
+        }
+        return List.of();
+    }
+
+    private static DataSource named(ServiceRegistry registry, String name) {
+        if (Service.Named.DEFAULT_NAME.equals(name)) {
+            return registry.get(DataSource.class);
+        }
+        return registry.<DataSource>first(Lookup.builder()
+                                                  .addContract(DataSource.class)
+                                                  .addQualifier(Qualifier.createNamed(name))
+                                                  .build())
+                .orElseThrow(() -> new ServiceRegistryException("Cannot find DataSource with name " + name
+                                                                        + " configured for "
+                                                                        + "CoherenceeEmbeddingStoreFactory."));
+    }
+
+    /**
+     * Creates and returns an {@link dev.langchain4j.store.embedding.coherence.CoherenceEmbeddingStore} configured using the provided
+     * configuration.
+     *
+     * @param config the configuration bean
+     * @return a configured instance of {@link dev.langchain4j.store.embedding.coherence.CoherenceEmbeddingStore}
+     */
+    private CoherenceEmbeddingStore buildStore(ServiceRegistry registry, CoherenceEmbeddingStoreConfig config) {
+        var builder = CoherenceEmbeddingStore.builder();
+        config.session().ifPresent(builder::session);
+        config.name().ifPresent(builder::name);
+        config.index().ifPresent(builder::index);
+        config.normalizeEmbeddings().ifPresent(builder::normalizeEmbeddings);
+
+        return builder.build();
+    }
+
+}
