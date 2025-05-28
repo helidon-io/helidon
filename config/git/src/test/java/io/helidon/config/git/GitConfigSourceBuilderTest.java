408489644753fc0479423199a1290c4b675feb6b
Joe DiPol <joe.dipol@oracle.com>
2025-05-27 17:39:29 -0700

    3.x: Upgrade jgit to 7.2.1 (#10161)
    * Upgrade jgit to 7.2.1

--- config/git/src/test/java/io/helidon/config/git/GitConfigSourceBuilderTest.java
+++ config/git/src/test/java/io/helidon/config/git/GitConfigSourceBuilderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2017, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -74,6 +74,7 @@ public class GitConfigSourceBuilderTest extends RepositoryTestCase {
                 return testMethodName;
             }
         };
+        super.testRoot.create();
 
         super.setUp();
 
