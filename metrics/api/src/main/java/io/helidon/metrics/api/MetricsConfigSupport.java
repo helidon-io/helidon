96edd1a88a8d2b9da76540d07bc6048761bbf6c8
Tim Quinn <tim.quinn@oracle.com>
2025-05-30 07:07:39 -0500

    Add support rest-request.enabled with dot in SE metrics config (#10177)
    Signed-off-by: Tim Quinn <tim.quinn@oracle.com>

--- metrics/api/src/main/java/io/helidon/metrics/api/MetricsConfigSupport.java
+++ metrics/api/src/main/java/io/helidon/metrics/api/MetricsConfigSupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2023, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,6 +90,14 @@ class MetricsConfigSupport {
             if (builder.scoping().isEmpty()) {
                 builder.scoping(ScopingConfig.create());
             }
+        }
+    }
+
+    static class RestRequestEnabledDecorator implements Prototype.OptionDecorator<MetricsConfig.BuilderBase<?, ?>, Optional<Boolean>> {
+
+        @Override
+        public void decorate(MetricsConfig.BuilderBase<?, ?> builder, Optional<Boolean> optionValue) {
+            optionValue.ifPresent(builder::restRequestEnabled);
         }
     }
 }
