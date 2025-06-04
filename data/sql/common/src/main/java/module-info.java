3c385bcb0fa4d52b4b8aeebfa3443bcc4e75b235
Tomáš Kraus <tomas.kraus@oracle.com>
2025-06-04 12:42:29 +0200

    Helidon Data API cleanup.
    Signed-off-by: Tomáš Kraus <tomas.kraus@oracle.com>

--- data/sql/common/src/main/java/module-info.java
+++ data/sql/common/src/main/java/module-info.java
@@ -21,6 +21,7 @@ module io.helidon.data.sql.common {
     requires transitive io.helidon.data;
     requires java.sql;
     requires io.helidon.builder.api;
+    requires io.helidon.common.config;
 
     exports io.helidon.data.sql.common;
 }
