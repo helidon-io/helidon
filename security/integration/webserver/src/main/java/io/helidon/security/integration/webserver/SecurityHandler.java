ca4c9a90f188416eb5377a08222581b39d193db2
Andrei Arlou <andrei.arlou@gmail.com>
2025-06-15 00:09:05 +0300

    3.x: Removed default header when abstain (#10255) (#10256)

--- security/integration/webserver/src/main/java/io/helidon/security/integration/webserver/SecurityHandler.java
+++ security/integration/webserver/src/main/java/io/helidon/security/integration/webserver/SecurityHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2018, 2025 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -513,8 +513,7 @@ public final class SecurityHandler implements Handler {
         abortRequest(res,
                      response,
                      Http.Status.UNAUTHORIZED_401.code(),
-                     Map.of(Http.Header.WWW_AUTHENTICATE,
-                                             List.of("Basic realm=\"Security Realm\"")));
+                     Map.of());
         future.complete(AtxResult.STOP);
         return true;
     }
