115dcbe97f51b1ef3aba786de51b105bbca85a61
Tim Quinn <tim.quinn@oracle.com>
2025-07-23 02:29:03 -0500

    Add integration test for limit listeners

---
+++ webserver/tests/resource-limits/src/test/java/io/helidon/webserver/tests/resourcelimit/TestSpanExporterProvider.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2025 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.webserver.tests.resourcelimit;
+
+import io.helidon.common.LazyValue;
+
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.traces.ConfigurableSpanExporterProvider;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+
+public class TestSpanExporterProvider implements ConfigurableSpanExporterProvider {
+
+    private static final LazyValue<TestSpanExporter> SPAN_EXPORTER = LazyValue.create(TestSpanExporter::new);
+
+    @Override
+    public SpanExporter createExporter(ConfigProperties configProperties) {
+        return SPAN_EXPORTER.get();
+    }
+
+    @Override
+    public String getName() {
+        return "in-memory";
+    }
+
+    static TestSpanExporter exporter() {
+        return SPAN_EXPORTER.get();
+    }
+}
