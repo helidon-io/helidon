12f2e012df5cd913d4c74852444528ce1081ee72
Tomáš Kraus <tomas.kraus@oracle.com>
2025-08-14 13:24:16 +0200

    Helidon Data MP docs.
    Signed-off-by: Tomáš Kraus <tomas.kraus@oracle.com>

---
+++ docs/src/main/java/io/helidon/docs/mp/data/PetService.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2025 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.docs.mp.data;
+
+import java.util.List;
+
+import io.helidon.docs.se.data.Keeper;
+import io.helidon.docs.se.data.SimpleSnippets.KeeperRepository;
+
+import jakarta.inject.Inject;
+
+// tag::session_access[]
+public class PetService {
+
+    // tag::repository_init[]
+    @Inject
+    private KeeperRepository pr;
+    // end::repository_init[]
+
+    public List<Keeper> keeperQuery(String name) {
+        return pr.call(em -> em.createQuery("SELECT k FROM Keeper k WHERE k.name = :name",
+                                            Keeper.class)
+                .setParameter("name", name)
+                .getResultList());
+    }
+
+}
+// end::session_access[]
