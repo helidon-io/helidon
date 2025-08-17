55cc5e39c0753abbcacaac60c0acf839776ce70e
Tim Quinn <tim.quinn@oracle.com>
2025-08-17 11:01:25 -0500

    Don't need the dep on otel semconv

--- webserver/observe/telemetry/tracing/src/main/java/module-info.java
+++ webserver/observe/telemetry/tracing/src/main/java/module-info.java
@@ -35,6 +35,4 @@ module io.helidon.webserver.observe.telemetry.tracing {
     requires io.helidon.tracing.config;
     requires io.helidon.webserver;
     requires io.helidon.tracing;
-
-    requires io.opentelemetry.semconv;
 }
