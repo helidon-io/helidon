f548f2aadd2d8740fab2299927929cd51b78d956
Tim Quinn <tim.quinn@oracle.com>
2025-08-16 23:30:11 -0500

    Add new files accidentally omited from the preceding push

---
+++ webserver/observe/telemetry/tracing/src/main/java/io/helidon/observe/telemetry/tracing/OpenTelemetryTracingSemanticConventionsProvider.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2025 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.observe.telemetry.tracing;
+
+import java.util.Optional;
+
+import io.helidon.common.uri.UriInfo;
+import io.helidon.http.HeaderNames;
+import io.helidon.service.registry.Service;
+import io.helidon.tracing.Span;
+import io.helidon.tracing.config.SpanTracingConfig;
+import io.helidon.webserver.http.RoutingRequest;
+import io.helidon.webserver.http.RoutingResponse;
+import io.helidon.webserver.observe.tracing.TracingSemanticConventions;
+import io.helidon.webserver.observe.tracing.spi.TracingSemanticConventionsProvider;
+
+@Service.Singleton
+class OpenTelemetryTracingSemanticConventionsProvider implements TracingSemanticConventionsProvider {
+
+    @Override
+    public TracingSemanticConventions create(SpanTracingConfig spanTracingConfig,
+                                             String socketName,
+                                             RoutingRequest request,
+                                             RoutingResponse response) {
+        return new SemanticConventions(spanTracingConfig, socketName, request, response);
+    }
+
+    /**
+     * Supplies span information conforming to the OpenTelemetry semantic conventions for requests.
+     * <p>
+     * See <a href="https://github.com/open-telemetry/semantic-conventions/blob/v1.29.0/docs/http/http-spans.md">the
+     * OpenTelemetry tracing semantic conventions.</a>
+     */
+    static class SemanticConventions implements TracingSemanticConventions {
+
+        // Required
+        private static final String HTTP_REQUEST_METHOD = "http.request.method";
+        private static final String URL_PATH = "url.path";
+        private static final String URL_SCHEME = "url.scheme";
+        private static final String ERROR_TYPE = "error.type";
+        private static final String HTTP_RESPONSE_STATUS_CODE = "http.response.status_code";
+        private static final String HTTP_ROUTE = "http.route";
+        private static final String SERVER_PORT = "server.port";
+        private static final String URL_QUERY = "url.query";
+        private static final String SERVER_ADDRESS = "server.address";
+        private static final String USER_AGENT_ORIGINAL = "user_agent.original";
+
+        private static final String HELIDON_SOCKET = "helidon.socket";
+
+        private final SpanTracingConfig spanTracingConfig;
+        private final String socketName;
+        private final RoutingRequest request;
+        private final RoutingResponse response;
+
+        private String methodText;
+        private Optional<String> matchingPattern;
+
+        SemanticConventions(SpanTracingConfig spanTracingConfig,
+                            String socketName,
+                            RoutingRequest request,
+                            RoutingResponse response) {
+            this.spanTracingConfig = spanTracingConfig;
+            this.socketName = socketName;
+            this.request = request;
+            this.response = response;
+        }
+
+        @Override
+        public String spanName() {
+            methodText = request.prologue().method().text();
+            matchingPattern = request.matchingPattern();
+            return methodText + matchingPattern
+                    .map(pattern -> " " + pattern)
+                    .orElse("");
+        }
+
+        @Override
+        public void update(Span.Builder<?> spanBuilder) {
+            UriInfo uriInfo = request.requestedUri();
+
+            spanBuilder.kind(Span.Kind.SERVER)
+                    .tag(HTTP_REQUEST_METHOD, methodText)
+                    .tag(URL_PATH, uriInfo.path().path())
+                    .tag(URL_SCHEME, uriInfo.scheme())
+                    .tag(HTTP_RESPONSE_STATUS_CODE, Integer.toString(response.status().code()))
+                    .update(b -> matchingPattern.ifPresent(route -> b.tag(HTTP_ROUTE, route)))
+                    .tag(SERVER_PORT, Integer.toString(request.localPeer().port()))
+                    .update(b -> {
+                        if (!request.query().isEmpty()) {
+                            b.tag(URL_QUERY, request.query().value());
+                        }
+                    })
+                    .tag(SERVER_ADDRESS, request.requestedUri().host())
+                    .update(b -> request.headers().first(HeaderNames.USER_AGENT)
+                            .ifPresent(agent -> b.tag(USER_AGENT_ORIGINAL, agent)));
+
+            // Segregated this because it's not in the OpenTelemetry semantic conventions.
+            spanBuilder.tag(HELIDON_SOCKET, socketName);
+        }
+
+        @Override
+        public void update(Span span) {
+        }
+
+        @Override
+        public void update(Span span, Exception e) {
+            span.tag(ERROR_TYPE, e.getClass().getName());
+        }
+    }
+}
---
+++ webserver/observe/telemetry/tracing/src/main/java/module-info.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2025 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import io.helidon.common.features.api.Features;
+import io.helidon.common.features.api.HelidonFlavor;
+
+/**
+ * OpenTelemetry implementation for telemetry.
+ */
+@Features.Name("Telemetry Tracing Semantic Conventions")
+@Features.Description("Support for Telemetry Semantic Conventions")
+@Features.Flavor({HelidonFlavor.SE})
+@Features.Path({"Telemetry/OpenTelemetry/SemConv"})
+@Features.Incubating
+module io.helidon.webserver.observe.telemetry.tracing {
+
+    requires io.helidon.service.registry;
+
+
+    requires static io.helidon.common.features.api;
+    requires io.helidon.webserver.observe.tracing;
+    requires io.helidon.tracing.config;
+    requires io.helidon.webserver;
+    requires io.helidon.tracing;
+
+    requires io.opentelemetry.semconv;
+}
