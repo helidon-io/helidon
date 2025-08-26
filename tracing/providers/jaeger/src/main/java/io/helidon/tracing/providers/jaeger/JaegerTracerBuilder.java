a78efacb776394aa713f721daef494c27d79ef10
Tim Quinn <tim.quinn@oracle.com>
2025-08-26 17:30:43 -0500

    Update default port to conform to change from jaeger exporter to otlp exporter

--- tracing/providers/jaeger/src/main/java/io/helidon/tracing/providers/jaeger/JaegerTracerBuilder.java
+++ tracing/providers/jaeger/src/main/java/io/helidon/tracing/providers/jaeger/JaegerTracerBuilder.java
@@ -157,7 +157,7 @@ public class JaegerTracerBuilder implements TracerBuilder<JaegerTracerBuilder> {
     static final boolean DEFAULT_ENABLED = true;
     static final String DEFAULT_HTTP_HOST = "localhost";
     static final int DEFAULT_SCHEDULE_DELAY = 5_000;
-    static final int DEFAULT_HTTP_PORT = 14250;
+    static final int DEFAULT_HTTP_PORT = 4317;
     static final int DEFAULT_MAX_QUEUE_SIZE = 2048;
     static final int DEFAULT_MAX_EXPORT_BATCH_SIZE = 512;
 
