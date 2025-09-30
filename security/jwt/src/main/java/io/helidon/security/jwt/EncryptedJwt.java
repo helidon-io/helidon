76e71896084f11bf511d93090f7be3f35177a0de
David Kral <david.k.kral@oracle.com>
2025-09-30 15:58:12 +0200

    copyright and checkstyle fix
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- security/jwt/src/main/java/io/helidon/security/jwt/EncryptedJwt.java
+++ security/jwt/src/main/java/io/helidon/security/jwt/EncryptedJwt.java
@@ -34,7 +34,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
