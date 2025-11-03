467dae929e6933ae2ac7502e686a2521d8d61e4f
David Kral <david.k.kral@oracle.com>
2025-11-03 14:17:22 +0100

    checkstyle
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- security/providers/jwt/src/main/java/io/helidon/security/providers/jwt/JwtProvider.java
+++ security/providers/jwt/src/main/java/io/helidon/security/providers/jwt/JwtProvider.java
@@ -24,7 +24,6 @@ import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 import io.helidon.common.Errors;
 import io.helidon.common.config.Config;
