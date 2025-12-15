d6fd30ca7b1e9709e3b1880a16d7ded95b434ba7
Tomas Langer <tomas.langer@oracle.com>
2025-12-15 10:35:14 +0100

    Remove preview flag from service registry. (#10907)
    Update documentation of module.

--- service/registry/src/main/java/module-info.java
+++ service/registry/src/main/java/module-info.java
@@ -18,13 +18,37 @@ import io.helidon.common.features.api.Features;
 import io.helidon.common.features.api.HelidonFlavor;
 
 /**
- * Core service registry, supporting {@link io.helidon.service.registry.Service.Provider}.
+ * Helidon Service Registry.
+ * <p>
+ * This module is a replacement for Java {@link java.util.ServiceLoader} that supports dependency injections and interception.
+ * It is fully build-time based, and when used together with Helidon Declarative features, and Service Maven plugin, it
+ * allows creation of applications that have no discovery at startup, and computed bindings.
+ * <p>
+ * The concept is based on the following principals:
+ * <ul>
+ *     <li>Build time processing - using annotation processors (we use "Codegen" in Helidon)</li>
+ *     <li>Reflection free where possible - all generated code is reflection free</li>
+ *     <li>Discovery free - when used together with the Service Maven Plugin and Declarative programming model, there is
+ *     no runtime discovery (i.e. no classpath scanning, no loading of resources etc.)</li>
+ * </ul>
+ *
+ * This module is a production feature of Helidon (we have moved it from Preview), when used with Helidon SE.
+ * It can be used to register custom instances (see {@link io.helidon.service.registry.Services#set(Class, Object[])}), or
+ * to get services that are "global" (see {@link io.helidon.service.registry.Services#get(Class)}).
+ * <p>
+ * The two main modules that are required to utilize the full power of service registry are the Service Codegen
+ * ({@code io.helidon.service:helidon-service-codegen}, to set it up as an annotation processor, also needs
+ * {@code io.helidon.codegen:helidon-codegen-apt}) - this module is required to declare new services,
+ * and the Service Maven Plugin ({@code io.helidon.service:helidon-service-maven-plugin}) - this module is only needed
+ * to bypass discovery and runtime service binding for Helidon Declarative applications.
+ *
+ * @see io.helidon.service.registry.Services
+ * @see io.helidon.service.registry.Service.Singleton
  */
 @Features.Name("Registry")
 @Features.Description("Service Registry")
 @Features.Flavor(HelidonFlavor.SE)
 @Features.Path("Registry")
-@Features.Preview
 module io.helidon.service.registry {
     requires static io.helidon.common.features.api;
 
