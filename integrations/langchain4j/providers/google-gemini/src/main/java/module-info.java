27a063c2d252a6b9ff71c12804577b27017307c8
Daniel Kec <daniel.kec@oracle.com>
2026-01-07 14:41:40 +0100

    [4.x] Bump-up LangChain4j to 1.10.0 (#10955)
    * Bump-up LangChain4j to 1.10.0
    
    * add Gemini provider
    
    Signed-off-by: Daniel Kec <daniel.kec@oracle.com>

---
+++ integrations/langchain4j/providers/google-gemini/src/main/java/module-info.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2025, 2026 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import io.helidon.common.features.api.Features;
+import io.helidon.common.features.api.HelidonFlavor;
+
+/**
+ * Provides integration with Google Gemini models.
+ */
+@Features.Name("Langchain4j Google Gemini")
+@Features.Description("Langchain4j Google Gemini Provider Integration")
+@Features.Flavor({HelidonFlavor.SE, HelidonFlavor.MP})
+@Features.Path({"Langchain4j", "Google Gemini"})
+@Features.Preview
+module io.helidon.integrations.langchain4j.providers.google.gemini {
+    requires static io.helidon.common.features.api;
+
+    requires langchain4j.google.ai.gemini;
+    requires langchain4j.core;
+
+    requires transitive io.helidon.service.registry;
+    requires transitive io.helidon.integrations.langchain4j;
+    requires transitive io.helidon.config;
+    requires langchain4j.http.client;
+    requires org.slf4j;
+
+    exports io.helidon.integrations.langchain4j.providers.gemini;
+}
