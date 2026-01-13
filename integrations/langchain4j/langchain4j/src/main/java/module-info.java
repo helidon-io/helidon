1e854279ff577fbcc412db6c8c400e71af8024ee
Daniel Kec <daniel.kec@oracle.com>
2026-01-13 14:36:21 +0100

    Support injection in guardrails #10714
    Signed-off-by: Daniel Kec <daniel.kec@oracle.com>

--- integrations/langchain4j/langchain4j/src/main/java/module-info.java
+++ integrations/langchain4j/langchain4j/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Oracle and/or its affiliates.
+ * Copyright (c) 2025, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,4 +43,6 @@ module io.helidon.integrations.langchain4j {
 
     provides dev.langchain4j.spi.services.TokenStreamAdapter
             with io.helidon.integrations.langchain4j.TokenStreamToStreamAdapter;
+    provides dev.langchain4j.spi.classloading.ClassInstanceFactory
+            with io.helidon.integrations.langchain4j.ServiceRegistryClassInstanceFactory;
 }
