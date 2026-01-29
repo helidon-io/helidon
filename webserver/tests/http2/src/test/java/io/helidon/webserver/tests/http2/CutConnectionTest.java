a9bf475092d9b468f5a87a83da59850b3c7c8667
Joe Di Pol <joe.dipol@oracle.com>
2026-01-29 08:38:10 -0800

    Update test to match logging.properties configuration

--- webserver/tests/http2/src/test/java/io/helidon/webserver/tests/http2/CutConnectionTest.java
+++ webserver/tests/http2/src/test/java/io/helidon/webserver/tests/http2/CutConnectionTest.java
@@ -115,7 +115,7 @@ public class CutConnectionTest {
             assertThat(exec.awaitTermination(TIME_OUT_SEC, TimeUnit.SECONDS), is(true));
             server.stop();
             SocketClosedLog log = ASSERTING_HANDLER.socketClosedLog.get(TIME_OUT_SEC, TimeUnit.SECONDS);
-            assertThat(log.record.getLevel(), is(Level.FINE));
+            assertThat(log.record.getLevel(), is(Level.FINER));
         } finally {
             WEBSERVER_LOGGER.removeHandler(ASSERTING_HANDLER);
             WEBSERVER_LOGGER.setLevel(originalLevel);
