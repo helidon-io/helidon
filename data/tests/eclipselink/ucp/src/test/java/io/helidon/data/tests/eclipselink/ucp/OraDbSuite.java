0c90394193c58d025b95e5386676c268e49b5feb
Romain Grecourt <romain.grecourt@oracle.com>
2026-02-19 17:27:36 -0800

    4.x: Fix testcontainers logging
    - Replace usage of SL4J with JUL
    - Fine tune testcontainers log levels
    
    Replace the Oracle Database docker image:
    - Use container-registry.oracle.com/database/free:latest-lite instead of container-registry.oracle.com/database/express
    - Improves the execution times significantly

--- data/tests/eclipselink/ucp/src/test/java/io/helidon/data/tests/eclipselink/ucp/OraDbSuite.java
+++ data/tests/eclipselink/ucp/src/test/java/io/helidon/data/tests/eclipselink/ucp/OraDbSuite.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, 2025 Oracle and/or its affiliates.
+ * Copyright (c) 2024, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,7 +40,7 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 public class OraDbSuite implements SuiteProvider {
     private static final System.Logger LOGGER = System.getLogger(OraDbSuite.class.getName());
     private static final DockerImageName IMAGE = DockerImageName.parse(
-            "container-registry.oracle.com/database/express");
+            "container-registry.oracle.com/database/free:latest-lite");
 
     private final TestContainerHandler containerHandler;
 
@@ -55,8 +55,9 @@ public class OraDbSuite implements SuiteProvider {
                 .ifPresent(password -> container.withEnv("ORACLE_PWD", password));
 
         container.withExposedPorts(this.containerHandler.originalPort())
-                .waitingFor(Wait.forHealthcheck()
-                                    .withStartupTimeout(Duration.ofMinutes(5)));
+                .withStartupAttempts(3)
+                .waitingFor(Wait.forLogMessage(".*DATABASE IS READY TO USE.*", 1)
+                        .withStartupTimeout(Duration.ofMinutes(5)));
     }
 
     @TestSuite.BeforeSuite
