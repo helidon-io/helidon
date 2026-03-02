653e108b05ebd8c6e1e701b8c1f2854f6b1a0843
Tim Quinn <tim.quinn@oracle.com>
2026-03-01 18:06:37 -0600

    Doc for using the OTel API directly

--- docs/src/main/java/io/helidon/docs/se/TelemetrySnippets.java
+++ docs/src/main/java/io/helidon/docs/se/TelemetrySnippets.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Oracle and/or its affiliates.
+ * Copyright (c) 2025, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,13 @@ package io.helidon.docs.se;
 // tag::snippet_1_imports[]
 import java.util.Map;
 import io.helidon.telemetry.otelconfig.HelidonOpenTelemetry;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 // end::snippet_1_imports[]
 
 class TelemetrySnippets {
@@ -40,6 +46,70 @@ class TelemetrySnippets {
 
         // end::snippet_1[]
     }
+
+    void snippet_2() {
+
+        // Example of user code using OpenTelemetry autoconfig.
+
+        // tag::snippet_2[]
+        OpenTelemetry otel = AutoConfiguredOpenTelemetrySdk.builder()
+                .setResultAsGlobal()
+                .build()
+                .getOpenTelemetrySdk();
+
+        // end::snippet_2[]
+    }
+
+    void snippet_3() {
+
+        // Example of user code using OpenTelemetry autoconfig and setting the global OpenTelemetry instance.
+
+        // tag::snippet_3[]
+        OpenTelemetry otel = AutoConfiguredOpenTelemetrySdk.builder()
+                .build()
+                .getOpenTelemetrySdk();
+
+        // end::snippet_3[]
+
+        // tag::snippet_3a[]
+        var globalOtel = GlobalOpenTelemetry.get();
+        // end::snippet_3a[]
+
+        // tag::snippet_3b[]
+        var meter = otel.getMeter("helidon-otel-example-app");
+        var tracer = otel.getTracer("helidon-otel-example-app");
+        // end::snippet_3b[]
+
+        // tag::snippet_3c[]
+        var myCounter = meter.counterBuilder("my-counter")
+                .setDescription("An example counter")
+                .build();
+        // ...
+        myCounter.add(1L);
+        // end::snippet_3c[]
+
+        // tag::snippet_3d[]
+        var mySpan = tracer.spanBuilder("my-span")
+                .setSpanKind(SpanKind.SERVER)
+                .startSpan();
+
+        try (Scope ignored = mySpan.makeCurrent()) {
+
+            // Do work worth tracing.
+
+            mySpan.setStatus(StatusCode.OK);
+        } catch (Throwable t) {
+            mySpan.setStatus(StatusCode.ERROR);
+        } finally {
+            mySpan.end();
+        }
+
+        // end::snippet_3d[]
+
+
+    }
+
+
 
     private OpenTelemetry prepareOpenTelemetry() {
         return HelidonOpenTelemetry.builder().build().openTelemetry();
