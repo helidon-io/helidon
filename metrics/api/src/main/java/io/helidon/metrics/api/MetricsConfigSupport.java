e82ed519ac50bd514d819780c68087040fa58fab
Tim Quinn <tim.quinn@oracle.com>
2026-03-04 07:04:26 -0600

    4.x Add support for exporting Helidon metrics via OTLP (#11231)

--- metrics/api/src/main/java/io/helidon/metrics/api/MetricsConfigSupport.java
+++ metrics/api/src/main/java/io/helidon/metrics/api/MetricsConfigSupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2025 Oracle and/or its affiliates.
+ * Copyright (c) 2023, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,6 +51,18 @@ class MetricsConfigSupport {
                 .get(key)
                 .asString()
                 .asOptional();
+    }
+
+    /**
+     * Indicates if publishers were specified, either by the application adding any programmatically or if the config
+     * contained the {@code publishers} node.
+     *
+     * @param metricsConfig the prepared {@code MetricsConfig}
+     * @return true if publishers were assigned or configured, false otherwise
+     */
+    @Prototype.PrototypeMethod
+    static boolean publishersConfigured(MetricsConfig metricsConfig) {
+        return !metricsConfig.publishers().isEmpty() || metricsConfig.config().get("publishers").exists();
     }
 
     @Prototype.ConfigFactoryMethod("tags")
