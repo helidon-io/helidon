17020013cd18a2c0e0399296aea6d6751eaa735b
Tomas Langer <tomas.langer@oracle.com>
2026-03-13 11:14:33 +0100

    Rebase on 27, fix issues

--- common/common/src/main/java/io/helidon/common/DeprecationSupport.java
+++ common/common/src/main/java/io/helidon/common/DeprecationSupport.java
@@ -27,6 +27,7 @@ import java.util.stream.Collectors;
  * <b>This is NOT part of any supported API. If you write code that depends on this, you do so at your own risk.
  * This code and its internal interfaces are subject to change or deletion without notice.</b>
  */
+@Api.Internal
 public final class DeprecationSupport {
 
     private static final System.Logger LOGGER = System.getLogger(DeprecationSupport.class.getName());
