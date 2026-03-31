054ae3c1e9d9bef55c9f2533b5edaa781c7f43f2
David Král <david.k.kral@oracle.com>
2026-03-31 15:47:09 +0200

    Codegen fixed
    Signed-off-by: David Král <david.k.kral@oracle.com>

---
+++ json/tests/binding/src/test/java/io/helidon/json/tests/GeneratedSourceMetadataTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2026 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.json.tests;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Year;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.startsWith;
+
+class GeneratedSourceMetadataTest {
+    private static final Path GENERATED_SOURCES = Path.of("target",
+                                                          "generated-test-sources",
+                                                          "test-annotations",
+                                                          "io",
+                                                          "helidon",
+                                                          "json",
+                                                          "tests");
+
+    @Test
+    void testGeneratedConverterMetadata() throws IOException {
+        assertGeneratedSourceMetadata(generatedSource("BasicTest_StringWrapper__GeneratedConverter"),
+                                      "io.helidon.json.codegen.JsonCodegen",
+                                      "io.helidon.json.tests.BasicTest.StringWrapper");
+    }
+
+    @Test
+    void testGeneratedBindingFactoryMetadata() throws IOException {
+        assertGeneratedSourceMetadata(generatedSource("GenericClassesTest_Container_BindingFactory"),
+                                      "io.helidon.json.codegen.JsonCodegen",
+                                      "io.helidon.json.tests.GenericClassesTest.Container<T>");
+    }
+
+    private static Path generatedSource(String typeName) {
+        return GENERATED_SOURCES.resolve(typeName + ".java");
+    }
+
+    private static void assertGeneratedSourceMetadata(Path generatedSource,
+                                                      String generator,
+                                                      String trigger) throws IOException {
+        assertThat("Generated source should exist: " + generatedSource, Files.exists(generatedSource), is(true));
+
+        String content = Files.readString(generatedSource);
+
+        assertThat(content, startsWith("/*"));
+        assertThat(content, containsString("Copyright (c) "));
+        assertThat(content, containsString(" Oracle and/or its affiliates."));
+        assertThat(content, containsString("Licensed under the Apache License, Version 2.0"));
+        assertThat(content, not(containsString("// This is a generated file (powered by Helidon).")));
+        assertThat(content, containsString("@Generated("));
+        assertThat(content, containsString("value = \"" + generator + "\""));
+        assertThat(content, containsString("trigger = \"" + trigger + "\""));
+    }
+}
