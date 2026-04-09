1f9db763f44dfb41f4906962bb3092425c887b47
Tomas Langer <tomas.langer@oracle.com>
2026-04-09 09:42:44 +0200

    Honor record components and type mappers in codegen APT (#11574)
    (cherry picked from commit 75e742a54a9b8a6a1d19dfedd5dc52e169d1188a)

--- codegen/apt/src/main/java/io/helidon/codegen/apt/AptTypeFactory.java
+++ codegen/apt/src/main/java/io/helidon/codegen/apt/AptTypeFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2025 Oracle and/or its affiliates.
+ * Copyright (c) 2023, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -273,7 +273,7 @@ public final class AptTypeFactory {
     }
 
     private static Optional<TypeName> createTypeName(Elements elements, Set<TypeMirror> processed, Element type) {
-        if (type instanceof VariableElement) {
+        if (type instanceof VariableElement || type.getKind() == ElementKind.RECORD_COMPONENT) {
             return createTypeName(elements, processed, type.asType());
         }
 
---
+++ codegen/apt/src/test/java/io/helidon/codegen/apt/AptProcessorTest.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright (c) 2026 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.codegen.apt;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+
+import io.helidon.codegen.CodegenContext;
+import io.helidon.codegen.CodegenOptions;
+import io.helidon.codegen.CodegenScope;
+import io.helidon.codegen.ElementInfoPredicates;
+import io.helidon.codegen.spi.TypeMapper;
+import io.helidon.codegen.testing.TestCompiler;
+import io.helidon.common.types.Annotation;
+import io.helidon.common.types.ElementKind;
+import io.helidon.common.types.TypeInfo;
+import io.helidon.common.types.TypeName;
+import io.helidon.common.types.TypedElementInfo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AptProcessorTest {
+    private static final TypeName MAPPED = TypeName.create("com.example.Mapped");
+
+    @Test
+    void testRecordComponentUsesDeclaredType() {
+        AtomicReference<TypedElementInfo> component = new AtomicReference<>();
+
+        var result = TestCompiler.builder()
+                .addProcessor(new AbstractProcessor() {
+                    private AptContext ctx;
+
+                    @Override
+                    public Set<String> getSupportedAnnotationTypes() {
+                        return Set.of("*");
+                    }
+
+                    @Override
+                    public SourceVersion getSupportedSourceVersion() {
+                        return SourceVersion.latestSupported();
+                    }
+
+                    @Override
+                    public synchronized void init(ProcessingEnvironment processingEnv) {
+                        super.init(processingEnv);
+                        this.ctx = AptContext.create(processingEnv, Set.of());
+                    }
+
+                    @Override
+                    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+                        if (roundEnv.processingOver()) {
+                            return false;
+                        }
+
+                        TypeElement type = processingEnv.getElementUtils().getTypeElement("com.example.ExampleRecord");
+                        component.set(AptTypeInfoFactory.create(ctx, type, ElementInfoPredicates.ALL_PREDICATE)
+                                              .orElseThrow()
+                                              .elementInfo()
+                                              .stream()
+                                              .filter(it -> it.kind() == ElementKind.RECORD_COMPONENT)
+                                              .filter(it -> "value".equals(it.elementName()))
+                                              .findFirst()
+                                              .orElseThrow());
+                        return false;
+                    }
+                })
+                .currentRelease()
+                .addSource("com/example/ExampleRecord.java", """
+                        package com.example;
+
+                        record ExampleRecord(String value) {
+                        }
+                        """)
+                .build()
+                .compile();
+
+        assertTrue(result.success(), "Compilation should succeed for a record source");
+        assertNotNull(component.get(), "Record component metadata should be available");
+        assertThat(component.get().typeName(), is(TypeName.create(String.class)));
+    }
+
+    @Test
+    void testTypeElementCreationAppliesTypeMappers() {
+        AtomicReference<TypeInfo> typeInfo = new AtomicReference<>();
+
+        var result = TestCompiler.builder()
+                .addProcessor(new AbstractProcessor() {
+                    private AptContext ctx;
+
+                    @Override
+                    public Set<String> getSupportedAnnotationTypes() {
+                        return Set.of("*");
+                    }
+
+                    @Override
+                    public SourceVersion getSupportedSourceVersion() {
+                        return SourceVersion.latestSupported();
+                    }
+
+                    @Override
+                    public synchronized void init(ProcessingEnvironment processingEnv) {
+                        super.init(processingEnv);
+                        CodegenOptions options = AptOptions.create(processingEnv);
+                        this.ctx = new AptContextImpl(processingEnv,
+                                                      options,
+                                                      Set.of(),
+                                                      new AptFiler(processingEnv, options),
+                                                      new AptLogger(processingEnv, options),
+                                                      CodegenScope.PRODUCTION,
+                                                      null) {
+                            @Override
+                            public List<TypeMapper> typeMappers() {
+                                return List.of(new TypeMapper() {
+                                    @Override
+                                    public boolean supportsType(TypeInfo type) {
+                                        return type.hasAnnotation(TypeName.create(Deprecated.class));
+                                    }
+
+                                    @Override
+                                    public Optional<TypeInfo> map(CodegenContext ctx, TypeInfo type) {
+                                        return Optional.of(TypeInfo.builder(type)
+                                                                   .addAnnotation(Annotation.create(MAPPED))
+                                                                   .build());
+                                    }
+                                });
+                            }
+                        };
+                    }
+
+                    @Override
+                    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+                        if (roundEnv.processingOver()) {
+                            return false;
+                        }
+
+                        TypeElement type = processingEnv.getElementUtils().getTypeElement("com.example.ExampleType");
+                        typeInfo.set(AptTypeInfoFactory.create(ctx, type, ElementInfoPredicates.ALL_PREDICATE)
+                                             .orElseThrow());
+                        return false;
+                    }
+                })
+                .currentRelease()
+                .addSource("com/example/ExampleType.java", """
+                        package com.example;
+
+                        @Deprecated
+                        class ExampleType {
+                        }
+                        """)
+                .build()
+                .compile();
+
+        assertTrue(result.success(), "Compilation should succeed for a source type");
+        assertNotNull(typeInfo.get(), "Type metadata should be available");
+        assertTrue(typeInfo.get().hasAnnotation(MAPPED), "Source types should be passed through type mappers");
+    }
+
+    @Test
+    void testAptProcessorDiscoversRecordTypesFromRecordComponentAnnotations() {
+        TypeName mappedType = TypeName.create("com.example.RecordMappedExample");
+        TestRecordTypeMapperProvider.reset();
+
+        var result = TestCompiler.builder()
+                .addProcessor(new AptProcessor())
+                .currentRelease()
+                .addSource("com/example/RecordMapped.java", """
+                        package com.example;
+
+                        import java.lang.annotation.ElementType;
+                        import java.lang.annotation.Target;
+
+                        @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+                        @interface RecordMapped {
+                        }
+                        """)
+                .addSource("com/example/RecordMappedExample.java", """
+                        package com.example;
+
+                        record RecordMappedExample(String key, @RecordMapped String value) {
+                        }
+                        """)
+                .build()
+                .compile();
+
+        assertTrue(result.success(), "Compilation should succeed for a record source");
+        assertTrue(TestRecordTypeMapperProvider.sawType(mappedType),
+                   "AptProcessor should discover records referenced only through record-component annotations");
+        assertTrue(TestRecordTypeMapperProvider.mappedType(mappedType),
+                   "Discovered record types should be passed through type mappers");
+    }
+}
