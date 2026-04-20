291dca1d1b0ca42df36e010d71fc8a2e277f9800
David Král <david.k.kral@oracle.com>
2026-04-20 16:16:09 +0200

    Json Schema changed to use Helidon JSON (#11373)
    Json Schema changed to use Helidon JSON
    
    Signed-off-by: David Kral <david.k.kral@oracle.com>

--- json/schema/schema/src/main/java/io/helidon/json/schema/SchemaItemBlueprint.java
+++ json/schema/schema/src/main/java/io/helidon/json/schema/SchemaItemBlueprint.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Oracle and/or its affiliates.
+ * Copyright (c) 2025, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import java.util.Optional;
 
 import io.helidon.builder.api.Option;
 import io.helidon.builder.api.Prototype;
-import io.helidon.metadata.hson.Hson;
+import io.helidon.json.JsonObject;
 
 /**
  * Common interface for all Json Schema items.
@@ -62,11 +62,9 @@ interface SchemaItemBlueprint {
      * This method servers mainly as a helpful tool for the Json generation.
      * It should not be used.
      *
-     * @param builder hson struct builder
-     * @deprecated this method is not intended to be broadly used. Will be removed later.
+     * @param builder json object builder
      */
-    @Deprecated(forRemoval = true, since = "4.3.0")
-    default void generate(Hson.Struct.Builder builder) {
+    default void generate(JsonObject.Builder builder) {
         title().ifPresent(title -> builder.set("title", title));
         description().ifPresent(description -> builder.set("description", description));
         builder.set("type", schemaType().name().toLowerCase());
