4d6fc3167bb5f240b2ef84cc7862cb78cebc13fd
Marius Volkhart <marius@volkhart.com>
2026-04-21 20:13:06 -0400

    Fix writeTo(OutputStream) writing excess bytes after partial read (#11738)
    OutputStream.write(byte[], off, len) takes a length as the third argument,
    not an end position. FixedBufferData passed writePosition (an end position)
    instead of writePosition - readPosition, and ReadOnlyArrayData passed length
    (total length) instead of length - position. Both bugs are invisible when the
    read position is zero, and manifest as excess bytes written to the stream once
    any bytes have been consumed from the buffer.
    
    GrowingBufferData already used the correct form and serves as the reference.

--- common/buffers/src/main/java/io/helidon/common/buffers/FixedBufferData.java
+++ common/buffers/src/main/java/io/helidon/common/buffers/FixedBufferData.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, 2025 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,7 +68,7 @@ class FixedBufferData implements BufferData {
     @Override
     public void writeTo(OutputStream out) {
         try {
-            out.write(bytes, readPosition, writePosition);
+            out.write(bytes, readPosition, writePosition - readPosition);
             readPosition = writePosition;
         } catch (IOException e) {
             throw new UncheckedIOException(e);
