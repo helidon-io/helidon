4c574bbe9b0d57561ab7d45a6db48fde106b5b6d
Tomas Langer <tomas.langer@oracle.com>
2026-05-12 13:25:01 +0200

    Fix Content-Location header constant (#11920)

--- common/http/src/test/java/io/helidon/common/http/HttpTest.java
+++ common/http/src/test/java/io/helidon/common/http/HttpTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2018, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,5 +49,10 @@ class HttpTest {
         assertThat(rs.reasonPhrase(), is("Custom reason phrase"));
         assertThat(rs.code(), is(TEMPORARY_REDIRECT_307.code()));
         assertThat(rs.family(), is(TEMPORARY_REDIRECT_307.family()));
+    }
+
+    @Test
+    void testContentLocationHeaderName() {
+        assertThat(Http.Header.CONTENT_LOCATION, is("Content-Location"));
     }
 }
