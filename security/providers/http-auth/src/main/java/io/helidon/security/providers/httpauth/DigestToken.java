19617b5d1cf232d8b4ce24b70c000aca211f7747
Tomas Langer <tomas.langer@oracle.com>
2026-05-18 16:41:58 +0200

    Avoid logging digest credential values

--- security/providers/http-auth/src/main/java/io/helidon/security/providers/httpauth/DigestToken.java
+++ security/providers/http-auth/src/main/java/io/helidon/security/providers/httpauth/DigestToken.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2018, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -238,13 +238,14 @@ class DigestToken {
         return user.digestHa1(realm, algorithm)
                 .map(ha1 -> {
                     String digest = digestFromHa1(ha1);
+                    boolean valid = MessageDigest.isEqual(digest.getBytes(StandardCharsets.UTF_8),
+                            response.getBytes(StandardCharsets.UTF_8));
 
-                    if (LOGGER.isLoggable(Level.DEBUG)) {
-                        LOGGER.log(Level.DEBUG, "Invalid h1 digest. Expected: {0}, got: {1}", ha1, digest);
+                    if (!valid && LOGGER.isLoggable(Level.DEBUG)) {
+                        LOGGER.log(Level.DEBUG, "Digest authentication failed");
                     }
 
-                    return MessageDigest.isEqual(response.getBytes(StandardCharsets.UTF_8),
-                            digest.getBytes(StandardCharsets.UTF_8));
+                    return valid;
                 })
                 .orElse(false);
     }
