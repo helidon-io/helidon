5d65b7e5ebc00c8cd9a32b182f2238abb43d8855
Tomas Langer <tomas.langer@oracle.com>
2026-05-28 18:29:16 +0200

    Fix MP config re-registration delegate (#12042)

--- config/config-mp/src/main/java/io/helidon/config/mp/MpConfigProviderResolver.java
+++ config/config-mp/src/main/java/io/helidon/config/mp/MpConfigProviderResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2025 Oracle and/or its affiliates.
+ * Copyright (c) 2019, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -172,11 +172,14 @@ public class MpConfigProviderResolver extends ConfigProviderResolver {
             config = delegate.delegate();
         }
 
-        if (null != currentConfig) {
+        ConfigDelegate newConfig;
+        if (null == currentConfig) {
+            newConfig = new ConfigDelegate(config);
+        } else {
             currentConfig.set(config);
+            newConfig = currentConfig;
         }
 
-        ConfigDelegate newConfig = new ConfigDelegate(config);
         CONFIGS.put(classLoader, newConfig);
 
         if (classLoader == Thread.currentThread().getContextClassLoader()) {
