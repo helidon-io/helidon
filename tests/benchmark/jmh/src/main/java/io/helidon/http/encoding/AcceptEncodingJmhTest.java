ea3a39a0e50986d724c72a0c89b493128dd440ac
Tomas Langer <tomas.langer@oracle.com>
2026-05-30 13:09:41 +0200

    Reduce Accept-Encoding parser allocations

---
+++ tests/benchmark/jmh/src/main/java/io/helidon/http/encoding/AcceptEncodingJmhTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2026 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.http.encoding;
+
+import java.util.List;
+
+import io.helidon.http.HeaderNames;
+import io.helidon.http.Headers;
+import io.helidon.http.WritableHeaders;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Benchmark)
+public class AcceptEncodingJmhTest {
+    private Headers weighted;
+    private Headers wildcard;
+    private List<String> serverOrder;
+
+    @Setup
+    public void setup() {
+        weighted = headers("gzip;q=1.0, identity, br;q=1.0, deflate;q=0.6");
+        wildcard = headers("*, gzip;q=0.8, identity;q=0");
+        serverOrder = List.of("br", "gzip", "deflate");
+    }
+
+    @Benchmark
+    public AcceptEncoding.CodingQuality parseBestWeighted() {
+        return AcceptEncoding.create(weighted)
+                .best(serverOrder)
+                .orElseThrow();
+    }
+
+    @Benchmark
+    public List<AcceptEncoding.CodingQuality> parseAcceptedWildcard() {
+        return AcceptEncoding.create(wildcard)
+                .acceptedCodings(true);
+    }
+
+    private static Headers headers(String acceptEncoding) {
+        WritableHeaders<?> headers = WritableHeaders.create();
+        headers.add(HeaderNames.ACCEPT_ENCODING, acceptEncoding);
+        return headers;
+    }
+}
