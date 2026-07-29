56232c4f4c9950a783061e91c21359887c008534
Joe Di Pol <joe.dipol@oracle.com>
2026-07-29 14:37:44 -0700

    4.x: Update release scripts to be more portable  (#12210)
    * Update release scripts to be more portable

--- etc/scripts/release.sh
+++ etc/scripts/release.sh
@@ -214,7 +214,7 @@ update_version(){
 }
 
 create_tag() {
-  local git_branch version current_version
+  local git_branch version current_version git_remote
 
   current_version=$(current_version)
   version=${current_version%-SNAPSHOT}
@@ -236,7 +236,20 @@ create_tag() {
 
   # Create and push a git tag
   git tag -f "${version}"
-  git push --force origin refs/tags/"${version}":refs/tags/"${version}"
+
+  push_remote=origin
+  if [ "${GITHUB_ACTIONS:-false}" != "true" ]; then
+    # Add an SSH based remote if not running in GitHub action
+    git_remote=$(git config --get remote.origin.url | \
+      sed -e "s,https://[^@]*@\([^/]*\)/,git@\1:," \
+          -e "s,https://\([^/]*\)/,git@\1:,")
+    git remote remove release > /dev/null 2>&1 || true
+    git remote add release "${git_remote}"
+    push_remote=release
+  fi
+
+  git push --force "${push_remote}" \
+        refs/tags/"${version}":refs/tags/"${version}"
 
   echo "version=${version}" >&6
   echo "tag=refs/tags/${version}" >&6
