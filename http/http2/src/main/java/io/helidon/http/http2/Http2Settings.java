44edf5b68a93407d627fe40af50ede28a7bea6dd
Tomas Langer <tomas.langer@oracle.com>
2026-08-12 23:51:38 +0200

    Skip unsupported HTTP/2 settings values

--- http/http2/src/main/java/io/helidon/http/http2/Http2Settings.java
+++ http/http2/src/main/java/io/helidon/http/http2/Http2Settings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -78,6 +78,8 @@ public final class Http2Settings implements Http2Frame<Http2Flag.SettingsFlags> 
             Http2Setting<?> http2Setting = Http2Setting.BY_ID.get(identifier);
             if (http2Setting != null) {
                 values.put(identifier, new SettingValue(http2Setting, http2Setting.read(frame)));
+            } else {
+                frame.skip(4);
             }
         }
         return new Http2Settings(values);
