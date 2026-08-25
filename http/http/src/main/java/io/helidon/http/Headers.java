a5f5f9b8124d088352fd4a0a06b050a1e97f1af0
Tomas Langer <tomas.langer@oracle.com>
2026-08-25 17:06:23 +0200

    Align containsToken Javadoc

--- http/http/src/main/java/io/helidon/http/Headers.java
+++ http/http/src/main/java/io/helidon/http/Headers.java
@@ -73,7 +73,7 @@ public interface Headers extends Iterable<Header> {
      * {@code Connection}, {@code TE}, or {@code Transfer-Encoding}.
      *
      * @param value expected header name and tokenized value(s)
-    * @return {@code true} if all expected tokens are present, ignoring case and surrounding whitespace
+     * @return {@code true} if all expected tokens are present, ignoring case and surrounding whitespace
      */
     default boolean containsToken(Header value) {
         if (!contains(value.headerName())) {
