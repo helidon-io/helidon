e9ace76a337d227bfc40691378113ea79e7d3514
Tomas Langer <tomas.langer@oracle.com>
2026-09-02 23:59:58 +0200

    Document sink lookup failures

--- webserver/webserver/src/main/java/io/helidon/webserver/http/ServerResponse.java
+++ webserver/webserver/src/main/java/io/helidon/webserver/http/ServerResponse.java
@@ -326,7 +326,8 @@ public interface ServerResponse {
      * @param sinkType type of sink
      * @param <T> type of sink returned
      * @return sink
-     * @throws UnsupportedOperationException if no sink is available for the requested type
+     * @throws UnsupportedOperationException if sinks are not supported by this implementation
+     * @throws io.helidon.http.HttpException if no sink provider is available for the requested type
      */
     default <T extends Sink<?>> T sink(GenericType<T> sinkType) {
         throw new UnsupportedOperationException("No sink available for type " + sinkType);
