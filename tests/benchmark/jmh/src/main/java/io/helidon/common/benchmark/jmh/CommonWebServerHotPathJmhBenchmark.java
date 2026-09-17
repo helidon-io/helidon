09494eab2adc05446a4957ae469a7d70d47aaf97
Tomas Langer <tomas.langer@oracle.com>
2026-09-17 14:10:44 +0200

    Implement benchmark listeners using the DataListener contract

--- tests/benchmark/jmh/src/main/java/io/helidon/common/benchmark/jmh/CommonWebServerHotPathJmhBenchmark.java
+++ tests/benchmark/jmh/src/main/java/io/helidon/common/benchmark/jmh/CommonWebServerHotPathJmhBenchmark.java
@@ -27,6 +27,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 
 import io.helidon.common.buffers.BufferData;
+import io.helidon.common.buffers.DataListener;
 import io.helidon.common.buffers.DataReader;
 import io.helidon.common.socket.NioSocket;
 import io.helidon.common.socket.SocketWriter;
@@ -230,11 +231,21 @@ public class CommonWebServerHotPathJmhBenchmark {
         @Setup(Level.Trial)
         public void setup() {
             DataReader reader = DataReader.create(() -> REQUEST);
-            reader.listener((state, _, _, length) -> state.bytesRead += length, this);
+            reader.listener(new DataListener<ListenerDataReaderState>() {
+                @Override
+                public void data(ListenerDataReaderState state, byte[] data, int position, int length) {
+                    state.bytesRead += length;
+                }
+            }, this);
             headers = new Http1Headers(reader, 4096, false);
             prologue = new Http1Prologue(reader, 1024, false);
             DataReader pipelinedReader = DataReader.create(() -> PIPELINED_REQUESTS);
-            pipelinedReader.listener((state, _, _, length) -> state.pipelinedBytesRead += length, this);
+            pipelinedReader.listener(new DataListener<ListenerDataReaderState>() {
+                @Override
+                public void data(ListenerDataReaderState state, byte[] data, int position, int length) {
+                    state.pipelinedBytesRead += length;
+                }
+            }, this);
             pipelinedHeaders = new Http1Headers(pipelinedReader, 4096, false);
             pipelinedPrologue = new Http1Prologue(pipelinedReader, 1024, false);
         }
