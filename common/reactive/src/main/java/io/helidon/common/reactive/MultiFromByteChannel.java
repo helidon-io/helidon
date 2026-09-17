5bcf2dc63fd876e84719a94dc4d3869853cde3db
Tomas Langer <tomas.langer@oracle.com>
2026-09-17 13:48:56 +0200

    Fix reactive cancellation cleanup and interrupt handling

--- common/reactive/src/main/java/io/helidon/common/reactive/MultiFromByteChannel.java
+++ common/reactive/src/main/java/io/helidon/common/reactive/MultiFromByteChannel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2024 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -74,6 +74,7 @@ class MultiFromByteChannel implements Multi<ByteBuffer> {
                     @Override
                     public void cancel() {
                         subscriber.cancel();
+                        closeChannel();
                         closeExecutor();
                     }
                 });
@@ -127,11 +128,7 @@ class MultiFromByteChannel implements Multi<ByteBuffer> {
         }
         // Last or not
         if (count < 0) {
-            try {
-                channel.close();
-            } catch (Exception e) {
-                LOGGER.log(Level.WARNING, "Cannot close readable byte channel! (Close attempt after fully read channel.)", e);
-            }
+            closeChannel();
             tryComplete();
             return true;
         } else {
@@ -181,7 +178,9 @@ class MultiFromByteChannel implements Multi<ByteBuffer> {
     }
 
     private synchronized void planNextTry(long afterMillis) {
-        executor.get().schedule(this::tryPublish, afterMillis, TimeUnit.MILLISECONDS);
+        if (!subscriber.isClosed()) {
+            executor.get().schedule(this::tryPublish, afterMillis, TimeUnit.MILLISECONDS);
+        }
     }
 
     private void tryComplete() {
@@ -190,8 +189,19 @@ class MultiFromByteChannel implements Multi<ByteBuffer> {
     }
 
     private void tryComplete(Throwable t) {
-        subscriber.close(sub -> sub.onError(t));
+        subscriber.close(sub -> {
+            closeChannel();
+            sub.onError(t);
+        });
         closeExecutor();
+    }
+
+    private void closeChannel() {
+        try {
+            channel.close();
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING, "Cannot close readable byte channel!", e);
+        }
     }
 
     private synchronized void closeExecutor() {
