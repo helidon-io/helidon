22837def634dc6cffa77dc6b68178a2682cf6bd6
Tomas Langer <tomas.langer@oracle.com>
2026-09-22 01:22:17 +0200

    Remove unused redirect test import

--- webclient/tests/http1/src/test/java/io/helidon/webclient/http1/Http1ClientTest.java
+++ webclient/tests/http1/src/test/java/io/helidon/webclient/http1/Http1ClientTest.java
@@ -30,7 +30,6 @@ import java.util.Map;
 import java.util.OptionalLong;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
