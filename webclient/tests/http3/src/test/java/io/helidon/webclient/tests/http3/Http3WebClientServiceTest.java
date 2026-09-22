00b067f60777460af9b065d99e72c3bb052bb090
Tomas Langer <tomas.langer@oracle.com>
2026-09-22 13:04:06 +0200

    Align HTTP/3 TLS test constructor parameters

--- webclient/tests/http3/src/test/java/io/helidon/webclient/tests/http3/Http3WebClientServiceTest.java
+++ webclient/tests/http3/src/test/java/io/helidon/webclient/tests/http3/Http3WebClientServiceTest.java
@@ -1008,8 +1008,8 @@ class Http3WebClientServiceTest {
         private final AtomicInteger getCount;
 
         private Http1OnlyTlsEnvironment(WebServer server,
-                                       Http3TlsSupport.Http3TlsMaterials tlsMaterials,
-                                       AtomicInteger getCount) {
+                                        Http3TlsSupport.Http3TlsMaterials tlsMaterials,
+                                        AtomicInteger getCount) {
             this.server = server;
             this.tlsMaterials = tlsMaterials;
             this.getCount = getCount;
