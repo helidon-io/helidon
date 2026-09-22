2ed80a8cdd7f08f3230b4249ea8a06d56f87f4cf
Tomas Langer <tomas.langer@oracle.com>
2026-09-22 10:47:18 +0200

    Synchronize HTTP/2 redirect lifecycle test send completion

--- webclient/tests/http2/src/test/java/io/helidon/webclient/tests/http2/FollowRedirectTest.java
+++ webclient/tests/http2/src/test/java/io/helidon/webclient/tests/http2/FollowRedirectTest.java
@@ -676,6 +676,7 @@ class FollowRedirectTest {
             assertThat(lifecycleUploadReceived.await(5, TimeUnit.SECONDS), is(true));
             RequestLifecycle source = service.requests().get("/lifecycle/start");
             RequestLifecycle target = service.requests().get("/lifecycle/final");
+            CompletableFuture.allOf(source.whenSent(), target.whenSent()).get(5, TimeUnit.SECONDS);
             assertThat("source whenSent must complete after the final upload, before response headers",
                        source.whenSent().isDone(),
                        is(true));
