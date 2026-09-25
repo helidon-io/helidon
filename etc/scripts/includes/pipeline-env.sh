e7349a76afab8ab5e8e4eb3222f63e2a76337ef4
Joe Di Pol <joe.dipol@oracle.com>
2026-09-25 11:54:14 -0700

    27.x: Update scripts to unset CDPATH (#12624)
    * Update scripts to unset CDPATH

--- etc/scripts/includes/pipeline-env.sh
+++ etc/scripts/includes/pipeline-env.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2018, 2023 Oracle and/or its affiliates.
+# Copyright (c) 2018, 2026 Oracle and/or its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -36,7 +36,7 @@ if [ -z "${WS_DIR}" ]; then
         exit 1
     fi
 
-    readonly WS_DIR=$(cd $(dirname -- "${1}") ; cd ${2} ; pwd -P)
+    readonly WS_DIR=$(CDPATH='' cd -- $(dirname -- "${1}") ; cd ${2} ; pwd -P)
 
 fi
 
